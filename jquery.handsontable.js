da095adcc7bdaa0803c44182034f863339de2e25
Marcin Warpechowski <warpech@gmail.com>
2012-07-23 15:35:11 +0200

    bugfix: when "colHeaders" property is set a long string, it will cover some columns (issue #72)

--- jquery.handsontable.js
+++ jquery.handsontable.js
@@ -2971,7 +2971,7 @@ handsontable.BlockedCols.prototype.create = function () {
  * Copy table row header onto the floating layer above the grid
  */
 handsontable.BlockedCols.prototype.refresh = function () {
-  var hlen = this.count(), h, th, i;
+  var hlen = this.count(), h, th, realTh, i, label;
   if (hlen > 0) {
     var $tbody = this.main.find('tbody');
     var tbody = $tbody[0];
@@ -2986,10 +2986,13 @@ handsontable.BlockedCols.prototype.refresh = function () {
     var realTrs = this.instance.table.find('tbody tr');
     for (i = 0; i < trsLen; i++) {
       for (h = 0; h < hlen; h++) {
+        label = this.headers[h].columnLabel(i);
+        realTh = realTrs[i].getElementsByClassName ? realTrs[i].getElementsByClassName(this.headers[h].className)[0] : $(realTrs[i]).find('.' + this.headers[h].className.replace(/\s/i, '.'))[0];
+        realTh.innerHTML = label;
         th = trs[i].getElementsByClassName ? trs[i].getElementsByClassName(this.headers[h].className)[0] : $(trs[i]).find('.' + this.headers[h].className.replace(/\s/i, '.'))[0];
-        th.innerHTML = this.headers[h].columnLabel(i);
+        th.innerHTML = label;
         this.instance.minWidthFix(th);
-        th.style.height = realTrs.eq(i).children().first()[this.heightMethod]() + 'px';
+        th.style.height = $(realTh)[this.heightMethod]() + 'px';
       }
     }
 
