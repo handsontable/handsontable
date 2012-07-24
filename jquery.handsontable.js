eba24c2361056b9a14a1ffb5d99e2a5432db1e72
Cody Littley <smellystring@gmail.com>
2012-07-24 15:00:39 +1000

    Fixed typo that was causing bugs in one of the cases

--- jquery.handsontable.js
+++ jquery.handsontable.js
@@ -2185,7 +2185,7 @@
 			return [x2,y2,x1,y1];
 		}
 		else if(x1 < x2 && y1 > y2) {
-			return [x1,y2,x2,y2];
+			return [x1,y2,x2,y1];
 		}
 		else if(x1 > x2 && y1 < y2) {
 			return [x2,y1,x1,y2];
