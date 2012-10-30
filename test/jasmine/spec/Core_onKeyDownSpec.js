15fb4068a78c726d4d534c4d7e8ca454f30f7f2b
Marcin Warpechowski <warpech@gmail.com>
2012-10-31 00:10:11 +0100

    bugfix: when editing cells with Tab Key - TAB should complete the cell and move selection to the right (issue #125)

--- test/jasmine/spec/Core_onKeyDownSpec.js
+++ test/jasmine/spec/Core_onKeyDownSpec.js
@@ -1,28 +1,80 @@
-describe('Core_onKeyDown', function () {
-  var $container,
-    id = 'testContainer';
+describe('Core_keepEmptyRows', function () {
+  var id = 'testContainer';
 
   beforeEach(function () {
-    $container = $('<div id="' + id + '"></div>').appendTo('body');
+    this.$container = $('<div id="' + id + '"></div>').appendTo('body');
   });
 
   afterEach(function () {
-    if($container) {
-      $container.remove();
+    if (this.$container) {
+      this.$container.remove();
     }
   });
 
   it('should advance to next cell when TAB is pressed', function () {
     //https://github.com/warpech/jquery-handsontable/issues/151
-    $container.handsontable();
-    $container.handsontable('selectCell', 0, 0);
+    handsontable();
+    selectCell(0, 0);
+    keyDownUp('tab');
+    expect(getSelected()).toEqual([0, 1, 0, 1]);
+  });
 
-    var keydown = $.Event('keydown');
-    keydown.keyCode = 9; //tab
-    $container.find('textarea.handsontableInput').trigger(keydown);
+  it('while editing, should finish editing and advance to next cell when TAB is pressed', function () {
+    //https://github.com/warpech/jquery-handsontable/issues/215
+    handsontable();
+    selectCell(1, 1);
+    keyDownUp('enter');
+    this.$keyboardProxy.val('Ted');
+    keyDownUp('tab');
+    expect(getData()[1][1]).toEqual('Ted');
+    expect(getSelected()).toEqual([1, 2, 1, 2]);
+  });
 
-    var output = $container.handsontable('getSelected');
-    expect(output[0]).toEqual(0);
-    expect(output[1]).toEqual(1);
+  it('while editing, should finish editing and advance to lower cell when down arrow is pressed', function () {
+    //https://github.com/warpech/jquery-handsontable/issues/215
+    handsontable();
+    selectCell(1, 1);
+    keyDownUp('enter');
+    this.$keyboardProxy.val('Ted');
+    keyDownUp('arrow_down');
+    expect(getData()[1][1]).toEqual('Ted');
+    expect(getSelected()).toEqual([2, 1, 2, 1]);
+  });
+
+  it('while editing, should finish editing and advance to upper cell when up arrow is pressed', function () {
+    //https://github.com/warpech/jquery-handsontable/issues/215
+    handsontable();
+    selectCell(1, 1);
+    keyDownUp('enter');
+    this.$keyboardProxy.val('Ted');
+    keyDownUp('arrow_up');
+    expect(getData()[1][1]).toEqual('Ted');
+    expect(getSelected()).toEqual([0, 1, 0, 1]);
+  });
+
+  it('while editing, should finish editing and advance to next cell when right arrow is pressed', function () {
+    //https://github.com/warpech/jquery-handsontable/issues/215
+    handsontable();
+    selectCell(1, 1);
+    keyDownUp('enter');
+    this.$keyboardProxy.val('Ted');
+    keyDownUp('arrow_right');
+    expect(getData()[1][1]).toEqual('Ted');
+    expect(getSelected()).toEqual([1, 2, 1, 2]);
+  });
+
+  it('while editing, should finish editing and advance to previous cell when left arrow is pressed and the text cursor is at position 0', function () {
+    //this test almost always *succeeds* in Chrome so be sure to also test in FF, IE
+    //https://github.com/warpech/jquery-handsontable/issues/215
+    runs(function () {
+      handsontable();
+      selectCell(2, 2);
+      keyDownUp('enter');
+      this.$keyboardProxy.val('Ted');
+      setCaretPosition(0); //set's text cursor to the beginning of the textarea
+      keyDownUp('arrow_left');
+      expect(getData()[2][2]).toEqual('Ted');
+      expect(getSelected()).toEqual([2, 1, 2, 1]);
+    });
   });
 });
