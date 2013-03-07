a7f740c897e6020781a6be5d1b5237cf5a4667f7
Marcin Warpechowski <warpech@gmail.com>
2013-03-07 01:23:11 +0100

    tests: add a simple test for column sorting

---
+++ test/jasmine/spec/plugins/columnSortingSpec.js
@@ -0,0 +1,49 @@
+describe('ColumnSorting', function () {
+  var id = 'testContainer';
+
+  beforeEach(function () {
+    this.$container = $('<div id="' + id + '"></div>').appendTo('body');
+  });
+
+  afterEach(function () {
+    if (this.$container) {
+      this.$container.remove();
+    }
+  });
+
+  var arrayOfObjects = function () {
+    return [
+      {id: 1, name: "Ted", lastName: "Right"},
+      {id: 2, name: "Frank", lastName: "Honest"},
+      {id: 3, name: "Joan", lastName: "Well"},
+      {id: 4, name: "Sid", lastName: "Strong"},
+      {id: 5, name: "Jane", lastName: "Neat"},
+      {id: 6, name: "Chuck", lastName: "Jackson"},
+      {id: 7, name: "Meg", lastName: "Jansen"},
+      {id: 8, name: "Rob", lastName: "Norris"},
+      {id: 9, name: "Sean", lastName: "O'Hara"},
+      {id: 10, name: "Eve", lastName: "Branson"}
+    ];
+  };
+
+  it('should sort numbers descending after 2 clicks on table header', function () {
+    handsontable({
+      data: arrayOfObjects(),
+      colHeaders: true,
+      columnSorting: true
+    });
+
+    waitsFor(nextFrame, 'next frame', 60);
+
+    runs(function () {
+      this.$container.find('th span.columnSorting').first().click();
+      this.$container.find('th span.columnSorting').first().click();
+    });
+
+    waitsFor(nextFrame, 'next frame', 60);
+
+    runs(function () {
+      expect(this.$container.find('tr td').first().html()).toEqual('10');
+    });
+  });
+});
