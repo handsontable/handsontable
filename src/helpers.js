13785a2b1d5223164bf15362ee5f3da58c1b861d
Marcin Warpechowski <warpech@gmail.com>
2013-03-31 14:51:43 +0200

    remove redundant semicolon

--- src/helpers.js
+++ src/helpers.js
@@ -70,7 +70,7 @@ Handsontable.helper.randomString = function () {
     return Math.floor((1 + Math.random()) * 0x10000)
       .toString(16)
       .substring(1);
-  };
+  }
 
   return s4() + s4() + s4() + s4();
 };
