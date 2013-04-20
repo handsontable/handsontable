0e797b6adcdf0c3eedc789aa0530b3384361deda
tansin <mtansin@web.de>
2013-04-20 21:56:28 +0200

    Feature: Excel like sorting. Excel or Google Table ignore empty fields when sorting the column so that empty fields remain always at the bottom of table.

--- src/plugins/columnSorting.js
+++ src/plugins/columnSorting.js
@@ -45,10 +45,10 @@ function HandsontableColumnSorting() {
       if (a[1] === b[1]) {
         return 0;
       }
-      if (a[1] === null) {
+      if (a[1] === null || a[1] === '') {
         return 1;
       }
-      if (b[1] === null) {
+      if (b[1] === null || b[1] === '') {
         return -1;
       }
       if (a[1] < b[1]) return instance.sortOrder ? -1 : 1;
