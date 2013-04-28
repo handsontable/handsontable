405004e44d9b0fae4fc0be9d63c8b01a4cdb117f
Marcin Warpechowski <warpech@gmail.com>
2013-04-29 01:15:51 +0200

    use `data-jsfiddle` attribute to run examples in jsFiddle

--- demo/js/samples.js
+++ demo/js/samples.js
@@ -31,84 +31,92 @@
 
   $(function () {
     //if (!$.browser.msie || parseInt($.browser.version, 10) > 6) { //syntax coloring does not work well with IE7
-      $('.descLayout pre.javascript code').each(function () {
-        var $script = $(this);
-        var code = trimCodeBlock($script[0].innerHTML);
-        $script[0].innerHTML = code.join('\n');
-      });
+    $('.descLayout pre.javascript code').each(function () {
+      var $script = $(this);
+      var code = trimCodeBlock($script[0].innerHTML);
+      $script[0].innerHTML = code.join('\n');
+    });
 
-      $('.codeLayout script').each(function () {
-        var $script = $(this);
-        var $pre = $('<pre class="javascript"></pre>');
-        var $code = $('<code></code>');
-        var code = trimCodeBlock($script[0].innerHTML);
-        code = code.join('<br>').replace(/  /g, "&nbsp;&nbsp;");
-        $code[0].innerHTML = code;
-        $pre.append($code);
-        $pre.insertAfter($script);
-      });
-      hljs.initHighlighting();
+    $('.codeLayout script').each(function () {
+      var $script = $(this);
+      var $pre = $('<pre class="javascript"></pre>');
+      var $code = $('<code></code>');
+      var code = trimCodeBlock($script[0].innerHTML);
+      code = code.join('<br>').replace(/  /g, "&nbsp;&nbsp;");
+      $code[0].innerHTML = code;
+      $pre.append($code);
+      $pre.insertAfter($script);
+    });
+    hljs.initHighlighting();
     //}
 
+    //http://stackoverflow.com/questions/2419749/get-selected-elements-outer-html
+    jQuery.fn.outerHTML = function (s) {
+      return s
+        ? this.before(s).remove()
+        : jQuery("<p>").append(this.eq(0).clone()).html();
+    };
+
     $('.jsFiddleLink').on('click', function () {
+      var keys = ['common'];
+
+      var runfiddle = $(this).data('runfiddle');
+      if (!runfiddle) {
+        throw new Error("Edit in jsFiddle button does not contain runfiddle data");
+      }
+      keys.push(runfiddle);
+
       var css = [];
+      var js = '';
+      var html = '';
 
       css.push('</style><!-- Ugly Hack due to jsFiddle issue: http://goo.gl/BUfGZ -->\n');
-      css.push('<script src="http://handsontable.com/lib/jquery.min.js"></script>\n');
-      css.push('<script src="http://handsontable.com/dist/jquery.handsontable.full.js"></script>\n');
 
-      if (window.Bootstrap) { // these need to be before jquery.handsontable.full.css
-        css.push('<script type="text/javascript" src="http://handsontable.com/demo/bootstrap/js/bootstrap.min.js"></script>\n');
-        css.push('<link rel="stylesheet" type="text/css" href="http://handsontable.com/demo/bootstrap/css/bootstrap.min.css">\n');
+      for (var i = 0, ilen = keys.length; i < ilen; i++) {
+        $('[data-jsfiddle=' + keys[i] + ']').each(function () {
+          var tag;
+          var $this = $(this);
+          if (this.nodeName === 'LINK') {
+            tag = $this.outerHTML();
+          }
+          else if (this.nodeName === 'SCRIPT' && $this.attr('src')) {
+            tag = $this.outerHTML();
+          }
+          else if (this.nodeName === 'SCRIPT') {
+            js += trimCodeBlock($this.html(), 2).join('\n') + '\n';
+          }
+          else { //DIV
+            var clone = $this.clone();
+            var clonedExample = clone.find('div[id^="example"]');
+            clonedExample.html(''); //clear example HTML, just leave container
+            var originalHT = $this.find('div[id^="example"]');
+            if (originalHT.data('originalStyle')) {
+              clonedExample.attr('style', originalHT.data('originalStyle'));
+            }
+            clone.find('a[name]').remove();
+            clone.find('.handsontable.hidden').remove();
+            html += trimCodeBlock(clone.html()).join('\n');
+          }
+          if (tag) {
+            tag = tag.replace(' data-jsfiddle="' + keys[i] + '"', '');
+            tag = tag.replace('href="css/', 'href="http://handsontable.com/demo/css/');
+            tag = tag.replace('href="../', 'href="http://handsontable.com/');
+            tag = tag.replace('src="../', 'src="http://handsontable.com/');
+            css.push(tag)
+          }
+        });
       }
 
-      css.push('<link rel="stylesheet" media="screen" href="http://handsontable.com/dist/jquery.handsontable.full.css">\n');
-      css.push('<link rel="stylesheet" media="screen" href="http://handsontable.com/demo/css/samples.css">\n');
-
-      if (window.Bootstrap) {
-        css.push('<link rel="stylesheet" media="screen" href="http://handsontable.com/extensions/jquery.handsontable.bootstrap.css">\n');
-      }
-
-      if ($.ui && $.ui.datepicker) {
-        css.push('<script src="http://handsontable.com/lib/jquery-ui/js/jquery-ui.custom.min.js"></script>\n');
-        css.push('<link rel="stylesheet" media="screen" href="http://handsontable.com/lib/jquery-ui/css/ui-bootstrap/jquery-ui.custom.css">\n');
-      }
-
-      if (window.Backbone) {
-        css.push('<script src="http://handsontable.com/demo/js/backbone/lodash.underscore.js"></script>\n');
-        css.push('<script src="http://handsontable.com/demo/js/backbone/backbone.js"></script>\n');
-        css.push('<script src="http://handsontable.com/demo/js/backbone/backbone-relational/backbone-relational.js"></script>\n');
-      }
-
-      css.push('<style type="text/css">\nbody {background: white; margin: 20px;}\nh2 {margin: 20px 0;}\n\n');
-      css = css.join('');
-
-      $('style.common').each(function () {
-        css += trimCodeBlock($(this).html()).join('\n') + '\n';
-      });
-
-      var js = '$(document).ready(function () {\n\n';
+      css.push('');
+      css.push('<style type="text/css">');
+      css.push('body {background: white; margin: 20px;}');
+      css.push('h2 {margin: 20px 0;}');
+      css = css.join('\n');
 
       js += trimCodeBlock(bindDumpButton.toString(), 2).join('\n') + '\n';
       js += '  bindDumpButton();\n\n';
 
-      $('script.common').each(function () {
-        js += trimCodeBlock($(this).html(), 2).join('\n') + '\n';
-      });
-      $(this).parents('.codeLayout').find('script').not('.common').each(function () {
-        js += trimCodeBlock($(this).html(), 2).join('\n') + '\n';
-      });
-      js += '});';
-
-      var clone = $(this).parents('.rowLayout').find('.descLayout .pad').clone();
-      var example = clone.find('div[id^="example"]');
-      example.html('');
-      var originalHT = $(this).parents('.rowLayout').find('.descLayout .pad div[id^="example"]');
-      if (originalHT.data('originalStyle')) {
-        example.attr('style', originalHT.data('originalStyle'));
-      }
-      clone.find('a[name]').remove();
-      var html = trimCodeBlock(clone.html()).join('\n');
+      js = '$(document).ready(function () {\n\n' + js + '});';
 
       var form = $('<form action="http://jsfiddle.net/api/post/library/pure/" method="post" target="_blank">' +
         '<input type="text" name="title" value="Handsontable example">' +
