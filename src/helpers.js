14d8831d5f90bfdc206361bf16ef40805778cd80
Paweł Smoleński <pwl.smolenski@gmail.com>
2013-09-11 15:42:50 +0200

    Added isArray helper

--- src/helpers.js
+++ src/helpers.js
@@ -75,6 +75,10 @@ Handsontable.helper.isNumeric = function (n) {
            t == 'object' ? !!n && typeof n.valueOf() == "number" && !(n instanceof Date) : false;
 };
 
+Handsontable.helper.isArray = function (obj) {
+  return Object.prototype.toString.call(obj).match(/array/i) !== null;
+}
+
 /**
  * Checks if child is a descendant of given parent node
  * http://stackoverflow.com/questions/2234979/how-to-check-in-javascript-if-one-element-is-a-child-of-another
