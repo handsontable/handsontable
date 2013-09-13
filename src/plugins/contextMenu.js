e29766d1fae98aa2ecbaf39c7c4206c9b09db6b5
Marcin Warpechowski <warpech@gmail.com>
2013-09-13 20:17:15 +0200

    bugfix: context-menu-layer is not removed from DOM after $.contextMenu destroy

--- src/plugins/contextMenu.js
+++ src/plugins/contextMenu.js
@@ -170,6 +170,12 @@
   function destroyContextMenu() {
     var id = this.rootElement[0].id;
     $.contextMenu('destroy', "#" + id + ' table, #' + id + ' div');
+
+    /*
+     * There is a bug in $.contextMenu: 'destroy' does not remove layer when selector is provided. When the below line
+     * is removed, running the context menu tests in Jasmine will produce invisible layers that are never removed from DOM
+     */
+    $(document.querySelectorAll('#context-menu-layer')).remove();
   }
 
   Handsontable.PluginHooks.add('afterInit', init);
