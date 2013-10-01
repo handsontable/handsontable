99d8a35fbc6e54e23813c567e95608af43726eda
Anthony McLin <git@anthonymclin.com>
2013-10-01 11:56:47 -0700

    Autocomplete renderer should affect cell not inner div

--- src/renderers/autocompleteRenderer.js
+++ src/renderers/autocompleteRenderer.js
@@ -28,7 +28,9 @@ Handsontable.AutocompleteRenderer = function (instance, TD, row, col, prop, valu
     instance.rootElement.on('mouseup', '.htAutocompleteArrow', instance.acArrowListener); //this way we don't bind event listener to each arrow. We rely on propagation instead
   }
 
-  Handsontable.TextRenderer(instance, TEXT, row, col, prop, value, cellProperties);
+  Handsontable.TextRenderer(instance, TD, row, col, prop, value, cellProperties);
+  var escaped = Handsontable.helper.stringify(value);
+  instance.view.wt.wtDom.fastInnerText(TEXT, escaped);
 
   if (!TEXT.firstChild) { //http://jsperf.com/empty-node-if-needed
     //otherwise empty fields appear borderless in demo/renderers.html (IE)
