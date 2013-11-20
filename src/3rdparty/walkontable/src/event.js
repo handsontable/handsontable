d6d24bb894735f836f37755930b7d710bd46e5d9
Marcin Warpechowski <warpech@gmail.com>
2013-11-20 09:32:55 +0100

    bugfix: redo double click handler, because double click did not open cell editor in IE8

--- src/3rdparty/walkontable/src/event.js
+++ src/3rdparty/walkontable/src/event.js
@@ -6,29 +6,28 @@ function WalkontableEvent(instance) {
 
   this.wtDom = this.instance.wtDom;
 
-  var dblClickOrigin = [null, null, null, null];
-  this.instance.dblClickTimeout = null;
+  var dblClickOrigin = [null, null];
+  var dblClickTimeout = [null, null];
 
   var onMouseDown = function (event) {
     var cell = that.parentCell(event.target);
 
-    if (cell.TD && cell.TD.nodeName === 'TD') {
+    if (that.wtDom.hasClass(event.target, 'corner')) {
+      that.instance.getSetting('onCellCornerMouseDown', event, event.target);
+    }
+    else if (cell.TD && cell.TD.nodeName === 'TD') {
       if (that.instance.hasSetting('onCellMouseDown')) {
         that.instance.getSetting('onCellMouseDown', event, cell.coords, cell.TD);
       }
     }
-    else if (that.wtDom.hasClass(event.target, 'corner')) {
-      that.instance.getSetting('onCellCornerMouseDown', event, event.target);
-    }
 
     if (event.button !== 2) { //if not right mouse button
       if (cell.TD && cell.TD.nodeName === 'TD') {
-        dblClickOrigin.shift();
-        dblClickOrigin.push(cell.TD);
-      }
-      else if (that.wtDom.hasClass(event.target, 'corner')) {
-        dblClickOrigin.shift();
-        dblClickOrigin.push(event.target);
+        dblClickOrigin[0] = cell.TD;
+        clearTimeout(dblClickTimeout[0]);
+        dblClickTimeout[0] = setTimeout(function () {
+          dblClickOrigin[0] = null;
+        }, 1000);
       }
     }
   };
@@ -65,33 +64,23 @@ function WalkontableEvent(instance) {
     if (event.button !== 2) { //if not right mouse button
       var cell = that.parentCell(event.target);
 
-      if (cell.TD && cell.TD.nodeName === 'TD') {
-        dblClickOrigin.shift();
-        dblClickOrigin.push(cell.TD);
-      }
-      else {
-        dblClickOrigin.shift();
-        dblClickOrigin.push(event.target);
-      }
-
-      if (dblClickOrigin[3] !== null && dblClickOrigin[3] === dblClickOrigin[2]) {
-        if (that.instance.dblClickTimeout && dblClickOrigin[2] === dblClickOrigin[1] && dblClickOrigin[1] === dblClickOrigin[0]) {
-          if (cell.TD) {
-            that.instance.getSetting('onCellDblClick', event, cell.coords, cell.TD);
-          }
-          else if (that.wtDom.hasClass(event.target, 'corner')) {
-            that.instance.getSetting('onCellCornerDblClick', event, cell.coords, cell.TD);
-          }
-
-          clearTimeout(that.instance.dblClickTimeout);
-          that.instance.dblClickTimeout = null;
+      if (cell.TD === dblClickOrigin[0] && cell.TD === dblClickOrigin[1]) {
+        if (that.wtDom.hasClass(event.target, 'corner')) {
+          that.instance.getSetting('onCellCornerDblClick', event, cell.coords, cell.TD);
         }
-        else {
-          clearTimeout(that.instance.dblClickTimeout);
-          that.instance.dblClickTimeout = setTimeout(function () {
-            that.instance.dblClickTimeout = null;
-          }, 500);
+        else if (cell.TD) {
+          that.instance.getSetting('onCellDblClick', event, cell.coords, cell.TD);
         }
+
+        dblClickOrigin[0] = null;
+        dblClickOrigin[1] = null;
+      }
+      else if (cell.TD === dblClickOrigin[0]) {
+        dblClickOrigin[1] = cell.TD;
+        clearTimeout(dblClickTimeout[1]);
+        dblClickTimeout[1] = setTimeout(function () {
+          dblClickOrigin[1] = null;
+        }, 1000);
       }
     }
   };
@@ -110,9 +99,14 @@ WalkontableEvent.prototype.parentCell = function (elem) {
     cell.coords = this.instance.wtTable.getCoords(TD);
     cell.TD = TD;
   }
-  else if (this.wtDom.hasClass(elem, 'wtBorder') && this.wtDom.hasClass(elem, 'current') && !this.wtDom.hasClass(elem, 'corner')) {
+  else if (this.wtDom.hasClass(elem, 'wtBorder') && this.wtDom.hasClass(elem, 'current')) {
     cell.coords = this.instance.selections.current.selected[0];
     cell.TD = this.instance.wtTable.getCell(cell.coords);
   }
   return cell;
+};
+
+WalkontableEvent.prototype.destroy = function () {
+  clearTimeout(this.dblClickTimeout0);
+  clearTimeout(this.dblClickTimeout1);
 };
