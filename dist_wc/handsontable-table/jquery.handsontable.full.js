647032840cf0a1ade70fe4536e428aea1781ae4a
Einar Jonsson <einar@codelette.com>
2014-05-13 11:27:18 +0000

    label verison as overcast-patch1

--- dist_wc/handsontable-table/jquery.handsontable.full.js
+++ dist_wc/handsontable-table/jquery.handsontable.full.js
@@ -6,7 +6,7 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Tue May 13 2014 11:14:08 GMT+0000 (GMT)
+ * Date: Tue May 13 2014 11:27:04 GMT+0000 (GMT)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
