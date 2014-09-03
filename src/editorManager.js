b6e3bb7f045fd5bb90d186192e40a0f9ca47645a
Jan Siegel <js.ziggle@gmail.com>
2014-09-03 11:21:22 +0200

    Fix for opening the editor with a uppercase letter starts entry with a lowercase letter #HOT-21

--- src/editorManager.js
+++ src/editorManager.js
@@ -31,13 +31,16 @@
             if (!activeEditor.isWaiting()) {
               if (!Handsontable.helper.isMetaKey(event.keyCode) && !ctrlDown && !that.isEditorOpened()) {
 
-                var charCode = event.which || event.keyCode;
+                var charCode = event.keyCode;
                 var charStr = String.fromCharCode(charCode);
-                if (event.keyCode < 96) {
+                var isUp = (charCode >= 65 && charCode <= 90) ? true : false; // uppercase
+                var isLow = (charCode >= 97 && charCode <= 122) ? true : false; // lowercase
+                // uppercase w/out shift or lowercase with shift == caps lock
+                if ((isUp && !event.shiftKey) || (isLow && event.shiftKey)) {
                   charStr = charStr.toLowerCase();
                 }
-
                 that.openEditor(charStr);
+
                 event.preventDefault();
                 event.stopPropagation(); //required by HandsontableEditor
                 return;
