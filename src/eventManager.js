5c3b271ca29b5d47ae6b4b5e25e4796820817c76
Marcin Warpechowski <warpech@gmail.com>
2014-10-06 20:58:34 +0200

    cleanup: rename `bubbling` to `useCapture` (as in MDN and MSDN; because value false means "bubbling", true means "capture")

--- src/eventManager.js
+++ src/eventManager.js
@@ -12,9 +12,9 @@ Handsontable.eventManager = function (instance) {
     instance.eventListeners = [];
   }
 
-  var addEvent = function (element, event, delegate, callback, bubbling) {
+  var addEvent = function (element, event, delegate, callback, useCapture) {
       if (typeof delegate === 'function') {
-        bubbling = callback;
+        useCapture = callback;
         callback = delegate;
         delegate = null;
       } else {
@@ -29,37 +29,37 @@ Handsontable.eventManager = function (instance) {
         }
       }
 
-      bubbling = bubbling || false;
+      useCapture = useCapture || false;
 
       instance.eventListeners.push({
         element: element,
         event: event,
         delegate: delegate,
         callback: callback,
-        bubbling: bubbling
+        useCapture: useCapture
       });
 
       if (window.addEventListener) {
-        element.addEventListener(event, callback, bubbling)
+        element.addEventListener(event, callback, useCapture)
       } else {
         element.attachEvent('on' + event, callback);
       }
     },
-    removeEvent = function (element, event, delegate, callback, bubbling){
+    removeEvent = function (element, event, delegate, callback, useCapture){
       if(typeof delegate === 'function') {
-        bubbling = callback;
+        useCapture = callback;
         callback = delegate;
       }
       else {
         throw new Error("Remove delegate event not implemented");
       }
 
-      bubbling = bubbling || false;
+      useCapture = useCapture || false;
 
       if (element.detachEvent) {
         element.detachEvent('on' + event, handler);
       } else {
-        element.removeEventListener(event, callback, bubbling);
+        element.removeEventListener(event, callback, useCapture);
       }
     },
     serveImmediatePropagation = function () {
@@ -79,10 +79,10 @@ Handsontable.eventManager = function (instance) {
       while(instance.eventListeners.length > 0) {
        var event = instance.eventListeners.pop();
         if(event.delegate) {
-          removeEvent(event.element, event.event, event.delegate, event.callback, event.bubbling);
+          removeEvent(event.element, event.event, event.delegate, event.callback, event.useCapture);
         }
         else {
-          removeEvent(event.element, event.event, event.callback, event.bubbling);
+          removeEvent(event.element, event.event, event.callback, event.useCapture);
         }
       }
     };
