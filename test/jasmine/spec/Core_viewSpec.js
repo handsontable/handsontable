0942402c42ed590c3a53c8c057c4665f7b9f32fa
Krzysztof Budnik <budnix@gmail.com>
2015-01-20 10:36:14 +0100

    Fixed stretch columns test

--- test/jasmine/spec/Core_viewSpec.js
+++ test/jasmine/spec/Core_viewSpec.js
@@ -615,8 +615,8 @@ describe('Core_view', function () {
 
 
       var result = navigator.platform == "Win32" && !navigator.userAgent.match(/MSIE 10/) ? 86 : 87; // IE10 hack
-      expect(masterTH[1].offsetWidth).toEqual(result);
-      expect(overlayTH[1].offsetWidth).toEqual(result); //if you get 90, it means it is calculated before scrollbars were applied
+      expect(masterTH[1].offsetWidth).toBeAroundValue(result);
+      expect(overlayTH[1].offsetWidth).toBeAroundValue(result); //if you get 90, it means it is calculated before scrollbars were applied
 
       expect(masterTH[2].offsetWidth).toEqual(overlayTH[2].offsetWidth);
       expect(masterTH[3].offsetWidth).toEqual(overlayTH[3].offsetWidth);
