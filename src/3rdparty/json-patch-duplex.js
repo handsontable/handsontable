03931c38a984926ed191f6e406e3da8d76c62404
Stuart Watt <stuart@morungos.com>
2015-03-31 15:30:41 -0400

    Patched upstream to not assume `this` is defined

--- src/3rdparty/json-patch-duplex.js
+++ src/3rdparty/json-patch-duplex.js
@@ -4,7 +4,12 @@
 * (c) 2013 Joachim Wester
 * MIT license
 */
-var __extends = this.__extends || function (d, b) {
+
+// Patched this line from upstream to handle undefined this
+// - var __extends = this.__extends || function (d, b) {
+// + var __extends = (this && this.__extends) || function (d, b) {
+
+var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
     __.prototype = b.prototype;
