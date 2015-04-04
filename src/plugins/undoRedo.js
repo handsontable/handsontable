f0733f04d6947bd61e98d27445875573c497afa1
slat <sam.bendall@gmail.com>
2015-04-04 19:03:55 +1300

    Fix redo insert_row index

--- src/plugins/undoRedo.js
+++ src/plugins/undoRedo.js
@@ -215,7 +215,7 @@
   };
   Handsontable.UndoRedo.CreateRowAction.prototype.redo = function (instance, redoneCallback) {
     instance.addHookOnce('afterCreateRow', redoneCallback);
-    instance.alter('insert_row', this.index + 1, this.amount);
+    instance.alter('insert_row', this.index, this.amount);
   };
 
   Handsontable.UndoRedo.RemoveRowAction = function (index, data) {
