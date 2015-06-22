34fad9dc704f81ab5bf0180740fc7cd216fb8f31
Krzysztof Budnik <budnix@gmail.com>
2015-06-22 11:29:10 +0200

    Improve checkbox editor behavior for #bad-value# state. #2563

--- src/editors/checkboxEditor.js
+++ src/editors/checkboxEditor.js
@@ -1,6 +1,7 @@
 
 import {registerEditor} from './../editors.js';
 import {BaseEditor} from './_baseEditor.js';
+import * as dom from './../dom.js';
 
 //Blank editor, because all the work is done by renderer
 var CheckboxEditor = BaseEditor.prototype.extend();
@@ -18,7 +19,7 @@ Handsontable.editors.CheckboxEditor = CheckboxEditor;
 CheckboxEditor.prototype.beginEditing = function () {
   var checkbox = this.TD.querySelector('input[type="checkbox"]');
 
-  if (checkbox) {
+  if (!dom.hasClass(checkbox, 'htBadValue')) {
     checkbox.click();
   }
 };
