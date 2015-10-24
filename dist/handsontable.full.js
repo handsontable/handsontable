7f2e4fad25e716eb6c9d45aedb2df53c888f8955
陈萍圆 <chenpingyuan@chenpingyuandeMacBook-Pro.local>
2015-10-24 14:04:50 +0800

    remove tag when copied

--- dist/handsontable.full.js
+++ dist/handsontable.full.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Fri Oct 23 2015 15:25:57 GMT+0800 (CST)
+ * Date: Sat Oct 24 2015 14:02:32 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Fri Oct 23 2015 15:25:57 GMT+0800 (CST)',
+  buildDate: 'Sat Oct 24 2015 14:02:32 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -19058,6 +19058,8 @@ if (typeof exports !== 'undefined') {
 //# 
 },{}],"copyPaste":[function(require,module,exports){
 "use strict";
+var $__SheetClip__;
+var SheetClip = ($__SheetClip__ = require("SheetClip"), $__SheetClip__ && $__SheetClip__.__esModule && $__SheetClip__ || {default: $__SheetClip__}).default;
 var instance;
 function copyPaste() {
   if (!instance) {
@@ -19179,7 +19181,30 @@ CopyPasteClass.prototype.copyable = function(string) {
   if (typeof string !== 'string' && string.toString === void 0) {
     throw new Error('copyable requires string parameter');
   }
-  this.elTextarea.value = string;
+  var parsedStr = SheetClip.parse(string),
+      rowItem,
+      rowSet,
+      dataSet = [];
+  for (var row = 0,
+      l = parsedStr.length; row < l; row++) {
+    rowSet = [];
+    for (var col = 0,
+        len = parsedStr[row].length; col < len; col++) {
+      rowItem = parsedStr[row][col];
+      try {
+        rowItem = $(rowItem).text();
+      } catch (e) {
+        console.log('copyable: ' + rowItem);
+      }
+      rowSet.push(rowItem);
+    }
+    dataSet.push(rowSet);
+  }
+  if (!SheetClip.stringify(dataSet).trim()) {
+    return;
+  }
+  this.elTextarea.htmlValue = string;
+  this.elTextarea.value = SheetClip.stringify(dataSet);
   this.selectNodeText(this.elTextarea);
 };
 CopyPasteClass.prototype.onCut = function(callback) {
@@ -19226,7 +19251,7 @@ CopyPasteClass.prototype.triggerPaste = function(event, string) {
   var _this = this;
   if (_this.pasteCallbacks) {
     setTimeout(function() {
-      var val = string || _this.elTextarea.value;
+      var val = string || _this.elTextarea.htmlValue;
       for (var i = 0,
           len = _this.pasteCallbacks.length; i < len; i++) {
         _this.pasteCallbacks[i](val, event);
@@ -19250,7 +19275,7 @@ CopyPasteClass.prototype.hasBeenDestroyed = function() {
 };
 
 //# 
-},{}],"es6collections":[function(require,module,exports){
+},{"SheetClip":"SheetClip"}],"es6collections":[function(require,module,exports){
 "use strict";
 (function(exports) {
   'use strict';
