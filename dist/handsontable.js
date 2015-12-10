e594e6274d6cb30a97af311479b1d4c9cf9e1f5f
zhouxiaoping <zhouxiaoping.map@gmail.com>
2015-12-10 19:34:27 +0800

    add function for setdatamap

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Fri Dec 04 2015 22:06:25 GMT+0800 (CST)
+ * Date: Thu Dec 10 2015 19:33:43 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Fri Dec 04 2015 22:06:25 GMT+0800 (CST)',
+  buildDate: 'Thu Dec 10 2015 19:33:43 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -4733,6 +4733,9 @@ Handsontable.Core = function Core(rootElement, userSettings) {
       return [[row, propOrCol, value]];
     }
   }
+  this.setCellDataToDataMap = function(row, col, value, source) {
+    datamap.set(row, col, value, source);
+  };
   this.setDataAtCell = function(row, col, value, source) {
     var input = setDataInputToArray(row, col, value),
         i,
