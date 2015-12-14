64fefcf3e95489fc241fbe0e883826c5604f66e2
zhouxiaoping <zhouxiaoping.map@gmail.com>
2015-12-15 03:06:16 +0800

    fix half of bug

--- dist/handsontable.full.js
+++ dist/handsontable.full.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Mon Dec 14 2015 16:52:38 GMT+0800 (CST)
+ * Date: Tue Dec 15 2015 03:04:59 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Mon Dec 14 2015 16:52:38 GMT+0800 (CST)',
+  buildDate: 'Tue Dec 15 2015 03:04:59 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -14954,6 +14954,9 @@ function TableView(instance) {
     if (that.settings.outsideClickDeselects) {
       instance.deselectCell();
     } else {
+      if ($(event.target).hasClass('s-header-back-icon')) {
+        $('.handsontableInput').html('');
+      }
       instance.destroyEditor();
     }
   });
