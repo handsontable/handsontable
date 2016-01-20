0e54f4c211ac1262cbddf12dafa676dc46bc5b47
zhouxiaoping <zhouxiaoping.map@gmail.com>
2016-01-20 15:23:14 +0800

    fix over 200line

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Wed Jan 20 2016 11:35:29 GMT+0800 (CST)
+ * Date: Wed Jan 20 2016 15:22:50 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Wed Jan 20 2016 11:35:29 GMT+0800 (CST)',
+  buildDate: 'Wed Jan 20 2016 15:22:50 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -2214,9 +2214,7 @@ var WalkontableOverlays = function WalkontableOverlays(wotInstance) {
       if (this.overlayScrollPositions.master.left !== tempScrollValue) {
         this.overlayScrollPositions.master.left = tempScrollValue;
         scrollValueChanged = true;
-        if (topOverlay) {
-          topOverlay.scrollLeft = tempScrollValue;
-        }
+        if (topOverlay) {}
         if (bottomOverlay) {
           bottomOverlay.scrollLeft = tempScrollValue;
         }
@@ -2225,9 +2223,7 @@ var WalkontableOverlays = function WalkontableOverlays(wotInstance) {
       if (this.overlayScrollPositions.master.top !== tempScrollValue) {
         this.overlayScrollPositions.master.top = tempScrollValue;
         scrollValueChanged = true;
-        if (leftOverlay) {
-          leftOverlay.scrollTop = tempScrollValue;
-        }
+        if (leftOverlay) {}
       }
     } else if (target === bottomOverlay) {
       tempScrollValue = getScrollLeft(target);
