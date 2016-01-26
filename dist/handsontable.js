cd7cd6bc79573f4bbf324fde94e2f4a423990598
zhouxiaoping <zhouxiaoping.map@gmail.com>
2016-01-26 19:32:55 +0800

    fix first line oversize

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Tue Jan 26 2016 19:16:43 GMT+0800 (CST)
+ * Date: Tue Jan 26 2016 19:32:26 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Tue Jan 26 2016 19:16:43 GMT+0800 (CST)',
+  buildDate: 'Tue Jan 26 2016 19:32:26 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -3186,6 +3186,9 @@ var WalkontableTableRenderer = function WalkontableTableRenderer(wtTable) {
         var height = this.wot.wtTable.getRowHeight(sourceRowIndex);
         if (height) {
           height++;
+          if (sourceRowIndex === 0) {
+            height++;
+          }
           TR.firstChild.style.height = height + 'px';
         } else {
           TR.firstChild.style.height = '';
