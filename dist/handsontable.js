dff88f4020b281b6479023b4619b69bc5aa3476b
zhouxiaoping <zhouxiaoping.map@gmail.com>
2016-01-28 17:52:16 +0800

    add editor

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Wed Jan 27 2016 16:06:46 GMT+0800 (CST)
+ * Date: Thu Jan 28 2016 17:51:48 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Wed Jan 27 2016 16:06:46 GMT+0800 (CST)',
+  buildDate: 'Thu Jan 28 2016 17:51:48 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -6145,6 +6145,7 @@ function EditorManager(instance, priv, selection) {
       selection.transformStart(-enterMoves.row, -enterMoves.col);
     } else {
       selection.transformStart(enterMoves.row, enterMoves.col, true);
+      instance.runHooks('afterMoveSelectionAfterEnter');
     }
   }
   function moveSelectionUp(shiftKey) {
@@ -6266,6 +6267,7 @@ function EditorManager(instance, priv, selection) {
       case KEY_CODES.ENTER:
         if (_this.isEditorOpened()) {
           if (activeEditor && activeEditor.state !== Handsontable.EditorState.WAITING) {
+            Handsontable.enterPress = true;
             _this.closeEditorAndSaveChanges(ctrlDown);
           }
           moveSelectionAfterEnter(event.shiftKey);
