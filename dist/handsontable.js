e41d6852ec13032327d729100439c1d075622bdb
zhouxiaoping <zhouxiaoping.map@gmail.com>
2016-03-14 15:57:56 +0800

    fix copypaste null

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Wed Mar 02 2016 17:08:27 GMT+0800 (CST)
+ * Date: Mon Mar 14 2016 15:57:23 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Wed Mar 02 2016 17:08:27 GMT+0800 (CST)',
+  buildDate: 'Mon Mar 14 2016 15:57:23 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -5095,6 +5095,9 @@ Handsontable.Core = function Core(rootElement, userSettings) {
     if (isHtml(value) && $(value)[0].tagName == 'TD') {
       return value;
     } else {
+      if (value === null) {
+        value = '';
+      }
       tempContainer = $('<div></div>');
       var td = $('<td>' + value + '</td>');
       var i,
