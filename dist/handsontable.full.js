58c4479deb10addc9594067c64a32c990c8b9cc1
zhouxiaoping <zhouxiaoping.map@gmail.com>
2016-03-31 17:04:50 +0800

    change sheepclip

--- dist/handsontable.full.js
+++ dist/handsontable.full.js
@@ -7,13 +7,13 @@
  * Licensed under the MIT license.
  * http://handsontable.com/
  *
- * Date: Mon Mar 14 2016 15:57:23 GMT+0800 (CST)
+ * Date: Thu Mar 31 2016 17:04:15 GMT+0800 (CST)
  */
 /*jslint white: true, browser: true, plusplus: true, indent: 4, maxerr: 50 */
 
 window.Handsontable = {
   version: '0.19.0',
-  buildDate: 'Mon Mar 14 2016 15:57:23 GMT+0800 (CST)',
+  buildDate: 'Thu Mar 31 2016 17:04:15 GMT+0800 (CST)',
 };
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Handsontable = f()}})(function(){var define,module,exports;return (function init(modules, cache, entry) {
   (function outer (modules, cache, entry) {
@@ -15592,7 +15592,9 @@ TableView.prototype.destroy = function() {
             str += val;
           }
         }
-        str += '\n';
+        if (r !== rLen - 1) {
+          str += '\n';
+        }
       }
       return str;
     }
