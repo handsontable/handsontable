f49c3fd9301a35c65d8327804b85c43acebb0397
swistach <swistach@gmail.com>
2016-10-12 16:50:33 +0200

    Clean up of the code. #3800

--- src/plugins/manualColumnMove/manualColumnMove.js
+++ src/plugins/manualColumnMove/manualColumnMove.js
@@ -102,16 +102,19 @@ class ManualColumnMove extends BasePlugin {
       return;
     }
 
-    if (this.columnsMapper._arrayMap.length === 0) {
+    let countCols = this.hot.countCols();
+    let columnsMapperLen = this.columnsMapper._arrayMap.length;
+
+    if (columnsMapperLen === 0) {
       this.columnsMapper.createMap(this.hot.countSourceCols() || this.hot.getSettings().startCols);
 
-    } else if (this.columnsMapper._arrayMap.length < this.hot.countCols()) {
-      let diff = this.hot.countCols() - this.columnsMapper._arrayMap.length;
+    } else if (columnsMapperLen < countCols) {
+      let diff = countCols - columnsMapperLen;
 
-      this.columnsMapper.insertItems(this.columnsMapper._arrayMap.length, diff);
+      this.columnsMapper.insertItems(columnsMapperLen, diff);
 
-    } else if (this.columnsMapper._arrayMap.length > this.hot.countCols()) {
-      let maxIndex = this.hot.countCols() - 1;
+    } else if (columnsMapperLen > countCols) {
+      let maxIndex = countCols - 1;
       let columnsToRemove = [];
 
       arrayEach(this.columnsMapper._arrayMap, (value, index, array) => {
