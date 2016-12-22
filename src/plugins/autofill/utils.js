c644b5a9ebe704249676abf90bcdbbc5b243b2e1
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2016-12-22 14:58:00 +0100

    Refactoring: ES5 code rewritten to ES6 code #3257

---
+++ src/plugins/autofill/utils.js
@@ -0,0 +1,33 @@
+export function getDeltas(start, end, data, direction) {
+  const rowsLength = data.length;
+  const columnsLength = data ? data[0].length : 0;
+  const deltas = [];
+
+  const diffRow = end.row - start.row;
+  const diffCol = end.col - start.col;
+
+  if (['down', 'up'].indexOf(direction) !== -1) {
+    const arr = [];
+
+    for (let col = 0; col <= diffCol; col++) {
+      let startValue = parseInt(data[0][col], 10);
+      let endValue = parseInt(data[rowsLength - 1][col], 10);
+      let delta = (direction === 'down' ? (endValue - startValue) : (startValue - endValue)) / (rowsLength - 1) || 0;
+
+      arr.push(delta);
+    }
+    deltas.push(arr);
+  }
+
+  if (['right', 'left'].indexOf(direction) !== -1) {
+    for (let row = 0; row <= diffRow; row++) {
+      let startValue = parseInt(data[row][0], 10);
+      let endValue = parseInt(data[row][columnsLength - 1], 10);
+      let delta = (direction === 'right' ? (endValue - startValue) : (startValue - endValue)) / (columnsLength - 1) || 0;
+
+      deltas.push([delta]);
+    }
+  }
+
+  return deltas;
+}
