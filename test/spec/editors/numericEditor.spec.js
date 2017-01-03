0ac9501de8a7bebac13f0f9567e0820ed9fbcd8a
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-01-03 11:35:33 +0100

    tests #3877

--- test/spec/editors/numericEditor.spec.js
+++ test/spec/editors/numericEditor.spec.js
@@ -407,4 +407,88 @@ describe('NumericEditor', function () {
     }).not.toThrow();
   });
 
+
+  describe('Cell corner is showed properly when changing focused cells #3877', function () {
+
+    var isFocusedCellDisplayingCornerTest = function (settings) {
+      var moveFromRow = settings.moveFromRow;
+      var moveFromCol = settings.moveFromCol;
+      var moveToRow = settings.moveToRow;
+      var moveToCol = settings.moveToCol;
+      var doneFunc = settings.doneFunc;
+
+      var $corner = $('.wtBorder.current.corner');
+      expect($corner.css('display')).toEqual('none');
+
+      selectCell(moveFromRow, moveFromCol);
+
+      setTimeout(function () {
+        expect($corner.css('display')).toEqual('block');
+
+        keyDown('enter');
+
+        setTimeout(function () {
+
+          expect($corner.css('display')).toEqual('none');
+
+          setTimeout(function () {
+
+            selectCell(moveToRow, moveToCol);
+
+            setTimeout(function () {
+
+              expect($corner.css('display')).toEqual('block');
+              doneFunc();
+
+            }, 100);
+          }, 100);
+        }, 100);
+      }, 100);
+    };
+
+    it("Moving from numeric editor to text editor", function (done) {
+      handsontable({
+        data: [
+          {id: 1, name: "Ted", lastName: "Right", money: 0}
+        ],
+        columns: [
+          {data: 'id'},
+          {data: 'name'},
+          {data: 'lastName'},
+          {data: 'money', type: 'numeric', format: '$0,0.00', language: 'en-US'}
+        ]
+      });
+
+      isFocusedCellDisplayingCornerTest({
+        moveFromRow: 0,
+        moveFromCol: 3,
+        moveToRow: 0,
+        moveToCol: 0,
+        doneFunc: done
+      });
+    });
+
+
+    it("Moving from text editor to numeric editor", function (done) {
+      handsontable({
+        data: [
+          {id: 1, name: "Ted", lastName: "Right", money: 0}
+        ],
+        columns: [
+          {data: 'id'},
+          {data: 'name'},
+          {data: 'lastName'},
+          {data: 'money', type: 'numeric', format: '$0,0.00', language: 'en-US'}
+        ]
+      });
+
+      isFocusedCellDisplayingCornerTest({
+        moveFromRow: 0,
+        moveFromCol: 1,
+        moveToRow: 0,
+        moveToCol: 3,
+        doneFunc: done
+      });
+    });
+  });
 });
