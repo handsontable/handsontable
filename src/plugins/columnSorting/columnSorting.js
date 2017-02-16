726ef532c50717febbc78616cd8fdc6de925e67f
Marco Petris <marco.petris@web.de>
2017-02-16 11:26:20 +0100

    bugfix undefined access to amount in columnSorting

--- src/plugins/columnSorting/columnSorting.js
+++ src/plugins/columnSorting/columnSorting.js
@@ -525,7 +525,9 @@ class ColumnSorting extends BasePlugin {
     if (!this.isSorted()) {
       return;
     }
-
+    if (amount === undefined) {
+      amount = 0;
+    }
     for (var i = 0; i < this.hot.sortIndex.length; i++) {
       if (this.hot.sortIndex[i][0] >= index) {
         this.hot.sortIndex[i][0] += amount;
