f3f053485f54c27c5c820c8f9e270776f6af8db8
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-06-29 13:31:47 +0200

    Style changes once again #4354

--- test/unit/helpers/TemplateLiteralTag.spec.js
+++ test/unit/helpers/TemplateLiteralTag.spec.js
@@ -1,36 +1,35 @@
 import {toSingleLine} from 'handsontable/helpers/templateLiteralTag';
 
-describe('Helper for template literals', () => {
+describe('Helpers for template literals', () => {
+  describe('toSingleLine', function () {
+    it('should strip two line string (string with whitespace at end of first line and indention at second one)', () => {
+      const text = toSingleLine`Hello world 
+        Hello world`;
 
-  it('should strip two line string (string with whitespaces at end of first line and indention at second one)', () => {
-    const text = toSingleLine`Hello world Hello world Hello world Hello world Hello world Hello world Hello world 
-      Hello world Hello world Hello world Hello world Hello world Hello world`;
+      expect(text).toEqual('Hello world Hello world');
+    });
 
-    expect(text).toEqual('Hello world Hello world Hello world Hello world Hello world Hello world Hello world ' +
-      'Hello world Hello world Hello world Hello world Hello world Hello world');
-  });
+    it('should strip two line string (string without whitespace at end of first line and indention at second one)', () => {
+      const text = toSingleLine`Hello world
+        Hello world`;
 
-  it('should strip two line string (string without whitespaces at end of first line and indention at second one)', () => {
-    const text = toSingleLine`Hello world Hello world Hello world Hello world Hello world Hello world Hello world
-      Hello world Hello world Hello world Hello world Hello world Hello world`;
+      expect(text).toEqual('Hello worldHello world');
+    });
 
-    expect(text).toEqual('Hello world Hello world Hello world Hello world Hello world Hello world Hello world' +
-      'Hello world Hello world Hello world Hello world Hello world Hello world');
-  });
+    it('should include literals and not remove whitespaces between them without necessary', () => {
+      const a = 'Hello';
+      const b = 'world';
+      const text = toSingleLine`${a}   ${b}`;
 
-  it('should include literals and not remove whitespaces between them without necessary', () => {
-    const a = 'Hello';
-    const b = 'world';
+      expect(text).toEqual('Hello   world');
+    });
 
-    const text = toSingleLine`${a}   ${b}`;
-    expect(text).toEqual('Hello   world');
-  });
+    it('should remove whitespaces from both sides of a string.', () => {
+      const a = '   Hello';
+      const b = 'world   ';
+      const text = toSingleLine`${a} ${b}`;
 
-  it('should remove whitespaces from both sides of a string.', () => {
-    const a = '   Hello';
-    const b = 'world   ';
-
-    const text = toSingleLine`${a} ${b}`;
-    expect(text).toEqual('Hello world');
+      expect(text).toEqual('Hello world');
+    });
   });
 });
