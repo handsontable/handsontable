a5c4dffb1ad7c47ca4cad2782bb512a50ee93a24
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-06-29 12:17:53 +0200

    Helper & tests #4354

---
+++ src/helpers/templateLiteralTag.js
@@ -0,0 +1,17 @@
+/**
+ * Tags a multiline string and return new one without line break characters and following spaces
+ * @param {Array.<String>} strings Parts of the entire string without expressions.
+ * @param {...String} expressions Expressions converted to strings, which are added to the entire string.
+ * @returns {String}
+ */
+export function toSingleLine(strings, ...expressions) {
+  let result = '';
+  let i = 0;
+
+  for (let i = 0; i < strings.length; i += 1) {
+    result += strings[i].replace(/(?:\r?\n\s+)/g, '');
+    result += expressions[i] ? expressions[i] : '';
+  }
+
+  return result.trim();
+}
