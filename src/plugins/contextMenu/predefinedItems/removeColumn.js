6ab6c9ce6e2ef9d72ea6a8570fd1abca09fdc41d
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-08-28 11:27:51 +0200

    ContextMenu change for tests #4457

--- src/plugins/contextMenu/predefinedItems/removeColumn.js
+++ src/plugins/contextMenu/predefinedItems/removeColumn.js
@@ -1,11 +1,26 @@
 import {getValidSelection} from './../utils';
+import L from './../../../i18n/index';
+import * as C from './../../../i18n/constants';
+import {createRange} from './../../../i18n/utils';
 
 export const KEY = 'remove_col';
 
 export default function removeColumnItem() {
   return {
     key: KEY,
-    name: 'Remove column',
+    name() {
+      const [, fromColumn, , toColumn] = this.getSelected();
+      const translationConfiguration = {};
+
+      if (fromColumn - toColumn !== 0) {
+        translationConfiguration.rangeOfColumns = createRange(
+          {index: fromColumn + 1, value: this.getColHeader(fromColumn)},
+          {index: toColumn + 1, value: this.getColHeader(toColumn)}
+        );
+      }
+
+      return L.getPhrase(C.CONTEXTMENU_ITEMS_REMOVE_COLUMN, translationConfiguration);
+    },
 
     callback(key, selection) {
       let amount = selection.end.col - selection.start.col + 1;
--- src/plugins/contextMenu/predefinedItems/removeRow.js
+++ src/plugins/contextMenu/predefinedItems/removeRow.js
@@ -1,11 +1,26 @@
 import {getValidSelection} from './../utils';
+import L from './../../../i18n/index';
+import * as C from './../../../i18n/constants';
+import {createRange} from './../../../i18n/utils';
 
 export const KEY = 'remove_row';
 
 export default function removeRowItem() {
   return {
     key: KEY,
-    name: 'Remove row',
+    name() {
+      const [fromRow, , toRow] = this.getSelected();
+      const translationConfiguration = {};
+
+      if (fromRow - toRow !== 0) {
+        translationConfiguration.rangeOfRows = createRange(
+          {index: fromRow + 1, value: this.getRowHeader(fromRow)},
+          {index: toRow + 1, value: this.getRowHeader(toRow)}
+        );
+      }
+
+      return L.getPhrase(C.CONTEXTMENU_ITEMS_REMOVE_ROW, translationConfiguration);
+    },
 
     callback(key, selection) {
       let amount = selection.end.row - selection.start.row + 1;
