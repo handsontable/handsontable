0d49326b677fddfe758e12b57196c71f21242cdb
Krzysztof Budnik <budnix@gmail.com>
2017-09-04 12:07:28 +0200

    Fixed typo

--- hot.config.js
+++ hot.config.js
@@ -1,12 +1,12 @@
 const moment = require('moment');
-const package = require('./package.json');
+const packageBody = require('./package.json');
 
 module.exports = {
   HOT_FILENAME: 'handsontable',
-  HOT_VERSION: package.version,
+  HOT_VERSION: packageBody.version,
   HOT_BASE_VERSION: '',
   HOT_PACKAGE_TYPE: 'ce',
-  HOT_PACKAGE_NAME: package.name,
+  HOT_PACKAGE_NAME: packageBody.name,
   HOT_BUILD_DATE: moment().format('DD/MM/YYYY HH:mm:ss'),
   HOT_RELEASE_DATE: '30/08/2017',
-}
+};
