cd673f409ce56108adb74042d96205e4c3497d1a
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-10-25 15:43:30 +0200

    Extra tests & test configuration  #4457

--- test/e2e/i18n/index.spec.js
+++ test/e2e/i18n/index.spec.js
@@ -2,6 +2,11 @@ describe('i18n', () => {
   const id = 'testContainer';
   const DEFAULT_LANGUAGE_CODE = 'en-US';
   const NOT_EXISTING_LANGUAGE_CODE = 'bs-GY';
+  const NOT_EXISTING_LANGUAGE_CODE2 = 'dd-Da';
+  const EXISTING_LANGUAGE_CODE = 'pl-PL';
+  const POLISH_LANGUAGE_CODE = EXISTING_LANGUAGE_CODE;
+  const INSERT_ROW_ABOVE_IN_DEFAULT_LANGUAGE = 'Insert row above';
+  const INSERT_ROW_ABOVE_IN_POLISH = 'Umieść wiersz powyżej';
 
   beforeEach(function() {
     this.$container = $(`<div id="${id}"></div>`).appendTo('body');
@@ -14,17 +19,7 @@ describe('i18n', () => {
     }
   });
 
-  describe('error / warning handling when changing languages', () => {
-    xit('should log warn in console when setting not existing language at start', () => {
-      const spy = spyOn(console, 'warn');
-
-      handsontable({
-        language: NOT_EXISTING_LANGUAGE_CODE
-      });
-
-      expect(spy).toHaveBeenCalled();
-    });
-
+  describe('translation doesn\'t throw errors', () => {
     it('should not throw error when setting not existing language at start', async () => {
       const spy = spyOn(window, 'onerror');
 
@@ -33,16 +28,6 @@ describe('i18n', () => {
       });
 
       await sleep(100);
-
-      expect(spy).not.toHaveBeenCalled();
-    });
-
-    it('should not log warn in console when setting directly default language at start', () => {
-      const spy = spyOn(console, 'warn');
-
-      handsontable({
-        language: DEFAULT_LANGUAGE_CODE
-      });
 
       expect(spy).not.toHaveBeenCalled();
     });
@@ -59,16 +44,6 @@ describe('i18n', () => {
       expect(spy).not.toHaveBeenCalled();
     });
 
-    it('should log warn when trying to set not existing language by updateSettings', () => {
-      const spy = spyOn(console, 'warn');
-
-      handsontable();
-
-      updateSettings({language: NOT_EXISTING_LANGUAGE_CODE});
-
-      expect(spy).toHaveBeenCalled();
-    });
-
     it('should not throw error when trying to set not existing language by updateSettings', async () => {
       const spy = spyOn(window, 'onerror');
 
@@ -81,18 +56,8 @@ describe('i18n', () => {
       expect(spy).not.toHaveBeenCalled();
     });
 
-    it('should not log warn in console when trying to set default language by updateSettings', () => {
-      const spy = spyOn(console, 'warn');
-
-      handsontable();
-
-      updateSettings({language: DEFAULT_LANGUAGE_CODE});
-
-      expect(spy).not.toHaveBeenCalled();
-    });
-
-    it('should not throw error when trying to set default language by updateSettings', async () => {
-      const spy = spyOn(console, 'warn');
+    it('should not throw error when trying to set directly default language by updateSettings', async () => {
+      const spy = spyOn(window, 'onerror');
 
       handsontable();
 
@@ -101,6 +66,110 @@ describe('i18n', () => {
       await sleep(100);
 
       expect(spy).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('contextMenu translation', () => {
+    it('should translate contextMenu UI when setting existing language at start', async () => {
+      handsontable({
+        language: POLISH_LANGUAGE_CODE,
+        contextMenu: ['row_above']
+      });
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_POLISH);
+    });
+
+    it('should not change default contextMenu UI when trying to set not existing language at start', async () => {
+      handsontable({
+        language: NOT_EXISTING_LANGUAGE_CODE,
+        contextMenu: ['row_above']
+      });
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_DEFAULT_LANGUAGE);
+    });
+
+    it('should translate contextMenu UI when setting existing language by updateSettings', async () => {
+      handsontable({
+        contextMenu: ['row_above']
+      });
+
+      updateSettings({language: POLISH_LANGUAGE_CODE});
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_POLISH);
+    });
+
+    it('should not change default contextMenu UI when trying to set not existing language by updateSettings #1', async () => {
+      handsontable({
+        contextMenu: ['row_above']
+      });
+
+      updateSettings({language: NOT_EXISTING_LANGUAGE_CODE});
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_DEFAULT_LANGUAGE);
+    });
+
+    it('should not change default contextMenu UI when trying to set not existing language by updateSettings #2', async () => {
+      handsontable({
+        language: NOT_EXISTING_LANGUAGE_CODE,
+        contextMenu: ['row_above']
+      });
+
+      updateSettings({language: NOT_EXISTING_LANGUAGE_CODE2});
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_DEFAULT_LANGUAGE);
+    });
+
+    it('should not change previously translated contextMenu UI when trying to set not existing language by updateSettings', async () => {
+      handsontable({
+        language: POLISH_LANGUAGE_CODE,
+        contextMenu: ['row_above']
+      });
+
+      updateSettings({language: NOT_EXISTING_LANGUAGE_CODE});
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
+
+      expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_POLISH);
     });
   });
 });
