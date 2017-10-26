741257820cf13f7547b10fa91f5b2c02ccff378b
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-10-26 08:55:53 +0200

    Extra tests #4457

--- test/e2e/i18n/index.spec.js
+++ test/e2e/i18n/index.spec.js
@@ -1,10 +1,12 @@
 describe('i18n', () => {
   const id = 'testContainer';
+
   const DEFAULT_LANGUAGE_CODE = 'en-US';
   const NOT_EXISTING_LANGUAGE_CODE = 'bs-GY';
   const NOT_EXISTING_LANGUAGE_CODE2 = 'dd-Da';
   const EXISTING_LANGUAGE_CODE = 'pl-PL';
   const POLISH_LANGUAGE_CODE = EXISTING_LANGUAGE_CODE;
+
   const INSERT_ROW_ABOVE_IN_DEFAULT_LANGUAGE = 'Insert row above';
   const INSERT_ROW_ABOVE_IN_POLISH = 'Umieść wiersz powyżej';
 
@@ -170,6 +172,50 @@ describe('i18n', () => {
       const $contextMenuItem = $('.htContextMenu tbody td:not(.htSeparator)');
 
       expect($contextMenuItem.text()).toEqual(INSERT_ROW_ABOVE_IN_POLISH);
+    });
+
+    it('should translate multi-level menu properly', async () => {
+      const ALIGN_LEFT_IN_POLISH = 'Lewo';
+
+      handsontable({
+        language: POLISH_LANGUAGE_CODE,
+        contextMenu: ['alignment']
+      });
+
+      selectCell(0, 0);
+      contextMenu();
+
+      await sleep(300);
+
+      const $menu = $('.htSubmenu');
+
+      $menu.simulate('mouseover');
+
+      await sleep(300);
+
+      const $submenuItem = $('.htContextMenu').eq(1).find('tbody td:not(.htSeparator)').eq(0);
+
+      expect($submenuItem.text()).toEqual(ALIGN_LEFT_IN_POLISH);
+    });
+
+    it('should choose proper form of phrase when translating', async () => {
+      const REMOVE_ROW_PLURAL_IN_DEFAULT_LANGUAGE = 'Remove rows';
+      const REMOVE_COLUMN_PLURAL_IN_DEFAULT_LANGUAGE = 'Remove columns';
+
+      handsontable({
+        contextMenu: ['remove_row', 'remove_col']
+      });
+
+      selectCell(0, 0, 2, 2);
+      contextMenu();
+
+      await sleep(300);
+
+      const $removeRowItem = $('.htContextMenu').eq(0).find('tbody td:not(.htSeparator)').eq(0);
+      const $removeColumnItem = $('.htContextMenu').eq(0).find('tbody td:not(.htSeparator)').eq(1);
+
+      expect($removeRowItem.text()).toEqual(REMOVE_ROW_PLURAL_IN_DEFAULT_LANGUAGE);
+      expect($removeColumnItem.text()).toEqual(REMOVE_COLUMN_PLURAL_IN_DEFAULT_LANGUAGE);
     });
   });
 });
