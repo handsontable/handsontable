a5f23aced622b76d46dbc6e3b0fc04102df06cb9
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-10-26 15:08:18 +0200

    Extra tests #4457

--- test/unit/i18n/index.spec.js
+++ test/unit/i18n/index.spec.js
@@ -1,4 +1,5 @@
 import {getLanguage, getLanguages, registerLanguage} from 'handsontable/i18n/dictionariesManager';
+import {register as registerFormatter} from 'handsontable/i18n/phraseFormatters';
 import plPL from 'handsontable/i18n/languages/pl-PL';
 import * as constants from 'handsontable/i18n/constants';
 import Handsontable from 'handsontable';
@@ -11,6 +12,8 @@ describe('i18n', () => {
     Handsontable.languages.register(plPL);
   });
 
+  const dictionaryKey = Handsontable.languages.constants.CONTEXTMENU_ITEMS_ROW_ABOVE;
+
   it('should extend Handsontable by language keys containing functions and constants for translation', () => {
     expect(Handsontable.languages.get).toBe(getLanguage);
     expect(Handsontable.languages.getAll).toBe(getLanguages);
@@ -18,16 +21,23 @@ describe('i18n', () => {
     expect(Handsontable.languages.constants).toBe(constants);
   });
 
-  it('should get translated phrase by Handsontable `getTranslatedPhrase` function', () => {
-    const dictionaryKey = Handsontable.languages.constants.CONTEXTMENU_ITEMS_ROW_ABOVE;
-
+  it('should get translated phrase by `getTranslatedPhrase` function', () => {
     expect(Handsontable.languages.getTranslatedPhrase(plPL.languageCode, dictionaryKey)).toEqual(plPL[dictionaryKey]);
   });
 
   it('should get `null` when trying to translate phrase by passing language code of not registered language', () => {
-    const dictionaryKey = Handsontable.languages.constants.CONTEXTMENU_ITEMS_ROW_ABOVE;
     const notRegisteredLanguageCode = 'br-Za';
 
     expect(Handsontable.languages.getTranslatedPhrase(notRegisteredLanguageCode, dictionaryKey)).toEqual(null);
+  });
+
+  it('should call formatters when `getTranslatedPhrase` function is called', () => {
+    let formatterWasRun = false;
+
+    registerFormatter('formatterChangingVariable', () => { formatterWasRun = true; });
+
+    Handsontable.languages.getTranslatedPhrase(plPL.languageCode, dictionaryKey);
+
+    expect(formatterWasRun).toEqual(true);
   });
 });
