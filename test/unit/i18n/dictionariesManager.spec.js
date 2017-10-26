bcecda6b215af5f49e0b6da84d7869b11a495f03
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-10-26 14:17:24 +0200

    Extra unit tests #4457

--- test/unit/i18n/dictionariesManager.spec.js
+++ test/unit/i18n/dictionariesManager.spec.js
@@ -1,16 +1,24 @@
-import {getLanguage, getLanguages} from 'handsontable/i18n/dictionariesManager';
+import {getLanguage, getLanguages, registerLanguage, DEFAULT_LANGUAGE_CODE} from 'handsontable/i18n/dictionariesManager';
 import plPL from 'handsontable/i18n/languages/pl-PL';
 
 describe('i18n dictionariesManager', () => {
-  it('should be empty at start', () => {
-    expect(getLanguages().length).toEqual(0);
+  it('should register automatically default language', () => {
+    const allLanguages = getLanguages();
+    const defaultLanguageIsRegistered = allLanguages.some((dictionary) => dictionary.languageCode === DEFAULT_LANGUAGE_CODE);
+
+    expect(defaultLanguageIsRegistered).toEqual(true);
   });
 
-  it('should log warn when trying to get not registered language', () => {
-    const spy = spyOn(console, 'warn');
+  it('should not register automatically imported /src languages', () => {
+    expect(getLanguage(plPL.languageCode)).toEqual(null);
+  });
 
-    getLanguage(plPL.languageCode);
+  it('should register language', () => {
+    // Note: please keep in mind that this language will be registered also for next unit tests!
+    // It's stored globally for already loaded Handsontable library.
 
-    expect(spy).toHaveBeenCalled();
+    registerLanguage(plPL);
+
+    expect(getLanguages().length).toEqual(2);
   });
 });
--- test/unit/i18n/index.spec.js
+++ test/unit/i18n/index.spec.js
@@ -1,13 +1,33 @@
+import {getLanguage, getLanguages, registerLanguage} from 'handsontable/i18n/dictionariesManager';
+import plPL from 'handsontable/i18n/languages/pl-PL';
+import * as constants from 'handsontable/i18n/constants';
 import Handsontable from 'handsontable';
 
 describe('i18n', () => {
-  it('should extend Handsontable by language keys containing functions', () => {
-    expect(typeof Handsontable.languages.get).toEqual('function');
-    expect(typeof Handsontable.languages.getAll).toEqual('function');
-    expect(typeof Handsontable.languages.register).toEqual('function');
+  beforeAll(() => {
+    // Note: please keep in mind that this language will be registered for all unit tests!
+    // It's stored globally for already loaded Handsontable library.
+
+    Handsontable.languages.register(plPL);
   });
 
-  it('should extend Handsontable by language keys containing constants for translation', () => {
-    expect(typeof Handsontable.languages.constants).toBe('object');
+  it('should extend Handsontable by language keys containing functions and constants for translation', () => {
+    expect(Handsontable.languages.get).toBe(getLanguage);
+    expect(Handsontable.languages.getAll).toBe(getLanguages);
+    expect(Handsontable.languages.register).toBe(registerLanguage);
+    expect(Handsontable.languages.constants).toBe(constants);
+  });
+
+  it('should get translated phrase by Handsontable `getTranslatedPhrase` function', () => {
+    const dictionaryKey = Handsontable.languages.constants.CONTEXTMENU_ITEMS_ROW_ABOVE;
+
+    expect(Handsontable.languages.getTranslatedPhrase(plPL.languageCode, dictionaryKey)).toEqual(plPL[dictionaryKey]);
+  });
+
+  it('should get `null` when trying to translate phrase by passing language code of not registered language', () => {
+    const dictionaryKey = Handsontable.languages.constants.CONTEXTMENU_ITEMS_ROW_ABOVE;
+    const notRegisteredLanguageCode = 'br-Za';
+
+    expect(Handsontable.languages.getTranslatedPhrase(notRegisteredLanguageCode, dictionaryKey)).toEqual(null);
   });
 });
