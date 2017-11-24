80f03165e79af3b0ab5e4a0da93e7f05cad60d28
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2017-11-24 11:42:04 +0100

    Updated constants #4457

--- src/i18n/constants.js
+++ src/i18n/constants.js
@@ -2,93 +2,94 @@
  * Constants for parts of translation.
  */
 
-export const CONTEXTMENU_ITEMS_ROW_ABOVE = 'ContextMenu:items.insertRowAbove';
-export const CONTEXTMENU_ITEMS_ROW_BELOW = 'ContextMenu:items.insertRowBelow';
-export const CONTEXTMENU_ITEMS_INSERT_LEFT = 'ContextMenu:items.insertColumnOnTheLeft';
-export const CONTEXTMENU_ITEMS_INSERT_RIGHT = 'ContextMenu:items.insertColumnOnTheRight';
-export const CONTEXTMENU_ITEMS_REMOVE_ROW = 'ContextMenu:items.removeRow';
-export const CONTEXTMENU_ITEMS_REMOVE_COLUMN = 'ContextMenu:items.removeColumn';
-export const CONTEXTMENU_ITEMS_UNDO = 'ContextMenu:items.undo';
-export const CONTEXTMENU_ITEMS_REDO = 'ContextMenu:items.redo';
-export const CONTEXTMENU_ITEMS_READ_ONLY = 'ContextMenu:items.readOnly';
-export const CONTEXTMENU_ITEMS_CLEAR_COLUMN = 'ContextMenu:items.clearColumn';
+export const CONTEXT_MENU_ITEMS_NAMESPACE = 'ContextMenu:items';
+export const CONTEXTMENU_ITEMS_ROW_ABOVE = `${CONTEXT_MENU_ITEMS_NAMESPACE}.insertRowAbove`;
+export const CONTEXTMENU_ITEMS_ROW_BELOW = `${CONTEXT_MENU_ITEMS_NAMESPACE}.insertRowBelow`;
+export const CONTEXTMENU_ITEMS_INSERT_LEFT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.insertColumnOnTheLeft`;
+export const CONTEXTMENU_ITEMS_INSERT_RIGHT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.insertColumnOnTheRight`;
+export const CONTEXTMENU_ITEMS_REMOVE_ROW = `${CONTEXT_MENU_ITEMS_NAMESPACE}.removeRow`;
+export const CONTEXTMENU_ITEMS_REMOVE_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.removeColumn`;
+export const CONTEXTMENU_ITEMS_UNDO = `${CONTEXT_MENU_ITEMS_NAMESPACE}.undo`;
+export const CONTEXTMENU_ITEMS_REDO = `${CONTEXT_MENU_ITEMS_NAMESPACE}.redo`;
+export const CONTEXTMENU_ITEMS_READ_ONLY = `${CONTEXT_MENU_ITEMS_NAMESPACE}.readOnly`;
+export const CONTEXTMENU_ITEMS_CLEAR_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.clearColumn`;
 
-export const CONTEXTMENU_ITEMS_COPY = 'ContextMenu:items.copy';
-export const CONTEXTMENU_ITEMS_CUT = 'ContextMenu:items.cut';
+export const CONTEXTMENU_ITEMS_COPY = `${CONTEXT_MENU_ITEMS_NAMESPACE}.copy`;
+export const CONTEXTMENU_ITEMS_CUT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.cut`;
 
-export const CONTEXTMENU_ITEMS_FREEZE_COLUMN = 'ContextMenu:items.freezeColumn';
-export const CONTEXTMENU_ITEMS_UNFREEZE_COLUMN = 'ContextMenu:items.unfreezeColumn';
+export const CONTEXTMENU_ITEMS_FREEZE_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.freezeColumn`;
+export const CONTEXTMENU_ITEMS_UNFREEZE_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.unfreezeColumn`;
 
-export const CONTEXTMENU_ITEMS_MERGE_CELLS = 'ContextMenu:items.mergeCells';
-export const CONTEXTMENU_ITEMS_UNMERGE_CELLS = 'ContextMenu:items.unmergeCells';
+export const CONTEXTMENU_ITEMS_MERGE_CELLS = `${CONTEXT_MENU_ITEMS_NAMESPACE}.mergeCells`;
+export const CONTEXTMENU_ITEMS_UNMERGE_CELLS = `${CONTEXT_MENU_ITEMS_NAMESPACE}.unmergeCells`;
 
-export const CONTEXTMENU_ITEMS_ADD_COMMENT = 'ContextMenu:items.addComment';
-export const CONTEXTMENU_ITEMS_EDIT_COMMENT = 'ContextMenu:items.editComment';
-export const CONTEXTMENU_ITEMS_REMOVE_COMMENT = 'ContextMenu:items.removeComment';
-export const CONTEXTMENU_ITEMS_READ_ONLY_COMMENT = 'ContextMenu:items.readOnlyComment';
+export const CONTEXTMENU_ITEMS_ADD_COMMENT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.addComment`;
+export const CONTEXTMENU_ITEMS_EDIT_COMMENT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.editComment`;
+export const CONTEXTMENU_ITEMS_REMOVE_COMMENT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.removeComment`;
+export const CONTEXTMENU_ITEMS_READ_ONLY_COMMENT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.readOnlyComment`;
 
-export const CONTEXTMENU_ITEMS_ALIGNMENT = 'ContextMenu:items.align';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_LEFT = 'ContextMenu:items.align.left';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_CENTER = 'ContextMenu:items.align.center';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_RIGHT = 'ContextMenu:items.align.right';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_JUSTIFY = 'ContextMenu:items.align.justify';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_TOP = 'ContextMenu:items.align.top';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_MIDDLE = 'ContextMenu:items.align.middle';
-export const CONTEXTMENU_ITEMS_ALIGNMENT_BOTTOM = 'ContextMenu:items.align.bottom';
+export const CONTEXTMENU_ITEMS_ALIGNMENT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_LEFT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.left`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_CENTER = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.center`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_RIGHT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.right`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_JUSTIFY = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.justify`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_TOP = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.top`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_MIDDLE = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.middle`;
+export const CONTEXTMENU_ITEMS_ALIGNMENT_BOTTOM = `${CONTEXT_MENU_ITEMS_NAMESPACE}.align.bottom`;
 
-export const CONTEXTMENU_ITEMS_BORDERS = 'ContextMenu:items.borders';
-export const CONTEXTMENU_ITEMS_BORDERS_TOP = 'ContextMenu:items.borders.top';
-export const CONTEXTMENU_ITEMS_BORDERS_RIGHT = 'ContextMenu:items.borders.right';
-export const CONTEXTMENU_ITEMS_BORDERS_BOTTOM = 'ContextMenu:items.borders.bottom';
-export const CONTEXTMENU_ITEMS_BORDERS_LEFT = 'ContextMenu:items.borders.left';
-export const CONTEXTMENU_ITEMS_REMOVE_BORDERS = 'ContextMenu:items.borders.remove';
+export const CONTEXTMENU_ITEMS_BORDERS = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders`;
+export const CONTEXTMENU_ITEMS_BORDERS_TOP = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders.top`;
+export const CONTEXTMENU_ITEMS_BORDERS_RIGHT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders.right`;
+export const CONTEXTMENU_ITEMS_BORDERS_BOTTOM = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders.bottom`;
+export const CONTEXTMENU_ITEMS_BORDERS_LEFT = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders.left`;
+export const CONTEXTMENU_ITEMS_REMOVE_BORDERS = `${CONTEXT_MENU_ITEMS_NAMESPACE}.borders.remove`;
 
-export const CONTEXTMENU_ITEMS_NESTED_ROWS_INSERT_CHILD = 'ContextMenu:items.nestedHeaders.insertChildRow';
-export const CONTEXTMENU_ITEMS_NESTED_ROWS_DETACH_CHILD = 'ContextMenu:items.nestedHeaders.detachFromParent';
+export const CONTEXTMENU_ITEMS_NESTED_ROWS_INSERT_CHILD = `${CONTEXT_MENU_ITEMS_NAMESPACE}.nestedHeaders.insertChildRow`;
+export const CONTEXTMENU_ITEMS_NESTED_ROWS_DETACH_CHILD = `${CONTEXT_MENU_ITEMS_NAMESPACE}.nestedHeaders.detachFromParent`;
 
-export const CONTEXTMENU_ITEMS_HIDE_COLUMN = 'ContextMenu:items.hideColumn';
-export const CONTEXTMENU_ITEMS_SHOW_COLUMN = 'ContextMenu:items.showColumn';
+export const CONTEXTMENU_ITEMS_HIDE_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.hideColumn`;
+export const CONTEXTMENU_ITEMS_SHOW_COLUMN = `${CONTEXT_MENU_ITEMS_NAMESPACE}.showColumn`;
 
-export const CONTEXTMENU_ITEMS_HIDE_ROW = 'ContextMenu:items.hideRow';
-export const CONTEXTMENU_ITEMS_SHOW_ROW = 'ContextMenu:items.showRow';
+export const CONTEXTMENU_ITEMS_HIDE_ROW = `${CONTEXT_MENU_ITEMS_NAMESPACE}.hideRow`;
+export const CONTEXTMENU_ITEMS_SHOW_ROW = `${CONTEXT_MENU_ITEMS_NAMESPACE}.showRow`;
 
 export const FILTERS_NAMESPACE = 'Filters:';
-export const FILTERS_CONDITIONS_NAMESPACE = 'Filters:conditions';
-export const FILTERS_CONDITIONS_NONE = 'Filters:conditions.none';
-export const FILTERS_CONDITIONS_EMPTY = 'Filters:conditions.isEmpty';
-export const FILTERS_CONDITIONS_NOT_EMPTY = 'Filters:conditions.isNotEmpty';
-export const FILTERS_CONDITIONS_EQUAL = 'Filters:conditions.isEqualTo';
-export const FILTERS_CONDITIONS_NOT_EQUAL = 'Filters:conditions.isNotEqualTo';
-export const FILTERS_CONDITIONS_BEGINS_WITH = 'Filters:conditions.beginsWith';
-export const FILTERS_CONDITIONS_ENDS_WITH = 'Filters:conditions.endsWith';
-export const FILTERS_CONDITIONS_CONTAINS = 'Filters:conditions.contains';
-export const FILTERS_CONDITIONS_NOT_CONTAIN = 'Filters:conditions.doesNotContain';
-export const FILTERS_CONDITIONS_BY_VALUE = 'Filters:conditions.byValue';
-export const FILTERS_CONDITIONS_GREATER_THAN = 'Filters:conditions.greaterThan';
-export const FILTERS_CONDITIONS_GREATER_THAN_OR_EQUAL = 'Filters:conditions.greaterThanOrEqualTo';
-export const FILTERS_CONDITIONS_LESS_THAN = 'Filters:conditions.lessThan';
-export const FILTERS_CONDITIONS_LESS_THAN_OR_EQUAL = 'Filters:conditions.lessThanOrEqualTo';
-export const FILTERS_CONDITIONS_BETWEEN = 'Filters:conditions.isBetween';
-export const FILTERS_CONDITIONS_NOT_BETWEEN = 'Filters:conditions.isNotBetween';
-export const FILTERS_CONDITIONS_AFTER = 'Filters:conditions.after';
-export const FILTERS_CONDITIONS_BEFORE = 'Filters:conditions.before';
-export const FILTERS_CONDITIONS_TODAY = 'Filters:conditions.today';
-export const FILTERS_CONDITIONS_TOMORROW = 'Filters:conditions.tomorrow';
-export const FILTERS_CONDITIONS_YESTERDAY = 'Filters:conditions.yesterday';
+export const FILTERS_CONDITIONS_NAMESPACE = `${FILTERS_NAMESPACE}conditions`;
+export const FILTERS_CONDITIONS_NONE = `${FILTERS_CONDITIONS_NAMESPACE}.none`;
+export const FILTERS_CONDITIONS_EMPTY = `${FILTERS_CONDITIONS_NAMESPACE}.isEmpty`;
+export const FILTERS_CONDITIONS_NOT_EMPTY = `${FILTERS_CONDITIONS_NAMESPACE}.isNotEmpty`;
+export const FILTERS_CONDITIONS_EQUAL = `${FILTERS_CONDITIONS_NAMESPACE}.isEqualTo`;
+export const FILTERS_CONDITIONS_NOT_EQUAL = `${FILTERS_CONDITIONS_NAMESPACE}.isNotEqualTo`;
+export const FILTERS_CONDITIONS_BEGINS_WITH = `${FILTERS_CONDITIONS_NAMESPACE}.beginsWith`;
+export const FILTERS_CONDITIONS_ENDS_WITH = `${FILTERS_CONDITIONS_NAMESPACE}.endsWith`;
+export const FILTERS_CONDITIONS_CONTAINS = `${FILTERS_CONDITIONS_NAMESPACE}.contains`;
+export const FILTERS_CONDITIONS_NOT_CONTAIN = `${FILTERS_CONDITIONS_NAMESPACE}.doesNotContain`;
+export const FILTERS_CONDITIONS_BY_VALUE = `${FILTERS_CONDITIONS_NAMESPACE}.byValue`;
+export const FILTERS_CONDITIONS_GREATER_THAN = `${FILTERS_CONDITIONS_NAMESPACE}.greaterThan`;
+export const FILTERS_CONDITIONS_GREATER_THAN_OR_EQUAL = `${FILTERS_CONDITIONS_NAMESPACE}.greaterThanOrEqualTo`;
+export const FILTERS_CONDITIONS_LESS_THAN = `${FILTERS_CONDITIONS_NAMESPACE}.lessThan`;
+export const FILTERS_CONDITIONS_LESS_THAN_OR_EQUAL = `${FILTERS_CONDITIONS_NAMESPACE}.lessThanOrEqualTo`;
+export const FILTERS_CONDITIONS_BETWEEN = `${FILTERS_CONDITIONS_NAMESPACE}.isBetween`;
+export const FILTERS_CONDITIONS_NOT_BETWEEN = `${FILTERS_CONDITIONS_NAMESPACE}.isNotBetween`;
+export const FILTERS_CONDITIONS_AFTER = `${FILTERS_CONDITIONS_NAMESPACE}.after`;
+export const FILTERS_CONDITIONS_BEFORE = `${FILTERS_CONDITIONS_NAMESPACE}.before`;
+export const FILTERS_CONDITIONS_TODAY = `${FILTERS_CONDITIONS_NAMESPACE}.today`;
+export const FILTERS_CONDITIONS_TOMORROW = `${FILTERS_CONDITIONS_NAMESPACE}.tomorrow`;
+export const FILTERS_CONDITIONS_YESTERDAY = `${FILTERS_CONDITIONS_NAMESPACE}.yesterday`;
 
-export const FILTERS_DIVS_FILTER_BY_CONDITION = 'Filters:labels.filterByCondition';
-export const FILTERS_DIVS_FILTER_BY_VALUE = 'Filters:labels.filterByValue';
+export const FILTERS_DIVS_FILTER_BY_CONDITION = `${FILTERS_NAMESPACE}labels.filterByCondition`;
+export const FILTERS_DIVS_FILTER_BY_VALUE = `${FILTERS_NAMESPACE}labels.filterByValue`;
 
-export const FILTERS_LABELS_CONJUNCTION = 'Filters:labels.conjunction';
-export const FILTERS_LABELS_DISJUNCTION = 'Filters:labels.disjunction';
+export const FILTERS_LABELS_CONJUNCTION = `${FILTERS_NAMESPACE}labels.conjunction`;
+export const FILTERS_LABELS_DISJUNCTION = `${FILTERS_NAMESPACE}labels.disjunction`;
 
-export const FILTERS_VALUES_BLANK_CELLS = 'Filters:values.blankCells';
+export const FILTERS_VALUES_BLANK_CELLS = `${FILTERS_NAMESPACE}values.blankCells`;
 
-export const FILTERS_BUTTONS_SELECT_ALL = 'Filters:buttons.selectAll';
-export const FILTERS_BUTTONS_CLEAR = 'Filters:buttons.clear';
-export const FILTERS_BUTTONS_OK = 'Filters:buttons.ok';
-export const FILTERS_BUTTONS_CANCEL = 'Filters:buttons.cancel';
+export const FILTERS_BUTTONS_SELECT_ALL = `${FILTERS_NAMESPACE}buttons.selectAll`;
+export const FILTERS_BUTTONS_CLEAR = `${FILTERS_NAMESPACE}buttons.clear`;
+export const FILTERS_BUTTONS_OK = `${FILTERS_NAMESPACE}buttons.ok`;
+export const FILTERS_BUTTONS_CANCEL = `${FILTERS_NAMESPACE}buttons.cancel`;
 
-export const FILTERS_BUTTONS_PLACEHOLDER_SEARCH = 'Filters:buttons.placeholder.search';
-export const FILTERS_BUTTONS_PLACEHOLDER_VALUE = 'Filters:buttons.placeholder.value';
-export const FILTERS_BUTTONS_PLACEHOLDER_SECOND_VALUE = 'Filters:buttons.placeholder.secondValue';
+export const FILTERS_BUTTONS_PLACEHOLDER_SEARCH = `${FILTERS_NAMESPACE}buttons.placeholder.search`;
+export const FILTERS_BUTTONS_PLACEHOLDER_VALUE = `${FILTERS_NAMESPACE}buttons.placeholder.value`;
+export const FILTERS_BUTTONS_PLACEHOLDER_SECOND_VALUE = `${FILTERS_NAMESPACE}buttons.placeholder.secondValue`;
