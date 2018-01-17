f2470c2a86fd378d1d1c2cf71d34c275f122bc17
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2018-01-17 11:34:44 +0100

    Extra unit tests

--- test/unit/i18n/index.spec.js
+++ test/unit/i18n/index.spec.js
@@ -1,7 +1,9 @@
 import {getLanguageDictionary, getLanguagesDictionaries, registerLanguageDictionary} from 'handsontable/i18n/dictionariesManager';
 import {register as registerPhraseFormatter} from 'handsontable/i18n/phraseFormatters';
 import plPL from 'handsontable/i18n/languages/pl-PL';
+import * as allLanguages from 'handsontable/i18n/languages';
 import * as constants from 'handsontable/i18n/constants';
+import {isObject, objectEach} from 'handsontable/helpers/object';
 import Handsontable from 'handsontable';
 
 describe('i18n', () => {
@@ -43,5 +45,44 @@ describe('i18n', () => {
     Handsontable.languages.getTranslatedPhrase(plPL.languageCode, INSERT_ROW_ABOVE_DICTIONARY_KEY);
 
     expect(formatterWasRun).toEqual(true);
+  });
+
+  it('should contain dictionaries with predefined keys as parts of dictionary (checking for typo)', () => {
+    // If you would like to extend any dictionary by a custom key, define also corresponding,
+    // exported constants for it inside `src/i18n/constants.js` file.
+
+    const predefinedDictionaryKeys = Object.values(constants);
+
+    objectEach(allLanguages, (dictionary) => {
+      objectEach(dictionary, (value, key) => {
+        if (key !== 'languageCode') {
+          expect(key).toBeDefined();
+          expect(predefinedDictionaryKeys.includes(key)).toEqual(true);
+        }
+      });
+    });
+  });
+
+  it('should contain dictionaries with proper language code and be named properly inside exports collection', () => {
+    // Connected with:
+    // b) name of language code inside separate language file from `src/i18n/languages` folder; it should be defined in format:
+    // two lowercase letters, hyphen, two uppercase letters, for example: `es-PY`.
+    // a) name of exports inside `src/i18n/languages/index.js` file; it should be defined in format:
+    // two lowercase letters, two uppercase letters, for example: `esPY`.
+    //
+    // For more information check language creation steps from the documentation:
+    // https://docs.handsontable.com/tutorial-internationalization.html#custom-languages
+
+    const properLanguageCode = /^([a-z]{2})-([A-Z]{2})$/;
+
+    objectEach(allLanguages, (dictionary, exportName) => {
+      if (isObject(dictionary)) {
+        const languageCode = dictionary.languageCode;
+        const correspondingExportName = languageCode && languageCode.replace('-', '');
+
+        expect(properLanguageCode.test(languageCode)).toEqual(true);
+        expect(exportName).toEqual(correspondingExportName);
+      }
+    });
   });
 });
