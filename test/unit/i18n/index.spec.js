7cdc58bc44012972cfe520c6c7170c83d8f6f2a2
Wojciech Szymanski <wojciech.piotr.szymanski@gmail.com>
2018-01-31 10:43:24 +0100

    Added custom matcher to helpers & removed creating UnitTest.html, because we use `fs` in tests

--- test/unit/i18n/index.spec.js
+++ test/unit/i18n/index.spec.js
@@ -17,7 +17,7 @@ describe('i18n', () => {
   const INSERT_ROW_ABOVE_DICTIONARY_KEY = 'ContextMenu:items.insertRowAbove';
   const INSERT_ROW_ABOVE_IN_POLISH_LANGUAGE = 'Wstaw wiersz powyżej';
 
-  it('should extend Handsontable by language keys containing functions and constants for translation', () => {
+  it('should extend Handsontable global by functions and constants for translation', () => {
     expect(Handsontable.languages.getLanguageDictionary).toBe(getLanguageDictionary);
     expect(Handsontable.languages.getLanguagesDictionaries).toBe(getLanguagesDictionaries);
     expect(Handsontable.languages.registerLanguageDictionary).toBe(registerLanguageDictionary);
@@ -48,85 +48,72 @@ describe('i18n', () => {
   });
 
   describe('Collection of language packs', () => {
-    // One of keys can be `__esModule` added by Babel. We separate out it.
-    const dictionariesExportNames = Object.keys(allLanguages).filter(
-      (exportedKey) => exportedKey !== '__esModule'
-    );
-    const dictionaries = dictionariesExportNames.map((exportName) => allLanguages[exportName]);
+    // One of keys can be `__esModule` added by Babel. We separate it.
+    const indexFileExportNames = Object.keys(allLanguages).filter((exportedKey) => exportedKey !== '__esModule');
+    const dictionaries = indexFileExportNames.map((exportName) => allLanguages[exportName]);
     const languageCodes = dictionaries.map((dictionary) => dictionary.languageCode);
 
     const keysGroupedByDictionary = dictionaries.map((dictionary) => Object.keys(dictionary));
-    const dictionariesKeys = [].concat([], ...keysGroupedByDictionary);
+    const dictionariesKeys = [].concat([], ...keysGroupedByDictionary).filter((key) => key !== 'languageCode');
 
     const valuesGroupedByDictionary = dictionaries.map((dictionary) => Object.values(dictionary));
     const dictionariesValuesWithArrays = [].concat([], ...valuesGroupedByDictionary);
     const dictionariesValuesWithoutArrays = [].concat([], ...dictionariesValuesWithArrays);
 
-    it('should export sorted alphabetically dictionaries from `src/i18n/languages` folder', () => {
-      // Every file inside `src/i18n/languages` folder should be exported inside `src/i18n/languages/index.js` file.
+    const filesInsideLanguageDictionary = fs.readdirSync('./src/i18n/languages');
+    const languagePacks = filesInsideLanguageDictionary.filter((name) => name !== 'index.js');
 
-      const filesInsideLanguageDictionary = fs.readdirSync('./src/i18n/languages');
-      const languagePacks = filesInsideLanguageDictionary.filter((name) => name !== 'index.js');
+    describe('with every dictionary from `src/i18n/languages` folder', () => {
+      // We take all dictionaries from export inside `src/i18n/languages/index.js` file. We do not use dynamic import to take them from particular language files.
+      // Workaround to stop next tests when below expectations fail. Without the conditions rest of tests is useless.
+      // Jasmine problem described: https://github.com/jasmine/jasmine/issues/414
+      expect(indexFileExportNames.length).toBeGreaterThan(0);
+      expect(languageCodes.length).toBeGreaterThan(0);
+      expect(indexFileExportNames.length).toEqual(languagePacks.length);
 
-      expect(languagePacks.map((languagePack) => languagePack.replace('.js', '')).sort()).toEqual(languageCodes);
-    });
+      it('should not contain two or more dictionaries with the same language code', () => {
+        expect(languageCodes).toEqual(Array.from(new Set(languageCodes).values()));
+      });
 
-    it('should not contain not matching elements', () => {
-      // Checking if only dictionaries was exported.
-      expect(dictionaries.every(
-        (dictionaryCandidate) => typeof dictionaryCandidate === 'object' &&
-          dictionaryCandidate.languageCode !== undefined
-      )).toBeTruthy();
+      describe('File `src/i18n/languages/index.js`', () => {
+        xit('should provide export names sorted alphabetically', () => {
+          // Does JavaScript Guarantee Object Property Order?: https://stackoverflow.com/a/32149345
+          expect(indexFileExportNames.length).toBeGreaterThan(0);
+          expect(indexFileExportNames).toEqual(indexFileExportNames.slice().sort());
+        });
 
-      // Checking if collection contains more than one dictionary (not only default language pack).
-      expect(dictionaries.length).toBeGreaterThan(1);
+        it('should provide export names corresponding to dictionaries file names', () => {
+          // two lowercase letters, two uppercase letters, for example: `esPY`.
+          expect(indexFileExportNames).toBeListFulfillingCondition((exportName) =>
+            languagePacks.map((languagePack) => languagePack.replace('.js', '').replace('-', '')).includes(exportName));
+        });
+      });
 
-      // Checking if keys inside dictionaries are strings.
-      expect(keysGroupedByDictionary.every((groupOfDictionaryKeys) => Array.isArray(groupOfDictionaryKeys))).toBeTruthy();
-      expect(dictionariesKeys.every((key) => typeof key === 'string')).toBeTruthy();
+      describe('Particular dictionary', () => {
+        it('should contain language code of proper format', () => {
+          // two lowercase letters, hyphen, two uppercase letters, for example: `es-PY`.
+          expect(languageCodes).toBeListFulfillingCondition((languageCode) => /^([a-z]{2})-([A-Z]{2})$/.test(languageCode));
+        });
 
-      // Checking if values inside dictionaries are strings or arrays.
-      expect(valuesGroupedByDictionary.every((groupOfDictionaryValues) =>
-        Array.isArray(groupOfDictionaryValues))).toBeTruthy();
-      expect(dictionariesValuesWithArrays.every((value) => typeof value === 'string' || Array.isArray(value))).toBeTruthy();
-      expect(dictionariesValuesWithoutArrays.every((value) => typeof value === 'string')).toBeTruthy();
-    });
+        it('should have file name corresponding to its language code', () => {
+          // two lowercase letters, hyphen, two uppercase letters and file extension, for example: `es-PY.js`.
+          expect(languagePacks).toBeListFulfillingCondition((languagePack) => languageCodes.includes(languagePack.replace('.js', '')));
+        });
 
-    it('should not contain two or more dictionaries with the same language code', () => {
-      expect(Array.from(new Set(languageCodes).values())).toEqual(languageCodes);
-    });
+        it('should contain only predefined keys (checking for typo)', () => {
+          // If you would like to extend any dictionary by a custom key, define also corresponding, exported constant for it inside the `src/i18n/constants.js` file.
+          const predefinedDictionaryKeys = Object.values(constants);
 
-    it('should contain dictionaries with proper language code and', () => {
-      // Every languageCode should have proper format.
-      // Name of language code inside separate language file from `src/i18n/languages` folder should match to a pattern:
-      // two lowercase letters, hyphen, two uppercase letters, for example: `es-PY`.
-      expect(languageCodes.find((languageCode) =>
-        /^([a-z]{2})-([A-Z]{2})$/.test(languageCode) === false
-      )).not.toBeDefined();
-    });
+          expect(dictionariesKeys).toBeListFulfillingCondition((dictionaryKey) => predefinedDictionaryKeys.includes(dictionaryKey));
+        });
 
-    it('should named properly export of dictionary inside exports object', () => {
-      // Every dictionary should have export with name corresponding to it's language code.
-      // Name of exports inside `src/i18n/languages/index.js` file should match to a pattern:
-      // two lowercase letters, two uppercase letters, for example: `esPY`.
-      expect(languageCodes.find((languageCode) =>
-        allLanguages[languageCode.replace('-', '')] === undefined
-      )).not.toBeDefined();
-    });
-
-    it('should contain dictionaries with predefined keys as parts of dictionary (checking for typo)', () => {
-      // If you would like to extend any dictionary by a custom key, define also corresponding,
-      // exported constant for it inside `src/i18n/constants.js` file.
-      const predefinedDictionaryKeys = Object.values(constants);
-
-      expect(dictionariesKeys.find((dictionaryKey) =>
-        dictionaryKey !== 'languageCode' && predefinedDictionaryKeys.includes(dictionaryKey) === false
-      )).not.toBeDefined();
-    });
-
-    it('should contain dictionaries values without unnecessary whitespace characters', () => {
-      expect(dictionariesValuesWithoutArrays.find((value) => value !== value.trim())).not.toBeDefined();
-      expect(dictionariesValuesWithoutArrays.find((value) => / {2,}/.test(value))).not.toBeDefined();
+        it('should contain values defined as strings or arrays of strings, without unnecessary whitespace characters', () => {
+          expect(dictionariesValuesWithArrays).toBeListFulfillingCondition((value) => typeof value === 'string' || Array.isArray(value));
+          expect(dictionariesValuesWithoutArrays).toBeListFulfillingCondition((value) => typeof value === 'string');
+          expect(dictionariesValuesWithoutArrays).toBeListFulfillingCondition((value) => value === value.trim());
+          expect(dictionariesValuesWithoutArrays).toBeListFulfillingCondition((value) => / {2,}/.test(value) === false);
+        });
+      });
     });
   });
 });
