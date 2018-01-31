87b553daf67b1edb5a5b0b570e9de4fab6157bb7
Julio Cesar Zuppa Araujo <juliozuppa@gmail.com>
2018-01-31 02:38:09 -0200

    created brazilian portuguese translation

--- src/i18n/languages/pt-BR.js
+++ src/i18n/languages/pt-BR.js
@@ -1,123 +1,101 @@
-(function webpackUniversalModuleDefinition(root, factory) {
-	if(typeof exports === 'object' && typeof module === 'object')
-		module.exports = factory(require("../../handsontable"));
-	else if(typeof define === 'function' && define.amd)
-		define(["../../handsontable"], factory);
-	else {
-		var a = typeof exports === 'object' ? factory(require("../../handsontable")) : factory(root["Handsontable"]);
-		for(var i in a) (typeof exports === 'object' ? exports : root)[i] = a[i];
-	}
-})(typeof self !== 'undefined' ? self : this, function(__WEBPACK_EXTERNAL_MODULE_0__) {
-return /******/ (function(modules) { // webpackBootstrap
-/******/ 	// The module cache
-/******/ 	var installedModules = {};
-/******/
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/
-/******/ 		// Check if module is in cache
-/******/ 		if(installedModules[moduleId]) {
-/******/ 			return installedModules[moduleId].exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = installedModules[moduleId] = {
-/******/ 			i: moduleId,
-/******/ 			l: false,
-/******/ 			exports: {}
-/******/ 		};
-/******/
-/******/ 		// Execute the module function
-/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-/******/
-/******/ 		// Flag the module as loaded
-/******/ 		module.l = true;
-/******/
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/
-/******/
-/******/ 	// expose the modules object (__webpack_modules__)
-/******/ 	__webpack_require__.m = modules;
-/******/
-/******/ 	// expose the module cache
-/******/ 	__webpack_require__.c = installedModules;
-/******/
-/******/ 	// define getter function for harmony exports
-/******/ 	__webpack_require__.d = function(exports, name, getter) {
-/******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, {
-/******/ 				configurable: false,
-/******/ 				enumerable: true,
-/******/ 				get: getter
-/******/ 			});
-/******/ 		}
-/******/ 	};
-/******/
-/******/ 	// getDefaultExport function for compatibility with non-harmony modules
-/******/ 	__webpack_require__.n = function(module) {
-/******/ 		var getter = module && module.__esModule ?
-/******/ 			function getDefault() { return module['default']; } :
-/******/ 			function getModuleExports() { return module; };
-/******/ 		__webpack_require__.d(getter, 'a', getter);
-/******/ 		return getter;
-/******/ 	};
-/******/
-/******/ 	// Object.prototype.hasOwnProperty.call
-/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-/******/
-/******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
-/******/
-/******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
-/******/ })
-/************************************************************************/
-/******/ ([
-/* 0 */
-/***/ (function(module, exports) {
+/**
+ * @preserve
+ * Authors: Júlio C. Zuppa
+ * Last updated: Jan 12, 2018
+ *
+ * Description: Definition file for brazilian portuguese - Brazil language-country.
+ */
+import * as C from '../constants';
 
-module.exports = __WEBPACK_EXTERNAL_MODULE_0__;
+const dictionary = {
+  languageCode: 'pt-BR',
+  [C.CONTEXTMENU_ITEMS_ROW_ABOVE]: 'Inserir linha acima',
+  [C.CONTEXTMENU_ITEMS_ROW_BELOW]: 'Inserir linha abaixo',
+  [C.CONTEXTMENU_ITEMS_INSERT_LEFT]: 'Inserir coluna esquerda',
+  [C.CONTEXTMENU_ITEMS_INSERT_RIGHT]: 'Inserir coluna direita',
+  [C.CONTEXTMENU_ITEMS_REMOVE_ROW]: ['Excluir linha', 'Excluir linhas'],
+  [C.CONTEXTMENU_ITEMS_REMOVE_COLUMN]: ['Excluir coluna', 'Excluir colunas'],
+  [C.CONTEXTMENU_ITEMS_UNDO]: 'Desfazer',
+  [C.CONTEXTMENU_ITEMS_REDO]: 'Refazer',
+  [C.CONTEXTMENU_ITEMS_READ_ONLY]: 'Somente leitura',
+  [C.CONTEXTMENU_ITEMS_CLEAR_COLUMN]: 'Limpar coluna',
 
-/***/ }),
-/* 1 */,
-/* 2 */,
-/* 3 */,
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT]: 'Alinhamento',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_LEFT]: 'Esquerda',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_CENTER]: 'Centralizado',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_RIGHT]: 'Direita',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_JUSTIFY]: 'Justificado',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_TOP]: 'Superior',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_MIDDLE]: 'Meio',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_BOTTOM]: 'Inferior',
 
-"use strict";
+  [C.CONTEXTMENU_ITEMS_FREEZE_COLUMN]: 'Congelar coluna',
+  [C.CONTEXTMENU_ITEMS_UNFREEZE_COLUMN]: 'Descongelar coluna',
 
+  [C.CONTEXTMENU_ITEMS_BORDERS]: 'Bordas',
+  [C.CONTEXTMENU_ITEMS_BORDERS_TOP]: 'Superior',
+  [C.CONTEXTMENU_ITEMS_BORDERS_RIGHT]: 'Direita',
+  [C.CONTEXTMENU_ITEMS_BORDERS_BOTTOM]: 'Inferior',
+  [C.CONTEXTMENU_ITEMS_BORDERS_LEFT]: 'Esquerda',
+  [C.CONTEXTMENU_ITEMS_REMOVE_BORDERS]: 'Excluir bordas(s)',
 
-exports.__esModule = true;
+  [C.CONTEXTMENU_ITEMS_ADD_COMMENT]: 'Incluir comentário',
+  [C.CONTEXTMENU_ITEMS_EDIT_COMMENT]: 'Editar comentário',
+  [C.CONTEXTMENU_ITEMS_REMOVE_COMMENT]: 'Remover comentário',
+  [C.CONTEXTMENU_ITEMS_READ_ONLY_COMMENT]: 'Comentário somente leitura',
 
-var _dictionary;
+  [C.CONTEXTMENU_ITEMS_MERGE_CELLS]: 'Mesclar células',
+  [C.CONTEXTMENU_ITEMS_UNMERGE_CELLS]: 'Desfazer mesclagem de células',
 
-var _handsontable = __webpack_require__(0);
+  [C.CONTEXTMENU_ITEMS_COPY]: 'Copiar',
+  [C.CONTEXTMENU_ITEMS_CUT]: 'Recortar',
 
-var _handsontable2 = _interopRequireDefault(_handsontable);
+  [C.CONTEXTMENU_ITEMS_NESTED_ROWS_INSERT_CHILD]: 'Inserir linha filha',
+  [C.CONTEXTMENU_ITEMS_NESTED_ROWS_DETACH_CHILD]: 'Desanexar da linha pai',
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+  [C.CONTEXTMENU_ITEMS_HIDE_COLUMN]: ['Ocultar coluna', 'Ocultar colunas'],
+  [C.CONTEXTMENU_ITEMS_SHOW_COLUMN]: ['Exibir coluna', 'Exibir colunas'],
 
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; } /**
-                                                                                                                                                                                                                   * @preserve
-                                                                                                                                                                                                                   * Authors: Júlio C. Zuppa
-                                                                                                                                                                                                                   * Last updated: Jan 12, 2018
-                                                                                                                                                                                                                   *
-                                                                                                                                                                                                                   * Description: Definition file for brazilian portuguese - Brazil language-country.
-                                                                                                                                                                                                                   */
+  [C.CONTEXTMENU_ITEMS_HIDE_ROW]: ['Ocultar linha', 'Ocultar linhas'],
+  [C.CONTEXTMENU_ITEMS_SHOW_ROW]: ['Exibir linha', 'Exibir linhas'],
 
+  [C.FILTERS_CONDITIONS_NONE]: 'Nenhum',
+  [C.FILTERS_CONDITIONS_EMPTY]: 'É vazio',
+  [C.FILTERS_CONDITIONS_NOT_EMPTY]: 'Não é vazio',
+  [C.FILTERS_CONDITIONS_EQUAL]: 'É igual a',
+  [C.FILTERS_CONDITIONS_NOT_EQUAL]: 'É diferente de',
+  [C.FILTERS_CONDITIONS_BEGINS_WITH]: 'Começa com',
+  [C.FILTERS_CONDITIONS_ENDS_WITH]: 'Termiona com',
+  [C.FILTERS_CONDITIONS_CONTAINS]: 'Contém',
+  [C.FILTERS_CONDITIONS_NOT_CONTAIN]: 'Não contém',
+  [C.FILTERS_CONDITIONS_GREATER_THAN]: 'Maior que',
+  [C.FILTERS_CONDITIONS_GREATER_THAN_OR_EQUAL]: 'Maior ou igual a',
+  [C.FILTERS_CONDITIONS_LESS_THAN]: 'Menor que',
+  [C.FILTERS_CONDITIONS_LESS_THAN_OR_EQUAL]: 'Maior ou igual a',
+  [C.FILTERS_CONDITIONS_BETWEEN]: 'Está entre',
+  [C.FILTERS_CONDITIONS_NOT_BETWEEN]: 'Não está entre',
+  [C.FILTERS_CONDITIONS_AFTER]: 'Depois',
+  [C.FILTERS_CONDITIONS_BEFORE]: 'Antes',
+  [C.FILTERS_CONDITIONS_TODAY]: 'Hoje',
+  [C.FILTERS_CONDITIONS_TOMORROW]: 'Amanhã',
+  [C.FILTERS_CONDITIONS_YESTERDAY]: 'Ontem',
 
-var C = _handsontable2.default.languages.dictionaryKeys;
+  [C.FILTERS_VALUES_BLANK_CELLS]: 'Células vazias',
 
-var dictionary = (_dictionary = {
-  languageCode: 'pt-BR'
-}, _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ROW_ABOVE, 'Inserir linha acima'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ROW_BELOW, 'Inserir linha abaixo'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_INSERT_LEFT, 'Inserir coluna esquerda'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_INSERT_RIGHT, 'Inserir coluna direita'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_REMOVE_ROW, ['Excluir linha', 'Excluir linhas']), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_REMOVE_COLUMN, ['Excluir coluna', 'Excluir colunas']), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_UNDO, 'Desfazer'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_REDO, 'Refazer'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_READ_ONLY, 'Somente leitura'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_CLEAR_COLUMN, 'Limpar coluna'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT, 'Alinhamento'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_LEFT, 'Esquerda'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_CENTER, 'Centralizado'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_RIGHT, 'Direita'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_JUSTIFY, 'Justificado'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_TOP, 'Superior'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_MIDDLE, 'Meio'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ALIGNMENT_BOTTOM, 'Inferior'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_FREEZE_COLUMN, 'Congelar coluna'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_UNFREEZE_COLUMN, 'Descongelar coluna'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_BORDERS, 'Bordas'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_BORDERS_TOP, 'Superior'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_BORDERS_RIGHT, 'Direita'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_BORDERS_BOTTOM, 'Inferior'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_BORDERS_LEFT, 'Esquerda'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_REMOVE_BORDERS, 'Excluir bordas(s)'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_ADD_COMMENT, 'Incluir comentário'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_EDIT_COMMENT, 'Editar comentário'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_REMOVE_COMMENT, 'Remover comentário'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_READ_ONLY_COMMENT, 'Comentário somente leitura'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_MERGE_CELLS, 'Mesclar células'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_UNMERGE_CELLS, 'Desfazer mesclagem de células'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_COPY, 'Copiar'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_CUT, 'Recortar'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_NESTED_ROWS_INSERT_CHILD, 'Inserir linha filha'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_NESTED_ROWS_DETACH_CHILD, 'Desanexar da linha pai'), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_HIDE_COLUMN, ['Ocultar coluna', 'Ocultar colunas']), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_SHOW_COLUMN, ['Exibir coluna', 'Exibir colunas']), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_HIDE_ROW, ['Ocultar linha', 'Ocultar linhas']), _defineProperty(_dictionary, C.CONTEXTMENU_ITEMS_SHOW_ROW, ['Exibir linha', 'Exibir linhas']), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_NONE, 'Nenhum'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_EMPTY, 'É vazio'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_NOT_EMPTY, 'Não é vazio'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_EQUAL, 'É igual a'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_NOT_EQUAL, 'É diferente de'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_BEGINS_WITH, 'Começa com'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_ENDS_WITH, 'Termiona com'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_CONTAINS, 'Contém'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_NOT_CONTAIN, 'Não contém'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_GREATER_THAN, 'Maior que'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_GREATER_THAN_OR_EQUAL, 'Maior ou igual a'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_LESS_THAN, 'Menor que'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_LESS_THAN_OR_EQUAL, 'Maior ou igual a'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_BETWEEN, 'Está entre'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_NOT_BETWEEN, 'Não está entre'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_AFTER, 'Depois'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_BEFORE, 'Antes'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_TODAY, 'Hoje'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_TOMORROW, 'Amanhã'), _defineProperty(_dictionary, C.FILTERS_CONDITIONS_YESTERDAY, 'Ontem'), _defineProperty(_dictionary, C.FILTERS_VALUES_BLANK_CELLS, 'Células vazias'), _defineProperty(_dictionary, C.FILTERS_DIVS_FILTER_BY_CONDITION, 'Filtrar por condição'), _defineProperty(_dictionary, C.FILTERS_DIVS_FILTER_BY_VALUE, 'Filtrar por valor'), _defineProperty(_dictionary, C.FILTERS_LABELS_CONJUNCTION, 'E'), _defineProperty(_dictionary, C.FILTERS_LABELS_DISJUNCTION, 'Ou'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_SELECT_ALL, 'Selecionar tudo'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_CLEAR, 'Limpar'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_OK, 'OK'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_CANCEL, 'Cancelar'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_PLACEHOLDER_SEARCH, 'Localizar'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_PLACEHOLDER_VALUE, 'Valor'), _defineProperty(_dictionary, C.FILTERS_BUTTONS_PLACEHOLDER_SECOND_VALUE, 'Segundo valor'), _dictionary);
+  [C.FILTERS_DIVS_FILTER_BY_CONDITION]: 'Filtrar por condição',
+  [C.FILTERS_DIVS_FILTER_BY_VALUE]: 'Filtrar por valor',
 
-_handsontable2.default.languages.registerLanguageDictionary(dictionary);
+  [C.FILTERS_LABELS_CONJUNCTION]: 'E',
+  [C.FILTERS_LABELS_DISJUNCTION]: 'Ou',
 
-exports.default = dictionary;
+  [C.FILTERS_BUTTONS_SELECT_ALL]: 'Selecionar tudo',
+  [C.FILTERS_BUTTONS_CLEAR]: 'Limpar',
+  [C.FILTERS_BUTTONS_OK]: 'OK',
+  [C.FILTERS_BUTTONS_CANCEL]: 'Cancelar',
 
-/***/ })
-/******/ ])["___"];
-});
+  [C.FILTERS_BUTTONS_PLACEHOLDER_SEARCH]: 'Localizar',
+  [C.FILTERS_BUTTONS_PLACEHOLDER_VALUE]: 'Valor',
+  [C.FILTERS_BUTTONS_PLACEHOLDER_SECOND_VALUE]: 'Segundo valor'
+};
+
+export default dictionary;
