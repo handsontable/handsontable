fb5b6bd165e278b1a8d592f47cb4064f1f471d63
buggythepirate <bugmenotspamalot@yahoo.com>
2018-02-05 14:32:24 +0100

    added french to the index
    added french to the index

--- src/i18n/languages/index.js
+++ src/i18n/languages/index.js
@@ -1,9 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 
+import deCH from './de-CH';
+import deDE from './de-DE';
 import enUS from './en-US';
 import plPL from './pl-PL';
+import frFR from './fr-FR';
 
 export {
+  deCH,
+  deDE,
   enUS,
-  plPL
+  plPL,
+  frFR
 };
