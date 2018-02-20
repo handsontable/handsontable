ae7e5079d504bf35c78b65618e05b091091cbac4
HandDot <kyouhei.fukuda0729@gmail.com>
2018-02-20 19:07:06 +0900

    Translations for Japanese Japan (#4756)

--- src/i18n/languages/index.js
+++ src/i18n/languages/index.js
@@ -3,6 +3,7 @@
 import deCH from './de-CH';
 import deDE from './de-DE';
 import enUS from './en-US';
+import jaJP from './ja-JP';
 import plPL from './pl-PL';
 import ptBR from './pt-BR';
 
@@ -10,6 +11,7 @@ export {
   deCH,
   deDE,
   enUS,
+  jaJP,
   plPL,
   ptBR
 };
