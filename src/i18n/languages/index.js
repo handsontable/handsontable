c4bb1b0b1934b714bf2bad61a8c0f7126405e070
buggythepirate <bugmenotspamalot@yahoo.com>
2018-02-22 10:59:50 +0100

    update due to changes to dev branch

--- src/i18n/languages/index.js
+++ src/i18n/languages/index.js
@@ -4,12 +4,17 @@ import deCH from './de-CH';
 import deDE from './de-DE';
 import enUS from './en-US';
 import frFR from './fr-FR';
+import jaJP from './ja-JP';
 import plPL from './pl-PL';
+import ptBR from './pt-BR';
+
 
 export {
   deCH,
   deDE,
   enUS,
   frFR,
-  plPL
+  jaJP,
+  plPL,
+  ptBR
 };
