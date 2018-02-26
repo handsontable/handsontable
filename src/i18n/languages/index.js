8035a4d684ff349a3e9d1ab2e5cbc1365c8ba49f
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2018-02-26 13:41:05 +0100

    French - France language reverted - long context menu text

--- src/i18n/languages/index.js
+++ src/i18n/languages/index.js
@@ -3,7 +3,6 @@
 import deCH from './de-CH';
 import deDE from './de-DE';
 import enUS from './en-US';
-import frFR from './fr-FR';
 import jaJP from './ja-JP';
 import plPL from './pl-PL';
 import ptBR from './pt-BR';
@@ -12,7 +11,6 @@ export {
   deCH,
   deDE,
   enUS,
-  frFR,
   jaJP,
   plPL,
   ptBR
