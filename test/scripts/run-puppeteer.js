3278238d5b1a92a2bc8c7321cbdf66812830710b
budnix <571316+budnix@users.noreply.github.com>
2018-03-28 11:48:14 +0200

    Change tests to truly headless and disable sandboxing
    Issue: #4925

--- test/scripts/run-puppeteer.js
+++ test/scripts/run-puppeteer.js
@@ -21,7 +21,10 @@ const cleanupFactory = (browser, server) => async (exitCode) => {
 };
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  });
   const page = await browser.newPage();
 
   page.setCacheEnabled(false);
