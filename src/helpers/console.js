71dfd579cca7d65d9854dae10c2a0a903d84a992
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2018-04-03 08:49:26 +0200

    Extra argument & documentation #4924

--- src/helpers/console.js
+++ src/helpers/console.js
@@ -10,26 +10,77 @@
 import {isDefined} from './mixed';
 import {toSingleLine} from './templateLiteralTag';
 
-export function logToConsole(message) {
-  if (isDefined(console)) {
+/**
+ * Logs message to the console if the `console` object is exposed.
+ *
+ * @param {String} message Message which will be logged.
+ * @param {Boolean} [onlyOneLine=true] If `true` transform message to a single line.
+ */
+export function logToConsole(message, onlyOneLine = true) {
+  if (isDefined(console) === false) {
+    return;
+  }
+
+  if (onlyOneLine) {
     console.log(toSingleLine`${message}`);
+
+  } else {
+    console.log(message);
   }
 };
+/**
+ * Logs warn to the console if the `console` object is exposed.
+ *
+ * @param {String} message Message which will be logged.
+ * @param {Boolean} [onlyOneLine=true] If `true` transform message to a single line.
+ */
+export function warnToConsole(message, onlyOneLine = true) {
+  if (isDefined(console) === false) {
+    return;
+  }
 
-export function warnToConsole() {
-  if (isDefined(console)) {
+  if (onlyOneLine) {
     console.warn(toSingleLine`${message}`);
+
+  } else {
+    console.warn(message);
   }
 };
 
-export function infoToConsole() {
-  if (isDefined(console)) {
+/**
+ * Logs info to the console if the `console` object is exposed.
+ *
+ * @param {String} message Message which will be logged.
+ * @param {Boolean} [onlyOneLine=true] If `true` transform message to a single line.
+ */
+export function infoToConsole(message, onlyOneLine = true) {
+  if (isDefined(console) === false) {
+    return;
+  }
+
+  if (onlyOneLine) {
     console.info(toSingleLine`${message}`);
+
+  } else {
+    console.info(message);
   }
 };
 
-export function errorToConsole() {
-  if (isDefined(console)) {
+/**
+ * Logs error to the console if the `console` object is exposed.
+ *
+ * @param {String} message Message which will be logged.
+ * @param {Boolean} [onlyOneLine=true] If `true` transform message to a single line.
+ */
+export function errorToConsole(message, onlyOneLine = true) {
+  if (isDefined(console) === false) {
+    return;
+  }
+
+  if (onlyOneLine) {
     console.error(toSingleLine`${message}`);
+
+  } else {
+    console.error(message);
   }
 };
