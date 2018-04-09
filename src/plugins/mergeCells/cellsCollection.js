e61d790da4e676f8971362145d3c92c69cafeb03
Jan Siegel <js.ziggle@gmail.com>
2018-04-09 11:24:42 +0200

    Fix a problem, where removing multiple merged cells at once caused a problem. #3473

--- src/plugins/mergeCells/cellsCollection.js
+++ src/plugins/mergeCells/cellsCollection.js
@@ -1,6 +1,6 @@
 import MergedCellCoords from './cellCoords';
 import {CellCoords, CellRange} from '../../3rdparty/walkontable/src/index';
-import {rangeEach} from '../../helpers/number';
+import {rangeEach, rangeEachReverse} from '../../helpers/number';
 import {warn} from '../../helpers/console';
 import {arrayEach} from '../../helpers/array';
 import {applySpanProperties} from './utils';
@@ -297,8 +297,10 @@ class MergedCellsCollection {
       currentMerge.shift(shiftVector, index);
     });
 
-    arrayEach(this.mergedCells, (currentMerge) => {
-      if (currentMerge.removed) {
+    rangeEachReverse(this.mergedCells.length - 1, 0, (i) => {
+      let currentMerge = this.mergedCells[i];
+
+      if (currentMerge && currentMerge.removed) {
         this.mergedCells.splice(this.mergedCells.indexOf(currentMerge), 1);
       }
     });
