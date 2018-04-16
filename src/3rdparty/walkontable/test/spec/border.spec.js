49d8a4da8c3894522fc64bb56961354a195fcb70
Piotr Laszczkowski <swistach@users.noreply.github.com>
2018-04-16 13:39:08 +0200

    Selection corner should be positioned correctly even if a window is a trimming container. #4961 (#5026)
    * Fix: Selection corner should be positioned correctly even if a window is a trimming container. #4961
    
    * Added: test for walkontable.border.corner positioning. #4961
    
    * Removed unnecessary empty line. #4961
    
    * Added: test for case when corner should be added to the last cell in the last row. #4961

--- src/3rdparty/walkontable/test/spec/border.spec.js
+++ src/3rdparty/walkontable/test/spec/border.spec.js
@@ -259,7 +259,7 @@ describe('WalkontableBorder', () => {
   it('should move the fill handle / corner border to the left, if in the position it would overlap the container (e.g.: far-right)', () => {
     $container.css({
       overflow: 'hidden',
-      width: '200px'
+      width: '200px',
     });
     const wt = new Walkontable.Core({
       table: $table[0],
@@ -317,4 +317,104 @@ describe('WalkontableBorder', () => {
     expect($container[0].clientWidth === $container[0].scrollWidth).toBe(true);
   });
 
+  it('should move the fill handle / corner border to the top, if in the position it would overlap the container (e.g.: far-bottom)', () => {
+    $container.css({
+      overflow: 'hidden',
+      height: 'auto',
+      marginTop: '2000px',
+    });
+
+    const wt = new Walkontable.Core({
+      table: $table[0],
+      data: getData,
+      totalRows: 5,
+      totalColumns: 1,
+      selections: [
+        new Walkontable.Selection({
+          border: {
+            width: 2,
+            color: 'green',
+            cornerVisible() {
+              return true;
+            }
+          }
+        }),
+        new Walkontable.Selection({})
+      ],
+      onCellMouseDown(event, coords, TD) {
+        wt.selections.getCell().clear();
+        wt.selections.getCell().add(coords);
+        wt.draw();
+      }
+    });
+
+    shimSelectionProperties(wt);
+    wt.draw();
+
+    const $td = $table.find('tbody tr:last-of-type td:last-of-type');
+    const $corner = $(wt.selections.getCell().getBorder(wt).corner);
+
+    $td.simulate('mousedown');
+
+    wt.draw();
+
+    expect($corner.css('width')).toBe('6px');
+    expect($corner.css('height')).toBe('6px');
+    expect($table.css('height')).toBe('116px');
+    expect($corner.position().top).toBe(109); // table.height - corner.height - corner.borderTop
+    expect($corner.position().left).toBe(45);
+    expect($container[0].clientHeight === $container[0].scrollHeight).toBe(true);
+  });
+
+  it('should move the corner border to the top-left, if is not enough area on the bottom-right corner of container', () => {
+    $container.css({
+      overflow: 'hidden',
+      height: 'auto',
+      width: '50px',
+      marginTop: '2000px',
+      marginLeft: '2000px',
+    });
+
+    const wt = new Walkontable.Core({
+      table: $table[0],
+      data: getData,
+      totalRows: 1,
+      totalColumns: 1,
+      selections: [
+        new Walkontable.Selection({
+          border: {
+            width: 2,
+            color: 'green',
+            cornerVisible() {
+              return true;
+            }
+          }
+        }),
+        new Walkontable.Selection({})
+      ],
+      onCellMouseDown(event, coords, TD) {
+        wt.selections.getCell().clear();
+        wt.selections.getCell().add(coords);
+        wt.draw();
+      }
+    });
+
+    shimSelectionProperties(wt);
+    wt.draw();
+
+    const $td = $table.find('tbody tr:last-of-type td:last-of-type');
+    const $corner = $(wt.selections.getCell().getBorder(wt).corner);
+
+    $td.simulate('mousedown');
+
+    wt.draw();
+
+    expect($corner.css('width')).toBe('6px');
+    expect($corner.css('height')).toBe('6px');
+    expect($table.css('height')).toBe('24px');
+    expect($corner.position().top).toBe(17); // table.height - corner.height - corner.borderTop
+    expect($corner.position().left).toBe(43);
+    expect($container[0].clientHeight === $container[0].scrollHeight).toBe(true);
+    expect($container[0].clientWidth === $container[0].scrollWidth).toBe(true);
+  });
 });
