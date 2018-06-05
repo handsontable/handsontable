1caf913566c3a4d23b5646970eada8d1bbfea043
Piotr Nowak <piotr.nowak.25.02.1978@gmail.com>
2018-06-05 15:05:56 +0200

    Add test for autoWrapCol i autoWrapRow #5135

---
+++ test/e2e/settings/autoWrapCol.spec.js
@@ -0,0 +1,53 @@
+describe('settings', () => {
+  var id = 'testContainer';
+
+  beforeEach(function() {
+    this.$container = $(`<div id="${id}"></div>`).appendTo('body');
+  });
+
+  afterEach(function() {
+    if (this.$container) {
+      destroy();
+      this.$container.remove();
+    }
+  });
+
+  describe('autoWrapCol', () => {
+    it('should move to the neighboring column when it reaches the end of the current', () => {
+      handsontable({
+        data: Handsontable.helper.createSpreadsheetData(5, 5)
+      });
+
+      selectCell(4, 0);
+
+      expect(getSelected()).toEqual([[4, 0, 4, 0]]);
+
+      keyDownUp('arrow_down');
+
+      expect(getSelected()).toEqual([[0, 1, 0, 1]]);
+
+      keyDownUp('arrow_up');
+
+      expect(getSelected()).toEqual([[4, 0, 4, 0]]);
+    });
+
+    it('should move to the start of the table when it reaches the end of the table', () => {
+      handsontable({
+        data: Handsontable.helper.createSpreadsheetData(5, 5)
+      });
+
+      selectCell(4, 4);
+
+      expect(getSelected()).toEqual([[4, 4, 4, 4]]);
+
+      keyDownUp('arrow_down');
+
+      expect(getSelected()).toEqual([[0, 0, 0, 0]]);
+
+      keyDownUp('arrow_up');
+
+      expect(getSelected()).toEqual([[4, 4, 4, 4]]);
+
+    });
+  });
+});
