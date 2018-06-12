2c54936b671522dc651b7bd9fbc0258899d22ab3
Jan Siegel <js.ziggle@gmail.com>
2018-06-12 11:45:38 +0200

    4.0.0

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -21,7 +21,7 @@
  * UNINTERRUPTED OR ERROR FREE.
  * 
  * Version: 4.0.0
- * Release date: 13/06/2018 (built at 07/06/2018 11:54:31)
+ * Release date: 13/06/2018 (built at 12/06/2018 11:38:53)
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -33754,7 +33754,7 @@ Handsontable.DefaultSettings = _defaultSettings2.default;
 Handsontable.EventManager = _eventManager2.default;
 Handsontable._getListenersCounter = _eventManager.getListenersCounter; // For MemoryLeak tests
 
-Handsontable.buildDate = '07/06/2018 11:54:31';
+Handsontable.buildDate = '12/06/2018 11:38:53';
 Handsontable.packageName = 'handsontable-pro';
 Handsontable.version = '4.0.0';
 
