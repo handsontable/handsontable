ebb790481319ff14c213fad669072f376de30159
NullIsNot0 <NullIsNot0@users.noreply.github.com>
2018-06-14 22:10:58 +0300

    Add lv-LV Latvian-Latvia translation

---
+++ src/i18n/languages/lv-LV.js
@@ -0,0 +1,101 @@
+/**
+ * @preserve
+ * Authors: Edgars Voroboks
+ * Last updated: Jun 14, 2018
+ *
+ * Description: Definition file for Latvian - Latvia language-country.
+ */
+import * as C from '../constants';
+
+const dictionary = {
+  languageCode: 'lv-LV',
+  [C.CONTEXTMENU_ITEMS_ROW_ABOVE]: 'Ievietot rindu augšā',
+  [C.CONTEXTMENU_ITEMS_ROW_BELOW]: 'Ievietot rindu apakšā',
+  [C.CONTEXTMENU_ITEMS_INSERT_LEFT]: 'Ievietot kolonnu pa kreisi',
+  [C.CONTEXTMENU_ITEMS_INSERT_RIGHT]: 'Ievietot kolonnu pa labi',
+  [C.CONTEXTMENU_ITEMS_REMOVE_ROW]: ['Dzēst rindu', 'Dzēst rindas'],
+  [C.CONTEXTMENU_ITEMS_REMOVE_COLUMN]: ['Dzēst kolonnu', 'Dzēst kolonnas'],
+  [C.CONTEXTMENU_ITEMS_UNDO]: 'Atsaukt',
+  [C.CONTEXTMENU_ITEMS_REDO]: 'Pārtaisīt',
+  [C.CONTEXTMENU_ITEMS_READ_ONLY]: 'Lasīšanas režīms',
+  [C.CONTEXTMENU_ITEMS_CLEAR_COLUMN]: 'Notīrīt kolonnu',
+
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT]: 'Izvietojums',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_LEFT]: 'Pa kreisi',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_CENTER]: 'Centrēts',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_RIGHT]: 'Pa labi',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_JUSTIFY]: 'Izlīdzināts',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_TOP]: 'Augšā',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_MIDDLE]: 'Pa vidu',
+  [C.CONTEXTMENU_ITEMS_ALIGNMENT_BOTTOM]: 'Apakšā',
+
+  [C.CONTEXTMENU_ITEMS_FREEZE_COLUMN]: 'Iesaldēt kolonnu',
+  [C.CONTEXTMENU_ITEMS_UNFREEZE_COLUMN]: 'Atsaldēt kolonnu',
+
+  [C.CONTEXTMENU_ITEMS_BORDERS]: 'Robežas',
+  [C.CONTEXTMENU_ITEMS_BORDERS_TOP]: 'Augšā',
+  [C.CONTEXTMENU_ITEMS_BORDERS_RIGHT]: 'Pa labi',
+  [C.CONTEXTMENU_ITEMS_BORDERS_BOTTOM]: 'Apakšā',
+  [C.CONTEXTMENU_ITEMS_BORDERS_LEFT]: 'Pa kreisi',
+  [C.CONTEXTMENU_ITEMS_REMOVE_BORDERS]: 'Noņemt robežu(-as)',
+
+  [C.CONTEXTMENU_ITEMS_ADD_COMMENT]: 'Pievienot komentāru',
+  [C.CONTEXTMENU_ITEMS_EDIT_COMMENT]: 'Labot komentāru',
+  [C.CONTEXTMENU_ITEMS_REMOVE_COMMENT]: 'Dzēst komentāru',
+  [C.CONTEXTMENU_ITEMS_READ_ONLY_COMMENT]: 'Lasīšanas režīma komentārs',
+
+  [C.CONTEXTMENU_ITEMS_MERGE_CELLS]: 'Sapludināt šūnas',
+  [C.CONTEXTMENU_ITEMS_UNMERGE_CELLS]: 'Nesapludināt šunas',
+
+  [C.CONTEXTMENU_ITEMS_COPY]: 'Kopēt',
+  [C.CONTEXTMENU_ITEMS_CUT]: 'Izgriezt',
+
+  [C.CONTEXTMENU_ITEMS_NESTED_ROWS_INSERT_CHILD]: 'Ievietot pakārtoto rindu',
+  [C.CONTEXTMENU_ITEMS_NESTED_ROWS_DETACH_CHILD]: 'Atdalīt no vecāka',
+
+  [C.CONTEXTMENU_ITEMS_HIDE_COLUMN]: ['Palēpt kolonnu', 'Palēpt kolonnas'],
+  [C.CONTEXTMENU_ITEMS_SHOW_COLUMN]: ['Rādīt kolonnu', 'Rādīt kolonnas'],
+
+  [C.CONTEXTMENU_ITEMS_HIDE_ROW]: ['Palēpt rindu', 'Paslēpt rindas'],
+  [C.CONTEXTMENU_ITEMS_SHOW_ROW]: ['Rādīt rindu', 'Rādīt rindas'],
+
+  [C.FILTERS_CONDITIONS_NONE]: 'Nekas',
+  [C.FILTERS_CONDITIONS_EMPTY]: 'Ir tukšs',
+  [C.FILTERS_CONDITIONS_NOT_EMPTY]: 'Nav tukšs',
+  [C.FILTERS_CONDITIONS_EQUAL]: 'Vienāds ar',
+  [C.FILTERS_CONDITIONS_NOT_EQUAL]: 'Nav vienāds ar',
+  [C.FILTERS_CONDITIONS_BEGINS_WITH]: 'Sākas ar',
+  [C.FILTERS_CONDITIONS_ENDS_WITH]: 'Beidzas ar',
+  [C.FILTERS_CONDITIONS_CONTAINS]: 'Satur',
+  [C.FILTERS_CONDITIONS_NOT_CONTAIN]: 'Nesatur',
+  [C.FILTERS_CONDITIONS_GREATER_THAN]: 'Lielāks par',
+  [C.FILTERS_CONDITIONS_GREATER_THAN_OR_EQUAL]: 'Lielāks vai vienāds ar',
+  [C.FILTERS_CONDITIONS_LESS_THAN]: 'Mazāks par',
+  [C.FILTERS_CONDITIONS_LESS_THAN_OR_EQUAL]: 'Mazāks vai vienāds ar',
+  [C.FILTERS_CONDITIONS_BETWEEN]: 'Ir starp',
+  [C.FILTERS_CONDITIONS_NOT_BETWEEN]: 'Nav starp',
+  [C.FILTERS_CONDITIONS_AFTER]: 'Pēc',
+  [C.FILTERS_CONDITIONS_BEFORE]: 'Pirms',
+  [C.FILTERS_CONDITIONS_TODAY]: 'Šodien',
+  [C.FILTERS_CONDITIONS_TOMORROW]: 'Rītdien',
+  [C.FILTERS_CONDITIONS_YESTERDAY]: 'Vakar',
+
+  [C.FILTERS_VALUES_BLANK_CELLS]: 'Tukšas šūnas',
+
+  [C.FILTERS_DIVS_FILTER_BY_CONDITION]: 'Filtrēt pēc nosacījuma',
+  [C.FILTERS_DIVS_FILTER_BY_VALUE]: 'Filtrēt pēc vērtības',
+
+  [C.FILTERS_LABELS_CONJUNCTION]: 'Un',
+  [C.FILTERS_LABELS_DISJUNCTION]: 'Vai',
+
+  [C.FILTERS_BUTTONS_SELECT_ALL]: 'Izvēlēties visu',
+  [C.FILTERS_BUTTONS_CLEAR]: 'Notīrīt',
+  [C.FILTERS_BUTTONS_OK]: 'Labi',
+  [C.FILTERS_BUTTONS_CANCEL]: 'Atcelt',
+
+  [C.FILTERS_BUTTONS_PLACEHOLDER_SEARCH]: 'Meklēt',
+  [C.FILTERS_BUTTONS_PLACEHOLDER_VALUE]: 'Vērtība',
+  [C.FILTERS_BUTTONS_PLACEHOLDER_SECOND_VALUE]: 'Otra vērtība'
+};
+
+export default dictionary;
