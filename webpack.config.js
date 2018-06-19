c101bd3f3481fa61fb7b5c1907c59f98f5a3c95d
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2018-06-19 10:29:53 +0200

    Resignation from using webpack --env in favor to process.env object (#80)

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ var path = require('path');
 var env = process.env.NODE_ENV;
 var configFactory = require('./.config/' + env);
 
-module.exports = function(envArgs) {
-  return configFactory.create(envArgs);
+// In some cases, npm env variables become rewritten to lower case names. To prevent this it is rewritten to the
+// original variable name so the --testPathPattern work in any case.
+if (process.env.npm_config_testpathpattern) {
+  process.env.npm_config_testPathPattern = process.env.npm_config_testpathpattern;
+}
+
+module.exports = function() {
+  return configFactory.create({
+    testPathPattern: process.env.npm_config_testPathPattern,
+  });
 };
