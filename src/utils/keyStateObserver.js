cb930a4b63bec140cf9cafc940d28a20ab132c6f
piotr <piotr.nowak@handsontable.com>
2018-08-08 12:35:01 +0200

    Activate multiselect mode only when is clicked CMD/CTRL

--- src/utils/keyStateObserver.js
+++ src/utils/keyStateObserver.js
@@ -1,5 +1,5 @@
 import EventManager from '../eventManager';
-import { isCtrlMetaKey, isKey } from '../helpers/unicode';
+import {isCtrlMetaKey, isKey, isPrintableChar} from '../helpers/unicode';
 
 const eventManager = new EventManager();
 const pressedKeys = new Set();
@@ -68,7 +68,9 @@ function isPressed(keyCodes) {
  * @return {Boolean}
  */
 function isPressedCtrlKey() {
-  return Array.from(pressedKeys.values()).some((_keyCode) => isCtrlMetaKey(_keyCode));
+  const values = Array.from(pressedKeys.values());
+
+  return values.some((_keyCode) => isCtrlMetaKey(_keyCode)) && values.every((_keyCode) => !isPrintableChar(_keyCode));
 }
 
 /**
