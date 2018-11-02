aa7684cdf1001a5e8f87ffe9e630cc662f15d229
Jason McNair <jason.daniel.mcnair@gmail.com>
2018-11-02 13:05:23 -0400

    issue-3381: preventing validation occurring prior to 'beforeChange'

--- src/core.js
+++ src/core.js
@@ -892,6 +892,8 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
   }
 
   function validateChanges(changes, source, callback) {
+    verifyChanges();
+
     const waitingForValidator = new ValidatorsQueue();
     const isNumericData = value => value.length > 0 && /^-?[\d\s]*(\.|,)?\d*$/.test(value);
 
@@ -935,6 +937,10 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
     waitingForValidator.checkIfQueueIsEmpty();
 
     function resolve() {
+      callback(); // called when async validators are resolved and beforeChange was not async
+    }
+
+    function verifyChanges() {
       let beforeChangeResult;
 
       if (changes.length) {
@@ -945,7 +951,6 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
           changes.splice(0, changes.length); // invalidate all changes (remove everything from array)
         }
       }
-      callback(); // called when async validators are resolved and beforeChange was not async
     }
   }
 
