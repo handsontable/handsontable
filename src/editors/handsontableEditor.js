22ba940de697a0dde86df922c8b90b5b378aef9d
swistach <swistach@gmail.com>
2018-12-11 15:05:59 +0100

    Added: JSDocs + suggestion to move callback into the editor's class

--- src/editors/handsontableEditor.js
+++ src/editors/handsontableEditor.js
@@ -4,60 +4,6 @@ import { setCaretPosition } from './../helpers/dom/element';
 import { stopImmediatePropagation, isImmediatePropagationStopped } from './../helpers/dom/event';
 import TextEditor from './textEditor';
 
-const onBeforeKeyDown = function(event) {
-  if (isImmediatePropagationStopped(event)) {
-    return;
-  }
-  const editor = this.getActiveEditor();
-
-  const innerHOT = editor.htEditor.getInstance();
-
-  let rowToSelect;
-  let selectedRow;
-
-  if (event.keyCode === KEY_CODES.ARROW_DOWN) {
-    if (!innerHOT.getSelectedLast() && !innerHOT.flipped) {
-      rowToSelect = 0;
-    } else if (innerHOT.getSelectedLast()) {
-      if (innerHOT.flipped) {
-        rowToSelect = innerHOT.getSelectedLast()[0] + 1;
-      } else if (!innerHOT.flipped) {
-        const lastRow = innerHOT.countRows() - 1;
-        selectedRow = innerHOT.getSelectedLast()[0];
-        rowToSelect = Math.min(lastRow, selectedRow + 1);
-      }
-    }
-  } else if (event.keyCode === KEY_CODES.ARROW_UP) {
-    if (!innerHOT.getSelectedLast() && innerHOT.flipped) {
-      rowToSelect = innerHOT.countRows() - 1;
-
-    } else if (innerHOT.getSelectedLast()) {
-      if (innerHOT.flipped) {
-        selectedRow = innerHOT.getSelectedLast()[0];
-        rowToSelect = Math.max(0, selectedRow - 1);
-      } else {
-        selectedRow = innerHOT.getSelectedLast()[0];
-        rowToSelect = selectedRow - 1;
-      }
-    }
-  }
-
-  if (rowToSelect !== void 0) {
-    if (rowToSelect < 0 || (innerHOT.flipped && rowToSelect > innerHOT.countRows() - 1)) {
-      innerHOT.deselectCell();
-    } else {
-      innerHOT.selectCell(rowToSelect, 0);
-    }
-    if (innerHOT.getData().length) {
-      event.preventDefault();
-      stopImmediatePropagation(event);
-
-      editor.instance.listen();
-      editor.TEXTAREA.focus();
-    }
-  }
-};
-
 /**
  * @private
  * @editor HandsontableEditor
@@ -65,19 +11,57 @@ const onBeforeKeyDown = function(event) {
  * @dependencies TextEditor
  */
 class HandsontableEditor extends TextEditor {
-  createElements(...args) {
-    super.createElements(...args);
+  /**
+   * Opens the editor and adjust its size.
+   */
+  open() {
+    this.addHook('beforeKeyDown', event => this.onBeforeKeyDown(event));
 
-    const DIV = document.createElement('DIV');
-    DIV.className = 'handsontableEditor';
-    this.TEXTAREA_PARENT.appendChild(DIV);
+    super.open();
 
-    this.htContainer = DIV;
-    this.assignHooks();
+    if (this.htEditor) {
+      this.htEditor.destroy();
+    }
+
+    if (this.htContainer.style.display === 'none') {
+      this.htContainer.style.display = '';
+    }
+
+    // Construct and initialise a new Handsontable
+    this.htEditor = new this.hot.constructor(this.htContainer, this.htOptions);
+    this.htEditor.init();
+    this.htEditor.rootElement.style.display = '';
+
+    if (this.cellProperties.strict) {
+      this.htEditor.selectCell(0, 0);
+    } else {
+      this.htEditor.deselectCell();
+    }
+
+    setCaretPosition(this.TEXTAREA, 0, this.TEXTAREA.value.length);
   }
 
-  prepare(td, row, col, prop, value, cellProperties, ...args) {
-    super.prepare(td, row, col, prop, value, cellProperties, ...args);
+  /**
+   * Closes the editor.
+   */
+  close() {
+    this.htEditor.rootElement.style.display = 'none';
+    this.removeHooksByKey('beforeKeyDown');
+    super.close();
+  }
+
+  /**
+   * Prepares editor's meta data and configuration of the internal Handsontable's instance.
+   *
+   * @param {Number} row
+   * @param {Number} col
+   * @param {Number|String} prop
+   * @param {HTMLTableCellElement} td
+   * @param {*} originalValue
+   * @param {Object} cellProperties
+   */
+  prepare(td, row, col, prop, value, cellProperties) {
+    super.prepare(td, row, col, prop, value, cellProperties);
 
     const parent = this;
     const options = {
@@ -110,55 +94,53 @@ class HandsontableEditor extends TextEditor {
     this.htOptions = options;
   }
 
-  open(...args) {
-    this.instance.addHook('beforeKeyDown', onBeforeKeyDown);
-
-    super.open(...args);
-
-    if (this.htEditor) {
-      this.htEditor.destroy();
-    }
-
-    if (this.htContainer.style.display === 'none') {
-      this.htContainer.style.display = '';
-    }
-
-    // Construct and initialise a new Handsontable
-    this.htEditor = new this.instance.constructor(this.htContainer, this.htOptions);
-    this.htEditor.init();
-    this.htEditor.rootElement.style.display = '';
-
-    if (this.cellProperties.strict) {
-      this.htEditor.selectCell(0, 0);
-    } else {
-      this.htEditor.deselectCell();
-    }
-
-    setCaretPosition(this.TEXTAREA, 0, this.TEXTAREA.value.length);
-  }
-
-  close(...args) {
-    this.htEditor.rootElement.style.display = 'none';
-    this.instance.removeHook('beforeKeyDown', onBeforeKeyDown);
-    super.close(...args);
-  }
-
-  focus(...args) {
-    super.focus(...args);
-  }
-
-  beginEditing(...args) {
-    const onBeginEditing = this.instance.getSettings().onBeginEditing;
+  /**
+   * Begins editing on a highlighted cell and hides fillHandle corner if was present.
+   *
+   * @param {*} newInitialValue
+   * @param {*} event
+   */
+  beginEditing(newInitialValue, event) {
+    const onBeginEditing = this.hot.getSettings().onBeginEditing;
 
     if (onBeginEditing && onBeginEditing() === false) {
       return;
     }
-    super.beginEditing(...args);
+
+    super.beginEditing(newInitialValue, event);
   }
 
-  finishEditing(...args) {
+  /**
+   * Sets focus state on the select element.
+   */
+  focus() {
+    super.focus();
+  }
+
+  /**
+   * Creates an editor's elements and adds necessary CSS classnames.
+   */
+  createElements() {
+    super.createElements();
+
+    const DIV = document.createElement('DIV');
+    DIV.className = 'handsontableEditor';
+    this.TEXTAREA_PARENT.appendChild(DIV);
+
+    this.htContainer = DIV;
+    this.assignHooks();
+  }
+
+  /**
+   * Finishes editing and start saving or restoring process for editing cell or last selected range.
+   *
+   * @param {Boolean} restoreOriginalValue If true, then closes editor without saving value from the editor into a cell.
+   * @param {Boolean} ctrlDown If true, then saveValue will save editor's value to each cell in the last selected range.
+   * @param {Function} callback
+   */
+  finishEditing(restoreOriginalValue, ctrlDown, callback) {
     if (this.htEditor && this.htEditor.isListening()) { // if focus is still in the HOT editor
-      this.instance.listen(); // return the focus to the parent HOT instance
+      this.hot.listen(); // return the focus to the parent HOT instance
     }
 
     if (this.htEditor && this.htEditor.getSelectedLast()) {
@@ -169,15 +151,82 @@ class HandsontableEditor extends TextEditor {
       }
     }
 
-    return super.finishEditing(...args);
+    return super.finishEditing(restoreOriginalValue, ctrlDown, callback);
   }
 
+  /**
+   * Assings afterDestroy callback to prevent memory leaks.
+   *
+   * @private
+   */
   assignHooks() {
-    this.instance.addHook('afterDestroy', () => {
+    this.hot.addHook('afterDestroy', () => {
       if (this.htEditor) {
         this.htEditor.destroy();
       }
     });
+  }
+
+  /**
+   * onBeforeKeyDown callback.
+   *
+   * @private
+   * @param {Event} event
+   */
+  onBeforeKeyDown(event) {
+    if (isImmediatePropagationStopped(event)) {
+      return;
+    }
+
+    const innerHOT = this.htEditor.getInstance();
+
+    let rowToSelect;
+    let selectedRow;
+
+    if (event.keyCode === KEY_CODES.ARROW_DOWN) {
+      if (!innerHOT.getSelectedLast() && !innerHOT.flipped) {
+        rowToSelect = 0;
+
+      } else if (innerHOT.getSelectedLast()) {
+        if (innerHOT.flipped) {
+          rowToSelect = innerHOT.getSelectedLast()[0] + 1;
+
+        } else if (!innerHOT.flipped) {
+          const lastRow = innerHOT.countRows() - 1;
+          selectedRow = innerHOT.getSelectedLast()[0];
+          rowToSelect = Math.min(lastRow, selectedRow + 1);
+        }
+      }
+
+    } else if (event.keyCode === KEY_CODES.ARROW_UP) {
+      if (!innerHOT.getSelectedLast() && innerHOT.flipped) {
+        rowToSelect = innerHOT.countRows() - 1;
+
+      } else if (innerHOT.getSelectedLast()) {
+        if (innerHOT.flipped) {
+          selectedRow = innerHOT.getSelectedLast()[0];
+          rowToSelect = Math.max(0, selectedRow - 1);
+        } else {
+          selectedRow = innerHOT.getSelectedLast()[0];
+          rowToSelect = selectedRow - 1;
+        }
+      }
+    }
+
+    if (rowToSelect !== void 0) {
+      if (rowToSelect < 0 || (innerHOT.flipped && rowToSelect > innerHOT.countRows() - 1)) {
+        innerHOT.deselectCell();
+      } else {
+        innerHOT.selectCell(rowToSelect, 0);
+      }
+      if (innerHOT.getData().length) {
+        event.preventDefault();
+        stopImmediatePropagation(event);
+
+        this.hot.listen();
+        this.TEXTAREA.focus();
+      }
+    }
   }
 }
 
