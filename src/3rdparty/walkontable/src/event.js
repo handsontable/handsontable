e75d08f4f3473cfc77968484aeb876c9a5f09a7e
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2018-12-18 13:05:09 +0100

    Fix unresponsive selection (#5678)
    This fix resolves an issue about not abling select a cell or edit a cell
    on devices which support both input methods (a touchscreen with an
    ability to plug in a mouse).

--- src/3rdparty/walkontable/src/event.js
+++ src/3rdparty/walkontable/src/event.js
@@ -6,6 +6,7 @@ import {
 } from './../../../helpers/dom/element';
 import { partial } from './../../../helpers/function';
 import { isTouchSupported } from './../../../helpers/feature';
+import { isMobileBrowser } from './../../../helpers/browser';
 import EventManager from './../../../eventManager';
 
 const privatePool = new WeakMap();
@@ -52,23 +53,9 @@ class Event {
     this.eventManager.addEventListener(this.instance.wtTable.TABLE, 'mouseover', event => this.onMouseOver(event));
     this.eventManager.addEventListener(this.instance.wtTable.TABLE, 'mouseout', event => this.onMouseOut(event));
 
-    if (isTouchSupported()) {
-      const onTouchStartProxy = (event) => {
-        const priv = privatePool.get(this);
-
-        priv.selectedCellBeforeTouchEnd = this.instance.selections.getCell().cellRange;
-        this.instance.touchApplied = true;
-
-        this.onTouchStart(event);
-      };
-      const onTouchEndProxy = (event) => {
-        this.instance.touchApplied = false;
-
-        this.onTouchEnd(event);
-      };
-
-      this.eventManager.addEventListener(this.instance.wtTable.holder, 'touchstart', event => onTouchStartProxy(event));
-      this.eventManager.addEventListener(this.instance.wtTable.holder, 'touchend', event => onTouchEndProxy(event));
+    const initTouchEvents = () => {
+      this.eventManager.addEventListener(this.instance.wtTable.holder, 'touchstart', event => this.onTouchStart(event));
+      this.eventManager.addEventListener(this.instance.wtTable.holder, 'touchend', event => this.onTouchEnd(event));
 
       if (!this.instance.momentumScrolling) {
         this.instance.momentumScrolling = {};
@@ -89,10 +76,22 @@ class Event {
           }
         }, 200);
       });
+    };
 
-    } else {
+    const initMouseEvents = () => {
       this.eventManager.addEventListener(this.instance.wtTable.holder, 'mouseup', event => this.onMouseUp(event));
       this.eventManager.addEventListener(this.instance.wtTable.holder, 'mousedown', event => this.onMouseDown(event));
+    };
+
+    if (isMobileBrowser()) {
+      initTouchEvents();
+    } else {
+      // PC like devices which support both methods (touchscreen and ability to plug-in mouse).
+      if (isTouchSupported()) {
+        initTouchEvents();
+      }
+
+      initMouseEvents();
     }
 
     this.eventManager.addEventListener(window, 'resize', () => {
@@ -297,6 +296,11 @@ class Event {
    * @param {MouseEvent} event
    */
   onTouchStart(event) {
+    const priv = privatePool.get(this);
+
+    priv.selectedCellBeforeTouchEnd = this.instance.selections.getCell().cellRange;
+    this.instance.touchApplied = true;
+
     this.onMouseDown(event);
   }
 
@@ -309,6 +313,8 @@ class Event {
   onTouchEnd(event) {
     const excludeTags = ['A', 'BUTTON', 'INPUT'];
     const target = event.target;
+
+    this.instance.touchApplied = false;
 
     // When the standard event was performed on the link element (a cell which contains HTML `a` element) then here
     // we check if it should be canceled. Click is blocked in a situation when the element is rendered outside
