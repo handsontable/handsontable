19720979cee5a9b1bb16d46d36020952982e3b6e
swistach <swistach@gmail.com>
2018-12-20 16:13:24 +0100

    Refactor tableview to ES6. WIP

--- src/helpers/mixed.js
+++ src/helpers/mixed.js
@@ -152,3 +152,19 @@ function _checkKeySchema(v) {
   return p === z;
 }
 /* eslint-enable */
+
+/**
+ * Cross-platform helper to clear text selection.
+ */
+export function clearTextSelection() {
+  // http://stackoverflow.com/questions/3169786/clear-text-selection-with-javascript
+  if (window.getSelection) {
+    if (window.getSelection().empty) { // Chrome
+      window.getSelection().empty();
+    } else if (window.getSelection().removeAllRanges) { // Firefox
+      window.getSelection().removeAllRanges();
+    }
+  } else if (document.selection) { // IE?
+    document.selection.empty();
+  }
+};
