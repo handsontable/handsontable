86e222dc0236f663851dfafbec5e0afdc11fdc02
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2019-02-11 13:32:18 +0100

    Remove deprecated selectCellByProp method (#5778)

--- src/core.js
+++ src/core.js
@@ -18,7 +18,6 @@ import {
   objectEach
 } from './helpers/object';
 import { arrayFlatten, arrayMap, arrayEach, arrayReduce } from './helpers/array';
-import { toSingleLine } from './helpers/templateLiteralTag';
 import { getPlugin } from './plugins';
 import { getRenderer } from './renderers';
 import { getValidator } from './validators';
@@ -3201,28 +3200,6 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
     preventScrollingToCell = false;
 
     return wasSelected;
-  };
-
-  /**
-   * Select the cell specified by the `row` and `prop` arguments, or a range finishing at `endRow`, `endProp`.
-   * By default, viewport will be scrolled to selection.
-   *
-   * @deprecated
-   * @memberof Core#
-   * @function selectCellByProp
-   * @param {Number} row Visual row index.
-   * @param {String} prop Property name.
-   * @param {Number} [endRow] visual end row index (if selecting a range).
-   * @param {String} [endProp] End property name (if selecting a range).
-   * @param {Boolean} [scrollToCell=true] If `true`, viewport will be scrolled to the selection.
-   * @param {Boolean} [changeListener=true] If `false`, Handsontable will not change keyboard events listener to himself.
-   * @returns {Boolean} `true` if selection was successful, `false` otherwise.
-   */
-  this.selectCellByProp = function(row, prop, endRow, endProp, scrollToCell = true, changeListener = true) {
-    warn(toSingleLine`Deprecation warning: This method is going to be removed in the next release.
-      If you want to select a cell using props, please use the \`selectCell\` method.`);
-
-    return this.selectCells([[row, prop, endRow, endProp]], scrollToCell, changeListener);
   };
 
   /**
