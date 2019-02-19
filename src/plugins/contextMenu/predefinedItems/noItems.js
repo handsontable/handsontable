a90c48111f2011bd765bd0ce0d68d613213cdf9e
swistach <swistach@gmail.com>
2019-02-19 14:30:44 +0100

    Added test spec + translation for noItems for pl-PL. #3865

--- src/plugins/contextMenu/predefinedItems/noItems.js
+++ src/plugins/contextMenu/predefinedItems/noItems.js
@@ -1,12 +1,12 @@
-import * as C from './../../../i18n/constants';
+import { CONTEXTMENU_ITEMS_NO_ITEMS } from './../../../i18n/constants';
 
-export const KEY = 'no-items';
+export const KEY = 'no_items';
 
 export default function noItemsItem() {
   return {
     key: KEY,
     name() {
-      return this.getTranslatedPhrase(C.CONTEXTMENU_ITEMS_NO_OPTIONS);
+      return this.getTranslatedPhrase(CONTEXTMENU_ITEMS_NO_ITEMS);
     },
     disabled: true,
     isCommand: false,
