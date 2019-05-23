2ccd9858f5bfc103b81575af948598f126e837a4
swistach <swistach@gmail.com>
2019-05-23 13:24:49 +0200

    Added: undoRedo stack will cache selection after data change. #6017

--- src/plugins/undoRedo/undoRedo.js
+++ src/plugins/undoRedo/undoRedo.js
@@ -28,10 +28,21 @@ function UndoRedo(instance) {
   this.undoneActions = [];
   this.ignoreNewActions = false;
 
-  instance.addHook('afterChange', (changes, source) => {
-    if (changes && source !== 'UndoRedo.undo' && source !== 'UndoRedo.redo' && source !== 'MergeCells') {
-      plugin.done(new UndoRedo.ChangeAction(changes));
+  instance.addHook('afterChange', function(changes, source) {
+    if (!changes || ['UndoRedo.undo', 'UndoRedo.redo', 'MergeCells'].includes(source)) {
+      return;
     }
+
+    let selected = [];
+
+    if (source === 'CopyPaste.paste') {
+      instance.addHookOnce('afterPaste', (...args) => selected.push(...args[1].map(range => [range.startRow, range.startCol, range.endRow, range.endCol])));
+
+    } else {
+      selected = changes.length > 1 ? this.getSelected() : [[changes[0][0], changes[0][1]]];
+    }
+
+    plugin.done(new UndoRedo.ChangeAction(changes, selected));
   });
 
   instance.addHook('afterCreateRow', (index, amount, source) => {
@@ -248,8 +259,9 @@ UndoRedo.Action.prototype.redo = function() {};
  *
  * @private
  */
-UndoRedo.ChangeAction = function(changes) {
+UndoRedo.ChangeAction = function(changes, selected) {
   this.changes = changes;
+  this.selected = selected;
   this.actionType = 'change';
 };
 inherit(UndoRedo.ChangeAction, UndoRedo.Action);
@@ -268,22 +280,24 @@ UndoRedo.ChangeAction.prototype.undo = function(instance, undoneCallback) {
   instance.setDataAtRowProp(data, null, null, 'UndoRedo.undo');
 
   for (let i = 0, len = data.length; i < len; i++) {
-    if (instance.getSettings().minSpareRows && data[i][0] + 1 + instance.getSettings().minSpareRows === instance.countRows() &&
+    const [row, column] = data[i];
+
+    if (instance.getSettings().minSpareRows && row + 1 + instance.getSettings().minSpareRows === instance.countRows() &&
       emptyRowsAtTheEnd === instance.getSettings().minSpareRows) {
 
-      instance.alter('remove_row', parseInt(data[i][0] + 1, 10), instance.getSettings().minSpareRows);
+      instance.alter('remove_row', parseInt(row + 1, 10), instance.getSettings().minSpareRows);
       instance.undoRedo.doneActions.pop();
-
     }
 
-    if (instance.getSettings().minSpareCols && data[i][1] + 1 + instance.getSettings().minSpareCols === instance.countCols() &&
+    if (instance.getSettings().minSpareCols && column + 1 + instance.getSettings().minSpareCols === instance.countCols() &&
       emptyColsAtTheEnd === instance.getSettings().minSpareCols) {
 
-      instance.alter('remove_col', parseInt(data[i][1] + 1, 10), instance.getSettings().minSpareCols);
+      instance.alter('remove_col', parseInt(column + 1, 10), instance.getSettings().minSpareCols);
       instance.undoRedo.doneActions.pop();
     }
   }
 
+  instance.selectCells(this.selected, false, false);
 };
 UndoRedo.ChangeAction.prototype.redo = function(instance, onFinishCallback) {
   const data = deepClone(this.changes);
@@ -294,6 +308,7 @@ UndoRedo.ChangeAction.prototype.redo = function(instance, onFinishCallback) {
 
   instance.addHookOnce('afterChange', onFinishCallback);
   instance.setDataAtRowProp(data, null, null, 'UndoRedo.redo');
+  instance.selectCells(this.selected, false, false);
 };
 
 /**
@@ -630,8 +645,12 @@ function init() {
 
 function onBeforeKeyDown(event) {
   const instance = this;
-
   const ctrlDown = (event.ctrlKey || event.metaKey) && !event.altKey;
+  const activeEditor = instance.getActiveEditor();
+
+  if (activeEditor && activeEditor.isOpened()) {
+    return;
+  }
 
   if (ctrlDown) {
     if (event.keyCode === 89 || (event.shiftKey && event.keyCode === 90)) { // CTRL + Y or CTRL + SHIFT + Z
