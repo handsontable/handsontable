8ebca43ae5a9d9a9c18146d1ac9a89210706b808
Piotr Laszczkowski <swistach@gmail.com>
2019-07-22 15:51:30 +0200

    WIP. Reverted: changes in map.init. #5751

--- src/translations/maps/baseMap.js
+++ src/translations/maps/baseMap.js
@@ -27,7 +27,7 @@ class BaseMap {
       this.list = this.initValuesOrFn.slice();
 
     } else {
-      this.list = Array.from({ length }, () => (this.initValuesOrFn));
+      rangeEach(length - 1, () => this.list.push(this.initValuesOrFn));
     }
 
     this.runLocalHooks('mapChanged');
