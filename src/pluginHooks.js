37de848c537262e356caf55fd92e567001ff5c62
piotr <piotr.nowak.25.02.1978@gmail.com>
2019-08-26 09:42:49 +0200

    Update src/pluginHooks.js
    Co-Authored-By: Krzysztof Budnik <571316+budnix@users.noreply.github.com>

--- src/pluginHooks.js
+++ src/pluginHooks.js
@@ -1828,7 +1828,7 @@ const REGISTERED_HOOKS = [
    * @event Hooks#beforeColumnCollapse
    * @param {Array} currentCollapsedColumns Current collapsible configuration - a list of collapsible physical column indexes.
    * @param {Array} destinationCollapsedColumns Destination collapsible configuration - a list of collapsible physical column indexes.
-   * @param {Boolean} collapsePossible `true`, if all of the col indexes aren't withing the hidden columns, `false` otherwise.
+   * @param {Boolean} collapsePossible `true`, if all of the column indexes aren't withing the hidden columns, `false` otherwise.
    * @returns {undefined|Boolean} If the callback returns `false`, the trimming action will not be completed.
    */
   'beforeColumnCollapse',
