61ba2fc5c6a75f2b50d2c18ce4ecc1b2fb0d864f
Piotr Laszczkowski <swistach@users.noreply.github.com>
2019-10-31 16:08:49 +0100

    Configurable ContextMenu's container (#6304)
    * Changed: contextMenu uses configurable container. #6283
    
    * Changed: contextMenu's positioning to support iframe. Adjustment in dragToScroll, #6283
    
    * Changed: event propagation for right click on contextMenu items. Reverted changes for mixed frames event propagation onkeydown. #6283
    
    * Changed: prevented opening native context menu by clicking the right mouse button on ContextMenu's item. #6283
    
    * Changed: contextMenu/dropdownMenu/FilterUISelect executes command on  hook instead of . First part of test specs adjustments. #6283
    
    * Adjusted: contextMenu's items will be executed on mouseup/contextmenu events. #6283
    
    * Changed: property name of UI container. Added: test specs for custom UI container. #6283
    
    * Changed: dropdownMenu requires colHeaders to work. #6283
    
    * Added: tests for added functionalities. #6283
    
    * Changed: default container for ContextMenu should be document.body instead document.documentElement. #6283
    
    * Changed: position's calculations have to base on event.view's scroll too. [skip ci] #6283
    
    * Added: test scenario to verify contextmenu UI elements' positioning is correct if documents are different. #6283

--- src/plugins/comments/test/comments.e2e.js
+++ src/plugins/comments/test/comments.e2e.js
@@ -347,7 +347,7 @@ describe('Comments', () => {
       clientY: Handsontable.dom.offset(addCommentButton).top + 5,
     });
 
-    $(addCommentButton).simulate('mousedown');
+    $(addCommentButton).simulate('mousedown').simulate('mouseup');
 
     const editor = hot.getPlugin('comments').editor.getInputElement();
 
@@ -371,7 +371,7 @@ describe('Comments', () => {
         return $(this).text() === 'Add comment';
       })[0];
 
-      $(addCommentButton).simulate('mousedown');
+      $(addCommentButton).simulate('mousedown').simulate('mouseup');
 
       const editor = hot.getPlugin('comments').editor.getInputElement();
 
@@ -397,7 +397,7 @@ describe('Comments', () => {
         return $(this).text() === 'Delete comment';
       })[0];
 
-      $(deleteCommentButton).simulate('mousedown');
+      $(deleteCommentButton).simulate('mousedown').simulate('mouseup');
 
       expect(getCellMeta(1, 1).comment).toEqual(void 0);
     });
@@ -423,7 +423,7 @@ describe('Comments', () => {
         return $(this).text() === 'Delete comment';
       })[0];
 
-      $(deleteCommentButton).simulate('mousedown');
+      $(deleteCommentButton).simulate('mousedown').simulate('mouseup');
 
       expect(getCellMeta(1, 1).comment).toEqual(void 0);
       expect(getCellMeta(2, 2).comment).toEqual(void 0);
@@ -450,7 +450,7 @@ describe('Comments', () => {
         return $(this).text() === 'Read-only comment';
       })[0];
 
-      $(readOnlyComment).simulate('mousedown');
+      $(readOnlyComment).simulate('mousedown').simulate('mouseup');
       $(document).simulate('mouseup');
 
       $(getCell(1, 1)).simulate('mouseover', {
@@ -494,7 +494,7 @@ describe('Comments', () => {
         return $(this).text() === 'Delete comment';
       })[0];
 
-      $(deleteCommentButton).simulate('mousedown');
+      $(deleteCommentButton).simulate('mousedown').simulate('mouseup');
 
       expect(afterSetCellMetaCallback).toHaveBeenCalledWith(1, 1, 'comment', undefined, undefined, undefined);
     });
