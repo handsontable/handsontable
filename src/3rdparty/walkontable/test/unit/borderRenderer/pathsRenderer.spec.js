5f245d473a4bb78aae03ac9a93a80fc2b8abcf8f
Marcin Warpechowski <warpech@gmail.com>
2019-12-05 17:24:05 +0100

    test: add unit test to verify current behavior of "compareStrokePriority"
    because I want to have a solid base when changing this behavior as discussed in https://github.com/handsontable/handsontable/issues/6453

---
+++ src/3rdparty/walkontable/test/unit/borderRenderer/pathsRenderer.spec.js
@@ -0,0 +1,50 @@
+import { compareStrokePriority } from 'walkontable/borderRenderer/svg/pathsRenderer';
+
+describe('borderRenderer', () => {
+  describe('pathsRenderer', () => {
+    describe('compareStrokePriority', () => {
+      it('should return 0 when given 2 borders that are the same', () => {
+        expect(compareStrokePriority('1px black horizontal', '1px black horizontal')).toBe(0);
+      });
+
+      it('should give priority to a wider border', () => {
+        expect(compareStrokePriority('2px black horizontal', '1px black horizontal')).toBe(1);
+        expect(compareStrokePriority('1px black horizontal', '2px black horizontal')).toBe(-1);
+
+        expect(compareStrokePriority('10px black horizontal', '2px black horizontal')).toBe(1);
+        expect(compareStrokePriority('2px black horizontal', '10px black horizontal')).toBe(-1);
+      });
+
+      it('should give priority to horizontal border', () => {
+        expect(compareStrokePriority('2px black horizontal', '2px black vertical')).toBe(1);
+        expect(compareStrokePriority('2px black vertical', '2px black horizontal')).toBe(-1);
+      });
+
+      it('should give priority to a darker border', () => {
+        expect(compareStrokePriority('1px black horizontal', '1px beige horizontal')).toBe(1);
+        expect(compareStrokePriority('1px beige horizontal', '1px black horizontal')).toBe(-1);
+
+        // TODO the below comparisons don't work as expected, see: https://github.com/handsontable/handsontable/issues/6453
+
+        // expect(compareStrokePriority('1px darkred horizontal', '1px red horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px red horizontal', '1px darkred horizontal')).toBe(-1);
+
+        // expect(compareStrokePriority('1px darkred horizontal', '1px RED horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px RED horizontal', '1px darkred horizontal')).toBe(-1);
+
+        // expect(compareStrokePriority('1px #000000 horizontal', '1px #FFFFFF horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px #FFFFFF horizontal', '1px #000000 horizontal')).toBe(-1);
+
+        // expect(compareStrokePriority('1px rgb(0, 0, 0) horizontal', '1px rgb(255, 255, 255) horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px rgb(255, 255, 255) horizontal', '1px rgb(0, 0, 0) horizontal')).toBe(-1);
+
+        // expect(compareStrokePriority('1px rgba(0, 0, 0, 1) horizontal', '1px rgba(255, 255, 255, 1) horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px rgba(255, 255, 255, 1) horizontal', '1px rgba(0, 0, 0, 1) horizontal')).toBe(-1);
+
+        // expect(compareStrokePriority('1px black horizontal', '1px white horizontal')).toBe(1);
+        // expect(compareStrokePriority('1px white horizontal', '1px black horizontal')).toBe(-1);
+      });
+    });
+  });
+
+});
