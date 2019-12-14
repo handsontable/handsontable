68f7c3fa92a28424aad8162640c4d2dbee8d593c
Marcin Warpechowski <warpech@gmail.com>
2019-12-14 23:09:38 +0100

    refactor: change the color object to a simple array
    this consumes less memory and is more performant

--- src/3rdparty/walkontable/src/borderRenderer/svg/color.js
+++ src/3rdparty/walkontable/src/borderRenderer/svg/color.js
@@ -1,7 +1,7 @@
 import normalizeColor from 'normalize-css-color';
 
 /**
- * Converts keyword, short hex, long hex, rgb, rgba colors to [r, g, b] array
+ * Converts keyword, short hex, long hex, rgb, rgba colors to [r, g, b, a] array
  *
  * orange, oRaNgE, #FFA500, #ffa500, rgb(255,165,0) -> {"r": 255, "g": 165, "b": 0, "a": 1}
  * #FA0 -> {"r": 255, "g": 170, "b": 0, "a": 1}
@@ -9,19 +9,20 @@ import normalizeColor from 'normalize-css-color';
  * hsla(39,100%,50%,.2) -> { r: 255, g: 166, b: 0, a: 0.2 }
  *
  * @param {String} cssColor
- * @returns {Object} An object with properties: r, g, b, a
+ * @returns {Array} An array with items: [r, g, b, a]
  */
 export function convertCssColorToRGBA(cssColor) {
   const nullableColor = normalizeColor(cssColor.toLowerCase());
   const colorInt = nullableColor === null ? 0x00000000 : nullableColor;
-  return normalizeColor.rgba(colorInt);
+  const { r, g, b, a } = normalizeColor.rgba(colorInt);
+  return [r, g, b, a];
 }
 
 /**
  * Compares the luminance of two colors.
  *
- * @param {Object} rgba1 An object with properties: r, g, b, a
- * @param {Object} rgba2 An object with properties: r, g, b, a
+ * @param {Array} rgba1 An array with items: [r, g, b, a]
+ * @param {Array} rgba2 An array with items: [r, g, b, a]
  * @returns {Number} 1 if rgba1 has a higher luminance (is lighter) than rgba2, 0 if rgba1 has the same luminance as rgba2, -1 if rgba1 has a lower luminance (is darker) than rgba2
  */
 export function compareLuminance(rgba1, rgba2) {
@@ -39,10 +40,10 @@ export function compareLuminance(rgba1, rgba2) {
 /**
  * Calculates luminance of a color using ITU-R Recommendation BT.709 (aka Rec. 709, BT.709, and ITU709). The lower the luminance, the darker the color appears.
  *
- * @param {Object} rgba An object with properties: r, g, b, a
+ * @param {Array} rgba An array with items: [r, g, b, a]
  * @returns {Number} A number in range 0-255
  */
 export function getLuminance(rgba) {
-  const { r, g, b } = rgba;
+  const [r, g, b] = rgba;
   return (r * 0.2126) + (g * 0.7152) + (b * 0.0722);
 }
--- src/3rdparty/walkontable/test/unit/borderRenderer/color.spec.js
+++ src/3rdparty/walkontable/test/unit/borderRenderer/color.spec.js
@@ -6,72 +6,72 @@ describe('borderRenderer', () => {
       describe('convertCssColorToRGBA', () => {
         it('should convert colors to normalized rgb', () => {
           expect(convertCssColorToRGBA('orange'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 1 });
+            .toEqual([255, 165, 0, 1]);
 
           expect(convertCssColorToRGBA('oRaNgE'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 1 });
+            .toEqual([255, 165, 0, 1]);
 
           expect(convertCssColorToRGBA('#FFA500'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 1 });
+            .toEqual([255, 165, 0, 1]);
 
           expect(convertCssColorToRGBA('#ffa500'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 1 });
+            .toEqual([255, 165, 0, 1]);
 
           expect(convertCssColorToRGBA('#FA0'))
-            .toEqual({ r: 255, g: 170, b: 0, a: 1 });
+            .toEqual([255, 170, 0, 1]);
 
           expect(convertCssColorToRGBA('#fa0'))
-            .toEqual({ r: 255, g: 170, b: 0, a: 1 });
+            .toEqual([255, 170, 0, 1]);
 
           expect(convertCssColorToRGBA('rgb(255,165,0)'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 1 });
+            .toEqual([255, 165, 0, 1]);
 
           expect(convertCssColorToRGBA('rgba(255,165,0,.2)'))
-            .toEqual({ r: 255, g: 165, b: 0, a: 0.2 });
+            .toEqual([255, 165, 0, 0.2]);
 
           expect(convertCssColorToRGBA('hsl(39,100%,50%)'))
-            .toEqual({ r: 255, g: 166, b: 0, a: 1 });
+            .toEqual([255, 166, 0, 1]);
 
           expect(convertCssColorToRGBA('hsla(39,100%,50%,.2)'))
-            .toEqual({ r: 255, g: 166, b: 0, a: 0.2 });
+            .toEqual([255, 166, 0, 0.2]);
         });
       });
 
       describe('getLuminance', () => {
         it('should return luminance in range 0-255', () => {
-          expect(getLuminance({ r: 0, g: 0, b: 0, a: 1 }))
+          expect(getLuminance([0, 0, 0, 1]))
             .toEqual(0);
 
-          expect(Math.round(getLuminance({ r: 100, g: 50, b: 150, a: 1 })))
+          expect(Math.round(getLuminance([100, 50, 150, 1])))
             .toEqual(68);
 
-          expect(Math.round(getLuminance({ r: 50, g: 100, b: 150, a: 1 })))
+          expect(Math.round(getLuminance([50, 100, 150, 1])))
             .toEqual(93);
 
-          expect(getLuminance({ r: 100, g: 100, b: 100, a: 1 }))
+          expect(getLuminance([100, 100, 100, 1]))
             .toEqual(100);
 
-          expect(Math.round(getLuminance({ r: 150, g: 100, b: 50, a: 1 })))
+          expect(Math.round(getLuminance([150, 100, 50, 1])))
             .toEqual(107);
 
-          expect(Math.round(getLuminance({ r: 255, g: 255, b: 255, a: 1 })))
+          expect(Math.round(getLuminance([255, 255, 255, 1])))
             .toEqual(255);
         });
       });
 
       describe('compareLuminance', () => {
         it('should return 0 if both colors are equal', () => {
-          expect(compareLuminance({ r: 0, g: 0, b: 0, a: 1 }, { r: 0, g: 0, b: 0, a: 1 }))
+          expect(compareLuminance([0, 0, 0, 1], [0, 0, 0, 1]))
             .toEqual(0);
         });
 
         it('should return 1 if the first parameter is a lighter color', () => {
-          expect(compareLuminance({ r: 1, g: 1, b: 1, a: 1 }, { r: 0, g: 0, b: 0, a: 1 }))
+          expect(compareLuminance([1, 1, 1, 1], [0, 0, 0, 1]))
             .toEqual(1);
         });
 
         it('should return -1 if the first parameter is a darker color', () => {
-          expect(compareLuminance({ r: 0, g: 0, b: 0, a: 1 }, { r: 1, g: 1, b: 1, a: 1 }))
+          expect(compareLuminance([0, 0, 0, 1], [1, 1, 1, 1]))
             .toEqual(-1);
         });
       });
