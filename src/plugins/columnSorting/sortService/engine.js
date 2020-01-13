09ecbda6f38e8accad45149e434ba60812ee5d8d
budnix <571316+budnix@users.noreply.github.com>
2020-01-13 13:02:08 +0100

    Remove mergeSort and use buildin Array.sort
    Issue: #6091

--- src/plugins/columnSorting/sortService/engine.js
+++ src/plugins/columnSorting/sortService/engine.js
@@ -1,4 +1,3 @@
-import mergeSort from '../../../utils/sortingAlgorithms/mergeSort';
 import { getRootComparator } from './registry';
 
 export const DO_NOT_SWAP = 0;
@@ -8,5 +7,5 @@ export const FIRST_AFTER_SECOND = 1;
 export function sort(indexesWithData, rootComparatorId, ...argsForRootComparator) {
   const rootComparator = getRootComparator(rootComparatorId);
 
-  mergeSort(indexesWithData, rootComparator(...argsForRootComparator));
+  indexesWithData.sort(rootComparator(...argsForRootComparator));
 }
