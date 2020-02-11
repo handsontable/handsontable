da0f47623f93a07a2b59adc14ebb5eeac9012d84
Jeremy Smith <jeremy-smith@zencontrol.com>
2020-02-12 09:21:48 +1000

    Adds `afterAutofill` hook

--- src/plugins/autofill/autofill.js
+++ src/plugins/autofill/autofill.js
@@ -8,6 +8,7 @@ import {getDeltas, getDragDirectionAndRange, DIRECTIONS, getMappedFillHandleSett
 
 Hooks.getSingleton().register('modifyAutofillRange');
 Hooks.getSingleton().register('beforeAutofill');
+Hooks.getSingleton().register('afterAutofill');
 
 const INSERT_ROW_ALTER_ACTION_NAME = 'insert_row';
 const INTERVAL_FOR_ADDING_ROW = 200;
@@ -199,6 +200,7 @@ class Autofill extends BasePlugin {
       );
 
       this.setSelection(cornersOfSelectionAndDragAreas);
+      this.hot.runHooks('afterAutofill', startOfDragCoords, endOfDragCoords, selectionData);
 
     } else {
       // reset to avoid some range bug
