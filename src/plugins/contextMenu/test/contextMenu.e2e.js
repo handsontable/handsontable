d354348b19ec394ff5677bd59433f29a98defb74
Piotr Laszczkowski <swistach@gmail.com>
2020-03-15 16:45:28 +0100

    #6218 - Adds E2E test to check if event.defaultPrevented is true if the user triggered context menu on .htCore element - it should prevent native menu activation

--- src/plugins/contextMenu/test/contextMenu.e2e.js
+++ src/plugins/contextMenu/test/contextMenu.e2e.js
@@ -177,6 +177,31 @@ describe('ContextMenu', () => {
       expect($('.htContextMenu').is(':visible')).toBe(true);
     });
 
+    it('should not open context menu after right click on inner htCore', () => {
+      let eventPrevented = false;
+
+      const rewriteEventPrevention = (e) => {
+        eventPrevented = e.defaultPrevented;
+      };
+
+      document.addEventListener('contextmenu', rewriteEventPrevention);
+
+      handsontable({
+        contextMenu: true,
+        height: 100
+      });
+
+      contextMenu();
+
+      const contextMenuHtCore = $('.htContextMenu .ht_master .htCore')[0];
+      const contextmenuEvent = new MouseEvent('contextmenu', { bubbles: true, cancelable: true });
+
+      contextMenuHtCore.dispatchEvent(contextmenuEvent);
+      document.removeEventListener('contextmenu', rewriteEventPrevention);
+
+      expect(eventPrevented).toBe(true);
+    });
+
     it('should be possible to define a custom container for ContextMenu\'s UI elements', () => {
       const uiContainer = $('<div/>').addClass('uiContainer');
       spec().$container.append(uiContainer);
