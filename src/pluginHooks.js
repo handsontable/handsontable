640ea50c7ec13ece23f97a0d7ad78ca424859f6f
Anna Cieslik <scarletfog@gmail.com>
2020-08-20 17:52:29 +0200

    add missing param description for beforeRender

--- src/pluginHooks.js
+++ src/pluginHooks.js
@@ -874,6 +874,7 @@ const REGISTERED_HOOKS = [
    * @event Hooks#beforeRender
    * @param {boolean} isForced If `true` rendering was triggered by a change of settings or data; or `false` if
    *                           rendering was triggered by scrolling or moving selection.
+   * @param {boolean} skipRender If `true ` the next rendering cycle will be skipped.
    */
   'beforeRender',
 
