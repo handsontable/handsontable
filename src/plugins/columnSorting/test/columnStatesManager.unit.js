5bf152d25366f7fcf92a9e3fcfcc9db26b5d9230
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2020-09-16 14:03:16 +0200

    Changed unit tests #6397

--- src/plugins/columnSorting/test/columnStatesManager.unit.js
+++ src/plugins/columnSorting/test/columnStatesManager.unit.js
@@ -3,12 +3,12 @@ import {
   ASC_SORT_STATE,
   DESC_SORT_STATE
 } from 'handsontable/plugins/columnSorting/columnStatesManager';
-import { deepClone } from '../../../helpers/object';
+import { PhysicalIndexToValueMap as IndexToValueMap } from 'handsontable/translations';
 
 describe('ColumnSorting', () => {
   describe('ColumnStatesManager.updateAllColumnsProperties', () => {
     it('should update internal properties', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const columnStatesManager = new ColumnStatesManager(new IndexToValueMap());
       const compareFunctionFactory = function() {};
 
       columnStatesManager.updateAllColumnsProperties({
@@ -25,7 +25,7 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getAllColumnsProperties', () => {
     it('should return default columns properties', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const columnStatesManager = new ColumnStatesManager(new IndexToValueMap());
 
       expect(columnStatesManager.getAllColumnsProperties()).toEqual({
         sortEmptyCells: false,
@@ -35,7 +35,7 @@ describe('ColumnSorting', () => {
     });
 
     it('should return set columns properties', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const columnStatesManager = new ColumnStatesManager(new IndexToValueMap());
       const compareFunctionFactory = function() {};
 
       columnStatesManager.updateAllColumnsProperties({
@@ -56,12 +56,15 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.setSortStates', () => {
     it('should change state queue', () => {
-      const columnStatesManager = new ColumnStatesManager();
-      const initialState = deepClone(columnStatesManager.sortedColumnsStates);
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+      const initialState = columnStatesManager.getSortedColumnsStates();
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates([{ column: 0, sortOrder: ASC_SORT_STATE }]);
 
-      const stateAfterFunctionCall = deepClone(columnStatesManager.sortedColumnsStates);
+      const stateAfterFunctionCall = columnStatesManager.getSortedColumnsStates();
 
       expect(stateAfterFunctionCall).not.toEqual(initialState);
     });
@@ -69,13 +72,16 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getSortStates', () => {
     it('should return copy of states', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
       const newStatesQueue = [
         { column: 0, sortOrder: DESC_SORT_STATE },
         { column: 2, sortOrder: ASC_SORT_STATE },
         { column: 1, sortOrder: ASC_SORT_STATE },
       ];
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates(newStatesQueue);
 
       expect(columnStatesManager.getSortStates()).toEqual(newStatesQueue);
@@ -85,13 +91,16 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getColumnSortState', () => {
     it('should return copy of state', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
       const newStatesQueue = [
         { column: 0, sortOrder: DESC_SORT_STATE },
         { column: 2, sortOrder: ASC_SORT_STATE },
         { column: 1, sortOrder: ASC_SORT_STATE },
       ];
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates(newStatesQueue);
 
       expect(columnStatesManager.getColumnSortState(0)).toEqual(newStatesQueue[0]);
@@ -101,14 +110,21 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.isListOfSortedColumnsEmpty', () => {
     it('should return `true` when state queue is empty', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       expect(columnStatesManager.isListOfSortedColumnsEmpty()).toBeTruthy();
     });
 
     it('should return `true` when state of the only sorted column was changed to not sorted', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates([{ column: 0, sortOrder: ASC_SORT_STATE }]);
       columnStatesManager.setSortStates([]);
 
@@ -116,8 +132,11 @@ describe('ColumnSorting', () => {
     });
 
     it('should return `false` when state queue is not empty', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates([{ column: 0, sortOrder: ASC_SORT_STATE }]);
 
       expect(columnStatesManager.isListOfSortedColumnsEmpty()).toBeFalsy();
@@ -126,7 +145,11 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getSortedColumns', () => {
     it('should return list of all sorted columns (respecting the states queue)', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       columnStatesManager.setSortStates([
         { column: 0, sortOrder: DESC_SORT_STATE },
@@ -138,16 +161,22 @@ describe('ColumnSorting', () => {
     });
 
     it('should return empty array when no column has been sorted', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       expect(columnStatesManager.getSortedColumns()).toEqual([]);
     });
   });
 
   describe('ColumnStatesManager.getIndexOfColumnInSortQueue', () => {
     it('should return index of particular column in the states queue', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
 
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates([
         { column: 0, sortOrder: DESC_SORT_STATE },
         { column: 2, sortOrder: ASC_SORT_STATE },
@@ -160,7 +189,11 @@ describe('ColumnSorting', () => {
     });
 
     it('should return -1 when particular column does not exist in the states queue', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       expect(columnStatesManager.getIndexOfColumnInSortQueue(0)).toEqual(-1);
     });
@@ -168,7 +201,11 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getNumberOfSortedColumns', () => {
     it('should return number of sorted columns', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       columnStatesManager.setSortStates([
         { column: 0, sortOrder: DESC_SORT_STATE },
@@ -193,7 +230,11 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.isColumnSorted', () => {
     it('should return if particular column is sorted', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       columnStatesManager.setSortStates([{ column: 1, sortOrder: ASC_SORT_STATE }]);
 
@@ -204,13 +245,21 @@ describe('ColumnSorting', () => {
 
   describe('ColumnStatesManager.getSortOrderOfColumn', () => {
     it('should return `undefined` when column is not sorted', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
 
       expect(columnStatesManager.getSortOrderOfColumn(0)).toBeUndefined();
     });
 
     it('should return proper order when column is sorted', () => {
-      const columnStatesManager = new ColumnStatesManager();
+      const indexToValueMap = new IndexToValueMap();
+      const columnStatesManager = new ColumnStatesManager(indexToValueMap);
+
+      // Mocking map for sorting states when table have 3 columns.
+      indexToValueMap.init(3);
       columnStatesManager.setSortStates([
         { column: 0, sortOrder: DESC_SORT_STATE },
         { column: 1, sortOrder: ASC_SORT_STATE }
