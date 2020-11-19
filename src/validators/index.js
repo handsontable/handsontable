2f63b1e823c345a43fa3b9562cfef419206cdbce
Piotr Laszczkowski <swistach@gmail.com>
2020-11-19 15:07:21 +0100

    #7402 - Adjusts validators/index to auto-register built-in validators and to reexport functions

--- src/validators/index.js
+++ src/validators/index.js
@@ -1,34 +1,12 @@
-import staticRegister from '../utils/staticRegister';
-
-const {
-  register,
-  getItem,
-  hasItem,
-  getNames,
-  getValues,
-} = staticRegister('validators');
-
-/**
- * Retrieve validator function.
- *
- * @param {string} name Validator identification.
- * @returns {Function} Returns validator function.
- */
-function _getItem(name) {
-  if (typeof name === 'function') {
-    return name;
-  }
-  if (!hasItem(name)) {
-    throw Error(`No registered validator found under "${name}" name`);
-  }
-
-  return getItem(name);
-}
+import './autocompleteValidator';
+import './dateValidator';
+import './numericValidator';
+import './timeValidator';
 
 export {
-  register as registerValidator,
-  _getItem as getValidator,
-  hasItem as hasValidator,
-  getNames as getRegisteredValidatorNames,
-  getValues as getRegisteredValidators,
-};
+  getRegisteredValidatorNames,
+  getRegisteredValidators,
+  getValidator,
+  hasValidator,
+  registerValidator,
+} from './validators';
