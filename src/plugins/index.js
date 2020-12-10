6b4b7ff0c313842e63956246512698b0f709e8fa
Marcin Warpechowski <warpech@gmail.com>
2020-12-10 23:22:06 +0100

    SV perf improvement: exclude some plugins that we are not using

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,80 +1,80 @@
 // 'PersistentState' has to be initialized as first module to have priority in listening hooks
-import PersistentState from './persistentState/persistentState';
+// import PersistentState from './persistentState/persistentState';
 import AutoColumnSize from './autoColumnSize/autoColumnSize';
-import AutoFill from './autofill/autofill';
-import ManualRowResize from './manualRowResize/manualRowResize';
+// import AutoFill from './autofill/autofill';
+// import ManualRowResize from './manualRowResize/manualRowResize';
 import AutoRowSize from './autoRowSize/autoRowSize';
-import ColumnSorting from './columnSorting/columnSorting';
-import Comments from './comments/comments';
-import ContextMenu from './contextMenu/contextMenu';
+// import ColumnSorting from './columnSorting/columnSorting';
+// import Comments from './comments/comments';
+// import ContextMenu from './contextMenu/contextMenu';
 import CopyPaste from './copyPaste/copyPaste';
 import CustomBorders from './customBorders/customBorders';
-import DragToScroll from './dragToScroll/dragToScroll';
-import ManualColumnFreeze from './manualColumnFreeze/manualColumnFreeze';
-import ManualColumnMove from './manualColumnMove/manualColumnMove';
-import ManualColumnResize from './manualColumnResize/manualColumnResize';
-import ManualRowMove from './manualRowMove/manualRowMove';
+// import DragToScroll from './dragToScroll/dragToScroll';
+// import ManualColumnFreeze from './manualColumnFreeze/manualColumnFreeze';
+// import ManualColumnMove from './manualColumnMove/manualColumnMove';
+// import ManualColumnResize from './manualColumnResize/manualColumnResize';
+// import ManualRowMove from './manualRowMove/manualRowMove';
 import MergeCells from './mergeCells/mergeCells';
-import MultipleSelectionHandles from './multipleSelectionHandles/multipleSelectionHandles';
-import MultiColumnSorting from './multiColumnSorting/multiColumnSorting';
-import ObserveChanges from './observeChanges/observeChanges';
-import Search from './search/search';
+// import MultipleSelectionHandles from './multipleSelectionHandles/multipleSelectionHandles';
+// import MultiColumnSorting from './multiColumnSorting/multiColumnSorting';
+// import ObserveChanges from './observeChanges/observeChanges';
+// import Search from './search/search';
 import TouchScroll from './touchScroll/touchScroll';
-import UndoRedo from './undoRedo/undoRedo';
+// import UndoRedo from './undoRedo/undoRedo';
 import Base from './_base';
 
-import BindRowsWithHeaders from './bindRowsWithHeaders/bindRowsWithHeaders';
-import ColumnSummary from './columnSummary/columnSummary';
-import DropdownMenu from './dropdownMenu/dropdownMenu';
-import ExportFile from './exportFile/exportFile';
-import Filters from './filters/filters';
-import Formulas from './formulas/formulas';
-import HeaderTooltips from './headerTooltips/headerTooltips';
-import NestedHeaders from './nestedHeaders/nestedHeaders';
-import CollapsibleColumns from './collapsibleColumns/collapsibleColumns';
-import NestedRows from './nestedRows/nestedRows';
-import HiddenColumns from './hiddenColumns/hiddenColumns';
-import HiddenRows from './hiddenRows/hiddenRows';
-import TrimRows from './trimRows/trimRows';
+// import BindRowsWithHeaders from './bindRowsWithHeaders/bindRowsWithHeaders';
+// import ColumnSummary from './columnSummary/columnSummary';
+// import DropdownMenu from './dropdownMenu/dropdownMenu';
+// import ExportFile from './exportFile/exportFile';
+// import Filters from './filters/filters';
+// import Formulas from './formulas/formulas';
+// import HeaderTooltips from './headerTooltips/headerTooltips';
+// import NestedHeaders from './nestedHeaders/nestedHeaders';
+// import CollapsibleColumns from './collapsibleColumns/collapsibleColumns';
+// import NestedRows from './nestedRows/nestedRows';
+// import HiddenColumns from './hiddenColumns/hiddenColumns';
+// import HiddenRows from './hiddenRows/hiddenRows';
+// import TrimRows from './trimRows/trimRows';
 // The GanttChart plugin has to be imported as the last due to some bugs in the initialization process.
-import GanttChart from './ganttChart/ganttChart';
+// import GanttChart from './ganttChart/ganttChart';
 
 export {
   AutoColumnSize,
-  AutoFill,
+  // AutoFill,
   AutoRowSize,
   Base,
-  BindRowsWithHeaders,
-  CollapsibleColumns,
-  ColumnSorting,
-  ColumnSummary,
-  Comments,
-  ContextMenu,
+  // BindRowsWithHeaders,
+  // CollapsibleColumns,
+  // ColumnSorting,
+  // ColumnSummary,
+  // Comments,
+  // ContextMenu,
   CopyPaste,
   CustomBorders,
-  DragToScroll,
-  DropdownMenu,
-  ExportFile,
-  Filters,
-  Formulas,
-  GanttChart,
-  HeaderTooltips,
-  HiddenColumns,
-  HiddenRows,
-  ManualColumnFreeze,
-  ManualColumnMove,
-  ManualColumnResize,
-  ManualRowMove,
-  ManualRowResize,
+  // DragToScroll,
+  // DropdownMenu,
+  // ExportFile,
+  // Filters,
+  // Formulas,
+  // GanttChart,
+  // HeaderTooltips,
+  // HiddenColumns,
+  // HiddenRows,
+  // ManualColumnFreeze,
+  // ManualColumnMove,
+  // ManualColumnResize,
+  // ManualRowMove,
+  // ManualRowResize,
   MergeCells,
-  MultiColumnSorting,
-  MultipleSelectionHandles,
-  NestedHeaders,
-  NestedRows,
-  ObserveChanges,
-  PersistentState,
-  Search,
+  // MultiColumnSorting,
+  // MultipleSelectionHandles,
+  // NestedHeaders,
+  // NestedRows,
+  // ObserveChanges,
+  // PersistentState,
+  // Search,
   TouchScroll,
-  TrimRows,
-  UndoRedo,
+  // TrimRows,
+  // UndoRedo,
 };
