1a3b4d16d6d25b2624b5564d26ee2ced524df44a
@Siemienik Paweł <siemienik.pawel@gmail.com>
2020-12-22 02:30:18 +0100

    Generated legacy versions,

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,6 +35,24 @@ module.exports = {
           type: 'docsVersionDropdown',
           position: 'right',
           dropdownActiveClassDisabled: true,
+          // // COMMENT because it throws broking links error.
+          // // Or if set absolute path, it open links in new tab.
+          // // Instead of this, mock versions generated.
+          // dropdownItemsAfter: [
+          //   {to:"/docs/8.2.0/", label: "8.2.x"},
+          //   {to:"/docs/8.1.0/", label: "8.1.x"},
+          //   {to:"/docs/8.0.0/", label: "8.0.x"},
+          //   {to:"/docs/7.4.2/", label: "7.4.x"},
+          //   {to:"/docs/7.3.0/", label: "7.3.x"},
+          //   {to:"/docs/7.2.2/", label: "7.2.x"},
+          //   {to:"/docs/7.1.1/", label: "7.1.x"},
+          //   {to:"/docs/7.0.3/", label: "7.0.x"},
+          //   {to:"/docs/6.2.2/", label: "6.2.x"},
+          //   {to:"/docs/6.1.1/", label: "6.1.x"},
+          //   {to:"/docs/6.0.1/", label: "6.0.x"},
+          //   {to:"/docs/5.0.2/", label: "5.0.x"},
+          //   {to:"/docs/4.0.0/", label: "4.0.x"},
+          // ],
         },
       ],
     },
