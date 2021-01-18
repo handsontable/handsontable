381068dbb786318af41d37d0578bee402a7f949b
budnix <571316+budnix@users.noreply.github.com>
2021-01-18 15:32:56 +0100

    Add unit tests for plugin dependencies
    Issue: #7412

--- src/plugins/__tests__/pluginsDependencies.unit.js
+++ src/plugins/__tests__/pluginsDependencies.unit.js
@@ -3,11 +3,15 @@ import { BasePlugin } from '../base';
 import { registerPlugin } from '../';
 
 describe('plugins dependencies', () => {
-  it('should be possible to add dependencies', () => {
+  it('should throw an error when the dependencies are missing', () => {
     class CustomPlugin extends BasePlugin {
       static get PLUGIN_DEPS() {
         return [
           'plugin:NonExistingPlugin',
+          'cell-type:NonExistingCellType',
+          'editor:NonExistingEditor',
+          'renderer:NonExistingRenderer',
+          'validator:NonExistingValidator',
         ];
       }
     }
@@ -18,11 +22,33 @@ describe('plugins dependencies', () => {
 
     expect(() => {
       hot = new Handsontable(document.createElement('div'), {});
-    }).toThrowError([
-      'The CustomPlugin plugin requires the following modules:',
-      ' - NonExistingPlugin (plugin)',
-      'You have to import and register them manually.',
-    ].join('\n'));
+    }).toThrowError(`The CustomPlugin plugin requires the following modules:
+ - NonExistingPlugin (plugin)
+ - NonExistingCellType (cell-type)
+ - NonExistingEditor (editor)
+ - NonExistingRenderer (renderer)
+ - NonExistingValidator (validator)
+
+You have to import and register them manually.`);
+    expect(hot).toBeUndefined();
+  });
+
+  it('should throw an error when the dependency type is unknown', () => {
+    class CustomPlugin extends BasePlugin {
+      static get PLUGIN_DEPS() {
+        return [
+          'unknown-key:NonExistingPlugin',
+        ];
+      }
+    }
+
+    registerPlugin('CustomPlugin2', CustomPlugin);
+
+    let hot;
+
+    expect(() => {
+      hot = new Handsontable(document.createElement('div'), {});
+    }).toThrowError('Unknown plugin dependency type "unknown-key" was found.');
     expect(hot).toBeUndefined();
   });
 });
