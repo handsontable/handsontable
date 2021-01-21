0fe6279be67d5d815e7891e48091b721e3ef1614
Wojciech Szymański <141330+wszymanski@users.noreply.github.com>
2021-01-21 13:11:39 +0100

    Just small change in style #5082

--- test/e2e/Core_selection.spec.js
+++ test/e2e/Core_selection.spec.js
@@ -1832,11 +1832,9 @@ describe('Core_selection', () => {
           rowHeaders: true,
           colHeaders: true,
           cells(row) {
-            const cellProperties = {
+            return {
               disableVisualSelection: row === 1
             };
-
-            return cellProperties;
           },
           columns: [
             {},
