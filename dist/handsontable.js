b5cf5aad41bdfa1532947e30c4bf85485ccd16ba
Jan Siegel <js.ziggle@gmail.com>
2021-01-26 11:40:53 +0100

    8.3.0

--- dist/handsontable.js
+++ dist/handsontable.js
@@ -29,7 +29,7 @@
  * FROM USE OR INABILITY TO USE THIS SOFTWARE.
  * 
  * Version: 8.3.0
- * Release date: 27/01/2020 (built at 26/01/2021 10:49:31)
+ * Release date: 28/01/2020 (built at 26/01/2021 11:39:14)
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -3207,7 +3207,7 @@ function _injectProductInfo(key, element) {
 
   if (hasValidType || isNonCommercial || schemaValidity) {
     if (schemaValidity) {
-      var releaseDate = (0, _moment.default)("27/01/2020", 'DD/MM/YYYY');
+      var releaseDate = (0, _moment.default)("28/01/2020", 'DD/MM/YYYY');
       var releaseDays = Math.floor(releaseDate.toDate().getTime() / 8.64e7);
 
       var keyValidityDays = _extractTime(key);
@@ -39916,7 +39916,7 @@ Handsontable.Core = function (rootElement) {
 };
 
 Handsontable.packageName = 'handsontable';
-Handsontable.buildDate = "26/01/2021 10:49:31";
+Handsontable.buildDate = "26/01/2021 11:39:14";
 Handsontable.version = "8.3.0";
 Handsontable.languages = {
   dictionaryKeys: _registry.dictionaryKeys,
