52e488a5fcc13141cc76661a1f6a6fcc74bc2279
Jan Siegel <js.ziggle@gmail.com>
2021-02-10 12:03:33 +0100

    Modify the 'run-all' script with the yargs array parser.

--- scripts/run-all.mjs
+++ scripts/run-all.mjs
@@ -1,8 +1,8 @@
 /**
- * Run the provided command in all subprojects in a declared order..
+ * Run the provided command in all subprojects in a declared order.
  * For example:
  * `npm run all build`
- * will run the `build` command in all of the declared projects..
+ * will run the `build` command in all of the declared projects.
  */
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
@@ -12,7 +12,8 @@ import { spawnProcess } from './utils/index.mjs';
 // eslint-disable-next-line no-unused-expressions
 const argv = yargs(hideBin(process.argv))
   .alias('e', 'exclude')
-  .describe('e', 'Exclude a package from being run. Can be provided as a comma-separated list (with no spaces).')
+  .array('e')
+  .describe('e', 'Exclude a package (or packages) from being run.')
   .argv;
 
 (async() => {
@@ -26,7 +27,7 @@ const argv = yargs(hideBin(process.argv))
 
   /* eslint-disable no-await-in-loop,no-restricted-syntax */
   for (const project of ORDER) {
-    if (!(argv.exclude || '').split(',').includes(project)) {
+    if (!argv.exclude?.includes(project)) {
       await spawnProcess(`npm run in ${project} ${command} -- --if-present`);
     }
   }
