19c44316b7b6bf215d978783409db04b0126fcdb
Wojciech Szymański <141330+wszymanski@users.noreply.github.com>
2021-02-11 17:09:59 +0100

    Value population from an edited cell won't result in a console error (#7512)

--- src/plugins/hiddenRows/__tests__/editors.spec.js
+++ src/plugins/hiddenRows/__tests__/editors.spec.js
@@ -35,5 +35,59 @@ describe('HiddenRows', () => {
 
       expect($mainHolder.scrollTop()).toBe(startScrollTop);
     });
+
+    it('should populate value from an editor properly when there are some hidden rows & column header was selected', () => {
+      handsontable({
+        data: Handsontable.helper.createSpreadsheetData(5, 5),
+        colHeaders: true,
+        rowHeaders: true,
+        hiddenRows: {
+          rows: [0, 1],
+          indicators: true
+        }
+      });
+
+      const firstHeader = spec().$container.find('.ht_clone_top tr:eq(0) th:eq(1)');
+
+      simulateClick(firstHeader, 'LMB');
+
+      keyDownUp('enter');
+      keyDownUp('ctrl+enter');
+
+      expect(getData()).toEqual([
+        ['A3', 'B1', 'C1', 'D1', 'E1'],
+        ['A3', 'B2', 'C2', 'D2', 'E2'],
+        ['A3', 'B3', 'C3', 'D3', 'E3'],
+        ['A3', 'B4', 'C4', 'D4', 'E4'],
+        ['A3', 'B5', 'C5', 'D5', 'E5'],
+      ]);
+    });
+
+    it('should populate value from an editor properly when there are some hidden rows & corner was selected', () => {
+      handsontable({
+        data: Handsontable.helper.createSpreadsheetData(5, 5),
+        colHeaders: true,
+        rowHeaders: true,
+        hiddenRows: {
+          rows: [0, 1],
+          indicators: true
+        }
+      });
+
+      const corner = $('.ht_clone_top_left_corner .htCore').find('thead').find('th').eq(0);
+
+      simulateClick(corner, 'LMB');
+
+      keyDownUp('enter');
+      keyDownUp('ctrl+enter');
+
+      expect(getData()).toEqual([
+        ['A3', 'A3', 'A3', 'A3', 'A3'],
+        ['A3', 'A3', 'A3', 'A3', 'A3'],
+        ['A3', 'A3', 'A3', 'A3', 'A3'],
+        ['A3', 'A3', 'A3', 'A3', 'A3'],
+        ['A3', 'A3', 'A3', 'A3', 'A3'],
+      ]);
+    });
   });
 });
