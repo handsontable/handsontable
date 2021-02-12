012e89b577e9a1f7ca8be0db1b6100343844b532
Siemienik Pawel <siemienik.pawel@gmail.com>
2021-02-12 12:31:32 +0100

    Added Footer (https://github.com/handsontable/docs-md/pull/47)
    * Added Footer as is
    
    * Customize Footer

---
+++ src/theme/Footer/index.js
@@ -0,0 +1,109 @@
+/* eslint-disable */ // File swizzled from a docusaurus theme
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import React from 'react';
+import styles from './styles.module.css';
+
+function HotLogo() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg">
+      <use href="#handsontable-logo" />
+      <symbol viewBox="0 0 200.7 39.5" id="handsontable-logo">
+        <path d="M12.5,7.9H4.7V0H0v21.1h4.7V12h7.7v9.1h4.7V0h-4.7V7.9z M34.4,11.4c0-2.2-0.4-3.5-1.5-4.5c-1.2-1-3-1.6-5.3-1.6
+          c-4.3,0-6.5,1.7-6.8,5.2h4c0.2-1.2,1.1-1.8,2.7-1.8c1.5,0,2.3,0.5,2.3,1.4c0,0.6-0.3,0.9-1.4,1l-2.4,0.4c-4.3,0.7-6.1,2.2-6.1,5.1
+          s2.2,4.8,5.6,4.8c1.8,0,2.9-0.4,4.4-1.6c0.2,0.6,0.2,0.7,0.3,1.2h4.6c-0.5-0.8-0.6-1-0.6-2.6V11.4z M30,16.1
+          c-0.7,1.3-1.8,1.9-3.3,1.9c-1.4,0-2.3-0.6-2.3-1.6c0-0.9,0.7-1.5,2.3-1.8l1.6-0.3c0.8-0.2,1.2-0.3,1.6-0.5V16.1z M46.1,5.4
+          c-1.6,0-2.5,0.4-4.2,1.9V5.8h-4.4v15.3H42v-8.6c0-1.3,0.2-2,0.7-2.5c0.5-0.5,1.2-0.8,1.9-0.8c0.8,0,1.5,0.3,1.8,0.9
+          c0.4,0.6,0.5,1.2,0.5,2.7v8.3h4.5v-9.9C51.4,7.6,49.4,5.4,46.1,5.4z M64.5,7c-1.1-1.1-2.2-1.6-3.9-1.6c-4.1,0-6.8,3.2-6.8,8.1
+          c0,4.8,2.7,8,6.7,8c1.9,0,3.1-0.6,4.2-2.1v1.6H69V0h-4.5V7z M61.5,17.9c-2,0-3.2-1.7-3.2-4.4c0-2.7,1.3-4.4,3.2-4.4
+          c2,0,3.2,1.8,3.2,4.5C64.7,16.1,63.4,17.9,61.5,17.9z M80.4,11.7l-2.8-0.6c-1.4-0.3-1.9-0.6-1.9-1.2c0-0.8,0.9-1.3,2.1-1.3
+          c1.5,0,2.4,0.6,2.6,1.7h4.2c-0.4-3.2-2.8-5-6.7-5c-4.1,0-6.5,1.9-6.5,4.9c0,2.4,1.3,3.7,4.6,4.4l2.8,0.6c1.2,0.3,1.7,0.7,1.7,1.4
+          c0,0.9-0.9,1.5-2.3,1.5c-1.8,0-2.7-0.7-3-2.1h-4.2c0.3,3.4,2.9,5.4,7.1,5.4c4.2,0,6.9-2,6.9-5.2c0-1.2-0.4-2.4-1.1-3.1
+          C83.2,12.5,82.3,12.1,80.4,11.7z M94.6,5.4c-4.7,0-8.1,3.3-8.1,8c0,4.7,3.3,8.1,8.1,8.1c4.7,0,8.1-3.3,8.1-7.9
+          C102.7,8.7,99.5,5.4,94.6,5.4z M94.7,17.9c-2.3,0-3.6-1.6-3.6-4.5c0-2.8,1.3-4.5,3.6-4.5c2.3,0,3.6,1.6,3.6,4.5
+          C98.2,16.3,96.9,17.9,94.7,17.9z M113.7,5.4c-1.6,0-2.5,0.4-4.2,1.9V5.8h-4.4v15.3h4.5v-8.6c0-1.3,0.2-2,0.7-2.5
+          c0.5-0.5,1.2-0.8,1.9-0.8c0.8,0,1.5,0.3,1.8,0.9c0.4,0.6,0.5,1.2,0.5,2.7v8.3h4.5v-9.9C119,7.6,117,5.4,113.7,5.4z M127.2,1.2h-4.5
+          V6h-2.1v3.2h2.1v7c0,2.4,0.2,3.2,0.9,3.9c0.7,0.7,1.6,1,3.6,1h2.7v-3.4h-1.6c-1,0-1.1-0.2-1.1-2.6V9.2h2.7V6h-2.7V1.2z M145.5,11.4
+          c0-2.2-0.4-3.5-1.5-4.5c-1.2-1-3-1.6-5.3-1.6c-4.3,0-6.5,1.7-6.8,5.2h4c0.2-1.2,1.1-1.8,2.7-1.8c1.5,0,2.3,0.5,2.3,1.4
+          c0,0.6-0.3,0.9-1.4,1l-2.4,0.4c-4.3,0.7-6.1,2.2-6.1,5.1s2.2,4.8,5.6,4.8c1.8,0,2.9-0.4,4.4-1.6c0.2,0.6,0.2,0.7,0.3,1.2h4.6
+          c-0.5-0.8-0.6-1-0.6-2.6V11.4z M141.2,16.1c-0.7,1.3-1.8,1.9-3.3,1.9c-1.4,0-2.3-0.6-2.3-1.6c0-0.9,0.7-1.5,2.3-1.8l1.6-0.3
+          c0.8-0.2,1.2-0.3,1.6-0.5V16.1z M157.4,5.6c-1.5,0-2.7,0.4-4.2,1.5V0.2h-4.5v21.1h4.3v-1.7c1.2,1.6,2.4,2.2,4.2,2.1
+          c3.9,0,6.8-3.2,6.7-8.2C163.9,8.8,161.3,5.6,157.4,5.6z M156.3,18c-2,0-3.3-1.8-3.3-4.4c0-2.6,1.3-4.4,3.3-4.4c2,0,3.2,1.7,3.2,4.5
+          C159.5,16.3,158.2,18,156.3,18z M166.3,21.1h4.5V0h-4.5V21.1z M187.8,9.9c-1.1-2.9-3.6-4.5-6.8-4.5c-4.6,0-7.8,3.3-7.8,8.1
+          c0,4.8,3.1,8,7.8,8c3.6,0,6.5-1.9,7.3-4.9h-4.6c-0.6,0.9-1.5,1.4-2.7,1.4c-1.9,0-3.2-1.1-3.4-3.2h10.8
+          C188.6,12.6,188.4,11.2,187.8,9.9z M177.8,11.6c0.3-1.8,1.5-2.8,3.2-2.8c1.7,0,2.8,1,3,2.8H177.8z M190.9,26.1v9h9v-9H190.9z"
+        />
+      </symbol>
+    </svg>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className={`col col--3 ${styles.footerDescription}`}>
+            <HotLogo />
+            <p>Handsontable is a JavaScript data grid component for web applications.</p>
+            <p>
+              © 2012
+              -&nbsp;2021&nbsp;Handsoncode
+            </p>
+            <p>All trademarks belong to their respective owners.</p>
+            <p>
+              Handsontable documentation is licensed under
+              <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
+            </p>
+            <ul className="legal-links">
+              <li><a href="/terms-of-use">Terms of use</a></li>
+              <li><a href="/privacy">Privacy policy</a></li>
+            </ul>
+          </div>
+          <div className={`col col--2 col--offset-1 ${styles.footerLinks}`}>
+            <h4>WRAPPERS</h4>
+            <ul>
+              <li><a href="/docs/react" rel="noopener">React</a></li>
+              <li><a href="/docs/vue" rel="noopener">Vue</a></li>
+              <li><a href="/docs/angular" rel="noopener">Angular</a></li>
+            </ul>
+          </div>
+          <div className={`col col--2 ${styles.footerLinks}`}>
+            <h4>RESOURCES</h4>
+            <ul>
+              <li><a href="/docs/Core.html">API Reference</a></li>
+              <li><a href="/docs/tutorial-quick-start.html">Tutorials</a></li>
+              <li><a href="/knowledge-base">Knowledge base</a></li>
+              <li><a href="/blog/">Blog</a></li>
+              <li><a href="/branding">Branding</a></li>
+            </ul>
+          </div>
+          <div className={`col col--2 ${styles.footerLinks}`}>
+            <h4>SUPPORT</h4>
+            <ul>
+              <li><a href="//handsontable.com/contact?category=technical_support">Report an issue</a></li>
+              <li><a href="//github.com/handsontable/handsontable/issues" rel="noopener">GitHub issues</a></li>
+              <li><a href="//forum.handsontable.com" rel="noopener">Developers forum</a></li>
+            </ul>
+          </div>
+          <div className={`col col--2 ${styles.footerLinks}`}>
+            <h4>ABOUT</h4>
+            <ul>
+              <li><a href="/team">Our team</a></li>
+              <li><a href="/resellers">Resellers</a></li>
+              <li><a href="//twitter.com/handsontable" rel="noopener">Twitter</a></li>
+              <li><a href="/contact">Contact us</a></li>
+              <li><a href="//status.handsontable.com" rel="noopener">Uptime status</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
