a8e22a6d719a54ea5601d23329f2da74a902cc9e
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-11 10:34:36 +0100

    refactor: flatten component structure

--- examples/templates/angular/src/app/app.component.ts
+++ examples/templates/angular/src/app/app.component.ts
@@ -9,6 +9,18 @@ import Handsontable from 'handsontable';
 })
 export class AppComponent {
 
+  hotSettings: Handsontable.GridSettings = {
+    data: [
+        ['', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
+        ['2019', 10, 11, 12, 13],
+        ['2020', 20, 11, 14, 13],
+        ['2021', 30, 15, 12, 13]
+    ],
+    colHeaders: true,
+    rowHeaders: true,
+    licenseKey: 'non-commercial-and-evaluation'
+  };
+
   ngOnInit() {
     console.log(this.getDebugInfo());
   }
