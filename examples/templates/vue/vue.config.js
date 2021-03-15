3335db9bd712c2e0182b83919e2946fac63fa430
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-15 16:58:40 +0100

    change: extend vue build size limit

--- examples/templates/vue/vue.config.js
+++ examples/templates/vue/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    publicPath: '.'
+    publicPath: '.',
+    configureWebpack: {
+      performance: {
+        maxAssetSize: 2000000,
+        maxEntrypointSize: 2000000
+      }
+    }
 }
