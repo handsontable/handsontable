9f0de18576139f7b011b4e12e1bd5e836fec5970
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-03-18 10:43:32 +0100

    change: add passing tests to examples and fix CI testing URL

--- examples/scripts/code-examples.mjs
+++ examples/scripts/code-examples.mjs
@@ -191,8 +191,10 @@ switch (shellCommand) {
 
     examplesFolders.forEach((exampleDir, i) => {
       if (i < examplesFolders.length) {
+        const split = exampleDir.split('handsontable');
+        const splitLength = split.length;
         // this env is used in each `Smoke.spec.js` file inside code example directory
-        process.env.TEST_URL = `http://127.0.0.1:8080${exampleDir.split('handsontable')[1]}/dist`;
+        process.env.TEST_URL = `http://127.0.0.1:8080${split[splitLength - 1]}/dist`;
 
         runNpmCommandInExample(exampleDir, 'npm run jasmine');
       }
