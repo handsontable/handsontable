e5695c0d8bad8bb005f1027b4dfb43d3f88cee20
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-04-01 09:13:15 +0200

    The easiest way to sort checkboxes, without some edge cases #4047

---
+++ src/plugins/columnSorting/sortFunction/checkbox.js
@@ -0,0 +1,39 @@
+import { isEmpty } from '../../../helpers/mixed';
+import { DO_NOT_SWAP, FIRST_BEFORE_SECOND, FIRST_AFTER_SECOND } from '../sortService';
+
+/**
+ * Checkbox sorting compare function factory. Method get as parameters `sortOrder` and `columnMeta` and return compare function.
+ *
+ * @param {string} sortOrder Sort order (`asc` for ascending, `desc` for descending).
+ * @param {object} columnMeta Column meta object.
+ * @param {object} columnPluginSettings Plugin settings for the column.
+ * @returns {Function} The compare function.
+ */
+export function compareFunctionFactory(sortOrder, columnMeta, columnPluginSettings) {
+  const checkedTemplate = columnMeta.checkedTemplate;
+  const uncheckedTemplate = columnMeta.uncheckedTemplate;
+  
+  return function(value, nextValue) {
+    const { sortEmptyCells } = columnPluginSettings;
+
+    if (value === uncheckedTemplate && nextValue === checkedTemplate) {
+      if (sortOrder === 'asc') {
+        return FIRST_BEFORE_SECOND;
+      }
+      
+      return FIRST_AFTER_SECOND;
+    }
+
+    if (value === checkedTemplate && nextValue === uncheckedTemplate) {
+      if (sortOrder === 'asc') {
+        return FIRST_AFTER_SECOND;
+      }
+
+      return FIRST_BEFORE_SECOND;
+    }
+    
+    return DO_NOT_SWAP;
+  };
+}
+
+export const COLUMN_DATA_TYPE = 'checkbox';
