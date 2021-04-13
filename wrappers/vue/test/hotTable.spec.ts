67f033bb8e6038f7f4757eb709b131531919f36a
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-04-13 15:39:44 +0200

    Added extra test for regression #7548

--- wrappers/vue/test/hotTable.spec.ts
+++ wrappers/vue/test/hotTable.spec.ts
@@ -605,3 +605,42 @@ it('should be possible to pass props to the editor and renderer components', () 
 
   testWrapper.destroy();
 });
+
+describe('cooperation with NestedRows plugin', () => {
+  it('should display dataset properly #7548', async() => {
+    const testWrapper = mount(HotTable, {
+      propsData: {
+        data: [{
+          col1: 'parent1',
+          __children: [
+            {
+              col1: 'p1.c1',
+            }, {
+              col1: 'p1.c2',
+            }
+          ]
+        }, {
+          col1: 'parent2',
+          __children: [
+            {
+              col1: 'p2.c1',
+            }, {
+              col1: 'p2.c2',
+            }, {
+              col1: 'p2.c3',
+            }
+          ]
+        }],
+        rowHeaders: true,
+        colHeaders: true,
+        nestedRows: true,
+      }
+    });
+
+    expect(testWrapper.vm.hotInstance.countRows()).toEqual(7);
+
+    await Vue.nextTick();
+
+    expect(testWrapper.vm.hotInstance.countRows()).toEqual(7);
+  });
+});
