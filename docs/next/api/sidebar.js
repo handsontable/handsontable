ca0d9166ddcc73efc4aed53fa23d5a3d4d05034c
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-04-23 22:20:09 +0200

    FIX build

--- docs/next/api/sidebar.js
+++ docs/next/api/sidebar.js
@@ -23,7 +23,7 @@ module.exports = {
       path: `/${path.join(__dirname, '../').split('/').pop()}/api/plugins`,
       collapsable: false,
       children: fs.readdirSync(path.join(__dirname, './'))
-        .filter(f => ![...nonPublicPages, ...apiHighLevelPages].includes(f.split('.').shift()))
+        .filter(f => !['sidebar', ...nonPublicPages, ...apiHighLevelPages].includes(f.split('.').shift()))
     },
   ]
 };
