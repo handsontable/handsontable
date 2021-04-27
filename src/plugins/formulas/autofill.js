dcbaad93810287fe9b93b30ec3be107d703d6557
nikersify <nikersify@nikerino.com>
2021-04-27 17:42:26 +0200

    move autofill logic into a separate file

---
+++ src/plugins/formulas/autofill.js
@@ -0,0 +1,134 @@
+/**
+ * Registers on the formulas plugin instance.
+ *
+ * @param {object} pluginInstance The formulas plugin instance.
+ */
+export const registerAutofillHooks = (pluginInstance) => {
+  const lastAutofillSource = { value: undefined };
+
+  // Abuse the `modifyAutofillRange` hook to get the autofill start coordinates.
+  pluginInstance.addHook('modifyAutofillRange', (_, entireArea) => {
+    const [startRow, startCol, endRow, endCol] = entireArea;
+
+    lastAutofillSource.value = {
+      start: {
+        row: startRow,
+        col: startCol
+      },
+      end: {
+        row: endRow,
+        col: endCol
+      }
+    };
+  });
+
+  // Abuse pluginInstance hook to easily figure out the direction of the autofill
+  pluginInstance.addHook('beforeAutofillInsidePopulate', (index, direction, _input, _deltas, _, selected) => {
+    const autofillTargetSize = {
+      width: selected.col,
+      height: selected.row
+    };
+
+    const autofillSourceSize = {
+      width: Math.abs(lastAutofillSource.value.start.col - lastAutofillSource.value.end.col) + 1,
+      height: Math.abs(lastAutofillSource.value.start.row - lastAutofillSource.value.end.row) + 1
+    };
+
+    const paste = (
+      // The cell we're copy'ing to let HyperFormula adjust the references properly
+      sourceCellCoordinates,
+
+      // The cell we're pasting into
+      targetCellCoordinates
+    ) => {
+      pluginInstance.hyperformula.copy({
+        sheet: pluginInstance.hyperformula.getSheetId(pluginInstance.sheetName),
+        row: sourceCellCoordinates.row,
+        col: sourceCellCoordinates.col
+      }, 1, 1);
+
+      const [{ address }] = pluginInstance.hyperformula.paste({
+        sheet: pluginInstance.hyperformula.getSheetId(pluginInstance.sheetName),
+        row: targetCellCoordinates.row,
+        col: targetCellCoordinates.col
+      });
+
+      const value = pluginInstance.hyperformula.getCellSerialized(address);
+
+      return { value };
+    };
+
+    // Pretty much reimplements the logic from `src/plugins/autofill/autofill.js#fillIn`
+    switch (direction) {
+      case 'right': {
+        const targetCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row,
+          col: lastAutofillSource.value.start.col + index.col + autofillSourceSize.width
+        };
+
+        const sourceCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row,
+          col: (index.col % autofillSourceSize.width) + lastAutofillSource.value.start.col
+        };
+
+        return paste(sourceCellCoordinates, targetCellCoordinates);
+      }
+
+      case 'left': {
+        const targetCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row,
+          col: lastAutofillSource.value.start.col + index.col - autofillTargetSize.width
+        };
+
+        const fillOffset = autofillTargetSize.width % autofillSourceSize.width;
+
+        const sourceCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row,
+          col:
+          ((autofillSourceSize.width - fillOffset + index.col) %
+            autofillSourceSize.width) +
+          lastAutofillSource.value.start.col,
+        };
+
+        return paste(sourceCellCoordinates, targetCellCoordinates);
+      }
+
+      case 'down': {
+        const targetCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row + autofillSourceSize.height,
+          col: lastAutofillSource.value.start.col + index.col
+        };
+
+        const sourceCellCoordinates = {
+          row: (index.row % autofillSourceSize.height) + lastAutofillSource.value.start.row,
+          col: lastAutofillSource.value.start.col + index.col
+        };
+
+        return paste(sourceCellCoordinates, targetCellCoordinates);
+      }
+
+      case 'up': {
+        const targetCellCoordinates = {
+          row: lastAutofillSource.value.start.row + index.row - autofillTargetSize.height,
+          col: lastAutofillSource.value.start.col + index.col
+        };
+
+        const fillOffset = autofillTargetSize.height % autofillSourceSize.height;
+
+        const sourceCellCoordinates = {
+          row:
+          ((autofillSourceSize.height - fillOffset + index.row) %
+            autofillSourceSize.height) +
+          lastAutofillSource.value.start.row,
+          col: lastAutofillSource.value.start.col + index.col,
+        };
+
+        return paste(sourceCellCoordinates, targetCellCoordinates);
+      }
+
+      default: {
+        throw new Error('Unexpected direction parameter');
+      }
+    }
+  });
+};
--- src/plugins/formulas/formulas.js
+++ src/plugins/formulas/formulas.js
@@ -1,5 +1,6 @@
 import { HyperFormula } from 'hyperformula';
 import { BasePlugin } from '../base';
+import { registerAutofillHooks } from './autofill';
 
 export const PLUGIN_KEY = 'formulas';
 export const PLUGIN_PRIORITY = 260;
@@ -64,138 +65,7 @@ export class Formulas extends BasePlugin {
     this.addHook('afterRemoveRow', (...args) => this.onAfterRemoveRow(...args));
     this.addHook('afterRemoveCol', (...args) => this.onAfterRemoveCol(...args));
 
-    // Autofill hooks
-    {
-      // Scoped into this block instead of being on the whole class to prevent
-      // other places from messing with it.
-      const lastAutofillSource = { value: undefined };
-
-      // Abuse the `modifyAutofillRange` hook to get the autofill start coordinates.
-      this.addHook('modifyAutofillRange', (_, entireArea) => {
-        const [startRow, startCol, endRow, endCol] = entireArea;
-
-        lastAutofillSource.value = {
-          start: {
-            row: startRow,
-            col: startCol
-          },
-          end: {
-            row: endRow,
-            col: endCol
-          }
-        };
-      });
-
-      // Abuse this hook to easily figure out the direction of the autofill
-      this.addHook('beforeAutofillInsidePopulate', (index, direction, _input, _deltas, _, selected) => {
-        const autofillTargetSize = {
-          width: selected.col,
-          height: selected.row
-        };
-
-        const autofillSourceSize = {
-          width: Math.abs(lastAutofillSource.value.start.col - lastAutofillSource.value.end.col) + 1,
-          height: Math.abs(lastAutofillSource.value.start.row - lastAutofillSource.value.end.row) + 1
-        };
-
-        const paste = (
-          // The cell we're copy'ing to let HyperFormula adjust the references properly
-          sourceCellCoordinates,
-
-          // The cell we're pasting into
-          targetCellCoordinates
-        ) => {
-          this.hyperformula.copy({
-            sheet: this.hyperformula.getSheetId(this.sheetName),
-            row: sourceCellCoordinates.row,
-            col: sourceCellCoordinates.col
-          }, 1, 1);
-
-          const [{ address }] = this.hyperformula.paste({
-            sheet: this.hyperformula.getSheetId(this.sheetName),
-            row: targetCellCoordinates.row,
-            col: targetCellCoordinates.col
-          });
-
-          const value = this.hyperformula.getCellSerialized(address);
-
-          return { value };
-        };
-
-        // Pretty much reimplements the logic from `src/plugins/autofill/autofill.js#fillIn`
-        switch (direction) {
-          case 'right': {
-            const targetCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row,
-              col: lastAutofillSource.value.start.col + index.col + autofillSourceSize.width
-            };
-
-            const sourceCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row,
-              col: (index.col % autofillSourceSize.width) + lastAutofillSource.value.start.col
-            };
-
-            return paste(sourceCellCoordinates, targetCellCoordinates);
-          }
-
-          case 'left': {
-            const targetCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row,
-              col: lastAutofillSource.value.start.col + index.col - autofillTargetSize.width
-            };
-
-            const fillOffset = autofillTargetSize.width % autofillSourceSize.width;
-
-            const sourceCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row,
-              col:
-                ((autofillSourceSize.width - fillOffset + index.col) %
-                  autofillSourceSize.width) +
-                lastAutofillSource.value.start.col,
-            };
-
-            return paste(sourceCellCoordinates, targetCellCoordinates);
-          }
-
-          case 'down': {
-            const targetCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row + autofillSourceSize.height,
-              col: lastAutofillSource.value.start.col + index.col
-            };
-
-            const sourceCellCoordinates = {
-              row: (index.row % autofillSourceSize.height) + lastAutofillSource.value.start.row,
-              col: lastAutofillSource.value.start.col + index.col
-            };
-
-            return paste(sourceCellCoordinates, targetCellCoordinates);
-          }
-
-          case 'up': {
-            const targetCellCoordinates = {
-              row: lastAutofillSource.value.start.row + index.row - autofillTargetSize.height,
-              col: lastAutofillSource.value.start.col + index.col
-            };
-
-            const fillOffset = autofillTargetSize.height % autofillSourceSize.height;
-
-            const sourceCellCoordinates = {
-              row:
-                ((autofillSourceSize.height - fillOffset + index.row) %
-                  autofillSourceSize.height) +
-                lastAutofillSource.value.start.row,
-              col: lastAutofillSource.value.start.col + index.col,
-            };
-
-            return paste(sourceCellCoordinates, targetCellCoordinates);
-          }
-
-          default: {
-            throw new Error('Unexpected direction parameter');
-          }
-        }
-      });
-    }
+    registerAutofillHooks(this);
 
     super.enablePlugin();
   }
