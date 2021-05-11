b65e63b02ece6a234c17de5d492644650ead7c9e
Siemienik Pawel <siemienik.pawel@gmail.com>
2021-05-11 09:26:00 +0200

    New documentation: added tabs for code examples and preview (#7996)
    * ADD Tabs for HTML, CSS, Preview, and code. It allows us additional tabs by using `<tab name="Title">` in Markdown.
    FIX remove caching tokens length before iterate through the tokens, what enables adding tabs.
    FIX eos error in jsfiddle
    ADD --css flag
    ADD custom styles now are provided into jsfiddle

--- docs/.vuepress/containers/examples/jsfiddle.js
+++ docs/.vuepress/containers/examples/jsfiddle.js
@@ -2,17 +2,15 @@ const JSFIDDLE_ENDPOINT = 'https://jsfiddle.net/api/post/library/pure/';
 
 const { getDependencies } = require('../../handsontable-manager');
 
-const getCss = (version, preset) => {
-  return getDependencies(version, preset).reduce((p, c) =>
-    p +
-    (c[0] ? `<script src="${c[0]}"></script>\n` : '') +
-    (c[2] ? `<link type="text/css" rel="stylesheet" href="${c[2]}" /> \n` : ''),
-  '</style><!-- Ugly Hack due to jsFiddle issue -->\n');
-};
-
-const jsfiddle = (id, html, code, version, preset) => {
+const jsfiddle = (id, html, code, css, version, preset) => {
   const isBabelPanel = preset.includes('react') || preset.includes('vue');
   const isAngularPanel = preset.includes('angular');
+  const imports = getDependencies(version, preset).reduce(
+    (p, c) =>
+      p +
+      (c[0] ? `<script src="${c[0]}"></script>\n` : '') +
+      (c[2] ? `<link type="text/css" rel="stylesheet" href="${c[2]}" /> \n` : ''),
+    '');
 
   return `
     <form
@@ -25,8 +23,11 @@ const jsfiddle = (id, html, code, version, preset) => {
       <input type="text" name="title" readOnly value="Handsontable example" />
       <input type="text" name="wrap" readOnly value="d" />
       <textarea name="js" readOnly v-pre>${code}</textarea>
-      <textarea name="html" readOnly v-pre>${html}</textarea>
-      <textarea name="css" readOnly>${getCss(version, preset)}</textarea>
+      <textarea name="html" readOnly v-pre>
+        ${imports}
+        ${html}
+      </textarea>
+      <textarea name="css" readOnly>${css}</textarea>
       ${isBabelPanel ? '<input type="text" name="panel_js" value="3" readOnly>' : ''}
       ${isAngularPanel ? '<input type="text" name="panel_js" value="4" readOnly>' : ''}
   }
@@ -38,6 +39,5 @@ const jsfiddle = (id, html, code, version, preset) => {
 };
 
 module.exports = {
-  getCss,
   jsfiddle
 };
