30c68df7e4b9800f889076cacb5de5a61c558e69
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-05-17 12:34:10 +0200

    Saving progress - small refactor

--- docs/.vuepress/components/HelloWorld.vue
+++ docs/.vuepress/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div> 
     <tabs :options="{ useUrlFragment: false }"  @changed="tabClicked">
       <tab name="TypeScript" id="typescript">
-        <iframe src="https://codesandbox.io/embed/handsontable-typescript-data-grid-hello-world-app-145es?fontsize=14&hidenavigation=1&theme=dark&view=split&runonclick=1" 
+        <iframe v-if="selected === 'typescript'" src="https://codesandbox.io/embed/handsontable-typescript-data-grid-hello-world-app-145es?fontsize=14&hidenavigation=1&theme=dark&view=split&runonclick=1" 
           style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;" 
           title="Handsontable React Data Grid - Hello World App"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
@@ -10,7 +10,7 @@
         </iframe>
       </tab>
       <tab name="React" id="react">
-        <iframe src="https://codesandbox.io/embed/handsontable-react-data-grid-hello-world-app-yt46w?fontsize=14&hidenavigation=1&theme=dark&view=split"
+        <iframe v-if="selected === 'react'" src="https://codesandbox.io/embed/handsontable-react-data-grid-hello-world-app-yt46w?fontsize=14&hidenavigation=1&theme=dark&view=split&runonclick=1"
           style="width:100%; height:500px; border:0; border-radius: 4px; overflow:hidden;"
           title="Handsontable React Data Grid - Hello World App"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
