74eb99aa5a932b006b19fa19f7b071686fe60838
Jan Siegel <js.ziggle@gmail.com>
2021-05-27 15:52:48 +0200

    - Change link in the Formulas class description. - Add test cases.  #8110

--- src/plugins/formulas/formulas.js
+++ src/plugins/formulas/formulas.js
@@ -29,7 +29,7 @@ Hooks.getSingleton().register('afterFormulasValuesUpdate');
 
 /**
  * This plugin allows you to perform Excel-like calculations in your business applications. It does it by an
- * integration with our other product, [HyperFormula](https://handsontable.github.io/hyperformula/), which is a
+ * integration with our other product, [HyperFormula](https://github.com/handsontable/hyperformula/), which is a
  * powerful calculation engine with an extensive number of features.
  *
  * @plugin Formulas
