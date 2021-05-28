a91242a626450c2e9a5f001dad454c3c9e948216
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-05-28 15:44:47 +0200

    One more comment #8078

--- src/plugins/formulas/formulas.js
+++ src/plugins/formulas/formulas.js
@@ -162,7 +162,9 @@ export class Formulas extends BasePlugin {
 
     this.addHook('beforeAutofill', autofillHooks.beforeAutofill);
     this.addHook('afterAutofill', autofillHooks.afterAutofill);
+    // Handling undo actions just using HyperFormula's UndoRedo mechanism 
     this.addHook('beforeUndo', () => this.engine.undo());
+    // Handling redo actions just using HyperFormula's UndoRedo mechanism 
     this.addHook('beforeRedo', () => this.engine.redo());
 
     this.#engineListeners.forEach(([eventName, listener]) => this.engine.on(eventName, listener));
