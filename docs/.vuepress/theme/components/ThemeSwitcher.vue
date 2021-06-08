d422671504882dee8e20bfd16e2a3b9a3e4a91cc
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-06-08 16:38:15 +0200

    refactor toggling the css class

--- docs/.vuepress/theme/components/ThemeSwitcher.vue
+++ docs/.vuepress/theme/components/ThemeSwitcher.vue
@@ -3,20 +3,44 @@
 </template>
 
 <script>
-const CLASS_THEME_DARK = 'theme-dark'
+const CLASS_THEME_DARK = 'theme-dark';
+const DEFAULT_THEME_CLASS = 'theme-light';
+
+const getBodyElement = () => {
+  return document.querySelector('body');
+}
+
+const getBodyThemeClass = () => {
+  return getBodyElement().classList.contains(CLASS_THEME_DARK) ? CLASS_THEME_DARK : DEFAULT_THEME_CLASS;
+}
+
+const toggleClassOnBody = () => {
+  const body = getBodyElement();
+  const isDefaultTheme = body.classList.contains(DEFAULT_THEME_CLASS);
+
+  body.classList.remove(isDefaultTheme ? DEFAULT_THEME_CLASS : CLASS_THEME_DARK);
+  body.classList.add(isDefaultTheme ? CLASS_THEME_DARK : DEFAULT_THEME_CLASS);
+}
+
+const setClassOnBody = (className) => {
+  getBodyElement().classList.add(className);
+}
 
 export default {
   name: 'ThemeSwitcher',
   methods:{
     toggleTheme(){
-      document.querySelector('body').classList.toggle(CLASS_THEME_DARK) //todo: alert nie działa pod wszystkimi przeglądarkami, sprawdić !
-      this.current = document.querySelector('body').classList.contains(CLASS_THEME_DARK) ? 'dark' : 'light';
+      toggleClassOnBody();
+      this.current = getBodyThemeClass();
     }
   },
   data(){
     return {
-      current: document.querySelector('body').classList.contains(CLASS_THEME_DARK) ? 'dark' : 'light' //todo duplicated code, refactor
+      current: DEFAULT_THEME_CLASS
     }
+  },
+  beforeMount() {
+    setClassOnBody(DEFAULT_THEME_CLASS);
   }
 };
 </script>
