e35f00cbe19920c67384dccd4e8ae8ad357e2b18
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-06-11 15:47:26 +0200

    Applied refactor suggestion mentioned by @Siemienik within PR #8173

---
+++ docs/.vuepress/components/CodeSandboxIframe.vue
@@ -0,0 +1,23 @@
+<template>
+  <iframe
+    :src="srcUrl"
+    :title="title"
+    style="width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden;"
+    allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; 
+      midi; payment; usb; vr; xr-spatial-tracking"
+    sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+  ></iframe>
+</template>
+
+<script>
+export default {
+  name: 'CodeSandboxIframe',
+  props: ['id', 'title'],
+  computed: {
+    srcUrl() {
+      return 'https://codesandbox.io/embed/' + this.id + 
+        '?fontsize=14&hidenavigation=1&theme=dark&view=preview&runonclick=1';
+    }
+  }
+}
+</script>
--- docs/.vuepress/components/HelloWorld.vue
+++ docs/.vuepress/components/HelloWorld.vue
@@ -1,29 +1,9 @@
 <template>
-  <div> 
-    <tabs :options="{ useUrlFragment: false }"  @changed="tabClicked">
-      <tab name="JavaScript" id="js">
-        <iframe v-if="selected === 'js'" :src="jsSrc"
-          :style="styleAttribute"
-          :allow="allowAttribute"
-          :sandbox="sandboxAttribute"
-          title="Handsontable JavaScript Data Grid - Hello World App"
-        ></iframe>
-      </tab>
-      <tab name="TypeScript" id="typescript">
-        <iframe v-if="selected === 'typescript'" :src="tsSrc" 
-          :style="styleAttribute"
-          :allow="allowAttribute"
-          :sandbox="sandboxAttribute"
-          title="Handsontable TypeScript Data Grid - Hello World App"
-        ></iframe>
-      </tab>
-      <tab name="React" id="react">
-        <iframe v-if="selected === 'react'" :src="reactSrc"
-          :style="styleAttribute"
-          :allow="allowAttribute"
-          :sandbox="sandboxAttribute"
-          title="Handsontable React Data Grid - Hello World App"
-        ></iframe>
+  <div>
+    <tabs :options="{ useUrlFragment: false }" @changed="tabClicked">
+      <tab v-for="item in items" :name="item.name" :id="item.tabId">
+        <CodeSandboxIframe :id="item.codeSandboxId" :title="item.title" v-if="isSelected(item.tabId)">
+        </CodeSandboxIframe>
       </tab>
       <tab name="Vue" id="vue" :is-disabled="true"></tab>
       <tab name="Angular" id="angular" :is-disabled="true"></tab>
@@ -32,28 +12,42 @@
 </template>
 
 <script>
-const hrefStart='https://codesandbox.io/embed/';
-const hrefEnd='?fontsize=14&hidenavigation=1&theme=dark&view=preview&runonclick=1';
+import CodeSandboxIframe from './CodeSandboxIframe';
 
 export default {
   name: 'HelloWorld',
+  components: { CodeSandboxIframe },
   data() {
-    const getUrl = (id) => `${hrefStart}${id}${hrefEnd}`;
-
     return {
       selected: 'js',
-      jsSrc: getUrl('handsontable-javascript-data-grid-hello-world-app-dzx8f'),
-      tsSrc: getUrl('handsontable-typescript-data-grid-hello-world-app-145es'),
-      reactSrc: getUrl('handsontable-react-data-grid-hello-world-app-yt46w'),
-      styleAttribute: 'width: 100%; height: 500px; border: 0; border-radius: 4px; overflow: hidden;',
-      allowAttribute: 'accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; ' +
-        'microphone; midi; payment; usb; vr; xr-spatial-tracking',
-      sandboxAttribute: 'allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts',
+      items: [
+        { 
+          name: 'JavaScript',
+          tabId: 'js',
+          codeSandboxId: 'handsontable-javascript-data-grid-hello-world-app-dzx8f', 
+          title: 'Handsontable JavaScript Data Grid - Hello World App'
+        },
+        {
+          name: 'TypeScript',
+          tabId: 'typescript',
+          codeSandboxId: 'handsontable-typescript-data-grid-hello-world-app-145es',
+          title: 'Handsontable TypeScript Data Grid - Hello World App'
+        },
+        {
+          name: 'React',
+          tabId: 'react',
+          codeSandboxId: 'handsontable-react-data-grid-hello-world-app-yt46w',
+          title: 'Handsontable React Data Grid - Hello World App'
+        }
+      ]
     }
   },
   methods:{
     tabClicked(event) {
       this.selected = event.tab.id;
+    },
+    isSelected(id) {
+      return this.selected === id;
     }
   }
 
