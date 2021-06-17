91584254d4a863ba35d6abe8625373f0b5b652cf
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-06-17 16:11:49 +0200

    toggle logo

--- docs/.vuepress/theme/components/ThemeSwitcher.vue
+++ docs/.vuepress/theme/components/ThemeSwitcher.vue
@@ -1,39 +1,47 @@
 <template>
   <label  id="switch" class="switch">
-      <input type="checkbox" v-on:change="toggleTheme" id="slider" :checked="isLightTheme">
+      <input type="checkbox" v-on:change="toggleTheme" id="slider" :checked="!isDarkTheme">
       <span class="slider round"></span>
   </label>
 </template>
 
 <script>
 const CLASS_THEME_DARK = 'theme-dark';
+const SRC_LOGO_DEFAULT = '/img/handsontable-logo.svg'
+const SRC_LOGO_WHITE = '/img/handsontable-logo-white.svg';
 
-const getHTMLElement = () => {
-  return document.querySelector('html');
+const getDomElement = (selector) => {
+  return document.querySelector(selector);
 }
 
 const isDarkTheme = () => {
-  const htmlEl = getHTMLElement();
+  const htmlEl = getDomElement('html');
 
   return htmlEl.classList.contains(CLASS_THEME_DARK);
 }
 
-const toggleClassOnHTML = () => {
-  const htmlEl = getHTMLElement();
+const toggleClassOnHTML = (htmlDomElement) => {
+  isDarkTheme() ? htmlDomElement.classList.remove(CLASS_THEME_DARK) : htmlDomElement.classList.add(CLASS_THEME_DARK);
+}
 
-  isDarkTheme() ? htmlEl.classList.remove(CLASS_THEME_DARK) : htmlEl.classList.add(CLASS_THEME_DARK);
+const changeLogo = (isDarkTheme) => {
+  const logo = getDomElement('header.navbar .logo');
+  logo.src = isDarkTheme ? SRC_LOGO_WHITE : SRC_LOGO_DEFAULT;
 }
 
 export default {
   name: 'ThemeSwitcher',
   methods:{
     toggleTheme(){
-      toggleClassOnHTML();
+      const htmlEl = getDomElement('html');
+      toggleClassOnHTML(htmlEl);
+      this.isDarkTheme = isDarkTheme();
+      changeLogo(this.isDarkTheme);
     }
   },
   data() {
     return {
-      isLightTheme: !isDarkTheme()
+      isDarkTheme: isDarkTheme()
     }
   }
 };
