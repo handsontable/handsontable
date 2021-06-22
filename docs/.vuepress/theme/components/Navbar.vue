01fe18a5a55d7ed4008011ced0467587d81022ce
Oskar Kowalów <kowalow.oskar@gmail.com>
2021-06-22 16:34:18 +0200

    move component styles back to the component files

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -106,3 +106,50 @@ function css(el, property) {
   return win.getComputedStyle(el, null)[property];
 }
 </script>
+
+<style lang="stylus">
+$navbar-vertical-padding = 0.65rem
+$navbar-horizontal-padding = 1.5rem
+
+.navbar
+  padding $navbar-vertical-padding $navbar-horizontal-padding
+  line-height $navbarHeight - 1.4rem
+  z-index 2000
+  border-color: #e9eef2
+  a, span, img
+    display inline-block
+    font-weight 500
+    font-size: 14px
+  .home-link
+    margin-right 2rem
+    position relative
+    top .6rem
+    @media (max-width: $MQMobile) {
+      margin-right 1.5rem
+      top .4rem
+    }
+  #handsontable-logo
+    height $navbarHeight - 2.2rem
+    @media (max-width: $MQMobile) {
+      height 1rem
+    }
+  .links
+    box-sizing border-box
+    background-color white
+    white-space nowrap
+    font-size 0.9rem
+    position absolute
+    right $navbar-horizontal-padding
+    top $navbar-vertical-padding
+    display flex
+    align-items center
+    .search-box
+      flex: 0 0 auto
+      vertical-align top
+@media (max-width: $MQMobile)
+  .navbar
+    padding-left 4rem
+    .can-hide
+      display none
+
+</style>
--- docs/.vuepress/theme/components/ThemeSwitcher.vue
+++ docs/.vuepress/theme/components/ThemeSwitcher.vue
@@ -69,3 +69,114 @@ export default {
   }
 };
 </script>
+
+<style lang="stylus">
+.navbar {
+  .theme-switcher-nav-item {
+    display none !important
+  }
+
+  #switch {
+    @media (max-width: $MQMobile) {
+      display none
+    }
+  }
+}
+
+.sidebar {
+  @media (max-width: $MQMobile) {
+    .theme-switcher-nav-item {
+      display block
+
+      #switch {
+        display inline-block
+        top 0
+        left 0
+      }
+    }
+  }
+}
+
+/* The switch - the box around the slider */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 40px;
+  height: 22px;
+  margin-right 1.5rem
+  top: 8px;
+  left: -9px;
+
+  @media (max-width: $MQNavbarNarrow) {
+    left: -19px;
+    margin 0
+  }
+}
+
+/* Hide default HTML checkbox */
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+/* The slider */
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #ccc;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 26px;
+  width: 26px;
+  left: 0px;
+  bottom: 4px;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  -webkit-transition: 0.4s;
+  transition: 0.4s;
+  box-shadow: 0 0px 3px #2020203d;
+  background: #ffffff url('/img/light-theme-icon.svg');
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+input:checked + .slider {
+  background-color: #2196f3;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #2196f3;
+}
+
+input:checked + .slider:before {
+  -webkit-transform: translateX(16px);
+  -ms-transform: translateX(16px);
+  transform: translateX(16px);
+  background: #ffffff url('/img/dark-theme-icon.svg');
+  background-size: 70%;
+  background-repeat: no-repeat;
+  background-position: center;
+}
+
+/* Rounded sliders */
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+</style>
