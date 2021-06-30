d9d1972ef22de276251fe8e1e632aec1faf179f8
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-06-30 13:00:05 +0200

    reorganize

--- docs/.vuepress/tools/jsdoc-convert/jsdoc.js
+++ docs/.vuepress/tools/jsdoc-convert/jsdoc.js
@@ -102,75 +102,7 @@ editLink: false
 `;
 };
 
-/// post processing after markdown was generated
-const fixLinks = text => text
-  .replace(/\[([^\[]*?)]\(([^:@]*?)(#[^#]*?)?\)/g, '[$1](./$2/$3)') // @see https://regexr.com/603ja
-  .replace(/\.\/\//g, '');
-
-const clearEmptyOptionHeaders = text => text.replace(/## Options\n## Members/g, '## Members');
-const clearEmptyMembersHeaders = text => text.replace(/## Members\n## Methods/g, '## Methods');
-const clearEmptyFunctionsHeaders = text => text
-  .replace(/(## Methods\n)+$/g, '\n')
-  .replace(/(## Methods\n## Methods\n\n## Description)/g, '## Description');
-
-const fixTypes = text => text.replace(
-  /(::: signame |\*\*Returns\*\*:|\*\*See\*\*:|\*\*Emits\*\*:)( ?[^\n-]*)/g,
-  (_, part, signame) => {
-    let suffix = '';
-    let prefix = part;
-
-    if (part === '::: signame ') {
-      prefix = '_';
-      suffix = '_';
-    }
-
-    return prefix + signame
-      .replace(/([^\w`\[#])(`)?(IndexMapper)(#\w*)?(`)?/g, '$1[$2$3$4$5](./index-mapper/$4)')
-      .replace(/([^\w`\[#])(`)?(Handsontable|Core)(#\w*)?(`)?/g, '$1[$2$3$4$5](./core/$4)')
-      .replace(/([^\w`\[#])(`)?(Hooks)((#)(event:)?(\w*))?(`)?/g, '$1[$2$3$4$8](./hooks/$5$7)')
-      .replace(/([^\w`\[#])(`)?(BaseEditor)(#\w*)?(`)?/g, '$1[$2$3$4$5](./base-editor/$4)')
-      .replace(/([^\w`\[#])(`)?(CellCoords)(#\w*)?(`)?/g, '$1[$2$3$4$5](./coords/$4)')
-      .replace(/([^\w`\[#])(`)?(FocusableWrapper)(#\w*)?(`)?/g, '$1[$2$3$4$5](./focusable-element/$4)')
-      .replace(/\.</g, '&lt;')
-      .replace(/>/g, '&gt;')
-      .replace(/`\\\*`/, '`*`')
-      + suffix;
-  }
-);
-
-const fixCategories = text => text.replace(
-  /(\*\*Category\*\*: ?)([^\n- ]*)/g,
-  (_, part, signame) => `${part}[${signame}](${genSeoPermalink(signame).replace('-', '../')})`
-);
-
-const unescapeRedundant = text => text
-  .replace(/`[^`\n]*`/g, m => // get all inline codes
-    m.replace(/\&lt;/g, '<')
-      .replace(/\&gt;/g, '>')
-      .replace(/\.</g, '<')
-      .replace(/\\\*/g, '*')
-      .replace(/\\_/g, '_')
-  )
-  // fix randomly added quota to @default tag.
-  .replace(/\*\*Default\*\*: <code>&quot;((undefined)|(false)|(true))&quot;<\/code>/g, '**Default**: <code>$1</code>')
-  // remove redundant dot, which eslint enforce to add after list closing tag.
-  .replace(/<\/ul>\./g, '</ul>')
-  .replace(/&quot;&#x27;/g, '"')
-  .replace(/&#x27;&quot;/g, '"');
-
-const postProcessors = [
-  fixLinks,
-  clearEmptyOptionHeaders,
-  clearEmptyMembersHeaders,
-  clearEmptyFunctionsHeaders,
-  fixTypes,
-  fixCategories,
-  unescapeRedundant
-];
-
-const postProcess = initialText => postProcessors.reduce((text, postProcessor) => postProcessor(text), initialText);
-
-/// post processing before markdown will be generated and after jsdoc was parsed
+/// pre processing before markdown will be generated and after jsdoc was parsed
 const sort = data => data.sort((m, p) => {
   if (m.kind === 'constructor' || m.kind === 'class') return -1;
 
@@ -239,6 +171,74 @@ const preProcessors = [
 ];
 
 const preProcess = initialData => preProcessors.reduce((data, preProcessor) => preProcessor(data), initialData);
+
+/// post processing after markdown was generated
+const fixLinks = text => text
+  .replace(/\[([^\[]*?)]\(([^:@]*?)(#[^#]*?)?\)/g, '[$1](./$2/$3)') // @see https://regexr.com/603ja
+  .replace(/\.\/\//g, '');
+
+const clearEmptyOptionHeaders = text => text.replace(/## Options\n## Members/g, '## Members');
+const clearEmptyMembersHeaders = text => text.replace(/## Members\n## Methods/g, '## Methods');
+const clearEmptyFunctionsHeaders = text => text
+  .replace(/(## Methods\n)+$/g, '\n')
+  .replace(/(## Methods\n## Methods\n\n## Description)/g, '## Description');
+
+const fixTypes = text => text.replace(
+  /(::: signame |\*\*Returns\*\*:|\*\*See\*\*:|\*\*Emits\*\*:)( ?[^\n-]*)/g,
+  (_, part, signame) => {
+    let suffix = '';
+    let prefix = part;
+
+    if (part === '::: signame ') {
+      prefix = '_';
+      suffix = '_';
+    }
+
+    return prefix + signame
+      .replace(/([^\w`\[#])(`)?(IndexMapper)(#\w*)?(`)?/g, '$1[$2$3$4$5](./index-mapper/$4)')
+      .replace(/([^\w`\[#])(`)?(Handsontable|Core)(#\w*)?(`)?/g, '$1[$2$3$4$5](./core/$4)')
+      .replace(/([^\w`\[#])(`)?(Hooks)((#)(event:)?(\w*))?(`)?/g, '$1[$2$3$4$8](./hooks/$5$7)')
+      .replace(/([^\w`\[#])(`)?(BaseEditor)(#\w*)?(`)?/g, '$1[$2$3$4$5](./base-editor/$4)')
+      .replace(/([^\w`\[#])(`)?(CellCoords)(#\w*)?(`)?/g, '$1[$2$3$4$5](./coords/$4)')
+      .replace(/([^\w`\[#])(`)?(FocusableWrapper)(#\w*)?(`)?/g, '$1[$2$3$4$5](./focusable-element/$4)')
+      .replace(/\.</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/`\\\*`/, '`*`')
+      + suffix;
+  }
+);
+
+const fixCategories = text => text.replace(
+  /(\*\*Category\*\*: ?)([^\n- ]*)/g,
+  (_, part, signame) => `${part}[${signame}](${genSeoPermalink(signame).replace('-', '../')})`
+);
+
+const unescapeRedundant = text => text
+  .replace(/`[^`\n]*`/g, m => // get all inline codes
+    m.replace(/\&lt;/g, '<')
+      .replace(/\&gt;/g, '>')
+      .replace(/\.</g, '<')
+      .replace(/\\\*/g, '*')
+      .replace(/\\_/g, '_')
+  )
+  // fix randomly added quota to @default tag.
+  .replace(/\*\*Default\*\*: <code>&quot;((undefined)|(false)|(true))&quot;<\/code>/g, '**Default**: <code>$1</code>')
+  // remove redundant dot, which eslint enforce to add after list closing tag.
+  .replace(/<\/ul>\./g, '</ul>')
+  .replace(/&quot;&#x27;/g, '"')
+  .replace(/&#x27;&quot;/g, '"');
+
+const postProcessors = [
+  fixLinks,
+  clearEmptyOptionHeaders,
+  clearEmptyMembersHeaders,
+  clearEmptyFunctionsHeaders,
+  fixTypes,
+  fixCategories,
+  unescapeRedundant
+];
+
+const postProcess = initialText => postProcessors.reduce((text, postProcessor) => postProcessor(text), initialText);
 
 /// jsdoc2md integration
 const fromJsdoc = file => jsdoc2md.getTemplateDataSync({
