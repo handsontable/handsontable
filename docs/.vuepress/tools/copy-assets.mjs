01c834eeaa4b5613c3b0c8dbd0ee24dcc71823d4
budnix <571316+budnix@users.noreply.github.com>
2021-07-13 10:54:39 +0200

    Add local HoT languages file for "next" docs version
    Issue: #8421

--- docs/.vuepress/tools/copy-assets.mjs
+++ docs/.vuepress/tools/copy-assets.mjs
@@ -1,17 +1,22 @@
 import path from 'path';
-import { copyFileSync } from 'fs';
+import fs from 'fs';
 
-const TARGET_PATH = './.vuepress/public/';
+const TARGET_PATH = './.vuepress/public/handsontable/';
 const filesToMove = [
-  ['../dist/handsontable.full.css', 'handsontable.css'],
-  ['../dist/handsontable.full.js', 'handsontable.js'],
+  ['../dist/handsontable.full.css', 'handsontable.full.css'],
+  ['../dist/handsontable.full.js', 'handsontable.full.js'],
+  ['../dist/languages/all.js', 'languages/all.js'],
 ];
 
 filesToMove.forEach((fileSchema) => {
   const [from, target] = fileSchema;
+  const resolvedFrom = path.resolve(from);
+  const resolvedTarget = path.resolve(`${TARGET_PATH}/${target}`);
+  const dirName = path.dirname(resolvedTarget);
 
-  copyFileSync(
-    path.resolve(from),
-    path.resolve(`${TARGET_PATH}/${target}`)
-  );
+  if (!fs.existsSync(dirName)) {
+    fs.mkdirSync(dirName);
+  }
+
+  fs.copyFileSync(resolvedFrom, resolvedTarget);
 });
