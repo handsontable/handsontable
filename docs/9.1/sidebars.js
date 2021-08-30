6a83c579e577fceed345e46ca5300dd8dc06057d
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-08-30 15:46:07 +0200

    Remove temporary 9.1 version after tests were finished

--- docs/9.1/sidebars.js
+++
@@ -1,7 +0,0 @@
-const api = require('./api/sidebar').sidebar;
-const guides = require('./guides/sidebar').sidebar;
-const examples = require('./examples/sidebar').sidebar;
-
-module.exports = {
-  api, guides, examples
-};
