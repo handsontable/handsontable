379306fce63b40866f88488cd5759ac24aee1dcc
budnix <571316+budnix@users.noreply.github.com>
2021-09-22 13:30:25 +0200

    WoT change

--- src/3rdparty/walkontable/src/core.js
+++ src/3rdparty/walkontable/src/core.js
@@ -24,6 +24,7 @@ class Walkontable {
 
     // this is the namespace for global events
     this.guid = `wt_${randomString()}`;
+    this.guid2 = `wt_${randomString()}`;
     this.rootDocument = settings.table.ownerDocument;
     this.rootWindow = this.rootDocument.defaultView;
 
