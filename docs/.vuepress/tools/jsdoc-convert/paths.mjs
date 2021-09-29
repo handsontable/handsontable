126114111e4f6d992d35edfbbf7084b431cf637f
Jan Siegel <js.ziggle@gmail.com>
2021-09-29 16:20:32 +0200

    Fix the lint errors.

--- docs/.vuepress/tools/jsdoc-convert/paths.mjs
+++ docs/.vuepress/tools/jsdoc-convert/paths.mjs
@@ -4,13 +4,13 @@ import { fileURLToPath } from 'url';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export const buildPathsDeterminants = ({ pathToSource, pathToDist }) => {
-  const source = files => {
+  const source = (files) => {
     const sourceArray = [];
 
     files.forEach(file => sourceArray.push(path.join(__dirname, pathToSource, file)));
 
     return sourceArray;
-  }
+  };
 
   const flat = file => file.split('/').pop();
 
