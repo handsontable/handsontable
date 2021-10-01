e54d140c1fbd2bc743845029c604d403823b847f
Siemienik Pawel <siemienik.pawel@gmail.com>
2021-10-01 09:34:44 +0200

    Fix troubles with linking (#8793)
    resolves: https://github.com/handsontable/handsontable/issues/8789

--- docs/10.0/api/sidebar.js
+++ docs/10.0/api/sidebar.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { getBuildDocsVersion, getLatestVersion } = require('../../.vuepress/helpers');
 
 const apiHighLevelPages = [
   'introduction',
@@ -15,12 +16,10 @@ const plugins = fs.readdirSync(path.join(__dirname, './'))
     return file.includes('hotPlugin: true\n');
   }).map(fileName => fileName.split('.').shift());
 
-const { getLatestVersion } = require('../../.vuepress/helpers');
-
 const getUrlVersionPart = () => {
   const version = path.resolve(__dirname, '../').split('/').pop();
 
-  return getLatestVersion() === version ? '' : `/${version}`;
+  return getLatestVersion() === version || getBuildDocsVersion() ? '' : `/${version}`;
 };
 
 module.exports = {
