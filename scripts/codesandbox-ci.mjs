1ef3187e38019003564cab0a6c5e397b49d4bfaf
Jan Siegel <js.ziggle@gmail.com>
2021-10-08 15:07:45 +0200

    - Correct eslint errors.

--- scripts/codesandbox-ci.mjs
+++ scripts/codesandbox-ci.mjs
@@ -6,9 +6,9 @@ import {
 import codesandboxConfig from '../.codesandbox/ci.json';
 
 /**
- * --------------------------------------------------------
+ * --------------------------------------------------------.
  * THIS SCRIPT IS MEANT TO BE USED FOR CODESANDBOX CI ONLY!
- * --------------------------------------------------------
+ * --------------------------------------------------------.
  *
  * This script goes through all the packages defined in the Codesandbox CI config file, installs their dependencies
  * independently (in their own subdirectories), copies the `handsontable/tmp` directory to their `node_modules` as
@@ -17,7 +17,7 @@ import codesandboxConfig from '../.codesandbox/ci.json';
  * Running this script locally will mess up the monorepo's dependency structure. Running the `clean:node_modules`
  * script and calling `npm i` in the root directory should bring it back to normal.
  */
-(async () => {
+(async() => {
   let spawnOptions = {};
 
   for (const packagePath of codesandboxConfig.packages) {
@@ -33,7 +33,7 @@ import codesandboxConfig from '../.codesandbox/ci.json';
         // We're copying instead of linking the directory, because linking only the `handsontable` package
         // caused issues with Handsontable's dependencies not being found by the wrappers.
         fse.copySync(
-          path.resolve(`./handsontable/tmp`),
+          path.resolve('./handsontable/tmp'),
           path.resolve(`${packagePath}/node_modules/handsontable`),
           { overwrite: true });
       }
