ba624ce4c1c8caff31daec3dd9e7292d8fc8e5c2
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2021-11-02 09:40:31 +0100

    Removed unnecessary debugging code #6095

--- handsontable/src/plugins/filters/conditionCollection.js
+++ handsontable/src/plugins/filters/conditionCollection.js
@@ -104,8 +104,6 @@ class ConditionCollection {
       v => (typeof v === 'string' ? v.toLocaleLowerCase(localeForColumn) : v));
     const name = conditionDefinition.name || conditionDefinition.command.key;
 
-    conditionDefinition.args.forEach(v => console.log(v))
-
     this.runLocalHooks('beforeAdd', column);
 
     const columnType = this.getOperation(column);
