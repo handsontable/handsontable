fe94a965f196789c0b5e1b045db135e737b3d201
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-11-05 02:00:06 +0100

    Apply `[dir=rtl]` only for those properties which depend on direction. That removes a lot of messy styles. According to: https://github.com/handsontable/handsontable/pull/8881#pullrequestreview-791766681

--- handsontable/.config/loader/sass-rtl-loader.js
+++ handsontable/.config/loader/sass-rtl-loader.js
@@ -1,33 +1,52 @@
-function scssRtl(source) {
-  return `
-@mixin load($direction: ltr) {
-  $inline-start: left;
-  $inline-end: right;
-  
-  @if $direction == rtl {
-    $inline-start: right;
-    $inline-end: left;
+const trim = (properties) => {
+  return properties.match(
+    /[^;]*(inline-start|inline-end)[^;]*;/gm // remove all redundant properties
+  )?.join('');
+};
+
+const splitSources = (source) => {
+  let rtl = '';
+  const ltr = source.replace(
+    /([^}]*){([^{]*(inline-end|inline-start)[^}]*)}/gm, // detect ltr/rtl dependent properties
+    (all, selector, properties) => {
+      rtl += `${selector}{${trim(properties)}}`;
+
+      return `${selector}{${properties}}`;
+    });
+
+  return [ltr, rtl];
+};
+
+const polyfillDirection = (direction, source) => {
+  switch (direction) {
+    case 'ltr':
+      return source.replace(/inline-start/g, 'left').replace(/inline-end/g, 'right');
+    case 'rtl':
+      return source.replace(/inline-start/g, 'right').replace(/inline-end/g, 'left');
+    default:
+      return source;
   }
+};
 
-${source}
+const applyRtlStyles = (source) => {
+  const [ltrSource, rtlSource] = splitSources(source);
 
-}
-
-@include load();
-
+  return `
+${polyfillDirection('ltr', ltrSource)}
+    
 [dir=rtl] {
-  @include load(rtl);
+${polyfillDirection('rtl', rtlSource)}
 }
 `;
-}
+};
 
-module.exports =  function(source) {
-  
-  if(this.resource.endsWith('.scss')){
-    return scssRtl(source);
+module.exports = function(source) {
+  if (this.resource.endsWith('.scss')) {
+    return applyRtlStyles(source);
   }
-  if(this.resource.endsWith('.sass')){
+  if (this.resource.endsWith('.sass')) {
     this.getLogger('sass-rtl-loader').warn('RTL features for SASS are not supported. Please convert to SCSS.');
   }
+
   return source;
-}
+};
