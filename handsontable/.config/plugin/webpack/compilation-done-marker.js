765062803b97434e6a48e23f6980d7199cdf7371
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2021-11-30 10:46:16 +0100

    Add changelog entry

--- handsontable/.config/plugin/webpack/compilation-done-marker.js
+++ handsontable/.config/plugin/webpack/compilation-done-marker.js
@@ -4,9 +4,10 @@ module.exports = function() {
       compiler.hooks.done.tap('CompilationDoneMarkerPlugin', () => {
         // 3rd party scripts use these non-printable characters to detect (by analyzing
         // standard output stream) when the build process is done (mostly in watch mode).
-        // See: ./handsontable/test/scripts/run-puppeteer-on-code-change.mjs script.
+        // See: ./handsontable/test/scripts/run-puppeteer-on-watchers-change.mjs script.
+        // eslint-disable-next-line no-console, no-restricted-globals
         console.log('\x9d\t\x9d');
       });
     }
   };
-}
+};
