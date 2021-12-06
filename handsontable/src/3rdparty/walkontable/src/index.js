d998e5aefb57c6e23f42ced0fc31346031acc484
@Siemienik Paweł <siemienik.pawel@gmail.com>
2021-12-06 15:59:15 +0100

    REFACTORING Remove redundant exports in walkontable

--- handsontable/src/3rdparty/walkontable/src/index.js
+++ handsontable/src/3rdparty/walkontable/src/index.js
@@ -4,28 +4,8 @@ import ViewportRowsCalculator from './calculator/viewportRows';
 import CellCoords from './cell/coords';
 import CellRange from './cell/range';
 
-import ColumnFilter from './filter/column';
-import RowFilter from './filter/row';
-import MasterTable from './table/master';
-import {
-  LeftOverlay,
-  TopOverlay,
-  TopLeftCornerOverlay,
-  BottomOverlay,
-  BottomLeftCornerOverlay,
-} from './overlay';
-
-import Border from './border';
 import Walkontable from './core';
-import Event from './event';
-import Overlays from './overlays';
-import Scroll from './scroll';
 import Selection from './selection';
-import Settings from './settings';
-import * as Renderer from './renderer';
-import { OrderView, SharedOrderView } from './utils/orderView';
-import Viewport from './viewport';
-import { getListenersCounter } from './../../../eventManager';
 
 export {
   ViewportColumnsCalculator,
@@ -34,29 +14,7 @@ export {
   CellCoords,
   CellRange,
 
-  ColumnFilter,
-  RowFilter,
-
-  MasterTable,
-
-  LeftOverlay,
-  TopOverlay,
-  TopLeftCornerOverlay,
-  BottomOverlay,
-  BottomLeftCornerOverlay,
-
-  Border,
   Walkontable as default,
   Walkontable as Core,
-  Event,
-  Overlays,
-  Scroll,
-  Selection,
-  Settings,
-  Renderer,
-  OrderView,
-  SharedOrderView,
-  Viewport,
-
-  getListenersCounter
+  Selection
 };
