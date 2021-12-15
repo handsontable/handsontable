ce7a709ca78d6cf001148f4eb9bfb509d3819e8a
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2021-12-15 14:04:06 +0100

    TimeEditor proof-of-concept (#9042)

--- handsontable/types/cellTypes/timeType/timeType.d.ts
+++ handsontable/types/cellTypes/timeType/timeType.d.ts
@@ -1,17 +1,17 @@
 import { CellTypeObject } from '../base';
-import { TextEditor } from '../../editors/textEditor';
+import { TimeEditor } from '../../editors/timeEditor';
 import { textRenderer } from '../../renderers/textRenderer';
 import { timeValidator } from '../../validators/timeValidator';
 
 export const CELL_TYPE: 'time';
 export interface TimeCellType extends CellTypeObject {
-  editor: typeof TextEditor;
+  editor: typeof TimeEditor;
   renderer: typeof textRenderer;
   validator: typeof timeValidator;
 }
 
 export namespace TimeCellType {
-  export { TextEditor as editor };
+  export { TimeEditor as editor };
   export { textRenderer as renderer };
   export { timeValidator as validator };
 }
