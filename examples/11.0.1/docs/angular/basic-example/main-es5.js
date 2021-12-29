b8237dc4a42db53798bacf475c58a675fb37fade
jansiegel <jansiegel@users.noreply.github.com>
2021-12-29 07:57:29 +0000

    deploy: ffd8626f4aa4e87244fce94a2f47ed6abe169263

---
+++ examples/11.0.1/docs/angular/basic-example/main-es5.js
@@ -0,0 +1,611 @@
+(function () {
+  function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+  (window["webpackJsonp"] = window["webpackJsonp"] || []).push([["main"], {
+    /***/
+    0:
+    /*!***************************!*\
+      !*** multi ./src/main.ts ***!
+      \***************************/
+
+    /*! no static exports found */
+
+    /***/
+    function _(module, exports, __webpack_require__) {
+      module.exports = __webpack_require__(
+      /*! /home/runner/work/handsontable/handsontable/examples/11.0.1/docs/angular/basic-example/src/main.ts */
+      "zUnb");
+      /***/
+    },
+
+    /***/
+    "AytR":
+    /*!*****************************************!*\
+      !*** ./src/environments/environment.ts ***!
+      \*****************************************/
+
+    /*! exports provided: environment */
+
+    /***/
+    function AytR(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "environment", function () {
+        return environment;
+      }); // This file can be replaced during build by using the `fileReplacements` array.
+      // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+      // The list of file replacements can be found in `angular.json`.
+
+
+      var environment = {
+        production: false
+      };
+      /*
+       * For easier debugging in development mode, you can import the following file
+       * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+       *
+       * This import should be commented out in production mode because it will have a negative impact
+       * on performance if an error is thrown.
+       */
+      // import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
+      /***/
+    },
+
+    /***/
+    "D9WN":
+    /*!***************************************************!*\
+      !*** ../node_modules/moment/locale sync ^\.\/.*$ ***!
+      \***************************************************/
+
+    /*! no static exports found */
+
+    /***/
+    function D9WN(module, exports, __webpack_require__) {
+      var map = {
+        "./af": "ggbP",
+        "./af.js": "ggbP",
+        "./ar": "zaxx",
+        "./ar-dz": "fyYX",
+        "./ar-dz.js": "fyYX",
+        "./ar-kw": "jogf",
+        "./ar-kw.js": "jogf",
+        "./ar-ly": "zWXE",
+        "./ar-ly.js": "zWXE",
+        "./ar-ma": "6Nbo",
+        "./ar-ma.js": "6Nbo",
+        "./ar-sa": "ooRU",
+        "./ar-sa.js": "ooRU",
+        "./ar-tn": "ZPeb",
+        "./ar-tn.js": "ZPeb",
+        "./ar.js": "zaxx",
+        "./az": "sTnJ",
+        "./az.js": "sTnJ",
+        "./be": "mOKi",
+        "./be.js": "mOKi",
+        "./bg": "oaWn",
+        "./bg.js": "oaWn",
+        "./bm": "TQ38",
+        "./bm.js": "TQ38",
+        "./bn": "6K5y",
+        "./bn.js": "6K5y",
+        "./bo": "vPIy",
+        "./bo.js": "vPIy",
+        "./br": "afE2",
+        "./br.js": "afE2",
+        "./bs": "JNFb",
+        "./bs.js": "JNFb",
+        "./ca": "NQcR",
+        "./ca.js": "NQcR",
+        "./cs": "0V88",
+        "./cs.js": "0V88",
+        "./cv": "e/5r",
+        "./cv.js": "e/5r",
+        "./cy": "HTX9",
+        "./cy.js": "HTX9",
+        "./da": "oBlQ",
+        "./da.js": "oBlQ",
+        "./de": "DPpk",
+        "./de-at": "7epm",
+        "./de-at.js": "7epm",
+        "./de-ch": "mq6o",
+        "./de-ch.js": "mq6o",
+        "./de.js": "DPpk",
+        "./dv": "FyKI",
+        "./dv.js": "FyKI",
+        "./el": "U5Cp",
+        "./el.js": "U5Cp",
+        "./en-SG": "IIgg",
+        "./en-SG.js": "IIgg",
+        "./en-au": "2tm8",
+        "./en-au.js": "2tm8",
+        "./en-ca": "bxNt",
+        "./en-ca.js": "bxNt",
+        "./en-gb": "YmdP",
+        "./en-gb.js": "YmdP",
+        "./en-ie": "gLHG",
+        "./en-ie.js": "gLHG",
+        "./en-il": "rTiS",
+        "./en-il.js": "rTiS",
+        "./en-nz": "Odsn",
+        "./en-nz.js": "Odsn",
+        "./eo": "GjBA",
+        "./eo.js": "GjBA",
+        "./es": "SKOl",
+        "./es-do": "8wa+",
+        "./es-do.js": "8wa+",
+        "./es-us": "YL9C",
+        "./es-us.js": "YL9C",
+        "./es.js": "SKOl",
+        "./et": "+JHh",
+        "./et.js": "+JHh",
+        "./eu": "pAPS",
+        "./eu.js": "pAPS",
+        "./fa": "zhSd",
+        "./fa.js": "zhSd",
+        "./fi": "/BTK",
+        "./fi.js": "/BTK",
+        "./fo": "K/JZ",
+        "./fo.js": "K/JZ",
+        "./fr": "wehu",
+        "./fr-ca": "UKIa",
+        "./fr-ca.js": "UKIa",
+        "./fr-ch": "sIcz",
+        "./fr-ch.js": "sIcz",
+        "./fr.js": "wehu",
+        "./fy": "RmWi",
+        "./fy.js": "RmWi",
+        "./ga": "s5bY",
+        "./ga.js": "s5bY",
+        "./gd": "BWwv",
+        "./gd.js": "BWwv",
+        "./gl": "794S",
+        "./gl.js": "794S",
+        "./gom-latn": "jixF",
+        "./gom-latn.js": "jixF",
+        "./gu": "Uz2K",
+        "./gu.js": "Uz2K",
+        "./he": "dSB2",
+        "./he.js": "dSB2",
+        "./hi": "0vPu",
+        "./hi.js": "0vPu",
+        "./hr": "fnly",
+        "./hr.js": "fnly",
+        "./hu": "FI+y",
+        "./hu.js": "FI+y",
+        "./hy-am": "ZxHn",
+        "./hy-am.js": "ZxHn",
+        "./id": "KxDR",
+        "./id.js": "KxDR",
+        "./is": "H+sD",
+        "./is.js": "H+sD",
+        "./it": "GyEJ",
+        "./it-ch": "jSwB",
+        "./it-ch.js": "jSwB",
+        "./it.js": "GyEJ",
+        "./ja": "km6H",
+        "./ja.js": "km6H",
+        "./jv": "WnjQ",
+        "./jv.js": "WnjQ",
+        "./ka": "WXVf",
+        "./ka.js": "WXVf",
+        "./kk": "zJNk",
+        "./kk.js": "zJNk",
+        "./km": "ZuGE",
+        "./km.js": "ZuGE",
+        "./kn": "VCFo",
+        "./kn.js": "VCFo",
+        "./ko": "EpfC",
+        "./ko.js": "EpfC",
+        "./ku": "FvgC",
+        "./ku.js": "FvgC",
+        "./ky": "Pfmo",
+        "./ky.js": "Pfmo",
+        "./lb": "wSTE",
+        "./lb.js": "wSTE",
+        "./lo": "IKWk",
+        "./lo.js": "IKWk",
+        "./lt": "wUrv",
+        "./lt.js": "wUrv",
+        "./lv": "xVOH",
+        "./lv.js": "xVOH",
+        "./me": "riUx",
+        "./me.js": "riUx",
+        "./mi": "b1YN",
+        "./mi.js": "b1YN",
+        "./mk": "yPwC",
+        "./mk.js": "yPwC",
+        "./ml": "dS3T",
+        "./ml.js": "dS3T",
+        "./mn": "8J7P",
+        "./mn.js": "8J7P",
+        "./mr": "Cla+",
+        "./mr.js": "Cla+",
+        "./ms": "Vbbc",
+        "./ms-my": "qelJ",
+        "./ms-my.js": "qelJ",
+        "./ms.js": "Vbbc",
+        "./mt": "YkvB",
+        "./mt.js": "YkvB",
+        "./my": "4lZK",
+        "./my.js": "4lZK",
+        "./nb": "4dXb",
+        "./nb.js": "4dXb",
+        "./ne": "dhqa",
+        "./ne.js": "dhqa",
+        "./nl": "oPI1",
+        "./nl-be": "XLIu",
+        "./nl-be.js": "XLIu",
+        "./nl.js": "oPI1",
+        "./nn": "T9ox",
+        "./nn.js": "T9ox",
+        "./pa-in": "Ly9c",
+        "./pa-in.js": "Ly9c",
+        "./pl": "MX9p",
+        "./pl.js": "MX9p",
+        "./pt": "VVP3",
+        "./pt-br": "qatJ",
+        "./pt-br.js": "qatJ",
+        "./pt.js": "VVP3",
+        "./ro": "2xJO",
+        "./ro.js": "2xJO",
+        "./ru": "eqS4",
+        "./ru.js": "eqS4",
+        "./sd": "6Huv",
+        "./sd.js": "6Huv",
+        "./se": "opZB",
+        "./se.js": "opZB",
+        "./si": "Ueyn",
+        "./si.js": "Ueyn",
+        "./sk": "YIvA",
+        "./sk.js": "YIvA",
+        "./sl": "s2ex",
+        "./sl.js": "s2ex",
+        "./sq": "9o/V",
+        "./sq.js": "9o/V",
+        "./sr": "CZE8",
+        "./sr-cyrl": "xXeP",
+        "./sr-cyrl.js": "xXeP",
+        "./sr.js": "CZE8",
+        "./ss": "z3ad",
+        "./ss.js": "z3ad",
+        "./sv": "AVN5",
+        "./sv.js": "AVN5",
+        "./sw": "y2+C",
+        "./sw.js": "y2+C",
+        "./ta": "i/qy",
+        "./ta.js": "i/qy",
+        "./te": "Zov1",
+        "./te.js": "Zov1",
+        "./tet": "6ucV",
+        "./tet.js": "6ucV",
+        "./tg": "cLEX",
+        "./tg.js": "cLEX",
+        "./th": "cYD4",
+        "./th.js": "cYD4",
+        "./tl-ph": "+Ls2",
+        "./tl-ph.js": "+Ls2",
+        "./tlh": "sCZo",
+        "./tlh.js": "sCZo",
+        "./tr": "Nx06",
+        "./tr.js": "Nx06",
+        "./tzl": "x0TR",
+        "./tzl.js": "x0TR",
+        "./tzm": "eHpI",
+        "./tzm-latn": "ceDr",
+        "./tzm-latn.js": "ceDr",
+        "./tzm.js": "eHpI",
+        "./ug-cn": "a1x9",
+        "./ug-cn.js": "a1x9",
+        "./uk": "jAw7",
+        "./uk.js": "jAw7",
+        "./ur": "UZ6Q",
+        "./ur.js": "UZ6Q",
+        "./uz": "eYI/",
+        "./uz-latn": "MTcW",
+        "./uz-latn.js": "MTcW",
+        "./uz.js": "eYI/",
+        "./vi": "riLI",
+        "./vi.js": "riLI",
+        "./x-pseudo": "ESkz",
+        "./x-pseudo.js": "ESkz",
+        "./yo": "tL86",
+        "./yo.js": "tL86",
+        "./zh-cn": "/cTU",
+        "./zh-cn.js": "/cTU",
+        "./zh-hk": "dH0k",
+        "./zh-hk.js": "dH0k",
+        "./zh-tw": "0+Cr",
+        "./zh-tw.js": "0+Cr"
+      };
+
+      function webpackContext(req) {
+        var id = webpackContextResolve(req);
+        return __webpack_require__(id);
+      }
+
+      function webpackContextResolve(req) {
+        if (!__webpack_require__.o(map, req)) {
+          var e = new Error("Cannot find module '" + req + "'");
+          e.code = 'MODULE_NOT_FOUND';
+          throw e;
+        }
+
+        return map[req];
+      }
+
+      webpackContext.keys = function webpackContextKeys() {
+        return Object.keys(map);
+      };
+
+      webpackContext.resolve = webpackContextResolve;
+      module.exports = webpackContext;
+      webpackContext.id = "D9WN";
+      /***/
+    },
+
+    /***/
+    "Sy1n":
+    /*!**********************************!*\
+      !*** ./src/app/app.component.ts ***!
+      \**********************************/
+
+    /*! exports provided: AppComponent */
+
+    /***/
+    function Sy1n(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "AppComponent", function () {
+        return AppComponent;
+      });
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! @angular/core */
+      "Ub9n");
+      /* harmony import */
+
+
+      var _handsontable_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @handsontable/angular */
+      "+P78");
+      /* harmony import */
+
+
+      var handsontable__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! handsontable */
+      "5DfS");
+
+      var AppComponent = /*#__PURE__*/function () {
+        function AppComponent() {
+          _classCallCheck(this, AppComponent);
+
+          this.hotSettings = {
+            data: [['', 'Tesla', 'Volvo', 'Toyota', 'Ford'], ['2019', 10, 11, 12, 13], ['2020', 20, 11, 14, 13], ['2021', 30, 15, 12, 13]],
+            colHeaders: true,
+            rowHeaders: true,
+            width: '100%',
+            height: '100%',
+            licenseKey: 'non-commercial-and-evaluation'
+          };
+        }
+
+        _createClass(AppComponent, [{
+          key: "ngOnInit",
+          value: function ngOnInit() {
+            console.log(this.getDebugInfo());
+          }
+        }, {
+          key: "getDebugInfo",
+          value: function getDebugInfo() {
+            var debug = 'Handsontable:';
+            debug += " v".concat(handsontable__WEBPACK_IMPORTED_MODULE_2__["default"].version);
+            debug += " (".concat(handsontable__WEBPACK_IMPORTED_MODULE_2__["default"].buildDate, ")");
+            debug += ' Wrapper:';
+            debug += " v".concat(_handsontable_angular__WEBPACK_IMPORTED_MODULE_1__["HotTableModule"].version);
+            debug += ' Angular:';
+            debug += " v".concat(_angular_core__WEBPACK_IMPORTED_MODULE_0__["VERSION"].full);
+            return debug;
+          }
+        }]);
+
+        return AppComponent;
+      }();
+
+      AppComponent.ɵfac = function AppComponent_Factory(t) {
+        return new (t || AppComponent)();
+      };
+
+      AppComponent.ɵcmp = _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵdefineComponent"]({
+        type: AppComponent,
+        selectors: [["app-root"]],
+        decls: 2,
+        vars: 1,
+        consts: [["id", "example"], [3, "settings"]],
+        template: function AppComponent_Template(rf, ctx) {
+          if (rf & 1) {
+            _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementStart"](0, "div", 0);
+
+            _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelement"](1, "hot-table", 1);
+
+            _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵelementEnd"]();
+          }
+
+          if (rf & 2) {
+            _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵadvance"](1);
+
+            _angular_core__WEBPACK_IMPORTED_MODULE_0__["ɵɵproperty"]("settings", ctx.hotSettings);
+          }
+        },
+        directives: [_handsontable_angular__WEBPACK_IMPORTED_MODULE_1__["HotTableComponent"]],
+        styles: ["#example[_ngcontent-%COMP%] {\n  box-sizing: border-box;\n  height: 100%;\n  width: 100%;\n  margin: 0;\n}\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2FwcC5jb21wb25lbnQuc2NzcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtFQUNFLHNCQUFBO0VBQ0EsWUFBQTtFQUNBLFdBQUE7RUFDQSxTQUFBO0FBQ0YiLCJmaWxlIjoiYXBwLmNvbXBvbmVudC5zY3NzIiwic291cmNlc0NvbnRlbnQiOlsiI2V4YW1wbGUge1xuICBib3gtc2l6aW5nOiBib3JkZXItYm94O1xuICBoZWlnaHQ6IDEwMCU7XG4gIHdpZHRoOiAxMDAlO1xuICBtYXJnaW46IDA7XG59XG4iXX0= */"]
+      });
+      /***/
+    },
+
+    /***/
+    "ZAI4":
+    /*!*******************************!*\
+      !*** ./src/app/app.module.ts ***!
+      \*******************************/
+
+    /*! exports provided: AppModule */
+
+    /***/
+    function ZAI4(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony export (binding) */
+
+
+      __webpack_require__.d(__webpack_exports__, "AppModule", function () {
+        return AppModule;
+      });
+      /* harmony import */
+
+
+      var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! @angular/platform-browser */
+      "vlQ6");
+      /* harmony import */
+
+
+      var _handsontable_angular__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @handsontable/angular */
+      "+P78");
+      /* harmony import */
+
+
+      var _app_component__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! ./app.component */
+      "Sy1n");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+      /*! @angular/core */
+      "Ub9n");
+
+      var AppModule = function AppModule() {
+        _classCallCheck(this, AppModule);
+      };
+
+      AppModule.ɵfac = function AppModule_Factory(t) {
+        return new (t || AppModule)();
+      };
+
+      AppModule.ɵmod = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineNgModule"]({
+        type: AppModule,
+        bootstrap: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]]
+      });
+      AppModule.ɵinj = _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵdefineInjector"]({
+        providers: [],
+        imports: [[_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"], _handsontable_angular__WEBPACK_IMPORTED_MODULE_1__["HotTableModule"]]]
+      });
+
+      (function () {
+        (typeof ngJitMode === "undefined" || ngJitMode) && _angular_core__WEBPACK_IMPORTED_MODULE_3__["ɵɵsetNgModuleScope"](AppModule, {
+          declarations: [_app_component__WEBPACK_IMPORTED_MODULE_2__["AppComponent"]],
+          imports: [_angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["BrowserModule"], _handsontable_angular__WEBPACK_IMPORTED_MODULE_1__["HotTableModule"]]
+        });
+      })();
+      /***/
+
+    },
+
+    /***/
+    "zUnb":
+    /*!*********************!*\
+      !*** ./src/main.ts ***!
+      \*********************/
+
+    /*! no exports provided */
+
+    /***/
+    function zUnb(module, __webpack_exports__, __webpack_require__) {
+      "use strict";
+
+      __webpack_require__.r(__webpack_exports__);
+      /* harmony import */
+
+
+      var _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(
+      /*! @angular/platform-browser */
+      "vlQ6");
+      /* harmony import */
+
+
+      var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(
+      /*! @angular/core */
+      "Ub9n");
+      /* harmony import */
+
+
+      var _app_app_module__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(
+      /*! ./app/app.module */
+      "ZAI4");
+      /* harmony import */
+
+
+      var _environments_environment__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(
+      /*! ./environments/environment */
+      "AytR");
+
+      if (_environments_environment__WEBPACK_IMPORTED_MODULE_3__["environment"].production) {
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["enableProdMode"])();
+      }
+
+      _angular_platform_browser__WEBPACK_IMPORTED_MODULE_0__["platformBrowser"]().bootstrapModule(_app_app_module__WEBPACK_IMPORTED_MODULE_2__["AppModule"])["catch"](function (err) {
+        return console.error(err);
+      });
+      /***/
+
+    },
+
+    /***/
+    "zn8P":
+    /*!******************************************************!*\
+      !*** ./$$_lazy_route_resource lazy namespace object ***!
+      \******************************************************/
+
+    /*! no static exports found */
+
+    /***/
+    function zn8P(module, exports) {
+      function webpackEmptyAsyncContext(req) {
+        // Here Promise.resolve().then() is used instead of new Promise() to prevent
+        // uncaught exception popping up in devtools
+        return Promise.resolve().then(function () {
+          var e = new Error("Cannot find module '" + req + "'");
+          e.code = 'MODULE_NOT_FOUND';
+          throw e;
+        });
+      }
+
+      webpackEmptyAsyncContext.keys = function () {
+        return [];
+      };
+
+      webpackEmptyAsyncContext.resolve = webpackEmptyAsyncContext;
+      module.exports = webpackEmptyAsyncContext;
+      webpackEmptyAsyncContext.id = "zn8P";
+      /***/
+    }
+  }, [[0, "runtime", "vendor"]]]);
+})();
+//# sourceMappingURL=main-es5.js.map
