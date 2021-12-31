7ed44a641fdc9f439fb2adb2f571c6b555979695
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2021-12-31 15:31:51 +0100

    Add changelog entry file

--- handsontable/src/editors/autocompleteEditor/autocompleteEditor.js
+++ handsontable/src/editors/autocompleteEditor/autocompleteEditor.js
@@ -465,7 +465,7 @@ export class AutocompleteEditor extends HandsontableEditor {
     if (isPrintableChar(event.keyCode) || event.keyCode === KEY_CODES.BACKSPACE ||
       event.keyCode === KEY_CODES.DELETE || event.keyCode === KEY_CODES.INSERT) {
       // for Windows 10 + FF86 there is need to add delay to make sure that the value taken from
-      // the textarea is the freshed value. Otherwise the list of choices do not update correctly (see #7570).
+      // the textarea is the freshest value. Otherwise the list of choices does not update correctly (see #7570).
       // On the more modern version of the FF (~ >=91) it seems that the issue is not present.
       let timeOffset = 10;
 
