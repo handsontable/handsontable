4dcf444c744f49d2405b85f3dac85b35ad180234
Siemienik Pawel <siemienik.pawel@gmail.com>
2022-01-14 19:57:50 +0100

    FIX collapsible columns button placement for the rtl mode. Cover both the ltr and the rtl modes by test (#9114)

--- handsontable/src/plugins/collapsibleColumns/__tests__/collapsibleColumns.spec.js
+++ handsontable/src/plugins/collapsibleColumns/__tests__/collapsibleColumns.spec.js
@@ -2533,6 +2533,19 @@ describe('CollapsibleColumns', () => {
   });
 
   describe('collapsible button', () => {
+    it('should be placed in correct place', () => {
+      handsontable({
+        data: Handsontable.helper.createSpreadsheetData(10, 10),
+        nestedHeaders: [
+          ['A1', { label: 'B1', colspan: 4 }, 'F1', 'G1', 'H1', 'I1', 'J1'],
+          ['A2', { label: 'B2', colspan: 2 }, { label: 'D2', colspan: 2 }, 'F2', 'G2', 'H2', 'I2', 'J2'],
+        ],
+        collapsibleColumns: true
+      });
+      expect(window.getComputedStyle(getCell(-1, 3).querySelector('.collapsibleIndicator'))
+        .getPropertyValue('right')).toEqual('5px');
+    });
+
     it('should call "toggleCollapsibleSection" internally with correct toggle state ' +
        '(depends if the clicked header is already collapsed or not)', () => {
       handsontable({
