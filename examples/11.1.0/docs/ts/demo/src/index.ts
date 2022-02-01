0ae34e6bf16af6925da272363906e671f8016474
Marcin Warpechowski <warpech@gmail.com>
2022-02-01 17:35:12 +0100

    Put "Demo" examples into the repo (#9160)
    * add JS demo based on the template
    
    * add a practical example of cloning a template
    
    * sq add JS demo
    
    * copy Angular example to the demo folder
    
    * modify the Angular demo according to the current demo deployed on .com website
    
    source: https://codesandbox.io/s/handsontable-angular-data-grid-hello-world-app-dhojw?from-embed
    
    * fix errors Namespace 'Handsontable.renderers' has no exported member 'Base'.
    
    as seen in https://github.com/handsontable/handsontable/pull/8953
    
    * copy spec from JS Demo to Angular Demo
    
    * copy React example to the demo folder
    
    * modify the React demo according to the current demo deployed on .com website
    
    source: https://codesandbox.io/s/handsontable-react-data-grid-hello-world-app-bhipr?from-embed
    
    * files created automatically by running `npm run build` twice
    
    after the first run, before these files are created, the compilation fails
    
    * add missing types
    
    without these, the app didn't build
    
    * copy Vue example template to the demo folder
    
    * modify the Vue demo according to the current demo deployed on .com website
    
    source: https://codesandbox.io/s/handsontable-vue-data-grid-hello-world-app-5qgo7?from-embed
    
    * fix errors Namespace 'Handsontable.renderers' has no exported member 'Base'.
    
    as seen in https://github.com/handsontable/handsontable/pull/8953
    
    * add TypeScript demo
    
    as seen in https://github.com/handsontable/handsontable/pull/8953
    
    * copy demo examples from `next` to `11.1.0`
    
    cp -r next/docs/angular/demo 11.1.0/docs/angular/demo
    cp -r next/docs/react/demo 11.1.0/docs/react/demo
    cp -r next/docs/vue/demo 11.1.0/docs/vue/demo
    cp -r next/docs/js/demo 11.1.0/docs/js/demo
    cp -r next/docs/ts 11.1.0/docs/ts
    
    * ignore "dist" files in the examples
    
    because my Sublime Merge almost hanged after I've build a few examples, producing 127 dist files
    
    * add "ts" to the workspace regexps
    
    as pointed out in the review https://github.com/handsontable/handsontable/pull/9160#pullrequestreview-867682366
    
    * add precautionary script improvements
    
    copied from now-closed PR #8953
    
    I didn't copy the improvement:
    
            // TODO: remove `--legacy-peer-deps` eventually
            await execa('npm', ['install', '--no-audit', '--legacy-peer-deps'], {
    
    from `install-subpackages.mjs`, because that seems not needed anymore
    
    * refresh examples' package-lock.json
    
    pushed the result of
    
    npm run examples:install 11.1.0
    npm run examples:install next
    
    * ignore the lockfiles of specific examples

---
+++ examples/11.1.0/docs/ts/demo/src/index.ts
@@ -0,0 +1,77 @@
+import Handsontable from "handsontable";
+import "handsontable/dist/handsontable.min.css";
+import "pikaday/css/pikaday.css";
+
+import { data } from "./constants";
+import { progressBarRenderer, starsRenderer } from "./customRenderers";
+
+import {
+  alignHeaders,
+  drawCheckboxInRowHeaders,
+  addClassesToRows,
+  changeCheckboxCell
+} from "./hooksCallbacks";
+
+const example = document.getElementById("example1");
+// @ts-ignore
+new Handsontable(example, {
+  data,
+  height: 450,
+  colWidths: [140, 126, 192, 100, 100, 90, 90, 110, 97],
+  colHeaders: [
+    "Company name",
+    "Country",
+    "Name",
+    "Sell date",
+    "Order ID",
+    "In stock",
+    "Qty",
+    "Progress",
+    "Rating"
+  ],
+  columns: [
+    { data: 1, type: "text" },
+    { data: 2, type: "text" },
+    { data: 3, type: "text" },
+    {
+      data: 4,
+      type: "date",
+      allowInvalid: false
+    },
+    { data: 5, type: "text" },
+    {
+      data: 6,
+      type: "checkbox",
+      className: "htCenter"
+    },
+    {
+      data: 7,
+      type: "numeric"
+    },
+    {
+      data: 8,
+      renderer: progressBarRenderer,
+      readOnly: true,
+      className: "htMiddle"
+    },
+    {
+      data: 9,
+      renderer: starsRenderer,
+      readOnly: true,
+      className: "star htCenter"
+    }
+  ],
+  dropdownMenu: true,
+  hiddenColumns: {
+    indicators: true
+  },
+  contextMenu: true,
+  multiColumnSorting: true,
+  filters: true,
+  rowHeaders: true,
+  afterGetColHeader: alignHeaders,
+  afterGetRowHeader: drawCheckboxInRowHeaders,
+  afterOnCellMouseDown: changeCheckboxCell,
+  beforeRenderer: addClassesToRows,
+  licenseKey: "non-commercial-and-evaluation"
+});
