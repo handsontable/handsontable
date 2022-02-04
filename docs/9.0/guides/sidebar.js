2a54501841a8d7bc064cf83dcde58f742a0aa44e
kirszenbaum <jakub.wisniewski@handsontable.com>
2022-02-04 21:13:23 +0100

    Removing changes from older docs versions

--- docs/9.0/guides/sidebar.js
+++ docs/9.0/guides/sidebar.js
@@ -1,6 +1,5 @@
 const gettingStartedItems = [
   'guides/getting-started/introduction',
-  'guides/getting-started/demo',
   'guides/getting-started/installation',
   'guides/getting-started/binding-to-data',
   'guides/getting-started/saving-data',
