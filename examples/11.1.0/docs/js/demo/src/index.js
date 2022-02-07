bfe5f2ca70f48eac3eddd072e9ca2be2264a65a5
Marcin Warpechowski <warpech@gmail.com>
2022-02-08 00:20:19 +0100

    Switch some columns in the Demo example (#9202)
    * JS demo: move basic text columns to the right
    
    * Angular demo: move basic text columns to the right
    
    * React demo: move basic text columns to the right
    
    * TS demo: move basic text columns to the right
    
    * Vue demo: move basic text columns to the right
    
    * update JS demo tests
    
    * update Angular demo tests
    
    * trigger CI

--- examples/11.1.0/docs/js/demo/src/index.js
+++ examples/11.1.0/docs/js/demo/src/index.js
@@ -17,28 +17,26 @@ const example = document.getElementById("example");
 new Handsontable(example, {
   data,
   height: 450,
-  colWidths: [140, 126, 192, 100, 100, 90, 90, 110, 97],
+  colWidths: [140, 192, 100, 90, 90, 110, 97, 100, 126],
   colHeaders: [
     "Company name",
-    "Country",
     "Name",
     "Sell date",
-    "Order ID",
     "In stock",
     "Qty",
     "Progress",
-    "Rating"
+    "Rating",
+    "Order ID",
+    "Country"
   ],
   columns: [
     { data: 1, type: "text" },
-    { data: 2, type: "text" },
     { data: 3, type: "text" },
     {
       data: 4,
       type: "date",
       allowInvalid: false
     },
-    { data: 5, type: "text" },
     {
       data: 6,
       type: "checkbox",
@@ -59,7 +57,9 @@ new Handsontable(example, {
       renderer: starRenderer,
       readOnly: true,
       className: "star htCenter"
-    }
+    },
+    { data: 5, type: "text" },
+    { data: 2, type: "text" }
   ],
   dropdownMenu: true,
   hiddenColumns: {
