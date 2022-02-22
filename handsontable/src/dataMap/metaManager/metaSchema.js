39eee695283575f26adda0e642208280e4525539
Marcin Warpechowski <warpech@gmail.com>
2022-02-22 11:44:45 +0100

    Add ar-AR language in a few places where it was missing (#9227)
    * add arAR language in a few places where it was missing
    
    * add ar-AR to JSDoc

--- handsontable/src/dataMap/metaManager/metaSchema.js
+++ handsontable/src/dataMap/metaManager/metaSchema.js
@@ -2457,6 +2457,8 @@ export default () => {
      * | Setting             | Description                 |
      * | ------------------- | --------------------------- |
      * | `'en-US'` (default) | English - United States     |
+     * | `'ar-AR'`           | Arabic - Global             |
+     * | `'de-CH'`           | German - Switzerland        |
      * | `'de-DE'`           | German - Germany            |
      * | `'es-MX'`           | Spanish - Mexico            |
      * | `'fr-FR'`           | French - France             |
