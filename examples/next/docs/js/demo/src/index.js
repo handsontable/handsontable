1cc365d8a3b4badafdc2fa8159ee4a4193d306dc
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-04-27 15:13:45 +0200

    Make demo more readable

--- examples/next/docs/js/demo/src/index.js
+++ examples/next/docs/js/demo/src/index.js
@@ -2,7 +2,7 @@ import Handsontable from "handsontable";
 import "handsontable/dist/handsontable.min.css";
 import "pikaday/css/pikaday.css";
 
-import { data, generateArabicData } from "./constants";
+import { generateExampleData, isArabicDemoEnabled } from "./utils";
 import { progressBarRenderer, starRenderer } from "./customRenderers";
 import "./styles.css";
 
@@ -12,13 +12,12 @@ import {
   changeCheckboxCell
 } from "./hooksCallbacks";
 
-const urlParams = new URLSearchParams(location.search.replace(/^\?/, ""));
-const isArabicExample = urlParams.get("arabicExample") === "1";
 const example = document.getElementById("example");
 
 new Handsontable(example, {
-  data: isArabicExample ? generateArabicData() : data,
-  layoutDirection: isArabicExample ? "rtl" : "ltr",
+  data: generateExampleData(),
+  layoutDirection: isArabicDemoEnabled() ? "rtl" : "ltr",
+  language: isArabicDemoEnabled() ? "ar-AR" : "en-US",
   height: 450,
   colWidths: [140, 192, 100, 90, 90, 110, 97, 100, 126],
   colHeaders: [
@@ -39,7 +38,6 @@ new Handsontable(example, {
       data: 4,
       type: "date",
       allowInvalid: false,
-      dateFormat: isArabicExample ? "DD.MM.YYYY" : "DD/MM/YYYY"
     },
     {
       data: 6,
