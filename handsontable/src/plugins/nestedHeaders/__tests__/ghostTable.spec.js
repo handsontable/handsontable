324fc514d9f61f4764e30d353580a34909227e64
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-06-07 11:59:20 +0200

    Add tests for columns widths calculations co-op with hidden columns

--- handsontable/src/plugins/nestedHeaders/__tests__/ghostTable.spec.js
+++ handsontable/src/plugins/nestedHeaders/__tests__/ghostTable.spec.js
@@ -127,5 +127,96 @@ describe('NestedHeaders', () => {
         expect(widthAfterUpdate).toBeAroundValue(120);
       });
     });
+
+    describe('with hidden columns', () => {
+      it('should calculate the columns widths when some columns are hidden on table initialization', () => {
+        handsontable({
+          data: createSpreadsheetData(3, 10),
+          nestedHeaders: [
+            ['A', { label: 'B', colspan: 8 }],
+            ['D', { label: 'E', colspan: 4 }, { label: 'F', colspan: 4 }],
+            ['H', { label: 'I', colspan: 2 }, { label: 'J', colspan: 2 }, { label: 'K', colspan: 2 },
+              { label: 'This is a very long header to test', colspan: 2 }],
+            ['N', 'O', 'Longer text', 'Longer text', 'R', 'S', 'T', 'U', 'V'],
+          ],
+          hiddenColumns: {
+            columns: [0, 2]
+          }
+        });
+
+        const ghostTable = getPlugin('nestedHeaders').ghostTable;
+
+        expect(ghostTable.widthsMap.getValueAtIndex(0)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(1)).toBeAroundValue(19);
+        expect(ghostTable.widthsMap.getValueAtIndex(2)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(3)).toBeAroundValue(70);
+        expect(ghostTable.widthsMap.getValueAtIndex(4)).toBeAroundValue(18);
+        expect(ghostTable.widthsMap.getValueAtIndex(5)).toBeAroundValue(16);
+        expect(ghostTable.widthsMap.getValueAtIndex(6)).toBeAroundValue(17);
+        expect(ghostTable.widthsMap.getValueAtIndex(7)).toBeAroundValue(88);
+        expect(ghostTable.widthsMap.getValueAtIndex(8)).toBeAroundValue(88);
+        expect(ghostTable.widthsMap.getValueAtIndex(9)).toBe(null);
+      });
+
+      it('should recalculate the columns widths after hiding columns', () => {
+        handsontable({
+          data: createSpreadsheetData(3, 10),
+          nestedHeaders: [
+            ['A', { label: 'B', colspan: 8 }],
+            ['D', { label: 'E', colspan: 4 }, { label: 'F', colspan: 4 }],
+            ['H', { label: 'I', colspan: 2 }, { label: 'J', colspan: 2 }, { label: 'K', colspan: 2 },
+              { label: 'This is a very long header to test', colspan: 2 }],
+            ['N', 'O', 'Longer text', 'Longer text', 'R', 'S', 'T', 'U', 'V'],
+          ],
+          hiddenColumns: true,
+        });
+
+        getPlugin('hiddenColumns').hideColumns([1, 3, 7]);
+
+        const ghostTable = getPlugin('nestedHeaders').ghostTable;
+
+        expect(ghostTable.widthsMap.getValueAtIndex(0)).toBeAroundValue(19);
+        expect(ghostTable.widthsMap.getValueAtIndex(1)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(2)).toBeAroundValue(70);
+        expect(ghostTable.widthsMap.getValueAtIndex(3)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(4)).toBeAroundValue(18);
+        expect(ghostTable.widthsMap.getValueAtIndex(5)).toBeAroundValue(16);
+        expect(ghostTable.widthsMap.getValueAtIndex(6)).toBeAroundValue(17);
+        expect(ghostTable.widthsMap.getValueAtIndex(7)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(8)).toBeAroundValue(178);
+        expect(ghostTable.widthsMap.getValueAtIndex(9)).toBe(null);
+      });
+
+      it('should recalculate the columns widths after showing columns', () => {
+        handsontable({
+          data: createSpreadsheetData(3, 10),
+          nestedHeaders: [
+            ['A', { label: 'B', colspan: 8 }],
+            ['D', { label: 'E', colspan: 4 }, { label: 'F', colspan: 4 }],
+            ['H', { label: 'I', colspan: 2 }, { label: 'J', colspan: 2 }, { label: 'K', colspan: 2 },
+              { label: 'This is a very long header to test', colspan: 2 }],
+            ['N', 'O', 'Longer text', 'Longer text', 'R', 'S', 'T', 'U', 'V'],
+          ],
+          hiddenColumns: {
+            columns: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+          }
+        });
+
+        getPlugin('hiddenColumns').showColumns([2, 4, 7, 8]);
+
+        const ghostTable = getPlugin('nestedHeaders').ghostTable;
+
+        expect(ghostTable.widthsMap.getValueAtIndex(0)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(1)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(2)).toBeAroundValue(70);
+        expect(ghostTable.widthsMap.getValueAtIndex(3)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(4)).toBeAroundValue(19);
+        expect(ghostTable.widthsMap.getValueAtIndex(5)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(6)).toBe(null);
+        expect(ghostTable.widthsMap.getValueAtIndex(7)).toBeAroundValue(88);
+        expect(ghostTable.widthsMap.getValueAtIndex(8)).toBeAroundValue(88);
+        expect(ghostTable.widthsMap.getValueAtIndex(9)).toBe(null);
+      });
+    });
   });
 });
