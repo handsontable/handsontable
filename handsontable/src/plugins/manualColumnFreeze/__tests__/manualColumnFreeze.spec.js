b63b16bcaf39aec2be6562aa0f33156cf573c846
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2022-06-07 13:13:05 +0200

    Moved tests to separate file #9248

--- handsontable/src/plugins/manualColumnFreeze/__tests__/manualColumnFreeze.spec.js
+++ handsontable/src/plugins/manualColumnFreeze/__tests__/manualColumnFreeze.spec.js
@@ -63,72 +63,6 @@ describe('manualColumnFreeze', () => {
 
       expect(getDataAtCell(0, 0)).toBe('E1');
     });
-
-    describe('Hooks', () => {
-      it('should trigger the `beforeColumnFreeze` and `afterColumnFreeze` hooks with proper ' +
-        'parameters (all columns aren\'t fixed)', () => {
-        const beforeColumnFreezeCallback = jasmine.createSpy('beforeColumnFreeze');
-        const afterColumnFreezeCallback = jasmine.createSpy('afterColumnFreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnFreeze: beforeColumnFreezeCallback,
-          afterColumnFreeze: afterColumnFreezeCallback,
-        });
-
-        hot.getPlugin('manualColumnFreeze').freezeColumn(3);
-
-        expect(beforeColumnFreezeCallback).toHaveBeenCalledWith(3, true);
-        expect(afterColumnFreezeCallback).toHaveBeenCalledWith(3, true);
-        expect(hot.getDataAtRow(0)).toEqual(['D1', 'A1', 'B1', 'C1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-
-      it('should trigger the `beforeColumnFreeze` and `afterColumnFreeze` hooks with proper ' +
-        'parameters (all columns ARE fixed)', () => {
-        const beforeColumnFreezeCallback = jasmine.createSpy('beforeColumnFreeze');
-        const afterColumnFreezeCallback = jasmine.createSpy('afterColumnFreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnFreeze: beforeColumnFreezeCallback,
-          afterColumnFreeze: afterColumnFreezeCallback,
-          fixedColumnsStart: 10,
-        });
-
-        hot.getPlugin('manualColumnFreeze').freezeColumn(0);
-
-        expect(beforeColumnFreezeCallback).toHaveBeenCalledWith(0, false);
-        expect(afterColumnFreezeCallback).toHaveBeenCalledWith(0, false);
-        expect(hot.getDataAtRow(0)).toEqual(['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-
-      it('should not freeze and not trigger the `afterColumnFreeze` hook after try of freezing column, when ' +
-        '`beforeColumnFreeze` return false', () => {
-        const afterColumnFreezeCallback = jasmine.createSpy('afterColumnFreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnFreeze() {
-            return false;
-          },
-          afterColumnFreeze: afterColumnFreezeCallback,
-        });
-
-        hot.getPlugin('manualColumnFreeze').freezeColumn(5);
-
-        expect(afterColumnFreezeCallback).not.toHaveBeenCalled();
-        expect(hot.getDataAtRow(0)).toEqual(['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-    });
   });
 
   describe('unfreezeColumn', () => {
@@ -214,77 +148,9 @@ describe('manualColumnFreeze', () => {
       expect(hot.toPhysicalColumn(2)).toEqual(1);
       expect(hot.toPhysicalColumn(9)).toEqual(8);
     });
-
-    describe('Hooks', () => {
-      it('should trigger the `beforeColumnUnfreeze` and `afterColumnUnfreeze` hooks with proper ' +
-        'parameters (some columns are fixed)', () => {
-        const beforeColumnUnfreezeCallback = jasmine.createSpy('beforeColumnUnfreeze');
-        const afterColumnUnfreezeCallback = jasmine.createSpy('afterColumnUnfreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnUnfreeze: beforeColumnUnfreezeCallback,
-          afterColumnUnfreeze: afterColumnUnfreezeCallback,
-          fixedColumnsStart: 1,
-        });
-
-        hot.getPlugin('manualColumnFreeze').unfreezeColumn(0);
-
-        expect(beforeColumnUnfreezeCallback).toHaveBeenCalledWith(0, true);
-        expect(afterColumnUnfreezeCallback).toHaveBeenCalledWith(0, true);
-        expect(hot.getDataAtRow(0)).toEqual(['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-
-      it('should trigger the `beforeColumnUnfreeze` and `afterColumnUnfreeze` hooks with proper ' +
-        'parameters (no columns are fixed)', () => {
-        const beforeColumnUnfreezeCallback = jasmine.createSpy('beforeColumnUnfreeze');
-        const afterColumnUnfreezeCallback = jasmine.createSpy('afterColumnUnfreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnUnfreeze: beforeColumnUnfreezeCallback,
-          afterColumnUnfreeze: afterColumnUnfreezeCallback,
-        });
-
-        hot.getPlugin('manualColumnFreeze').unfreezeColumn(0);
-
-        expect(beforeColumnUnfreezeCallback).toHaveBeenCalledWith(0, false);
-        expect(afterColumnUnfreezeCallback).toHaveBeenCalledWith(0, false);
-        expect(hot.getDataAtRow(0)).toEqual(['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-
-      it('should not freeze and not trigger the `afterColumnUnfreeze` hook after try of freezing column, when ' +
-        '`beforeColumnUnfreeze` return false', () => {
-        const afterColumnUnfreezeCallback = jasmine.createSpy('afterColumnUnfreeze');
-
-        const hot = handsontable({
-          data: Handsontable.helper.createSpreadsheetData(10, 10),
-          rowHeaders: true,
-          colHeaders: true,
-          manualColumnFreeze: true,
-          beforeColumnUnfreeze() {
-            return false;
-          },
-          afterColumnUnfreeze: afterColumnUnfreezeCallback,
-          fixedColumnsStart: 1,
-        });
-
-        hot.getPlugin('manualColumnFreeze').unfreezeColumn(0);
-
-        expect(afterColumnUnfreezeCallback).not.toHaveBeenCalled();
-        expect(hot.getDataAtRow(0)).toEqual(['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1', 'I1', 'J1']);
-      });
-    });
   });
 
   describe('functionality', () => {
-
     it('should add a \'freeze column\' context menu entry for non-fixed columns', () => {
       const hot = handsontable({
         data: Handsontable.helper.createSpreadsheetData(10, 10),
