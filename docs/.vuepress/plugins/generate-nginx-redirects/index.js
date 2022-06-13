58f22536b20b41dcee9e1f83be89048bf42eed96
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-06-13 10:49:27 +0200

    add nginx variables VuePress plugin

--- docs/.vuepress/plugins/generate-nginx-redirects/index.js
+++ docs/.vuepress/plugins/generate-nginx-redirects/index.js
@@ -41,8 +41,8 @@ module.exports = (options, context) => {
         logger.error('The "currentVersion" and/or "latestVersion" is not set.');
       }
 
-      if ($page.latestVersion && $page.currentVersion === $page.latestVersion && $page.frontmatter.permalink) {
-        const from = dedupeSlashes(`/${$page.latestVersion}${$page.frontmatter.permalink}/`);
+      if ($page.frontmatter.permalink) {
+        const from = dedupeSlashes(`${$page.frontmatter.permalink}/`);
         const to = dedupeSlashes(`/${context.base}${$page.frontmatter.permalink}/`);
 
         // http://nginx.org/en/docs/http/ngx_http_rewrite_module.html#rewrite
