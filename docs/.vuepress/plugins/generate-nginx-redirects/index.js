f829d1651ddbb932e872a3cb9c608ffd597f163e
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-06-20 14:00:10 +0200

    Add docker tags versioning

--- docs/.vuepress/plugins/generate-nginx-redirects/index.js
+++ docs/.vuepress/plugins/generate-nginx-redirects/index.js
@@ -42,8 +42,8 @@ module.exports = (options, context) => {
       }
 
       if ($page.frontmatter.permalink) {
-        const from = dedupeSlashes(`${$page.frontmatter.permalink}/`);
-        const to = dedupeSlashes(`/${context.base}${$page.frontmatter.permalink}/`);
+        const from = dedupeSlashes(`/${$page.frontmatter.permalink}/`);
+        const to = dedupeSlashes(`/${$page.frontmatter.permalink}/`);
 
         // http://nginx.org/en/docs/http/ngx_http_rewrite_module.html#rewrite
         const redirectRule = `rewrite ^${from}?$ ${to} permanent;`;
