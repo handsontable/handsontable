9dfa8c5bb31308a39693b12b45788ba4a28aac58
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2022-06-22 10:47:42 +0200

    WIP: Removed conditional block #9577

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,6 @@ const highlight = require('./highlight');
 const snippets = require('./containers/snippets');
 const examples = require('./containers/examples');
 const sourceCodeLink = require('./containers/sourceCodeLink');
-const conditionalBlock = require('./containers/conditionalBlock');
 const nginxRedirectsPlugin = require('./plugins/generate-nginx-redirects');
 const assetsVersioningPlugin = require('./plugins/assets-versioning');
 const extendPageDataPlugin = require('./plugins/extend-page-data');
@@ -134,7 +133,6 @@ module.exports = {
       sidebarLinkSelector: '.table-of-contents a',
       headerAnchorSelector: '.header-anchor'
     }],
-    ['container', conditionalBlock],
     ['container', snippets],
     ['container', examples],
     ['container', sourceCodeLink],
