b9529d16e400e75da3b28375ea79d287dd8feba8
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2022-06-22 17:36:36 +0200

    Removing markups for conditional containers (without removing new line characters) #9577

--- docs/.vuepress/plugins/markdown-it-conditional-container/index.js
+++ docs/.vuepress/plugins/markdown-it-conditional-container/index.js
@@ -4,10 +4,30 @@ const { getEnvDocsFramework, parseFramework, getDefaultFramework, getNormalizedP
 module.exports = function conditionalContainer(markdown) {
   const openAndCloseTagOneliner = /::: only-for (((react|javascript) ?)+)(.*?):::$/ms;
   const foundOpenTokenContent = /(?:\n?)::: only-for (((react|javascript) ?)+)(.*?)$/;
-  const foundCloseTokenContent = /\n:::$/;
+  const foundCloseTokenContent = /(?:\n?):::$/;
   const markupForCustomContainer = ':::';
   const capturedGroupIndex = 1;
   let endIndex;
+
+  const removeValue = ({ token, regexp, env }) => {
+    // Removing value from token's content.
+    token.content = token.content.replace(regexp, '');
+
+    const childrenIndex = token.children.findIndex(
+      childrenToken => regexp.test(childrenToken.content));
+
+    // Removing token's children also representing removed value.
+    if (childrenIndex !== -1) {
+      token.children.splice(childrenIndex, 1);
+
+    } else {
+      // eslint-disable-next-line no-console
+      console.error(`${chalk.red('\nUnexpected error thrown while removing conditional container' +
+        ` Please check how "${env.frontmatter.permalink}" site parsed from ` +
+        `"${getNormalizedPath(env.relativePath)}" file looks like.`
+      )}`);
+    }
+  };
 
   const findAndRemove = (state) => {
     const relativePath = state.env?.relativePath; // Sometimes the `env` key is an empty object.
@@ -16,6 +36,7 @@ module.exports = function conditionalContainer(markdown) {
       return;
     }
 
+    const env = state.env;
     const frameworkId = getEnvDocsFramework() || parseFramework(relativePath) || getDefaultFramework();
 
     for (let index = state.tokens.length - 1; index >= 0; index -= 1) {
@@ -28,6 +49,10 @@ module.exports = function conditionalContainer(markdown) {
 
         if (onlyForFrameworks.includes(frameworkId) === false) {
           state.tokens.splice(index, 1);
+
+        } else {
+          removeValue({ token, regexp: foundOpenTokenContent, env });
+          removeValue({ token, regexp: foundCloseTokenContent, env });
         }
 
       } else if (isNotNativeContainer && foundCloseTokenContent.test(token.content)) {
@@ -40,24 +65,11 @@ module.exports = function conditionalContainer(markdown) {
           const indexAfterCurrentToken = index + 1;
 
           state.tokens.splice(indexAfterCurrentToken, endIndex - indexAfterCurrentToken + 1);
-          // Removing value from token's content.
-          token.content = token.content.replace(foundOpenTokenContent, '');
-
-          const childrenIndex = token.children.findIndex(
-            childrenToken => foundOpenTokenContent.test(childrenToken.content));
-
-          // Removing token's children also representing removed value.
-          if (childrenIndex !== -1) {
-            token.children.splice(childrenIndex, 1);
-
-          } else {
-            // eslint-disable-next-line no-console
-            console.error(`${chalk.red('\nUnexpected error for removing conditional container ("markdown-it" plugin).' +
-              ` Please check how "${state.env.frontmatter.permalink}" site parsed from ` +
-              `"${getNormalizedPath(relativePath)}" file looks like.`
-            )}`);
-          }
+        } else {
+          removeValue({ token, regexp: foundCloseTokenContent, env });
         }
+
+        removeValue({ token, regexp: foundOpenTokenContent, env });
       }
     }
   };
