3846deac9293b41f01259dbcf710c20c4cb7257a
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2022-06-22 16:16:43 +0200

    Removing custom containers using regexp properly #9577

--- docs/.vuepress/plugins/markdown-it-conditional-container/index.js
+++ docs/.vuepress/plugins/markdown-it-conditional-container/index.js
@@ -1,8 +1,12 @@
-const { getEnvDocsFramework, parseFramework, getDefaultFramework } = require('../../helpers');
+const chalk = require('chalk');
+const { getEnvDocsFramework, parseFramework, getDefaultFramework, getNormalizedPath } = require('../../helpers');
 
 module.exports = function conditionalContainer(markdown) {
-  const foundOpenTokenType = 'container_only-for_open';
-  const foundCloseTokenType = 'container_only-for_close';
+  const openAndCloseTagOneliner = /::: only-for (((react|javascript) ?)+)(.*?):::$/ms;
+  const foundOpenTokenContent = /(?:\n?)::: only-for (((react|javascript) ?)+)(.*?)$/;
+  const foundCloseTokenContent = /\n:::$/;
+  const markupForCustomContainer = ':::';
+  const capturedGroupIndex = 1;
   let endIndex;
 
   const findAndRemove = (state) => {
@@ -16,16 +20,43 @@ module.exports = function conditionalContainer(markdown) {
 
     for (let index = state.tokens.length - 1; index >= 0; index -= 1) {
       const token = state.tokens[index];
+      // We don't create custom container intentionally. It can create paragraphs or break listed elements.
+      const isNotNativeContainer = token.markup !== markupForCustomContainer;
 
-      if (token.type === foundCloseTokenType) {
-        endIndex = index;
-      }
-
-      if (token.type === foundOpenTokenType) {
-        const onlyForFrameworks = token.info.replace(/\s*only-for /, '').split(' ');
+      if (isNotNativeContainer && openAndCloseTagOneliner.test(token.content)) {
+        const onlyForFrameworks = openAndCloseTagOneliner.exec(token.content)[capturedGroupIndex].split(' ');
 
         if (onlyForFrameworks.includes(frameworkId) === false) {
-          state.tokens.splice(index, endIndex - index + 1);
+          state.tokens.splice(index, 1);
+        }
+
+      } else if (isNotNativeContainer && foundCloseTokenContent.test(token.content)) {
+
+        endIndex = index;
+      } else if (isNotNativeContainer && foundOpenTokenContent.test(token.content)) {
+        const onlyForFrameworks = foundOpenTokenContent.exec(token.content)[capturedGroupIndex].split(' ');
+
+        if (onlyForFrameworks.includes(frameworkId) === false) {
+          const indexAfterCurrentToken = index + 1;
+
+          state.tokens.splice(indexAfterCurrentToken, endIndex - indexAfterCurrentToken + 1);
+          // Removing value from token's content.
+          token.content = token.content.replace(foundOpenTokenContent, '');
+
+          const childrenIndex = token.children.findIndex(
+            childrenToken => foundOpenTokenContent.test(childrenToken.content));
+
+          // Removing token's children also representing removed value.
+          if (childrenIndex !== -1) {
+            token.children.splice(childrenIndex, 1);
+
+          } else {
+            // eslint-disable-next-line no-console
+            console.error(`${chalk.red('\nUnexpected error for removing conditional container ("markdown-it" plugin).' +
+              ` Please check how "${state.env.frontmatter.permalink}" site parsed from ` +
+              `"${getNormalizedPath(relativePath)}" file looks like.`
+            )}`);
+          }
         }
       }
     }
