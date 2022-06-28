84a900612e0a825eb412cdbf744c8737df76989c
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-06-28 11:53:31 +0200

    Fix eslint complains

--- docs/.vuepress/helpers.js
+++ docs/.vuepress/helpers.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const execa = require('execa');
+
 const versionFromBranchRegExp = /^docs-snapshot\/(\d+\.\d+)$/;
 
 /**
--- docs/.vuepress/theme/utils/remoteDocsData.js
+++ docs/.vuepress/theme/utils/remoteDocsData.js
@@ -1,5 +1,14 @@
 let docsVersionsCache = null;
 
+/**
+ * Fetches the Docs data like all available docs versions or what is the docs latest version
+ * from remote JSON file.
+ *
+ * @param {object} params The params object.
+ * @param {string} params.buildMode The Docs build mode.
+ * @param {string} params.currentVersion The current docs build version.
+ * @returns {object}
+ */
 export async function fetchDocsData({ buildMode, currentVersion }) {
   if (docsVersionsCache === null) {
     const pathVersion = buildMode === 'production' ? '' : `${currentVersion}/`;
