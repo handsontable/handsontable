58b8583a13f54c5c5af271be1969563f4cc9a7ef
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-06-28 14:49:20 +0200

    Fix API generation

--- docs/.vuepress/tools/jsdoc-convert/configuration.mjs
+++ docs/.vuepress/tools/jsdoc-convert/configuration.mjs
@@ -1,34 +1,27 @@
-// import fs from 'fs';
-// import path from 'path';
-// import { fileURLToPath } from 'url';
-
-// const contentPath = path.resolve(path.dirname(fileURLToPath(import.meta.url)), '../../../content');
-// const contentDir = fs.existsSync(contentPath) ? 'content' : 'next';
-
 export default {
-  pathToSource: '../../../../src',
+  pathToSource: '../../../../handsontable/src',
   pathToDist: '../../../content/api',
-  urlPrefix: '/9.0/api/',
+  urlPrefix: '/api/',
   seo: {
     'dataMap/metaManager/metaSchema.js': {
       title: 'Options',
       metaTitle: 'Options - API Reference - Handsontable Documentation',
-      permalink: '/9.0/api/options'
+      permalink: '/api/options'
     },
     'pluginHooks.js': {
       title: 'Hooks',
       metaTitle: 'Hooks - API Reference - Handsontable Documentation',
-      permalink: '/9.0/api/hooks'
+      permalink: '/api/hooks'
     },
     'core.js': {
       title: 'Core',
       metaTitle: 'Core - API Reference - Handsontable Documentation',
-      permalink: '/9.0/api/core'
+      permalink: '/api/core'
     },
     '3rdparty/walkontable/src/cell/coords.js': {
       title: 'CellCoords',
       metaTitle: 'CellCoords - API Reference - Handsontable Documentation',
-      permalink: '/9.0/api/coords'
+      permalink: '/api/coords'
     }
   }
 };
