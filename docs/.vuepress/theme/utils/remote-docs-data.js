7106a7bee1bb01d7cf436cb648db0fb36deb69b5
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-07-05 12:26:36 +0200

    Add support for staging

---
+++ docs/.vuepress/theme/utils/remote-docs-data.js
@@ -0,0 +1,17 @@
+let docsVersionsCache = null;
+
+/**
+ * Fetches the Docs data like all available docs versions or what is the docs latest version
+ * from remote JSON file.
+ *
+ * @returns {object}
+ */
+export async function fetchDocsData() {
+  if (docsVersionsCache === null) {
+    const response = await fetch(`${window.location.origin}/docs/docs-data.json`);
+
+    docsVersionsCache = await response.json();
+  }
+
+  return docsVersionsCache;
+}
