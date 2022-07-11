6d4e985e3b81f081e489968c9b96a8d773d79b66
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-07-11 13:12:36 +0200

    Add canonical URLs dumper plugin

---
+++ docs/.vuepress/plugins/dump-canonicals/index.js
@@ -0,0 +1,43 @@
+const fsp = require('fs').promises;
+const path = require('path');
+const { logger } = require('@vuepress/shared-utils');
+const {
+  getThisDocsVersion,
+} = require('../../helpers');
+
+const pluginName = 'hot/dump-canonicals';
+const canonicals = {
+  v: getThisDocsVersion(),
+  urls: [],
+};
+
+module.exports = (options, context) => {
+  const outputFile = options.outputFile || path.resolve(context.publicDir, 'canonicals.conf');
+
+  return {
+    name: pluginName,
+
+    /**
+     * Based on the permalink of the latest docs version generate nginx redirect rules.
+     *
+     * @param {object} $page The $page value of the page you’re currently reading.
+     */
+    async extendPageData($page) {
+      if ($page.frontmatter.permalink) {
+        // Remove the slash ('/') from the beginning of the URL path to reduce file size
+        canonicals.urls.push($page.frontmatter.permalink.replace(/^\//, ''));
+      }
+    },
+
+    /**
+     * Save collected canonical URLs to the file.
+     */
+    async ready() {
+      try {
+        await fsp.writeFile(outputFile, JSON.stringify(canonicals));
+      } catch (ex) {
+        logger.error(`Something bad happens while writing to the file (${outputFile}): ${ex}`);
+      }
+    }
+  };
+};
