5ba6af6291d7d7c26939d6ae8f9c6ca391cf37c5
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-07-12 11:16:43 +0200

    Merge 2 plugins into one

--- docs/.vuepress/plugins/dump-docs-data/docs-versions.js
+++ docs/.vuepress/plugins/dump-docs-data/docs-versions.js
@@ -1,13 +1,6 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { promises as fsp } from 'fs';
-import { Octokit } from '@octokit/rest';
-import semver from 'semver';
-import utils from './utils.js';
-
-const { logger } = utils;
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const buildMode = process.env.BUILD_MODE;
+const semver = require('semver');
+const { Octokit } = require('@octokit/rest');
+const { logger } = require('@vuepress/shared-utils');
 
 /**
  * Min Docs version that is listed in the Docs version dropdown menu.
@@ -57,8 +50,8 @@ async function readFromGitHub() {
 
   versions = tags.slice(0, tags.indexOf(MIN_DOCS_VERSION) + 1);
 
-  logger.log(`Fetched the following Docs versions: ${versions.join(', ')}`);
-  logger.log(`GitHub API rate limits:
+  logger.info(`Fetched the following Docs versions: ${versions.join(', ')}`);
+  logger.info(`GitHub API rate limits:
   Limit: ${releases.headers['x-ratelimit-limit']}
   Used: ${releases.headers['x-ratelimit-used']}
   Remaining: ${releases.headers['x-ratelimit-remaining']}
@@ -70,31 +63,28 @@ async function readFromGitHub() {
   };
 }
 
-let docsData = null;
+async function fetchDocsVersions() {
+  let docsData = null;
 
-// for building use always the GH API
-if (process.env.DOCS_BASE && process.env.DOCS_BASE !== 'latest') {
-  docsData = await readFromGitHub();
-} else {
-  // for local development use the latest Docs image...
-  try {
-    docsData = await readFromDocsLatest();
-  } catch { // ...or GH API as fallback
+  // for building use always the GH API
+  if (process.env.DOCS_BASE && process.env.DOCS_BASE !== 'latest') {
     docsData = await readFromGitHub();
+  } else {
+    // for local development use the latest Docs image...
+    try {
+      docsData = await readFromDocsLatest();
+    } catch { // ...or GH API as fallback
+      docsData = await readFromGitHub();
+    }
   }
-}
 
-if (docsData !== null) {
-  const dataPath = path.resolve(__dirname, '../../.vuepress/public/docs-data.json');
-
-  if (buildMode !== 'production') {
+  if (process.env.BUILD_MODE !== 'production') {
     docsData.versions = ['next', ...docsData.versions];
   }
 
-  await fsp.writeFile(dataPath, JSON.stringify(docsData));
-
-  logger.success('Dumped Docs data to', dataPath);
-} else {
-  logger.error('Could not fetch and/or write Docs data to the file.');
-  process.exit(1);
+  return docsData;
 }
+
+module.exports = {
+  fetchDocsVersions,
+};
