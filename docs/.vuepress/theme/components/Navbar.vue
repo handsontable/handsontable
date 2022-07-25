aeafdc796601ad1f9f034153e3e8c55e9f4294f4
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2022-07-25 16:09:19 +0200

    Merged changes for Navbar component (took @budnix changes)

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -39,11 +39,7 @@ import SidebarButton from '@theme/components/SidebarButton.vue';
 import NavLinks from '@theme/components/NavLinks.vue';
 import VersionsDropdown from '@theme/components/VersionsDropdown.vue';
 import ThemeSwitcher from '@theme/components/ThemeSwitcher.vue';
-<<<<<<< HEAD
 import FrameworksDropdown from '@theme/components/FrameworksDropdown.vue';
-import { ensureExt } from '@vuepress/theme-default/util';
-=======
->>>>>>> develop
 import Logo from './Logo.vue';
 
 export default {
@@ -58,26 +54,6 @@ export default {
     ThemeSwitcher,
     Logo
   },
-<<<<<<< HEAD
-  methods: {
-    versionedUrl(url) {
-      const framework = this.$page.currentFramework ?
-        `/${this.$page.currentFramework}${this.$page.frameworkSuffix}` : '';
-
-      // Already have version and framework placed in the url for some reason and a `RouterLink` process it properly.
-      if (this.$page.DOCS_VERSION && (this.$page.DOCS_FRAMEWORK || this.$page.isFrameworked === false)) {
-        return ensureExt(url);
-
-      } else if (this.$page.currentVersion === this.$page.latestVersion) {
-        return ensureExt(`${framework}${url}`);
-
-      } else {
-        return ensureExt(`/${this.$page.currentVersion}${framework}${url}`);
-      }
-    },
-  },
-=======
->>>>>>> develop
   data() {
     return {
       linksWrapMaxWidth: null
