7bca0025ddda1e72ba376dbd2f066049da37075b
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-08-17 10:43:01 +0200

    Add support for describing API docs individually

--- docs/.vuepress/tools/jsdoc-convert/renderer/seo.mjs
+++ docs/.vuepress/tools/jsdoc-convert/renderer/seo.mjs
@@ -4,35 +4,43 @@ export const buildHeaderWriter = ({ seo, urlPrefix }) => {
   const genSeoTitle = file => escape(
     file.replace(/(^.*\/)?(.*?)\.[.a-zA-Z]*$/, '$2') // Get first filename segment (to the first dot) without full path
   ).replace(/(^[a-z])/, m => m.toUpperCase()); // To upper first letter
-  const seoTitle = file => seo[file] && seo[file].title || genSeoTitle(file);
 
   const genSeoMetaTitle = (file, isPlugin) => {
-    return `${seoTitle(file)} - ${isPlugin ? 'Plugin' : 'API Reference'} - Handsontable Documentation`;
+    return `${genSeoTitle(file)} - ${isPlugin ? 'Plugin' : 'API Reference'} - Handsontable Documentation`;
   };
-  const seoMetaTitle = (file, isPlugin) => seo[file] && seo[file].metaTitle || genSeoMetaTitle(file, isPlugin);
 
-  const genSeoPermalink = file => escape(file
+  const genSeoPermalink = file => urlPrefix + escape(file
     .replace(/(^.*\/)?(.*)\.[a-zA-Z]*$/, '$2') // Get filename without full path and extension
   )
     .replace(/([a-z])([A-Z]+)/g, '$1-$2') // Separate words
     .toLowerCase();
-  const seoPermalink = file => seo[file] && seo[file].permalink || urlPrefix + genSeoPermalink(file);
 
-  const seoCanonicalUrl = file => seoPermalink(file).replace('/next', '');
+  // Simple converter from JS object to YAML
+  const toYaml = (meta, indent = 0) => {
+    return Object.keys(meta).map((key) => {
+      if (typeof meta[key] === 'object') {
+        return `${key}:\n${toYaml(meta[key], indent + 2)}`;
+      }
+
+      return `${' '.repeat(indent)}${key}: ${meta[key]}`;
+    }).join('\n');
+  };
 
   return (file, isPlugin) => {
-    const title = seoTitle(file, isPlugin);
+    const pageMeta = Object.assign({
+      title: genSeoTitle(file),
+      metaTitle: genSeoMetaTitle(file, isPlugin),
+      permalink: genSeoPermalink(file),
+      canonicalUrl: genSeoPermalink(file),
+      hotPlugin: isPlugin,
+      editLink: false,
+    }, seo[file] || {});
 
     return `---
-title: ${title}
-metaTitle: ${seoMetaTitle(file, isPlugin)}
-permalink: ${seoPermalink(file)}
-canonicalUrl: ${seoCanonicalUrl(file)}
-hotPlugin: ${isPlugin ? 'true' : 'false'}
-editLink: false
+${toYaml(pageMeta)}
 ---
 
-# ${title}
+# ${pageMeta.title}
 
 [[toc]]
 `;
