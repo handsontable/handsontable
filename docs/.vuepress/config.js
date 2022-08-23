de391ae1bcb6af3a12c46a261b49e29f4ce244fe
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-08-23 15:07:47 +0200

    Fix header anchoring

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,6 +8,7 @@ const nginxVariablesPlugin = require('./plugins/generate-nginx-variables');
 const extendPageDataPlugin = require('./plugins/extend-page-data');
 const firstHeaderInjection = require('./plugins/markdown-it-header-injection');
 const conditionalContainer = require('./plugins/markdown-it-conditional-container');
+const activeHeaderLinksPlugin = require('./plugins/active-header-links');
 const {
   getDocsBaseUrl,
   getThisDocsVersion,
@@ -118,9 +119,10 @@ module.exports = {
       hostname: getDocsBaseUrl(),
       exclude: ['/404.html']
     }],
-    ['@vuepress/active-header-links', {
+    [activeHeaderLinksPlugin, {
       sidebarLinkSelector: '.table-of-contents a',
-      headerAnchorSelector: '.header-anchor'
+      headerAnchorSelector: '.header-anchor',
+      anchorTopOffset: 75,
     }],
     ['container', examples(getThisDocsVersion(), base)],
     ['container', sourceCodeLink],
