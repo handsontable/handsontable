5df7fe6d7751633e9a9bf79a4188ec95a1cb4828
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-08-24 10:24:08 +0200

    Fix header anchoring (#9821)

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -40,60 +40,104 @@ const buildActiveHeaderLinkHandler = () => {
  * For the first page load, the GA automatically registers pageview. For other
  * route changes, the event is triggered manually.
  */
-let isFirstPageLoaded = true;
+let isFirstPageGALoaded = true;
+let isPageLoaded = false;
 
 export default async({ router, siteData, isServer }) => {
-  if (!isServer) {
-    const currentVersion = siteData.pages[0].currentVersion;
-    const buildMode = siteData.pages[0].buildMode;
-    let pathVersion = '';
+  if (isServer) {
+    return;
+  }
 
-    if (buildMode !== 'production') {
-      pathVersion = `${currentVersion}/`;
+  const currentVersion = siteData.pages[0].currentVersion;
+  const buildMode = siteData.pages[0].buildMode;
+  let pathVersion = '';
+
+  if (buildMode !== 'production') {
+    pathVersion = `${currentVersion}/`;
+  }
+
+  const response = await fetch(`${window.location.origin}/docs/${pathVersion}data/common.json`);
+  const docsData = await response.json();
+  const canonicalURLs = new Map(docsData.urls);
+
+  siteData.pages.forEach((page) => {
+    const frontmatter = page.frontmatter;
+    const canonicalUrl = frontmatter.canonicalUrl;
+    const canonicalUrlNorm = canonicalUrl.replace(/^\/docs\//, '').replace(/\/$/, '');
+
+    if (canonicalURLs.has(canonicalUrlNorm) && canonicalURLs.get(canonicalUrlNorm) !== '') {
+      const docsVersion = canonicalURLs.get(canonicalUrlNorm);
+
+      frontmatter.canonicalUrl = canonicalUrl.replace(/^\/docs\//, `/docs/${docsVersion}/`);
     }
 
-    const response = await fetch(`${window.location.origin}/docs/${pathVersion}data/common.json`);
-    const docsData = await response.json();
-    const canonicalURLs = new Map(docsData.urls);
+    page.versions = docsData.versions;
+    page.latestVersion = docsData.latestVersion;
+  });
 
-    siteData.pages.forEach((page) => {
-      const frontmatter = page.frontmatter;
-      const canonicalUrl = frontmatter.canonicalUrl;
-      const canonicalUrlNorm = canonicalUrl.replace(/^\/docs\//, '').replace(/\/$/, '');
+  router.options.scrollBehavior = function(to, from, savedPosition) {
+    if (this.app.$vuepress.$get('disableScrollBehavior')) {
+      return false;
+    }
 
-      if (canonicalURLs.has(canonicalUrlNorm) && canonicalURLs.get(canonicalUrlNorm) !== '') {
-        const docsVersion = canonicalURLs.get(canonicalUrlNorm);
+    let scrollPosition = { x: 0, y: 0 }; // page without hash
 
-        frontmatter.canonicalUrl = canonicalUrl.replace(/^\/docs\//, `/docs/${docsVersion}/`);
-      }
+    if (savedPosition) {
+      scrollPosition = savedPosition; // page from the browser navigation (back/forward)
 
-      page.versions = docsData.versions;
-      page.latestVersion = docsData.latestVersion;
+    } else if (to.hash) {
+      scrollPosition = {
+        selector: to.hash,
+        // top offset that matches to the "scroll-padding-top" (.vuepress/theme/styles/index.styl@34)
+        // mostly it's the height of the top header plus some margin
+        offset: { x: 0, y: 75 }
+      };
+    }
+
+    let scrollResolver;
+
+    const scrollPromise = new Promise((resolve) => {
+      scrollResolver = resolve;
     });
 
-    themeLoader();
-
-    if (typeof window.ga === 'function') {
-      router.afterEach((to) => {
-        if (!isFirstPageLoaded) {
-          ga.getAll().forEach((tracker) => {
-            if (tracker.get('trackingId') === GA_ID) {
-              // Collect the page view in the next browser event loop cycle to make sure
-              // that the VuePress finished render new page completely (change the URL address
-              // and document title).
-              setTimeout(() => {
-                tracker.set('page', router.app.$withBase(to.fullPath));
-                tracker.send('pageview');
-              });
-            }
-          });
-        }
-
-        isFirstPageLoaded = false;
-      });
+    if (isPageLoaded) {
+      if (to.path === from.path) {
+        scrollResolver(scrollPosition);
+      } else {
+        setTimeout(() => scrollResolver(scrollPosition));
+      }
+    } else {
+      window.onload = () => {
+        isPageLoaded = true;
+        scrollResolver(scrollPosition);
+      };
     }
 
-    router.afterEach(buildRegisterCleaner(instanceRegister));
-    router.afterEach(buildActiveHeaderLinkHandler());
+    return scrollPromise;
+  };
+
+  themeLoader();
+
+  if (typeof window.ga === 'function') {
+    router.afterEach((to) => {
+      if (!isFirstPageGALoaded) {
+        ga.getAll().forEach((tracker) => {
+          if (tracker.get('trackingId') === GA_ID) {
+            // Collect the page view in the next browser event loop cycle to make sure
+            // that the VuePress finished render new page completely (change the URL address
+            // and document title).
+            setTimeout(() => {
+              tracker.set('page', router.app.$withBase(to.fullPath));
+              tracker.send('pageview');
+            });
+          }
+        });
+      }
+
+      isFirstPageGALoaded = false;
+    });
   }
+
+  router.afterEach(buildRegisterCleaner(instanceRegister));
+  router.afterEach(buildActiveHeaderLinkHandler());
 };
