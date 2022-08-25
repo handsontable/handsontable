612ab748a25151bc79ab5583bf524a675a0aaaba
Jakub Wiśniewski <jakub.wisniewski@handsontable.com>
2022-08-25 11:29:08 +0200

    Add meta titles and descriptions

--- docs/.vuepress/tools/jsdoc-convert/configuration.mjs
+++ docs/.vuepress/tools/jsdoc-convert/configuration.mjs
@@ -6,26 +6,26 @@ export default {
   seo: {
     'Options.md': {
       title: 'Configuration options',
-      metaTitle: '"API reference: Options - JavaScript Data Grid | Handsontable"',
+      metaTitle: 'Options API reference - JavaScript Data Grid | Handsontable',
       description: 'A complete list of Handsontable\'s configuration options that let you customize your data grid instance.',
       react: {
-        metaTitle: '"API reference: Options - React Data Grid | Handsontable"',
+        metaTitle: 'Options API reference - React Data Grid | Handsontable',
       },
     },
     'Hooks.md': {
       title: 'Hooks',
-      metaTitle: "'API reference: Hooks - JavaScript Data Grid | Handsontable'",
+      metaTitle: 'Hooks API reference - JavaScript Data Grid | Handsontable',
       description: 'A complete list of Handsontable\'s API hooks (callbacks) that let you run your code before or after specific data grid actions.',
       react: {
-        metaTitle: "'API reference: Hooks - React Data Grid | Handsontable'",
+        metaTitle: 'Hooks API reference - React Data Grid | Handsontable',
       },
     },
     'Core.md': {
       title: 'Core methods',
-      metaTitle: "'API reference: Core - JavaScript Data Grid | Handsontable'",
+      metaTitle: 'Core API reference - JavaScript Data Grid | Handsontable',
       description: 'A complete list of the public API methods of Handsontable\'s core that let you control your data grid programmatically.',
       react: {
-        metaTitle: "'API reference: Core methods - React Data Grid | Handsontable'",
+        metaTitle: 'Core methods API reference - React Data Grid | Handsontable',
       },
     },
   }
