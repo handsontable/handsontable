df21ed925845db42ecb22204ffe205bae0e96deb
Marcin Warpechowski <warpech@gmail.com>
2022-08-31 13:46:09 +0200

    npm run docs:lint:fix

--- docs/.vuepress/components/DemoJS.vue
+++ docs/.vuepress/components/DemoJS.vue
@@ -3,10 +3,10 @@
 </template>
 
 <script>
-import { convertRelativeToAbsoluteAttributeUrl } from "./demo-common";
+import { convertRelativeToAbsoluteAttributeUrl } from './demo-common';
 
 export default {
-  name: "DemoJS",
+  name: 'DemoJS',
   props: {
     fullVersionNumber: {
       required: true,
@@ -14,40 +14,46 @@ export default {
   },
   mounted() {
     const url = `https://examples.handsontable.com/examples/${this.fullVersionNumber}/docs/js/demo/index.html`;
+
     fetch(url)
       .then((response) => {
         return response.text();
       })
       .then((html) => {
-        var parser = new DOMParser();
-        var doc = parser.parseFromString(html, "text/html");
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
 
         const headNodes = Array.from(doc.head.childNodes).filter((node) => {
-          if (node.nodeName == "LINK" && node.rel === "stylesheet") {
-            convertRelativeToAbsoluteAttributeUrl(node, node, "href", url);
+          if (node.nodeName == 'LINK' && node.rel === 'stylesheet') {
+            convertRelativeToAbsoluteAttributeUrl(node, node, 'href', url);
+
             return true;
           }
+
           return false;
         });
 
         const bodyNodes = Array.from(doc.body.childNodes).map((node) => {
-          if (node.nodeName == "SCRIPT") {
+          if (node.nodeName == 'SCRIPT') {
             // scripts need to be cloned, otherwise Chrome refuses to run them
-            const newNode = document.createElement("script");
-            if (node.hasAttribute("src")) {
-              convertRelativeToAbsoluteAttributeUrl(node, newNode, "src", url);
+            const newNode = document.createElement('script');
+
+            if (node.hasAttribute('src')) {
+              convertRelativeToAbsoluteAttributeUrl(node, newNode, 'src', url);
             }
             newNode.innerText = node.innerText;
+
             return newNode;
           }
+
           return node;
         });
 
         this.$el.append(...headNodes, ...bodyNodes);
       })
-      .catch(function (err) {
+      .catch((err) => {
         // There was an error
-        console.warn("Something went wrong.", err);
+        console.warn('Something went wrong.', err);
       });
   },
 };
--- docs/.vuepress/components/demo-common.js
+++ docs/.vuepress/components/demo-common.js
@@ -1,5 +1,12 @@
+/**
+ * @param sourceNode
+ * @param targetNode
+ * @param attrName
+ * @param baseUrl
+ */
 export function convertRelativeToAbsoluteAttributeUrl(sourceNode, targetNode, attrName, baseUrl) {
   const relUrl = sourceNode.getAttribute(attrName);
   const absUrl = new URL(relUrl, baseUrl);
+
   targetNode.setAttribute(attrName, absUrl);
 }
