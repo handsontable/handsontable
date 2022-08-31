a408a88e668e315a9064b3a6397806afffd1cabb
Marcin Warpechowski <warpech@gmail.com>
2022-08-31 13:51:25 +0200

    manual lint fixes

--- docs/.vuepress/components/DemoReact.vue
+++ docs/.vuepress/components/DemoReact.vue
@@ -24,7 +24,7 @@ export default {
         const doc = parser.parseFromString(html, 'text/html');
 
         const headNodes = Array.from(doc.head.childNodes).filter((node) => {
-          if (node.nodeName == 'LINK' && node.rel === 'stylesheet') {
+          if (node.nodeName === 'LINK' && node.rel === 'stylesheet') {
             convertRelativeToAbsoluteAttributeUrl(node, node, 'href', url);
 
             return true;
@@ -34,7 +34,7 @@ export default {
         });
 
         const bodyNodes = Array.from(doc.body.childNodes).map((node) => {
-          if (node.nodeName == 'SCRIPT') {
+          if (node.nodeName === 'SCRIPT') {
             // scripts need to be cloned, otherwise Chrome refuses to run them
             const newNode = document.createElement('script');
 
@@ -52,8 +52,8 @@ export default {
         this.$el.append(...headNodes, ...bodyNodes);
       })
       .catch((err) => {
-        // There was an error
-        console.warn('Something went wrong.', err);
+        // eslint-disable-next-line no-console
+        console.warn('Fetching the example failed.', err);
       });
   },
 };
--- docs/.vuepress/components/demo-common.js
+++ docs/.vuepress/components/demo-common.js
@@ -1,8 +1,10 @@
-/**
- * @param sourceNode
- * @param targetNode
- * @param attrName
- * @param baseUrl
+/** .........
+ * Given a base URL, transform a URL in a DOM node's attribute from a relative to an absolute value.
+ *
+ * @param {Node} sourceNode The DOM node from which to get the attribute value.
+ * @param {Node} targetNode The DOM node to which set the attribute value.
+ * @param {string} attrName The name of the attribute that contains a URL to be transformed.
+ * @param {string} baseUrl The base URL that will be the basis of the relative to absolute transformation.
  */
 export function convertRelativeToAbsoluteAttributeUrl(sourceNode, targetNode, attrName, baseUrl) {
   const relUrl = sourceNode.getAttribute(attrName);
