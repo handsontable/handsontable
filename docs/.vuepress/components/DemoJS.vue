211959bee4b3c1bc2bdeaaa45d21fe5b8ff628fd
Marcin Warpechowski <warpech@gmail.com>
2022-08-31 10:16:53 +0200

    replace inline scripts with a component
    because inline scripts did not work correctly in the production build of VuePress

---
+++ docs/.vuepress/components/DemoJS.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="example-container">
+    <div id="example"></div>
+    <script defer src="https://examples.handsontable.com/examples/12.1.1/docs/js/demo/src.3f3ef8b8.js"></script>
+    <style>
+      @import 'https://examples.handsontable.com/examples/12.1.1/docs/js/demo/src.4d67a99b.css';
+    </style>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemoJS',
+};
+</script>
