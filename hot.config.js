b3b2fb5b12ef9a64f881690517d12c2228fe9aa3
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-09-20 16:06:08 +0200

    Rebuild package v12.1.3

--- hot.config.js
+++ hot.config.js
@@ -5,5 +5,5 @@ module.exports = {
   HOT_VERSION: '12.1.3',
   HOT_PACKAGE_NAME: 'handsontable',
   HOT_BUILD_DATE: moment().format('DD/MM/YYYY HH:mm:ss'),
-  HOT_RELEASE_DATE: '19/09/2022',
+  HOT_RELEASE_DATE: '22/09/2022',
 };
