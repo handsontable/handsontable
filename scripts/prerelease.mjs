6ac6507b693c5dc1fe8a6e7a2b6f6e6ea1c7ddd4
maciek <maciej.b@handsontable.com>
2022-10-17 22:04:13 +0200

    publish in npm after test 22:04

--- scripts/prerelease.mjs
+++ scripts/prerelease.mjs
@@ -1,9 +1,12 @@
 import { displayErrorMessage } from './utils/console.mjs';
 import { setVersion } from './utils/pre-release.mjs';
+import mainPackageJson from '../../package.json' assert { type: 'json' };
 
 import moment from 'moment';
-import fs from 'fs-extra';
+//import fs from 'fs-extra';
 import ChildProcess from 'child_process';
+
+const workspacePackages = mainPackageJson.workspaces;
 
 const PACKAGES_SETTINGS = {
   MAIN: {
@@ -28,7 +31,7 @@ const PACKAGES_SETTINGS = {
   },
 };
 
-const FILE_NAME = 'package.json';
+// const FILE_NAME = 'package.json';
 const MAIN_PATH = process.cwd();
 
 const hash = ChildProcess.execSync('git rev-parse HEAD')
@@ -54,7 +57,7 @@ Object.entries(PACKAGES_SETTINGS).forEach(([key, item]) => {
 }); 
 */
 
-setVersion(newVersionNumber);
+setVersion(newVersionNumber, workspacePackages);
 
 Object.entries(PACKAGES_SETTINGS).forEach(([key, item]) => {
   process.chdir(MAIN_PATH);
