6f0ec66add33d2830e28ec0a59785be02453bd05
maciek <maciej.b@handsontable.com>
2022-10-18 22:31:41 +0200

    publish in npm after test 22:31

--- scripts/prerelease.mjs
+++ scripts/prerelease.mjs
@@ -3,34 +3,6 @@ import { setVersion } from './utils/pre-release.mjs';
 
 import execa from 'execa';
 import moment from 'moment';
-//import fs from 'fs-extra';
-//import ChildProcess from 'child_process';
-
-/* const PACKAGES_SETTINGS = {
-  MAIN: {
-    NAME: 'tmp-hot',
-    PATH: 'handsontable/tmp',
-  },
-  REACT: {
-    NAME: 'tmp-hot-react',
-    PATH: 'wrappers/react',
-  },
-  ANGULAR: {
-    NAME: 'tmp-hot-angular',
-    PATH: 'wrappers/angular',
-  },
-  VUE: {
-    NAME: 'tmp-hot-vue',
-    PATH: 'wrappers/vue',
-  },
-  VUE3: {
-    NAME: 'tmp-hot-vue3',
-    PATH: 'wrappers/vue3',
-  },
-}; */
-
-// const FILE_NAME = 'package.json';
-// const MAIN_PATH = process.cwd();
 
 const { stdout: hash } = await execa.command('git rev-parse HEAD');
 const date = moment().format('YYYYMMDD');
@@ -45,5 +17,5 @@ try {
   await execa.command('npm run publish-all');
 } catch (error) {
   displayErrorMessage(`error during publishing`);
-  process.exit(error.exitCode);
+  process.exit(error);
 }
