c93df3a1f52c4fe7278a8e19956e8f78a975a386
maciek <maciej.b@handsontable.com>
2022-10-18 23:47:54 +0200

    publish in npm after test 23:47

--- scripts/prerelease.mjs
+++ scripts/prerelease.mjs
@@ -1,14 +1,12 @@
 import { displayErrorMessage } from './utils/console.mjs';
 import { setVersion } from './utils/pre-release.mjs';
 
-import {
-  spawnProcess
-} from './utils/processes.mjs';
+import { spawnProcess } from './utils/processes.mjs';
 import moment from 'moment';
 
-const { stdout: hash } = await spawnProcess('git rev-parse HEAD');
+const hash = await spawnProcess('git rev-parse HEAD');
 const date = moment().format('YYYYMMDD');
-const newVersionNumber = `0.0.0-next-dev.${hash
+const newVersionNumber = `0.0.0-next-dev.${hash.stdout
   .toString()
   .trim()
   .slice(0, 7)}-${date}`;
