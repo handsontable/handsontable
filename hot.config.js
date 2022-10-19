9cbe74d40e87990b25cc234bca042f68d5065cff
maciek <maciej.b@handsontable.com>
2022-10-19 13:41:33 +0200

    publish in npm after test 13:41

--- hot.config.js
+++ hot.config.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 
 module.exports = {
   HOT_FILENAME: 'handsontable',
-  HOT_VERSION: '0.0.0-next-dev.acb49ff-20221019',
+  HOT_VERSION: '12.1.3',
   HOT_PACKAGE_NAME: 'handsontable',
   HOT_BUILD_DATE: moment().format('DD/MM/YYYY HH:mm:ss'),
   HOT_RELEASE_DATE: '22/09/2022',
