ac4730a1c08df423b202b7b2b44c48c62781dcc3
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-10-26 09:44:17 +0200

    Rename handsontable package name to tmp-hot

--- scripts/swap-package-links.mjs
+++ scripts/swap-package-links.mjs
@@ -17,8 +17,8 @@ if (pkgName) {
 }
 
 const PACKAGE_LOCATIONS = new Map([
-  ['handsontable', './handsontable/tmp'],
-  ['@handsontable/angular', './wrappers/angular/dist/hot-table'],
+  // ['handsontable', './handsontable/tmp'],
+  // ['@handsontable/angular', './wrappers/angular/dist/hot-table'],
   ['tmp-hot', './handsontable/tmp'],
   ['tmp-hot-angular', './wrappers/angular/dist/hot-table']
 ]);
