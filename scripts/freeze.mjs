6b35b4f377ac1ae8e95327fcb1103f5d90e85c5c
maciek <maciej.b@handsontable.com>
2022-11-16 11:41:01 +0100

    improve angular package

--- scripts/freeze.mjs
+++ scripts/freeze.mjs
@@ -110,22 +110,22 @@ displaySeparator();
   await spawnProcess('npm run all build -- --e examples');
 
   // Install the "next" package for examples
-  await spawnProcess('npm run examples:install next');
+  // await spawnProcess('npm run examples:install next');
 
   // Build the examples/next directory.
-  await spawnProcess('npm run in examples build');
+  // await spawnProcess('npm run in examples build');
 
   // Test all packages.
-  await spawnProcess('npm run all test');
+  // await spawnProcess('npm run all test');
 
   // Verify if the bundles have the same (and correct) version.
-  await spawnProcess('node --experimental-json-modules ./scripts/verify-bundles.mjs');
+  // await spawnProcess('node --experimental-json-modules ./scripts/verify-bundles.mjs');
 
   // Generate the CHANGELOG.md file.
-  await spawnProcess('npm run changelog consume', { stdin: 'pipe' });
+  // await spawnProcess('npm run changelog consume', { stdin: 'pipe' });
 
   // Create the examples/[version] directory.
-  await spawnProcess(`npm run examples:version ${finalVersion}`);
+  // await spawnProcess(`npm run examples:version ${finalVersion}`);
 
   if (argv.commit === true) {
     // Commit the changes to the release branch.
