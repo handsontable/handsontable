4d6700c3372a17fdfd66da7f6f7fce54b3758c20
maciek <maciej.b@handsontable.com>
2022-11-22 20:00:19 +0100

    improve workflow file

--- scripts/visual-test.mjs
+++ scripts/visual-test.mjs
@@ -1,6 +1,6 @@
 import { spawnProcess } from './utils/processes.mjs';
 
-process.chdir("./visual-test");
+process.chdir('./visual-test');
 await spawnProcess('npm install');
-await spawnProcess('npx playwright install');
+await spawnProcess('npx playwright install --with-deps');
 await spawnProcess('npx playwright test');
