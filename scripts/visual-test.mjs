3e24c2406ed21db3d3083f6ba1eed7509b40f04c
maciek <maciej.b@handsontable.com>
2022-11-23 12:56:41 +0100

    change version of packages

--- scripts/visual-test.mjs
+++ scripts/visual-test.mjs
@@ -4,7 +4,7 @@ process.chdir('./visual-test');
 await spawnProcess('npm install');
 await spawnProcess('npx playwright install --with-deps');
 await spawnProcess('npx playwright test');
-await spawnProcess('npm install --save-dev @argos-ci/cli');
-console.log(process);
+// await spawnProcess('npm install --save-dev @argos-ci/cli');
+// console.log(process);
 await spawnProcess('npx @argos-ci/cli upload ./tests');
 
