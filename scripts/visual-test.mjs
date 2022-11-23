a3f259c261b481fb4d70eaf539c1cc0f39c9f095
maciek <maciej.b@handsontable.com>
2022-11-23 02:13:41 +0100

    send screenshots to argos

--- scripts/visual-test.mjs
+++ scripts/visual-test.mjs
@@ -3,5 +3,7 @@ import { spawnProcess } from './utils/processes.mjs';
 process.chdir('./visual-test');
 await spawnProcess('npm install');
 await spawnProcess('npx playwright install --with-deps');
-await spawnProcess('npm install --save-dev @argos-ci/cli');
 await spawnProcess('npx playwright test');
+await spawnProcess('npm install --save-dev @argos-ci/cli');
+await spawnProcess('npx @argos-ci/cli upload ./visual-test/tests');
+
