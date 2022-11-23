b1180ec33e1382fa51a9d028b15c10560b511a5e
maciek <maciej.b@handsontable.com>
2022-11-23 02:38:11 +0100

    change snapshots folder

--- scripts/visual-test.mjs
+++ scripts/visual-test.mjs
@@ -5,5 +5,5 @@ await spawnProcess('npm install');
 await spawnProcess('npx playwright install --with-deps');
 await spawnProcess('npx playwright test');
 await spawnProcess('npm install --save-dev @argos-ci/cli');
-await spawnProcess('npx @argos-ci/cli upload ./snapshots');
+await spawnProcess('npx @argos-ci/cli upload snapshots');
 
