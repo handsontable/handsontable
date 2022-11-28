b202c3cbad2c752466890d3b50dd40675f1fdd2e
maciek <maciej.b@handsontable.com>
2022-11-28 23:10:31 +0100

    auto refresh all of images in other branches if develop has been updated

---
+++ scripts/visual-test-update-comparision-on-other-branches.mjs
@@ -0,0 +1,26 @@
+/* eslint-disable max-len */
+import { spawnProcess } from './utils/processes.mjs';
+
+const baseBranches = ['develop'];
+const currentBranchName = (await spawnProcess('git rev-parse --abbrev-ref HEAD', { silent: true })).stdout;
+const isBaseBranch = baseBranches.includes(currentBranchName);
+
+console.log('isBaseBranch', isBaseBranch);
+
+if (isBaseBranch) {
+  const listOfActiveBranches = (await spawnProcess('git branch', { silent: true })).stdout;
+
+  // eslint-disable-next-line quotes
+  listOfActiveBranches.split("\n").forEach((item) => {
+    item = item.trim();
+    console.log(item);
+
+    if (item.startsWith('*')) {
+      return;
+    }
+
+    spawnProcess(`git checkout ${item}`);
+    spawnProcess('git commit --amend --no-edit');
+    spawnProcess(`git push origin ${item}`);
+  });
+}
--- scripts/visual-test-upload-screenshots.mjs
+++ scripts/visual-test-upload-screenshots.mjs
@@ -1,34 +1,11 @@
 /* eslint-disable max-len */
 import { spawnProcess } from './utils/processes.mjs';
 
-const baseBranches = ['develop'];
-
 const commitSha = (await spawnProcess('git rev-parse HEAD', { silent: true }))
   .stdout
   .toString()
   .slice(0, 7);
 
-const currentBranchName = (await spawnProcess('git rev-parse --abbrev-ref HEAD', { silent: true })).stdout;
-
 process.chdir('./visual-test');
 await spawnProcess('npx @argos-ci/cli upload tests/screenshots');
 await spawnProcess(`npx viswiz build --image-dir ./tests/screenshots --message last-commit-message --revision ${commitSha}`);
-
-const isBaseBranch = baseBranches.includes(currentBranchName);
-
-if (isBaseBranch) {
-  const listOfActiveBranches = (await spawnProcess('git branch', { silent: true })).stdout;
-
-  // eslint-disable-next-line quotes
-  listOfActiveBranches.split("\n").forEach((item) => {
-    item = item.trim();
-
-    if (item.startsWith('*')) {
-      return;
-    }
-
-    spawnProcess(`git checkout ${item}`);
-    spawnProcess('git commit --amend --no-edit');
-    spawnProcess(`git push origin ${item}`);
-  });
-}
