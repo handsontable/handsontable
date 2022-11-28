91f3a6eb6937baa2b28a4cac54332bc7ef77c9da
maciek <maciej.b@handsontable.com>
2022-11-28 17:33:35 +0100

    add viswiz to  workflow

---
+++ scripts/visual-test-upload-screenshots.mjs
@@ -0,0 +1,5 @@
+import { spawnProcess } from './utils/processes.mjs';
+
+process.chdir('./visual-test');
+await spawnProcess('npx @argos-ci/cli upload tests/screenshots');
+await spawnProcess('npx viswiz build --image-dir ./tests/screenshots --message last-commit-message -revision revision');
--- scripts/visual-test.mjs
+++ scripts/visual-test.mjs
@@ -4,5 +4,3 @@ process.chdir('./visual-test');
 await spawnProcess('npm install');
 await spawnProcess('npx playwright install --with-deps');
 await spawnProcess('npx playwright test');
-await spawnProcess('npx @argos-ci/cli upload tests/screenshots');
-await spawnProcess('npx viswiz build --image-dir ./tests/screenshots --message last-commit-message -revision revision');
