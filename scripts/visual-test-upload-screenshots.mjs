3f9ae1e2e066c9449118cb9cbe0866c13087cbe9
maciek <maciej.b@handsontable.com>
2022-11-28 19:25:22 +0100

    changes test

--- scripts/visual-test-upload-screenshots.mjs
+++ scripts/visual-test-upload-screenshots.mjs
@@ -2,8 +2,13 @@
 import moment from 'moment';
 import { spawnProcess } from './utils/processes.mjs';
 
-const currentDate = moment().format('hhmmss');
+// const currentDate = moment().format('hhmmss');
+
+const commitSha = (await spawnProcess('git rev-parse HEAD', { silent: true }))
+  .stdout
+  .toString()
+  .slice(0, 7);
 
 process.chdir('./visual-test');
 await spawnProcess('npx @argos-ci/cli upload tests/screenshots');
-await spawnProcess(`npx viswiz build --image-dir ./tests/screenshots --message last-commit-message --revision ${currentDate}`);
+await spawnProcess(`npx viswiz build --image-dir ./tests/screenshots --message last-commit-message --revision ${commitSha}`);
