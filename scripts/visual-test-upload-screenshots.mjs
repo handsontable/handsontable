11ae4616f607495b0a862d5f0587df7c1143a64d
maciek <maciej.b@handsontable.com>
2022-11-28 18:17:59 +0100

    add viswiz to  workflow

--- scripts/visual-test-upload-screenshots.mjs
+++ scripts/visual-test-upload-screenshots.mjs
@@ -1,6 +1,12 @@
 /* eslint-disable max-len */
 import { spawnProcess } from './utils/processes.mjs';
 
+const commitSha = (await spawnProcess('git rev-parse HEAD', { silent: true }))
+  .stdout
+  .toString()
+  .slice(0, 7);
+
 process.chdir('./visual-test');
 await spawnProcess('npx @argos-ci/cli upload tests/screenshots');
-await spawnProcess('npx viswiz build --image-dir ./tests/screenshots --message last-commit-message --revision revision');
+await spawnProcess(`npx viswiz build --image-dir ./tests/screenshots --message last-commit-message --revision ${commitSha}`);
+
