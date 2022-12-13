12f2a585b3525ec2a35d3a232d52d3170126ff0f
maciek <maciej.b@handsontable.com>
2022-12-13 13:50:23 +0100

    remove unccessary services

--- visual-test/scripts/visual-tests-upload.mjs
+++ visual-test/scripts/visual-tests-upload.mjs
@@ -6,6 +6,3 @@ import execa from 'execa';
 
 console.log('Upload to Argos');
 await execa.command('npx @argos-ci/cli upload tests/screenshots', { stdout: 'inherit' });
-
-console.log('Upload to Percy');
-await execa.command('npx percy upload ./tests/screenshots', { stdout: 'inherit' });
