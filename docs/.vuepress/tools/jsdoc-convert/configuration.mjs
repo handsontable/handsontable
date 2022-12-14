a019eaf0a88062ecd3711620d027ea18ecb87e31
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2022-12-14 13:17:08 +0100

    Add missing page id for NestedRows.md file

--- docs/.vuepress/tools/jsdoc-convert/configuration.mjs
+++ docs/.vuepress/tools/jsdoc-convert/configuration.mjs
@@ -273,6 +273,7 @@ export default {
       metaTitle: 'NestedRows - JavaScript Data Grid | Handsontable',
       description: 'Use the NestedRows plugin with its API options, members and methods to display data in nested structures (where data spans multiple columns).',
       react: {
+        id: 'fvo6cybt',
         metaTitle: 'NestedRows - React Data Grid | Handsontable',
       },
     },
