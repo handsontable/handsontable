fb55721f69d977b9a8f9b37a79a54ff6e5f4505e
maciek <maciej.b@handsontable.com>
2022-12-19 21:35:22 +0100

    code cleaning

--- visual-tests/scripts/visual-tests-upload.mjs
+++ visual-tests/scripts/visual-tests-upload.mjs
@@ -1,8 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable no-restricted-globals */
-/* eslint-disable max-len */
-// import { spawnProcess } from './utils/processes.mjs';
 import execa from 'execa';
 
 console.log('Upload to Argos');
-await execa.command('npx @argos-ci/cli upload tests/screenshots', { stdout: 'inherit' });
+await execa.command('npx @argos-ci/cli upload tests/screenshots', { stdio: 'inherit' });
