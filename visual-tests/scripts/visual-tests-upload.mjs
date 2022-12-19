5cf1f7ad5e79e470a8b1bd6a8cbcb66e421323fc
maciek <maciej.b@handsontable.com>
2022-12-19 19:44:21 +0100

    execa.command => execa

--- visual-tests/scripts/visual-tests-upload.mjs
+++ visual-tests/scripts/visual-tests-upload.mjs
@@ -3,4 +3,4 @@
 import execa from 'execa';
 
 console.log('Upload to Argos');
-await execa.command('npx @argos-ci/cli upload tests/screenshots', { stdout: 'inherit' });
+await execa('npx @argos-ci/cli upload tests/screenshots', { stdout: 'inherit' });
