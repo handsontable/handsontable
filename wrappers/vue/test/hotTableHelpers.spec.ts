c7b0133066e22f7e0aa4e140abf1eb0607045d0d
Taiji Baba <master@tonextone.com>
2022-12-21 12:41:04 +0000

    Fix "TypeError: Converting circular structure to JSON" when passing `hyperformulaInstance`

--- wrappers/vue/test/hotTableHelpers.spec.ts
+++ wrappers/vue/test/hotTableHelpers.spec.ts
@@ -70,4 +70,21 @@ describe('prepareSettings', () => {
     expect(preparedSettings.settings).toBe(void 0);
     expect(preparedSettings.wrapperRendererCacheSize).toBe(void 0);
   });
+
+  it('should handle settings with circular structure', () => {
+
+    const circularStructure = {foo: 'bar'};
+    circularStructure.myself = circularStructure;
+
+    const propsMock_circular = {
+      readOnly: true,
+      whatever: circularStructure
+    };
+
+    const preparedSettings = prepareSettings(propsMock_circular, {});
+
+    expect(preparedSettings.readOnly).toBe(true);
+    expect(preparedSettings.whatever.foo).toBe('bar');
+    expect(preparedSettings.whatever.myself.foo).toBe('bar');
+  });
 });
