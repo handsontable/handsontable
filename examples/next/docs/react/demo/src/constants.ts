f79ceb29a8e758f3024121cbd7e909646241c0a0
maciek <maciej.b@handsontable.com>
2023-02-03 00:21:42 +0100

    React demo improvements

--- examples/next/docs/react/demo/src/constants.ts
+++ examples/next/docs/react/demo/src/constants.ts
@@ -1,55 +1,55 @@
 export const data = [{
-  companyName: "Tagcat",
-  country: "United Kingdom",
-  name: "Classic Vest",
-  sellDate: "11/10/2020",
-  orderId: "01-2331942",
+  companyName: 'Tagcat',
+  country: 'United Kingdom',
+  name: 'Classic Vest',
+  sellDate: '11/10/2020',
+  orderId: '01-2331942',
   inStock: true,
-  quantity: "172",
+  quantity: '172',
   progress: 20,
   rating: 2
 },
  {
-  companyName: "Zoomzone",
-  country: "Indonesia",
-  name: "Cycling Cap",
-  sellDate: "03/05/2020",
-  orderId: "88-2768633",
+  companyName: 'Zoomzone',
+  country: 'Indonesia',
+  name: 'Cycling Cap',
+  sellDate: '03/05/2020',
+  orderId: '88-2768633',
   inStock: true,
-  quantity: "188",
+  quantity: '188',
   progress: 60,
   rating: 2
 }, {
-  companyName: "Meeveo",
-  country: "United States",
-  name: "Full-Finger Gloves",
-  sellDate: "27/03/2020",
-  orderId: "51-6775945",
+  companyName: 'Meeveo',
+  country: 'United States',
+  name: 'Full-Finger Gloves',
+  sellDate: '27/03/2020',
+  orderId: '51-6775945',
   inStock: true,
-  quantity: "162",
+  quantity: '162',
   progress: 10,
   rating: 3
 }, {
-  companyName: "Buzzdog",
-  country: "Philippines",
-  name: "HL Mountain Frame",
-  sellDate: "29/08/2020",
-  orderId: "44-4028109",
+  companyName: 'Buzzdog',
+  country: 'Philippines',
+  name: 'HL Mountain Frame',
+  sellDate: '29/08/2020',
+  orderId: '44-4028109',
   inStock: true,
-  quantity: "133",
+  quantity: '133',
   progress: 70,
   rating: 1
 }, {
-  companyName: "Katz",
-  country: "India",
-  name: "Half-Finger Gloves",
-  sellDate: "02/10/2020",
-  orderId: "08-2758492",
+  companyName: 'Katz',
+  country: 'India',
+  name: 'Half-Finger Gloves',
+  sellDate: '02/10/2020',
+  orderId: '08-2758492',
   inStock: true,
-  quantity: "87",
+  quantity: '87',
   progress: 10,
   rating: 3
 }];
 
-export const SELECTED_CLASS = "selected";
-export const ODD_ROW_CLASS = "odd";
+export const SELECTED_CLASS = 'selected';
+export const ODD_ROW_CLASS = 'odd';
--- examples/next/docs/react/demo/src/index.tsx
+++ examples/next/docs/react/demo/src/index.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "pikaday/css/pikaday.css";
-import "./styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'pikaday/css/pikaday.css';
+import './styles.css';
 import Handsontable from 'handsontable';
-import { HotTable, HotColumn } from "@handsontable/react";
-import { data as defaultData } from "./constants";
-import { ProgressBarRenderer } from "./renderers/ProgressBar";
-import { StarsRenderer } from "./renderers/Stars";
+import { HotTable, HotColumn } from '@handsontable/react';
+import { data as defaultData } from './constants';
+import { ProgressBarRenderer } from './renderers/ProgressBar';
+import { StarsRenderer } from './renderers/Stars';
 
 import {
   drawCheckboxInRowHeaders,
   addClassesToRows,
   changeCheckboxCell,
   alignHeaders
-} from "./hooksCallbacks";
+} from './hooksCallbacks';
 
-import "handsontable/dist/handsontable.min.css";
+import 'handsontable/dist/handsontable.min.css';
 
 const data: Handsontable.RowObject[] = [];
 
@@ -30,15 +30,15 @@ const App = () => {
       height={450}
       colWidths={[140, 192, 100, 90, 90, 110, 97, 100, 126]}
       colHeaders={[
-        "Company name",
-        "Name",
-        "Sell date",
-        "In stock",
-        "Qty",
-        "Progress",
-        "Rating",
-        "Order ID",
-        "Country"
+        'Company name',
+        'Name',
+        'Sell date',
+        'In stock',
+        'Qty',
+        'Progress',
+        'Rating',
+        'Order ID',
+        'Country'
       ]}
       dropdownMenu={true}
       hiddenColumns={{
@@ -60,7 +60,7 @@ const App = () => {
       <HotColumn data="sellDate" type="date" allowInvalid={false} />
       <HotColumn data="inStock" type="checkbox" className="htCenter" />
       <HotColumn data="quantity" type="numeric" />
-      <HotColumn data="progress" className="htMiddle" type="numeric">
+      <HotColumn data="progress" className="htMiddle" type="numeric" allowInvalid={false} >
         {/* @ts-ignore Element inherits some props. It's hard to type it. */}
         <ProgressBarRenderer hot-renderer />
       </HotColumn>
@@ -74,7 +74,7 @@ const App = () => {
   );
 };
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
 
 console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate}) Wrapper: v${HotTable.version} React: v${React.version}`);
