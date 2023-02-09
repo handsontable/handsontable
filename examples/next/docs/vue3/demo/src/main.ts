899fbebf5c1a18256452c9e62d042df2a5cc2304
maciek <maciej.b@handsontable.com>
2023-02-09 15:45:52 +0100

    HoT Vue3 demo

--- examples/next/docs/vue3/demo/src/main.ts
+++ examples/next/docs/vue3/demo/src/main.ts
@@ -1,8 +1,8 @@
+import Handsontable from 'handsontable';
+import { HotTable } from '@handsontable/vue3';
 import { createApp } from 'vue'
 import App from './App.vue'
-//import Handsontable from 'handsontable';
-//import { HotTable } from '@handsontable/vue';
 
-//console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate}) Wrapper: v${HotTable.version} Vue: v 3`);
+console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate}) Wrapper: v${HotTable.version} Vue: 3`);
 
 createApp(App).mount('#app');
