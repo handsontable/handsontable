cfb229a8a4297329b6aa93beba7b2a31dc7e184b
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2023-03-22 14:45:11 +0100

    Missing changes

--- examples/next/docs/react/basic-example/src/index.js
+++ examples/next/docs/react/basic-example/src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 
-
 const container = document.getElementById("container");
 const root = createRoot(container);
 
--- examples/next/visual-tests/react/basic-example/src/index.js
+++ examples/next/visual-tests/react/basic-example/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const container = document.getElementById('container');
+const root = createRoot(container);
+
+root.render(<App />);
