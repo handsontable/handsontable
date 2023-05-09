538777fde59f7301d92ca63baa6b4da2db7ae3d0
Kamil Weber <kamil.weber@crafton.pl>
2023-05-09 15:29:18 +0200

    Add: Vue 3 docs

---
+++ examples/12.3.3/docs/vue3/basic-example/src/main.js
@@ -0,0 +1,12 @@
+import { createApp, h, version } from "vue";
+import App from './App.vue'
+import Handsontable from 'handsontable'
+import { HotTable } from '@handsontable/vue3'
+
+console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate}) Wrapper: v${HotTable.version} Vue: v${version}`);
+
+// Vue.config.productionTip = false
+
+createApp({
+  render: () => h(App)
+}).mount("#app");
