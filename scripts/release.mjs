726836cedad0506163f7b89f3b4dd7bfcc13149c
Jan Siegel <js.ziggle@gmail.com>
2023-06-21 12:26:16 +0200

    Correct lint errors.

--- scripts/release.mjs
+++ scripts/release.mjs
@@ -93,7 +93,7 @@ displaySeparator();
       const linesCount = parseInt((await execa.command('wc -l < .gitignore', {
         cwd: 'docs',
         shell: true
-      })).stdout);
+      })).stdout, 10);
 
       // Remove "/content/api/" entry from the ./docs/.gitignore file so generated API
       // docs can be committed to the branch.
@@ -105,11 +105,13 @@ displaySeparator();
       const newLinesCount = parseInt((await execa.command('wc -l < .gitignore', {
         cwd: 'docs',
         shell: true
-      })).stdout);
+      })).stdout, 10);
 
       if (newLinesCount + 1 !== linesCount && !Number.isNaN(newLinesCount)) {
-        displayErrorMessage('The docs/.gitignore file modified by the release script is incorrect. Continuing the script' +
-          ' execution would result in a broken documentation build.');
+        displayErrorMessage(
+          'The docs/.gitignore file modified by the release script is incorrect. Continuing the script' +
+          ' execution would result in a broken documentation build.'
+        );
         process.exit(1);
       }
     }
