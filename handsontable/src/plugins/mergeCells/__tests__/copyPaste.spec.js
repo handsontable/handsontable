6ed95269abe06cbdb1b99932dad4aafba7ef1234
Wojciech Szymanski <141330+wszymanski@users.noreply.github.com>
2023-08-01 10:45:18 +0200

    Added extra e2e test showing that values are changed after performing an undo action

--- handsontable/src/plugins/mergeCells/__tests__/copyPaste.spec.js
+++ handsontable/src/plugins/mergeCells/__tests__/copyPaste.spec.js
@@ -428,4 +428,60 @@ describe('MergeCells copy and paste', () => {
 
     expect(mergeCellsPlugin.mergedCellsCollection.mergedCells.length).toEqual(2);
   });
+
+  it('should restore a merge area after performing undo for paste action which already unmerged the area', () => {
+    const hot = handsontable({
+      data: Handsontable.helper.createSpreadsheetData(8, 8),
+      rowHeaders: true,
+      colHeaders: true,
+      mergeCells: [
+        { row: 1, col: 1, rowspan: 2, colspan: 2 },
+        { row: 3, col: 3, rowspan: 3, colspan: 3 }
+      ],
+    });
+
+    const clipboardEvent = getClipboardEvent();
+    const copyPastePlugin = getPlugin('CopyPaste');
+
+    clipboardEvent.clipboardData.setData('text/html', [
+      '<table><tbody><tr><td>A1</td><td>B1</td></tr></tbody></table>'
+    ].join('\r\n'));
+
+    selectCell(1, 1);
+
+    copyPastePlugin.onPaste(clipboardEvent);
+
+    hot.undo();
+
+    // TODO: It should be fixed (actions of changing values and toggling a merge state should be batched)
+    // expect(getCell(1, 1).rowSpan).toBe(2);
+    // expect(getCell(1, 1).colSpan).toBe(2);
+
+    expect(getData()).toEqual([
+      ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1'],
+      ['A2', 'B2', null, 'D2', 'E2', 'F2', 'G2', 'H2'],
+      ['A3', null, null, 'D3', 'E3', 'F3', 'G3', 'H3'],
+      ['A4', 'B4', 'C4', 'D4', null, null, 'G4', 'H4'],
+      ['A5', 'B5', 'C5', null, null, null, 'G5', 'H5'],
+      ['A6', 'B6', 'C6', null, null, null, 'G6', 'H6'],
+      ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'],
+      ['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8']
+    ]);
+
+    hot.redo();
+
+    expect(getCell(1, 1).rowSpan).toBe(1);
+    expect(getCell(1, 1).colSpan).toBe(1);
+
+    expect(getData()).toEqual([
+      ['A1', 'B1', 'C1', 'D1', 'E1', 'F1', 'G1', 'H1'],
+      ['A2', 'A1', 'B1', 'D2', 'E2', 'F2', 'G2', 'H2'],
+      ['A3', 'A1', 'B1', 'D3', 'E3', 'F3', 'G3', 'H3'],
+      ['A4', 'B4', 'C4', 'D4', null, null, 'G4', 'H4'],
+      ['A5', 'B5', 'C5', null, null, null, 'G5', 'H5'],
+      ['A6', 'B6', 'C6', null, null, null, 'G6', 'H6'],
+      ['A7', 'B7', 'C7', 'D7', 'E7', 'F7', 'G7', 'H7'],
+      ['A8', 'B8', 'C8', 'D8', 'E8', 'F8', 'G8', 'H8']
+    ]);
+  });
 });
