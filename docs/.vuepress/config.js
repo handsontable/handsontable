658f813f1791b9a6bf2907425d5095e23e901e61
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-08-21 12:23:09 +0200

    Revert CookieBot

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -62,6 +62,12 @@ module.exports = {
       crossorigin: ''
     }],
     ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    // Cookiebot - cookie consent popup
+    ['script', {
+      id: 'Cookiebot',
+      src: 'https://consent.cookiebot.com/uc.js',
+      'data-cbid': 'ef171f1d-a288-433f-b680-3cdbdebd5646'
+    }],
     ['script', {}, `\
 var DOCS_VERSION = '${getThisDocsVersion()}';
 `],
