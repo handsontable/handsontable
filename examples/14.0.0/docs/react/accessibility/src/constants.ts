d97b1561331bc1ea068a63018076ebe4677889ba
Jan Siegel <js.ziggle@gmail.com>
2023-10-27 14:14:42 +0200

    14.0.0

---
+++ examples/14.0.0/docs/react/accessibility/src/constants.ts
@@ -0,0 +1,3 @@
+
+export const SELECTED_CLASS = "selected";
+export const ODD_ROW_CLASS = "odd";
---
+++ examples/14.0.0/docs/react/accessibility/src/data.ts
@@ -0,0 +1,60 @@
+import {
+  randProductName,
+  randCompanyName,
+  randPastDate,
+  randBoolean,
+  randNumber,
+  randCountry,
+  seed,
+} from "@ngneat/falso";
+
+seed("42219");
+
+export const data = [
+  {
+    companyName: randCompanyName({ maxCharCount: 18 }),
+    productName: randProductName({ maxCharCount: 18 }),
+    sellDate: randPastDate().toLocaleDateString(),
+    inStock: randBoolean(),
+    qty: randNumber({ max: 500 }),
+    progress: randNumber({ max: 10 }),
+    rating: randNumber({ max: 5 }),
+    orderId: `${randNumber({ max: 2, min: 2 })}-${randNumber({
+      max: 7,
+      min: 7,
+    })}`,
+    country: "United Kingdom",
+    __children: [
+      {
+        companyName: randCompanyName({ maxCharCount: 18 }),
+      },
+      {
+        companyName: randCompanyName({ maxCharCount: 18 }),
+      },
+      {
+        companyName: randCompanyName({ maxCharCount: 18 }),
+      },
+    ],
+  },
+  ...Array.from({ length: 150 }, () => ({
+    companyName: randCompanyName({ maxCharCount: 18 }),
+    productName: randProductName({ maxCharCount: 18 }),
+    sellDate: randPastDate().toLocaleDateString(),
+    inStock: randBoolean(),
+    qty: randNumber({ max: 500 }),
+    progress: randNumber({ max: 10 }),
+    rating: randNumber({ max: 5 }),
+    orderId: `${randNumber({ max: 2, min: 2 })}-${randNumber({
+      max: 7,
+      min: 7,
+    })}`,
+    country: randCountry({ maxCharCount: 28 }),
+  })),
+];
+
+export const countries = data.reduce<string[]>((acc, curr) => {
+  if (acc.includes(curr.country)) {
+    return acc;
+  }
+  return [...acc, curr.country];
+}, []);
