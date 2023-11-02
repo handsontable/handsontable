a2a62872d1fedc385d39e07528b48eae33a5107e
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-11-02 14:18:21 +0100

    Cover with tests the fix for the `runOnlyIf` option

--- handsontable/src/shortcuts/__tests__/shortcutManager.spec.js
+++ handsontable/src/shortcuts/__tests__/shortcutManager.spec.js
@@ -594,4 +594,89 @@ describe('shortcutManager', () => {
       expect(firstSpy).toHaveBeenCalledTimes(0);
     });
   });
+
+  describe('`runOnlyIf` option', () => {
+    it('should execute the shortcut action when the option is not defined', () => {
+      const callback = jasmine.createSpy('callback');
+
+      handsontable();
+
+      const shortcutManager = getShortcutManager();
+      const gridContext = shortcutManager.addContext('test');
+
+      gridContext.addShortcut({
+        keys: [['enter']],
+        group: 'spy',
+        callback,
+      });
+
+      shortcutManager.setActiveContextName('test');
+      listen();
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should execute the shortcut action when the option returns `true`', () => {
+      const callback = jasmine.createSpy('callback');
+
+      handsontable();
+
+      const shortcutManager = getShortcutManager();
+      const gridContext = shortcutManager.addContext('test');
+
+      gridContext.addShortcut({
+        keys: [['enter']],
+        group: 'spy',
+        runOnlyIf: () => true,
+        callback,
+      });
+
+      shortcutManager.setActiveContextName('test');
+      listen();
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(1);
+    });
+
+    it('should not execute the shortcut action when the option returns different value than `true`', () => {
+      const callback = jasmine.createSpy('callback');
+      const runOnlyIf = jasmine.createSpy('runOnlyIf');
+
+      handsontable();
+
+      const shortcutManager = getShortcutManager();
+      const gridContext = shortcutManager.addContext('test');
+
+      gridContext.addShortcut({
+        keys: [['enter']],
+        group: 'spy',
+        runOnlyIf,
+        callback,
+      });
+
+      shortcutManager.setActiveContextName('test');
+      listen();
+
+      runOnlyIf.and.returnValue(false);
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(0);
+
+      runOnlyIf.and.returnValue(null);
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(0);
+
+      runOnlyIf.and.returnValue(undefined);
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(0);
+
+      runOnlyIf.and.returnValue([]);
+      keyDownUp('enter');
+
+      expect(callback).toHaveBeenCalledTimes(0);
+    });
+  });
 });
