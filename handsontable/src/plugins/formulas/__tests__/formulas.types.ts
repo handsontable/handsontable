60ea7786a918c8d1263af9061c624c47ea357cb7
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-12-12 13:38:33 +0100

    Add sheetName typing

--- handsontable/src/plugins/formulas/__tests__/formulas.types.ts
+++ handsontable/src/plugins/formulas/__tests__/formulas.types.ts
@@ -1,5 +1,22 @@
+import Hyperformula from 'hyperformula';
 import Handsontable from 'handsontable';
 
+new Handsontable(document.createElement('div'), {
+  formulas: {
+    engine: Hyperformula,
+  },
+});
+new Handsontable(document.createElement('div'), {
+  formulas: {
+    engine: Hyperformula.buildEmpty(),
+  },
+});
+new Handsontable(document.createElement('div'), {
+  formulas: {
+    engine: Hyperformula,
+    sheetName: 'sheet1',
+  },
+});
 const hot = new Handsontable(document.createElement('div'), {});
 const formulas = hot.getPlugin('formulas');
 
