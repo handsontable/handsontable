f9caa0851c183a62811aa254ea86e6f9dd98bae6
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-12-15 14:30:28 +0100

    Assign `version` property to parent component

--- wrappers/react/src/hotTable.tsx
+++ wrappers/react/src/hotTable.tsx
@@ -1,8 +1,14 @@
-import React, { useId, forwardRef } from 'react';
+import React, { useId, forwardRef, ForwardRefExoticComponent, RefAttributes } from 'react';
 import { HotTableClass } from './hotTableClass';
 import { HotTableProps } from './types';
 
-const HotTable = forwardRef<HotTableClass, HotTableProps>(({ children, ...props }, ref) => {
+interface Version {
+  version?: string;
+}
+
+type HotTable = ForwardRefExoticComponent<HotTableProps & RefAttributes<HotTableClass>> & Version;
+
+const HotTable: HotTable = forwardRef<HotTableClass, HotTableProps>(({ children, ...props }, ref) => {
   const generatedId = useId();
   const componentId = props.id ?? generatedId;
 
@@ -12,6 +18,8 @@ const HotTable = forwardRef<HotTableClass, HotTableProps>(({ children, ...props 
     </HotTableClass>
   );
 })
+
+HotTable.version = HotTableClass.version;
 
 export default HotTable;
 export { HotTable };
