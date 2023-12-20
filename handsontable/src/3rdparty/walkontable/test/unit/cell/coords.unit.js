3705a1e2dedaded0849f981345a07972abb45078
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2023-12-20 14:28:38 +0100

    Increase test coverage for CellCoords and CellRange classes

--- handsontable/src/3rdparty/walkontable/test/unit/cell/coords.unit.js
+++ handsontable/src/3rdparty/walkontable/test/unit/cell/coords.unit.js
@@ -1,9 +1,13 @@
 import CellCoords from 'walkontable/cell/coords';
 
+function createCoords(row, column) {
+  return new CellCoords(row, column);
+}
+
 describe('CellCoords', () => {
   describe('normalize()', () => {
     it('should normalize negative coordinates', () => {
-      const coords = new CellCoords(-2, -1);
+      const coords = createCoords(-2, -1);
 
       coords.normalize();
 
@@ -12,7 +16,7 @@ describe('CellCoords', () => {
     });
 
     it('should not normalize `null` coordinates (leave as it is)', () => {
-      const coords = new CellCoords();
+      const coords = createCoords();
 
       coords.normalize();
 
@@ -23,25 +27,25 @@ describe('CellCoords', () => {
 
   describe('isValid', () => {
     it('should return `false` when the row is not an integer type', () => {
-      expect(new CellCoords(null, 2).isValid({
+      expect(createCoords(null, 2).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(undefined, 2).isValid({
+      expect(createCoords(undefined, 2).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(1.1, 2).isValid({
+      expect(createCoords(1.1, 2).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords('1', 2).isValid({
+      expect(createCoords('1', 2).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
@@ -50,34 +54,25 @@ describe('CellCoords', () => {
     });
 
     it('should return `false` when the column is not an integer type', () => {
-      expect(new CellCoords(2, null).isValid({
+      expect(createCoords(2, null).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(2, undefined).isValid({
+      expect(createCoords(2, undefined).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(2, 1.1).isValid({
+      expect(createCoords(2, 1.1).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(2, '1').isValid({
-        countRows: 6,
-        countCols: 3,
-        countRowHeaders: 2,
-        countColHeaders: 4,
-      })).toBe(false);
-    });
-
-    it('should return `false` when the row exceed the total rows count', () => {
-      expect(new CellCoords(6, 2).isValid({
+      expect(createCoords(2, '1').isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
@@ -86,7 +81,16 @@ describe('CellCoords', () => {
     });
 
     it('should return `false` when the row exceed the total rows count', () => {
-      expect(new CellCoords(2, 3).isValid({
+      expect(createCoords(6, 2).isValid({
+        countRows: 6,
+        countCols: 3,
+        countRowHeaders: 2,
+        countColHeaders: 4,
+      })).toBe(false);
+    });
+
+    it('should return `false` when the row exceed the total rows count', () => {
+      expect(createCoords(2, 3).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
@@ -95,13 +99,13 @@ describe('CellCoords', () => {
     });
 
     it('should return `false` when the row is lower than the number of column headers', () => {
-      expect(new CellCoords(-5, 1).isValid({
+      expect(createCoords(-5, 1).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(-1, 1).isValid({
+      expect(createCoords(-1, 1).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 0,
@@ -110,13 +114,13 @@ describe('CellCoords', () => {
     });
 
     it('should return `false` when the column is lower than the number of row headers', () => {
-      expect(new CellCoords(1, -3).isValid({
+      expect(createCoords(1, -3).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 2,
         countColHeaders: 4,
       })).toBe(false);
-      expect(new CellCoords(1, -1).isValid({
+      expect(createCoords(1, -1).isValid({
         countRows: 6,
         countCols: 3,
         countRowHeaders: 0,
@@ -133,13 +137,13 @@ describe('CellCoords', () => {
       };
 
       // most top-left
-      expect(new CellCoords(-4, -2).isValid(tableParams)).toBe(true);
+      expect(createCoords(-4, -2).isValid(tableParams)).toBe(true);
       // most top-right
-      expect(new CellCoords(-4, 2).isValid(tableParams)).toBe(true);
+      expect(createCoords(-4, 2).isValid(tableParams)).toBe(true);
       // most bottom-right
-      expect(new CellCoords(5, 2).isValid(tableParams)).toBe(true);
+      expect(createCoords(5, 2).isValid(tableParams)).toBe(true);
       // most bottom-left
-      expect(new CellCoords(5, -2).isValid(tableParams)).toBe(true);
+      expect(createCoords(5, -2).isValid(tableParams)).toBe(true);
     });
 
     it('should return `true` when the coords points to the range within the table (no headers)', () => {
@@ -151,35 +155,35 @@ describe('CellCoords', () => {
       };
 
       // most top-left
-      expect(new CellCoords(0, 0).isValid(tableParams)).toBe(true);
+      expect(createCoords(0, 0).isValid(tableParams)).toBe(true);
       // most top-right
-      expect(new CellCoords(0, 2).isValid(tableParams)).toBe(true);
+      expect(createCoords(0, 2).isValid(tableParams)).toBe(true);
       // most bottom-right
-      expect(new CellCoords(5, 2).isValid(tableParams)).toBe(true);
+      expect(createCoords(5, 2).isValid(tableParams)).toBe(true);
       // most bottom-left
-      expect(new CellCoords(5, 0).isValid(tableParams)).toBe(true);
+      expect(createCoords(5, 0).isValid(tableParams)).toBe(true);
     });
   });
 
   describe('isEqual()', () => {
     it('should be equal to itself', () => {
-      const coords = new CellCoords(1, 1);
+      const coords = createCoords(1, 1);
       const result = coords.isEqual(coords);
 
       expect(result).toBe(true);
     });
 
     it('should be equal to another instance with the same row and column', () => {
-      const coords = new CellCoords(1, 1);
-      const coords2 = new CellCoords(1, 1);
+      const coords = createCoords(1, 1);
+      const coords2 = createCoords(1, 1);
       const result = coords.isEqual(coords2);
 
       expect(result).toBe(true);
     });
 
     it('should not be equal to an instance with different row or column', () => {
-      const coords = new CellCoords(1, 1);
-      const coords2 = new CellCoords(2, 1);
+      const coords = createCoords(1, 1);
+      const coords2 = createCoords(2, 1);
       const result = coords.isEqual(coords2);
 
       expect(result).toBe(false);
@@ -188,7 +192,7 @@ describe('CellCoords', () => {
 
   describe('clone()', () => {
     it('should clone the object', () => {
-      const coords = new CellCoords(3, 9);
+      const coords = createCoords(3, 9);
       const clone = coords.clone();
 
       expect(coords.row).toBe(clone.row);
@@ -199,8 +203,8 @@ describe('CellCoords', () => {
 
   describe('assign()', () => {
     it('should be possible to assign coords from other CellCoords instance', () => {
-      const coords = new CellCoords(0, 0, false);
-      const coords2 = new CellCoords(3, 9, true);
+      const coords = createCoords(0, 0);
+      const coords2 = createCoords(3, 9);
 
       coords.assign(coords2);
 
@@ -211,74 +215,73 @@ describe('CellCoords', () => {
     });
 
     it('should be possible to assign coords from literal object', () => {
-      const coords = new CellCoords(0, 0, true);
+      const coords = createCoords(0, 0);
 
       coords.assign({ row: 3 });
 
       expect(coords.row).toBe(3);
       expect(coords.col).toBe(0);
-      expect(coords.isRtl()).toBe(true);
+      expect(coords.isRtl()).toBe(false);
 
       coords.assign({ col: 4 });
 
       expect(coords.row).toBe(3);
       expect(coords.col).toBe(4);
-      expect(coords.isRtl()).toBe(true);
+      expect(coords.isRtl()).toBe(false);
 
       coords.assign({ row: -1, col: -2 });
 
       expect(coords.row).toBe(-1);
       expect(coords.col).toBe(-2);
-      expect(coords.isRtl()).toBe(true);
+      expect(coords.isRtl()).toBe(false);
     });
   });
 
   describe('isRtl()', () => {
     it('should return correct values', () => {
-      expect(new CellCoords(0, 0, false).isRtl()).toBe(false);
-      expect(new CellCoords(0, 0, true).isRtl()).toBe(true);
+      expect(createCoords(0, 0).isRtl()).toBe(false);
     });
   });
 
   describe('isCell()', () => {
     it('should return `false` when one of the axis point to the header (negative value)', () => {
-      expect(new CellCoords(-1, 9).isCell()).toBe(false);
-      expect(new CellCoords(9, -1).isCell()).toBe(false);
-      expect(new CellCoords(-1, -1).isCell()).toBe(false);
-      expect(new CellCoords(0, -1).isCell()).toBe(false);
-      expect(new CellCoords(-1, 0).isCell()).toBe(false);
+      expect(createCoords(-1, 9).isCell()).toBe(false);
+      expect(createCoords(9, -1).isCell()).toBe(false);
+      expect(createCoords(-1, -1).isCell()).toBe(false);
+      expect(createCoords(0, -1).isCell()).toBe(false);
+      expect(createCoords(-1, 0).isCell()).toBe(false);
     });
 
     it('should return `true` when all axis point to the cells range', () => {
-      expect(new CellCoords(0, 0).isCell()).toBe(true);
-      expect(new CellCoords(1, 0).isCell()).toBe(true);
-      expect(new CellCoords(0, 1).isCell()).toBe(true);
-      expect(new CellCoords(1, 1).isCell()).toBe(true);
-      expect(new CellCoords(100, 100).isCell()).toBe(true);
+      expect(createCoords(0, 0).isCell()).toBe(true);
+      expect(createCoords(1, 0).isCell()).toBe(true);
+      expect(createCoords(0, 1).isCell()).toBe(true);
+      expect(createCoords(1, 1).isCell()).toBe(true);
+      expect(createCoords(100, 100).isCell()).toBe(true);
     });
   });
 
   describe('isHeader()', () => {
     it('should return `false` when all axis point to the cells (positive value)', () => {
-      expect(new CellCoords(0, 0).isHeader()).toBe(false);
-      expect(new CellCoords(1, 0).isHeader()).toBe(false);
-      expect(new CellCoords(0, 1).isHeader()).toBe(false);
-      expect(new CellCoords(1, 1).isHeader()).toBe(false);
-      expect(new CellCoords(100, 100).isHeader()).toBe(false);
+      expect(createCoords(0, 0).isHeader()).toBe(false);
+      expect(createCoords(1, 0).isHeader()).toBe(false);
+      expect(createCoords(0, 1).isHeader()).toBe(false);
+      expect(createCoords(1, 1).isHeader()).toBe(false);
+      expect(createCoords(100, 100).isHeader()).toBe(false);
     });
 
     it('should return `true` when one of the axis point to the headers range', () => {
-      expect(new CellCoords(-1, 9).isHeader()).toBe(true);
-      expect(new CellCoords(9, -1).isHeader()).toBe(true);
-      expect(new CellCoords(-1, -1).isHeader()).toBe(true);
-      expect(new CellCoords(0, -1).isHeader()).toBe(true);
-      expect(new CellCoords(-1, 0).isHeader()).toBe(true);
+      expect(createCoords(-1, 9).isHeader()).toBe(true);
+      expect(createCoords(9, -1).isHeader()).toBe(true);
+      expect(createCoords(-1, -1).isHeader()).toBe(true);
+      expect(createCoords(0, -1).isHeader()).toBe(true);
+      expect(createCoords(-1, 0).isHeader()).toBe(true);
     });
   });
 
   describe('isSouthEastOf()', () => {
     it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-      const coords = new CellCoords(5, 5);
+      const coords = createCoords(5, 5);
 
       expect(coords.isSouthEastOf({ row: 0, col: 0 })).toBe(true);
       expect(coords.isSouthEastOf({ row: 0, col: 10 })).toBe(false);
@@ -289,7 +292,7 @@ describe('CellCoords', () => {
 
   describe('isSouthWestOf()', () => {
     it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-      const coords = new CellCoords(5, 5);
+      const coords = createCoords(5, 5);
 
       expect(coords.isSouthWestOf({ row: 0, col: 0 })).toBe(false);
       expect(coords.isSouthWestOf({ row: 0, col: 10 })).toBe(true);
@@ -300,7 +303,7 @@ describe('CellCoords', () => {
 
   describe('isNorthEastOf()', () => {
     it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-      const coords = new CellCoords(5, 5);
+      const coords = createCoords(5, 5);
 
       expect(coords.isNorthEastOf({ row: 0, col: 0 })).toBe(false);
       expect(coords.isNorthEastOf({ row: 0, col: 10 })).toBe(false);
@@ -311,7 +314,7 @@ describe('CellCoords', () => {
 
   describe('isNorthWestOf()', () => {
     it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-      const coords = new CellCoords(5, 5);
+      const coords = createCoords(5, 5);
 
       expect(coords.isNorthWestOf({ row: 0, col: 0 })).toBe(false);
       expect(coords.isNorthWestOf({ row: 0, col: 10 })).toBe(false);
@@ -320,49 +323,13 @@ describe('CellCoords', () => {
     });
   });
 
-  describe('RTL mode', () => {
-    describe('isSouthEastOf()', () => {
-      it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-        const coords = new CellCoords(5, 5, true);
-
-        expect(coords.isSouthEastOf({ row: 0, col: 0 })).toBe(false);
-        expect(coords.isSouthEastOf({ row: 0, col: 10 })).toBe(true);
-        expect(coords.isSouthEastOf({ row: 10, col: 0 })).toBe(false);
-        expect(coords.isSouthEastOf({ row: 10, col: 10 })).toBe(false);
-      });
-    });
-
-    describe('isSouthWestOf()', () => {
-      it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-        const coords = new CellCoords(5, 5, true);
-
-        expect(coords.isSouthWestOf({ row: 0, col: 0 })).toBe(true);
-        expect(coords.isSouthWestOf({ row: 0, col: 10 })).toBe(false);
-        expect(coords.isSouthWestOf({ row: 10, col: 0 })).toBe(false);
-        expect(coords.isSouthWestOf({ row: 10, col: 10 })).toBe(false);
-      });
-    });
-
-    describe('isNorthEastOf()', () => {
-      it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-        const coords = new CellCoords(5, 5, true);
-
-        expect(coords.isNorthEastOf({ row: 0, col: 0 })).toBe(false);
-        expect(coords.isNorthEastOf({ row: 0, col: 10 })).toBe(false);
-        expect(coords.isNorthEastOf({ row: 10, col: 0 })).toBe(false);
-        expect(coords.isNorthEastOf({ row: 10, col: 10 })).toBe(true);
-      });
-    });
-
-    describe('isNorthWestOf()', () => {
-      it('should correctly check the position of the CellCoords instance based on the passed coords', () => {
-        const coords = new CellCoords(5, 5, true);
-
-        expect(coords.isNorthWestOf({ row: 0, col: 0 })).toBe(false);
-        expect(coords.isNorthWestOf({ row: 0, col: 10 })).toBe(false);
-        expect(coords.isNorthWestOf({ row: 10, col: 0 })).toBe(true);
-        expect(coords.isNorthWestOf({ row: 10, col: 10 })).toBe(false);
-      });
+  describe('toObject()', () => {
+    it('should return literal object with `row` and `col` properties', () => {
+      expect(createCoords(-1, 9).toObject()).toEqual({ row: -1, col: 9 });
+      expect(createCoords(9, -1).toObject()).toEqual({ row: 9, col: -1 });
+      expect(createCoords(-1, -1).toObject()).toEqual({ row: -1, col: -1 });
+      expect(createCoords(0, -1).toObject()).toEqual({ row: 0, col: -1 });
+      expect(createCoords(-1, 0).toObject()).toEqual({ row: -1, col: 0 });
     });
   });
 });
