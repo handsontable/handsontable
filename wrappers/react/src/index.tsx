9c32c06c04ced3e2c1f0f8fc523c851bb8f626a3
adambar <hi@adambar.pl>
2024-02-21 21:52:20 +0100

    dont expose hotTableInner publicly

--- wrappers/react/src/index.tsx
+++ wrappers/react/src/index.tsx
@@ -1,6 +1,5 @@
 export * from './hotColumn';
 export * from './hotTable';
-export * from './hotTableInner';
 export { useHotEditor } from './hotEditor';
 export * from './types';
 export { default } from './hotTable';
