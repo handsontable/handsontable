e57f18d1dd3d86726f7b37c917c0f3acb86c9d22
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-02-21 07:30:32 +0100

    Fix build process

--- examples/next/visual-tests/react/demo/src/index.tsx
+++ examples/next/visual-tests/react/demo/src/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "@handsontable/pikaday/css/pikaday.css";
 import "./styles.css";
 import Handsontable from 'handsontable';
@@ -69,7 +69,7 @@ const App = () => {
 };
 
 const container = document.getElementById("container");
-const root = createRoot(container);
+const root = createRoot(container!);
 
 root.render(<App />);
 
