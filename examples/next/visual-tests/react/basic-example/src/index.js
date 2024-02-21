c29d7669b2c7b1ce14ce797553dfac1a346856ac
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-02-21 10:03:26 +0100

    Update root element id name

--- examples/next/visual-tests/react/basic-example/src/index.js
+++ examples/next/visual-tests/react/basic-example/src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const container = document.getElementById('container');
+const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(<App />);
