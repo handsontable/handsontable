282ffde0043b2f51cb8927ba55dd2d1145137774
Maciej Giergiczny <maciej.giergiczny@handsontable.com>
2024-02-28 07:47:45 +0100

    1766 review and simplify existing visual tests (#10820)
    * add page object class and modify tests
    
    * remove pageopbject and add helper functions
    
    * refactor more tests
    
    * fix broken test logic
    
    * trying to fix tests
    
    * remove screenshot from helper

--- visual-tests/tests/expand-dropdown-menu.spec.ts
+++ visual-tests/tests/expand-dropdown-menu.spec.ts
@@ -1,18 +1,14 @@
 import { test } from '../src/test-runner';
 import { helpers } from '../src/helpers';
+import { openHeaderDropdownMenu, selectFromDropdownMenu } from '../src/page-helpers';
 
 test(__filename, async({ page }) => {
-  const table = page.locator(helpers.selectors.mainTable);
+  await openHeaderDropdownMenu(2);
 
-  await table.waitFor();
-
-  const changeTypeButton = table.locator(helpers.findDropdownMenuExpander({ col: 2 }));
-
-  await changeTypeButton.click();
-
-  const dropdownMenu = page.locator(helpers.selectors.dropdownMenu);
-
+  // The dropdown menu should be open
   await page.screenshot({ path: helpers.screenshotPath() });
-  await dropdownMenu.locator('"Clear column"').click();
+  await selectFromDropdownMenu('"Clear column"');
+
+  // column should be cleared
   await page.screenshot({ path: helpers.screenshotPath() });
 });
