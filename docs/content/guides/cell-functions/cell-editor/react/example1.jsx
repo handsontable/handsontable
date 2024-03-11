cf4112c79dde0cb6f02f48c3116a5d918aea6f87
Adam Bar <adamrbar@gmail.com>
2024-03-11 12:52:13 +0100

    React wrapper: Sync updated docs with new structure (#10848)
    * Added beforeBeginEditing hook to the Cell editor guide API list (#10844)
    
    * Move code samples to separate files and update links in documentation (#10837)
    
    * include code snippets
    
    * Move code samples to separate files and update links in documentation
    
    * update demo file
    
    * Revert "Move code samples to separate files and update links in documentation"
    
    This reverts commit 1ea5f1d129b7904d70f65d54a64ac5cc0610eba5.
    
    * Move code samples to separate files and update links in documentation
    
    * fixup typo
    
    * fix typo
    
    * Update ESLint configuration and auto fix lint issues
    
    * fix lint
    
    * update angular demo
    
    * revert replaced dist files
    
    ---------
    
    Co-authored-by: Evan Seaward <seaward.evan@gmail.com>
    
    * React wrapper: Sync updated docs with new structure
    
    ---------
    
    Co-authored-by: Aleksandra Budnik <AMBudnik@users.noreply.github.com>
    Co-authored-by: adrianspdev <155736789+adrianspdev@users.noreply.github.com>
    Co-authored-by: Evan Seaward <seaward.evan@gmail.com>

---
+++ docs/content/guides/cell-functions/cell-editor/react/example1.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { HotTable, HotColumn, useHotEditor } from '@handsontable/react';
+import 'handsontable/dist/handsontable.full.min.css';
+
+// an editor component
+const EditorComponent = (props) => {
+  const mainElementRef = React.useRef();
+  const containerStyle = {
+    display: 'none',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    background: '#fff',
+    border: '1px solid #000',
+    padding: '15px',
+    zIndex: 999
+  };
+
+  // A hook that takes a set of methods that your custom editor needs to override.
+  // It also provides partial editor API in case other functions need to access it, like `finishEditing`.
+  const { value, setValue, finishEditing } = useHotEditor({
+    onOpen() {
+      mainElementRef.current.style.display = 'block';
+    },
+
+    onClose() {
+      mainElementRef.current.style.display = 'none';
+    },
+
+    onPrepare(row, col, prop, td, originalValue, cellProperties) {
+      const tdPosition = td.getBoundingClientRect();
+
+      // As the `prepare` method is triggered after selecting
+      // any cell, we're updating the styles for the editor element,
+      // so it shows up in the correct position.
+      mainElementRef.current.style.left = tdPosition.left + window.pageXOffset + 'px';
+      mainElementRef.current.style.top = tdPosition.top + window.pageYOffset + 'px';
+    }
+  });
+
+  const setLowerCase = React.useCallback(() => {
+    setValue(value.toString().toLowerCase());
+
+    // Close the editor by the editor API method.
+    finishEditing();
+  }, [setValue, value, finishEditing]);
+
+  const setUpperCase = React.useCallback(() => {
+    setValue(value.toString().toUpperCase());
+
+    // Close the editor by the editor API method.
+    finishEditing();
+  }, [setValue, value, finishEditing]);
+
+  const stopMousedownPropagation = React.useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  return (
+    <div
+      style={containerStyle}
+      ref={mainElementRef}
+      onMouseDown={stopMousedownPropagation}
+      id="editorElement"
+    >
+      <button onClick={setLowerCase}>
+        {value?.toLowerCase()}
+      </button>
+      <button onClick={setUpperCase}>
+        {value?.toUpperCase()}
+      </button>
+    </div>
+  );
+};
+
+const data = [
+  ['Obrien Fischer'],
+  ['Alexandria Gordon'],
+  ['John Stafford'],
+  ['Regina Waters'],
+  ['Kay Bentley'],
+  ['Emerson Drake'],
+  ['Dean Stapleton']
+];
+
+export const ExampleComponent = () => {
+  return (
+    <HotTable
+      data={data}
+      rowHeaders={true}
+      autoWrapRow={true}
+      autoWrapCol={true}
+      licenseKey="non-commercial-and-evaluation"
+    >
+      {/* add the `editor` prop to set the component as a Handsontable editor */}
+      <HotColumn width={250} editor={EditorComponent} />
+    </HotTable>
+  );
+};
+
+/* start:skip-in-preview */
+ReactDOM.render(<ExampleComponent />, document.getElementById('example1'));
+/* end:skip-in-preview */
---
+++ docs/content/guides/getting-started/react-redux/react/example6.jsx
@@ -0,0 +1,233 @@
+import React, { useEffect } from 'react';
+import { HexColorPicker } from 'react-colorful';
+import StarRatingComponent from 'react-star-rating-component';
+import { Provider, connect } from 'react-redux';
+import { createStore, combineReducers } from 'redux';
+import { HotTable, HotColumn, useHotEditor } from '@handsontable/react';
+import { registerAllModules } from 'handsontable/registry';
+import 'handsontable/dist/handsontable.full.min.css';
+
+// register Handsontable's modules
+registerAllModules();
+
+// a custom renderer component
+const ColorPickerRenderer = (props) => {
+  const colorboxStyle = {
+    background: props.value,
+    width: '21px',
+    height: '21px',
+    float: 'left',
+    marginRight: '5px'
+  };
+
+  return (
+    <>
+      <div style={colorboxStyle} />
+      <div>{props.value}</div>
+    </>
+  );
+};
+
+// a custom editor component
+const UnconnectedColorPickerEditor = (props) => {
+  const editorRef = React.useRef(null);
+
+  const editorContainerStyle = {
+    display: 'none',
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    zIndex: 999,
+    background: '#fff',
+    padding: '15px',
+    border: '1px solid #cecece'
+  };
+
+  const stopMousedownPropagation = React.useCallback((e) => {
+    e.stopPropagation();
+  }, []);
+
+  const { value, setValue, row, col, finishEditing } = useHotEditor({
+    onOpen() {
+      editorRef.current.style.display = 'block';
+    },
+
+    onClose() {
+      editorRef.current.style.display = 'none';
+    },
+
+    onPrepare(row, col, prop, td, originalValue, cellProperties) {
+      const tdPosition = td.getBoundingClientRect();
+
+      editorRef.current.style.left = tdPosition.left + window.pageXOffset + 'px';
+      editorRef.current.style.top = tdPosition.top + window.pageYOffset + 'px';
+    }
+  });
+
+  const applyColor = React.useCallback(() => {
+    const dispatch = props.dispatch;
+
+    if (col === 1) {
+      dispatch({
+        type: 'updateActiveStarColor',
+        row: row,
+        hexColor: value
+      });
+    } else if (col === 2) {
+      dispatch({
+        type: 'updateInactiveStarColor',
+        row: row,
+        hexColor: value
+      });
+    }
+    finishEditing();
+  }, [props.dispatch, value, row, col, finishEditing]);
+
+  return (
+    <div style={editorContainerStyle} ref={editorRef} onMouseDown={stopMousedownPropagation}>
+      <HexColorPicker
+        color={value}
+        onChange={setValue}
+      />
+      <button
+        style={{ width: '100%', height: '33px', marginTop: '10px' }}
+        onClick={applyColor}
+      >
+        Apply
+      </button>
+    </div>
+  );
+};
+
+const ColorPickerEditor = connect(function(state) {
+  return {
+    activeColors: state.appReducer.activeColors,
+    inactiveColors: state.appReducer.inactiveColors
+  };
+})(UnconnectedColorPickerEditor);
+
+// a Redux component
+const initialReduxStoreState = {
+  activeColors: [],
+  inactiveColors: []
+};
+
+const appReducer = (state = initialReduxStoreState, action) => {
+  switch (action.type) {
+    case 'initRatingColors': {
+      const { hotData } = action;
+
+      const activeColors = hotData.map((data) => data[1]);
+      const inactiveColors = hotData.map((data) => data[2]);
+
+      return {
+        ...state,
+        activeColors,
+        inactiveColors
+      };
+    }
+
+    case 'updateActiveStarColor': {
+      const rowIndex = action.row;
+      const newColor = action.hexColor;
+
+      const activeColorArray = [...state.activeColors];
+
+      activeColorArray[rowIndex] = newColor;
+
+      return {
+        ...state,
+        activeColors: activeColorArray
+      };
+    }
+
+    case 'updateInactiveStarColor': {
+      const rowIndex = action.row;
+      const newColor = action.hexColor;
+
+      const inactiveColorArray = [...state.inactiveColors];
+
+      inactiveColorArray[rowIndex] = newColor;
+
+      return {
+        ...state,
+        inactiveColors: inactiveColorArray
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+const actionReducers = combineReducers({ appReducer });
+const reduxStore = createStore(actionReducers);
+
+// a custom renderer component
+const UnconnectedStarRatingRenderer = ({
+  row,
+  col,
+  value,
+  activeColors,
+  inactiveColors
+}) => {
+  return (
+    <StarRatingComponent
+      name={`${row}-${col}`}
+      value={value}
+      starCount={5}
+      starColor={activeColors[row]}
+      emptyStarColor={inactiveColors[row]}
+      editing={true}
+    />
+  );
+};
+
+const StarRatingRenderer = connect((state) => ({
+  activeColors: state.appReducer.activeColors,
+  inactiveColors: state.appReducer.inactiveColors
+}))(UnconnectedStarRatingRenderer);
+
+const data = [
+  [1, '#ff6900', '#fcb900'],
+  [2, '#fcb900', '#7bdcb5'],
+  [3, '#7bdcb5', '#8ed1fc'],
+  [4, '#00d084', '#0693e3'],
+  [5, '#eb144c', '#abb8c3']
+];
+
+export const ExampleComponent = () => {
+  useEffect(() => {
+    reduxStore.dispatch({
+      type: 'initRatingColors',
+      hotData: data
+    });
+  }, []);
+
+  return (
+    <Provider store={reduxStore}>
+      <HotTable
+        data={data}
+        rowHeaders={true}
+        rowHeights={30}
+        colHeaders={['Rating', 'Active star color', 'Inactive star color']}
+        height="auto"
+        autoWrapRow={true}
+        autoWrapCol={true}
+        licenseKey="non-commercial-and-evaluation"
+      >
+        {/* add the `renderer` prop to set the component as a Handsontable renderer */}
+        <HotColumn width={100} type={'numeric'} renderer={StarRatingRenderer} />
+        {/* add the `renderer` and `editor` props to set the component as a Handsontable renderer and editor */}
+        <HotColumn width={150}
+                   renderer={ColorPickerRenderer}
+                   editor={ColorPickerEditor} />
+        <HotColumn width={150}
+                   renderer={ColorPickerRenderer}
+                   editor={ColorPickerEditor} />
+      </HotTable>
+    </Provider>
+  );
+};
+
+ReactDOM.render(<ExampleComponent />, document.getElementById('example6'));
