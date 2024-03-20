370f5e285f8a6ef234bebad41f9609526f082837
magierg <maciej.giergiczny@handsontable.com>
2024-03-20 13:06:04 +0100

    run against prod

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8080/docs',
+    baseURL: 'https://handsontable.com/docs/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -81,10 +81,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run docs:start',
-    url: 'http://localhost:8080/docs',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
+  // webServer: {
+  //   command: 'npm run docs:start',
+  //   url: 'http://localhost:8080/docs',
+  //   reuseExistingServer: !process.env.CI,
+  //   timeout: 120 * 1000,
+  // },
 });
