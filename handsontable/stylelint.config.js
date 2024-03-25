719e2116e2f2f34909e52c87f3c68235949c50a0
Evan Seaward <seaward.evan@gmail.com>
2024-03-25 14:42:03 +0100

    Update Angular dependencies

--- handsontable/stylelint.config.js
+++ handsontable/stylelint.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   rules: {
-    indentation: 2,
-    'max-empty-lines': 2,
-    'no-eol-whitespace': true,
-    'no-missing-end-of-source-newline': true,
-    'no-empty-first-line': true
   }
 };
