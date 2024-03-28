f852deddee6730ea62b6a8cce18e809f5c3ded4a
magierg <maciej.giergiczny@handsontable.com>
2024-03-28 12:09:53 +0100

    revert demo changes

--- examples/next/visual-tests/react/demo/src/index.tsx
+++ examples/next/visual-tests/react/demo/src/index.tsx
@@ -1,21 +1,72 @@
 import React from "react";
-import ReactDOM from "react-dom";import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ReactDOM from "react-dom";
+import "@handsontable/pikaday/css/pikaday.css";
+import "./styles.css";
 import Handsontable from 'handsontable';
-import { HotTable } from "@handsontable/react";
-import Scenario1 from './apps/Scenario1';
-import OriginalDemo from './apps/OriginalDemo';
+import { HotTable, HotColumn } from "@handsontable/react";
+import { data } from "./constants";
+import { ProgressBarRenderer } from "./renderers/ProgressBar";
+import { StarsRenderer } from "./renderers/Stars";
 
+import {
+  drawCheckboxInRowHeaders,
+  addClassesToRows,
+  changeCheckboxCell,
+  alignHeaders
+} from "./hooksCallbacks";
 
-function App() {
+import "handsontable/dist/handsontable.css";
+
+const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<OriginalDemo />} />
-        <Route path="/scenario1" element={<Scenario1 />} />
-      </Routes>
-    </Router>
+    <HotTable
+      data={data}
+      height={450}
+      colWidths={[140, 192, 100, 90, 90, 110, 97, 100, 126]}
+      colHeaders={[
+        "Company name",
+        "Name",
+        "Sell date",
+        "In stock",
+        "Qty",
+        "Progress",
+        "Rating",
+        "Order ID",
+        "Country"
+      ]}
+      dropdownMenu={true}
+      hiddenColumns={{
+        indicators: true
+      }}
+      contextMenu={true}
+      multiColumnSorting={true}
+      filters={true}
+      rowHeaders={true}
+      afterGetColHeader={alignHeaders}
+      beforeRenderer={addClassesToRows}
+      afterGetRowHeader={drawCheckboxInRowHeaders}
+      afterOnCellMouseDown={changeCheckboxCell}
+      mergeCells={true}
+      manualRowMove={true}
+      navigableHeaders={true}
+      licenseKey="non-commercial-and-evaluation"
+    >
+      <HotColumn data={1} />
+      <HotColumn data={3} />
+      <HotColumn data={4} type="date" allowInvalid={false} />
+      <HotColumn data={6} type="checkbox" className="htCenter" />
+      <HotColumn data={7} type="numeric" />
+      <HotColumn data={8} readOnly={true} className="htMiddle">
+        <ProgressBarRenderer hot-renderer />
+      </HotColumn>
+      <HotColumn data={9} readOnly={true} className="htCenter">
+        <StarsRenderer hot-renderer />
+      </HotColumn>
+      <HotColumn data={5} />
+      <HotColumn data={2} />
+    </HotTable>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
