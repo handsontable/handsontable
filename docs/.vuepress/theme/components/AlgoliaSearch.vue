046fb5c89d74aa4a8a5052aa7c7ef12f51018160
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-04-11 09:56:04 +0200

    fix lint

--- docs/.vuepress/theme/components/AlgoliaSearch.vue
+++ docs/.vuepress/theme/components/AlgoliaSearch.vue
@@ -3,26 +3,26 @@
 </template>
 
 <script>
-import docsearch from "@docsearch/js";
-import "@docsearch/css";
+import docsearch from '@docsearch/js';
+import '@docsearch/css';
 
 export default {
-  name: "AlgoliaSearch",
-  props: ["options"],
+  name: 'AlgoliaSearch',
+  props: ['options'],
   mounted() {
     this.initialize(this.options, this.$lang);
   },
   methods: {
     initialize(userOptions, lang) {
       docsearch({
-        container: "#algolia-search",
+        container: '#algolia-search',
         ...userOptions,
-        placeholder: this.$site.themeConfig.searchPlaceholder || "Search",
+        placeholder: this.$site.themeConfig.searchPlaceholder || 'Search',
         translations: {
           button: {
-            buttonText: this.$site.themeConfig.searchPlaceholder || "Search",
+            buttonText: this.$site.themeConfig.searchPlaceholder || 'Search',
             buttonAriaLabel:
-              this.$site.themeConfig.searchPlaceholder || "Search",
+              this.$site.themeConfig.searchPlaceholder || 'Search',
           },
         },
         searchParameters: {
