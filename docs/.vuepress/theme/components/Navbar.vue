c2548a26128d72d81001b8312ed5b378c9cf88f4
adrianspdev <155736789+adrianspdev@users.noreply.github.com>
2024-04-18 10:10:11 +0200

    Update docs/.vuepress/theme/components/Navbar.vue
    Co-authored-by: Evan Seaward <seaward.evan@gmail.com>

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -93,7 +93,7 @@ export default {
         ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
         : Math.sign(num) * Math.abs(num);
     },
-    async getStarts() {
+    async getStars() {
       try {
         const response = await fetch(
           "https://api.github.com/repos/handsontable/handsontable"
