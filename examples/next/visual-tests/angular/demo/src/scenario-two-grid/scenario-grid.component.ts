99c295523e2bdecd5a6f03915af542dba36055bb
magierg <maciej.giergiczny@handsontable.com>
2024-04-18 09:14:55 +0200

    fix some angular errors in test

--- examples/next/visual-tests/angular/demo/src/scenario-two-grid/scenario-grid.component.ts
+++ examples/next/visual-tests/angular/demo/src/scenario-two-grid/scenario-grid.component.ts
@@ -1,35 +1,24 @@
-import { Component, ViewEncapsulation } from "@angular/core";
-import { getScenarioDataTop, getScenarioDataBottom } from "./utils/constants";
-import { starsRenderer } from "./renderers/stars";
-import { progressBarRenderer } from "./renderers/progressBar";
+import { Component, ViewEncapsulation } from '@angular/core';
+import { getScenarioDataTop, getScenarioDataBottom } from './utils/constants';
+import { starsRenderer } from './renderers/stars';
+import { progressBarRenderer } from './renderers/progressBar';
 import { HyperFormula } from 'hyperformula';
-
-import {
-  alignHeaders,
-  drawCheckboxInRowHeaders,
-  addClassesToRows,
-  changeCheckboxCell
-} from "./utils/hooks-callbacks";
 
 @Component({
   encapsulation: ViewEncapsulation.None,
-  selector: "scenario-grid",
-  templateUrl: "./scenario-grid.component.html",
-  styleUrls: ["./scenario-grid.scss"]
+  selector: 'scenario-grid',
+  templateUrl: './scenario-grid.component.html',
+  styleUrls: ['./scenario-grid.scss'],
 })
 export class ScenarioGridComponent {
   datasetTop = getScenarioDataTop();
   datasetBottom = getScenarioDataBottom();
-  alignHeaders = alignHeaders;
-  drawCheckboxInRowHeaders = drawCheckboxInRowHeaders;
-  addClassesToRows = addClassesToRows;
-  changeCheckboxCell = changeCheckboxCell;
   progressBarRenderer = progressBarRenderer;
   starsRenderer = starsRenderer;
-  formulas={
+  formulas = {
     engine: HyperFormula,
-  }
-  nestedHeaders=[
+  };
+  nestedHeaders = [
     [
       { label: 'Product', colspan: 4 },
       { label: 'Category', colspan: 3 },
@@ -69,12 +58,11 @@ export class ScenarioGridComponent {
       type: 'average',
       destinationRow: 0,
       destinationColumn: 10,
-      // force this column summary to treat non-numeric values as numeric values
-      forceNumeric: true
+      forceNumeric: true,
     },
   ];
   hiddenColumns = {
-    indicators: true
+    indicators: true,
   };
-  licenseKey = "non-commercial-and-evaluation";
+  licenseKey = 'non-commercial-and-evaluation';
 }
