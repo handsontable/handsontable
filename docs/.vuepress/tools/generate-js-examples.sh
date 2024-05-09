2050d73c60628ac3afaecdf8814874853ba88afa
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-05-09 12:26:20 +0200

    Add a bash script that generates the JS files for all the TS example files

---
+++ docs/.vuepress/tools/generate-js-examples.sh
@@ -0,0 +1,16 @@
+ #!/usr/bin/sh 
+
+# This script generates the JS files for all the TS code examples. It skips the examples that already have a JS file.
+
+find content/guides -name "*.ts" -print0 | while read -d $'\0' ts_filename; do
+  dirname=$(dirname ${ts_filename})
+  js_filename="${ts_filename%.*}.js"
+    
+  if [ -f "$js_filename" ]; then
+    echo "Skipping $js_filename"
+  else
+    echo "Generating $js_filename"
+    tsc --target esnext --skipLibCheck $ts_filename > /dev/null
+    eslint --fix --no-ignore -c eslintrc.examples.js $js_filename
+  fi
+done
