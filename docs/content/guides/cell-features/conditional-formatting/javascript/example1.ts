52d5d3a708e758028fd74d98d7568a6f60d7bfc4
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-05-16 17:05:19 +0200

    Fix typings in dir docs/content/guides/cell-features

--- docs/content/guides/cell-features/conditional-formatting/javascript/example1.ts
+++ docs/content/guides/cell-features/conditional-formatting/javascript/example1.ts
@@ -45,7 +45,7 @@ new Handsontable(container, {
   data,
   licenseKey: 'non-commercial-and-evaluation',
   height: 'auto',
-  afterSelection(row, col, row2, col2) {
+  afterSelection(_row, _col, row2, col2) {
     const meta = this.getCellMeta(row2, col2);
 
     if (meta.readOnly) {
