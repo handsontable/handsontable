d8a61bdad6603ffc01b1508af89c70ae75feb634
Marek <marek@fachowo.co>
2024-05-17 09:00:41 +0200

    scss

--- handsontable/dist/handsontable.js
+++ handsontable/dist/handsontable.js
@@ -26,7 +26,7 @@
  * USE OR INABILITY TO USE THIS SOFTWARE.
  *
  * Version: 14.2.0
- * Release date: 06/03/2024 (built at 15/05/2024 08:52:40)
+ * Release date: 06/03/2024 (built at 16/05/2024 22:43:26)
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -109,7 +109,7 @@ Handsontable.hooks = _pluginHooks.default.getSingleton();
 Handsontable.CellCoords = _src.CellCoords;
 Handsontable.CellRange = _src.CellRange;
 Handsontable.packageName = 'handsontable';
-Handsontable.buildDate = "15/05/2024 08:52:40";
+Handsontable.buildDate = "16/05/2024 22:43:26";
 Handsontable.version = "14.2.0";
 Handsontable.languages = {
   dictionaryKeys: _registry.dictionaryKeys,
