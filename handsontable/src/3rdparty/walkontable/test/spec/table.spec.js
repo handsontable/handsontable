2a1ba734b83d67814642d7e79d50f076ef67adf2
Jan Siegel <js.ziggle@gmail.com>
2024-05-21 08:30:34 +0200

    - Make the instance not render when it's placed in a hidden container. - Add some lint adjustments - Add tests

--- handsontable/src/3rdparty/walkontable/test/spec/table.spec.js
+++ handsontable/src/3rdparty/walkontable/test/spec/table.spec.js
@@ -577,4 +577,24 @@ describe('WalkontableTable', () => {
     expect($('.ht_clone_inline_start')[0]).toHaveClass('ht_clone_left');
     expect($('.ht_clone_bottom_inline_start_corner')[0]).toHaveClass('ht_clone_bottom_left_corner');
   });
+
+  it('should not re-render the full table when the table has `display: none` declared', () => {
+    const cellRenderer = jasmine.createSpy('cellRenderer');
+    const wt = walkontable({
+      data: getData,
+      totalRows: getTotalRows,
+      totalColumns: getTotalColumns,
+      cellRenderer
+    });
+
+    wt.draw();
+
+    expect(cellRenderer).toHaveBeenCalledTimes(18);
+
+    spec().$wrapper.css('display', 'none');
+
+    wt.draw();
+
+    expect(cellRenderer).toHaveBeenCalledTimes(18);
+  });
 });
