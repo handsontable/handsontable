0b479ed20c834b8c2e1fee32acac136e32ed455e
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-05-21 16:01:44 +0200

    Make generate-js-examples script re-generate files even when exist

--- docs/.vuepress/tools/generate-js-examples.sh
+++ docs/.vuepress/tools/generate-js-examples.sh
@@ -5,16 +5,12 @@
 find content/guides -wholename "*/javascript/*.ts" -print0 | while read -d $'\0' ts_filename; do
   js_filename="${ts_filename%.*}.js"
     
+  tsc --target esnext --skipLibCheck $ts_filename > /dev/null
+
   if [ -f "$js_filename" ]; then
-    echo "Skipping $js_filename"
+    eslint --fix --no-ignore -c eslintrc.examples.js $js_filename > /dev/null
+    echo "Generated $js_filename"
   else
-    tsc --target esnext --skipLibCheck $ts_filename > /dev/null
-    
-      if [ -f "$js_filename" ]; then
-        eslint --fix --no-ignore -c eslintrc.examples.js $js_filename > /dev/null
-        echo "Generated $js_filename"
-      else 
-        echo "Failed to generate $js_filename"
-      fi
+    echo "Failed to generate $js_filename"
   fi
 done
