50bb8c0fa50ec80e58c28239e44ded5452d7a163
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2024-05-29 13:33:15 +0200

    Make generate-js-examples script run jobs in parallel to speed things up

--- docs/.vuepress/tools/generate-js-examples.sh
+++ docs/.vuepress/tools/generate-js-examples.sh
@@ -2,7 +2,10 @@
 
 # This script generates the JS files for all the TS code examples. It skips the examples that already have a JS file.
 
-find content/guides -wholename "*/javascript/*.ts" -print0 | while read -d $'\0' ts_filename; do
+jobs_limit=16
+
+generate_single_file() {
+  ts_filename="$1"
   js_filename="${ts_filename%.*}.js"
     
   tsc --target esnext --skipLibCheck $ts_filename > /dev/null
@@ -13,4 +16,18 @@ find content/guides -wholename "*/javascript/*.ts" -print0 | while read -d $'\0'
   else
     echo "Failed to generate $js_filename"
   fi
+}
+
+echo "Running $jobs_limit jobs in parallel..."
+
+find content/guides -wholename "*/javascript/*.ts" -print0 | while read -d $'\0' ts_filename; do
+  if test "$(jobs | wc -l)" -ge "$jobs_limit"; then
+    wait -n
+  fi
+
+  generate_single_file "$ts_filename" &
 done
+
+wait
+sleep 10
+echo "All jobs finished"
