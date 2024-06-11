b285394fe3b91da84c5409e1208282f2107fb00e
Kuba Sekowski <jakub.sekowski@handsontable.com>
2024-06-11 13:53:02 +0200

    TS code examples (#10990)
    * SPIKE - add script that compiles an example TS file to JS
    
    * Adjust the start script to make it work in Windows env
    
    * Extend the base eslintrc in the eslintrc for example files
    
    * Add a bash script that generates the JS files for all the TS example files
    
    * Add generate-js-examples step to the npm scripts
    
    * Make the generate-js-examples script apply only for the vanilla js examples
    
    * Renamed all js examples to *.ts without changing the content
    
    * Add typings for data array in the examples (array of arrays)
    
    * Add typings for HOT instance in all TS examples
    
    * Add typings for data array in the examples (array of objects)
    
    * Improve reporting in the generate-js-examples script
    
    * Add typings for the HOT instance in the examples
    
    * Improve typings in docs/content/guides/accessories-and-menus/context-menu/javascript/example3.ts
    
    * Improve typings in docs/content/guides/accessories-and-menus/export-to-csv/javascript/example1.ts
    
    * Add typing for .getPlugin statements
    
    * Change typing for HOT instance from Handsontable to Core
    
    * Add typings for custom renderers in the ts examples
    
    * Add missing import statements
    
    * Improve typings for arrays
    
    * replace Core type
    
    * add lint rules
    
    * update lint
    
    * update ts examples
    
    * Newline at the end of file gitignore
    
    * Remove unused vars in docs/content/guides/getting-started
    
    * Remove unused 'const hot' variables
    
    * Fix type error for html containers for the HOT instance
    
    * Fix typings in docs/content/guides/rows/row-prepopulating/javascript/example1.ts
    
    * Make all HOT instances variables names consistently 'hot'
    
    * Improve typings in docs/content/guides/getting-started/binding-to-data
    
    * add ts fixes and cleanup
    
    * Improve typings for cells() function in HOT configuration
    
    * Fix docs/content/guides/columns/column-filter/javascript/exampleFilterDifferentTypes.ts
    
    * Fix typings for docs/content/guides/getting-started/demo/javascript/example.ts
    
    * Fix type-null-checking in HTML elements
    
    * fix example
    
    * Fix typing for HTMLInputElement vars
    
    * Fix docs/content/guides/optimization/batch-operations/javascript/example1.ts
    
    * Fix typings in dir  docs/content/guides/cell-functions
    
    * Fix typings in dir docs/content/guides/columns
    
    * Fix typings in dir docs/content/guides/cell-features
    
    * Fix typings in dir docs/content/guides/cell-types
    
    * Fix typings in dir docs/content/guides/formulas
    
    * Fix typings in dir docs/content/guides/navigation
    
    * Fix typings in dir docs/content/guides/accessories-and-menus
    
    * Improve typings for docs/content/guides/getting-started/binding-to-data/javascript/example4.ts
    
    * Revert unnecessary changes
    
    * Revert unnecessary changes
    
    * Make generate-js-examples script re-generate files even when exist
    
    * Make sure generate-js-examples.sh is run in bash shell
    
    * Remove generate-js-examples script from the docs:start pipeline
    
    * Make generate-js-examples script run jobs in parallel to speed things up
    
    * Improve the dev experience with the generate-js-examples script
    
    * Fix merge errors
    
    * Fix merge errors
    
    * Make generat-js-examples script work in zsh env (MacOS)
    
    * Fix/docs visual cache logic (#11014)
    
    * make sure we evaluate the cache restore properly
    
    * build and serve for visual tests
    
    * increase timeout
    
    * fix lock
    
    * resolve docs lock
    
    * update locks
    
    ---------
    
    Co-authored-by: adrianspdev <adrian.dusinkiewicz@soppo.team>
    Co-authored-by: Maciej Giergiczny <maciej.giergiczny@handsontable.com>
    Co-authored-by: Evan Seaward <seaward.evan@gmail.com>

---
+++ docs/content/guides/rows/row-moving/javascript/example1.ts
@@ -0,0 +1,25 @@
+import Handsontable from 'handsontable';
+import 'handsontable/dist/handsontable.full.min.css';
+
+// generate an array of arrays with dummy data
+const data: string[][] = new Array(200) // number of rows
+  .fill(null)
+  .map((_, row) => new Array(20) // number of columns
+    .fill(null)
+    .map((_, column) => `${row}, ${column}`)
+  );
+
+const container = document.querySelector('#example1')!;
+
+new Handsontable(container, {
+  data,
+  width: '100%',
+  height: 320,
+  rowHeaders: true,
+  colHeaders: true,
+  colWidths: 100,
+  manualRowMove: true,
+  autoWrapRow: true,
+  autoWrapCol: true,
+  licenseKey: 'non-commercial-and-evaluation'
+});
