0031c607e0672708dac61e891abbc337d8e68f02
magierg <maciej.giergiczny@handsontable.com>
2024-06-28 13:03:25 +0100

    display env in job name

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -16,7 +16,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
