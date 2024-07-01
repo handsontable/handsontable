0f618898947dadaa86ae8f170b3e8da4367dbc04
magierg <maciej.giergiczny@handsontable.com>
2024-07-01 11:12:41 +0100

    turn off traces

--- docs/playwright.config.ts
+++ docs/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL ?? 'http://localhost:8080/docs',
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'off',
   },
 
   /* Configure projects for major browsers */
