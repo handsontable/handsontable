bc3dc722dc4386398b2a37d5426de072ec431977
Jan Siegel <js.ziggle@gmail.com>
2024-07-10 14:32:16 +0200

    Regenerate the new docs examples regarding the headerClassName option.

--- docs/content/guides/columns/column-header/javascript/example4.js
+++ docs/content/guides/columns/column-header/javascript/example4.js
@@ -1,22 +1,19 @@
 import Handsontable from 'handsontable';
 import 'handsontable/dist/handsontable.full.min.css';
+
 const container = document.querySelector('#example4');
 const hot = new Handsontable(container, {
-    data: [
-        ['A1', 'B1', 'C1'],
-        ['A2', 'B2', 'C2'],
-        ['A3', 'B3', 'C3'],
-    ],
-    colHeaders: true,
-    rowHeaders: true,
-    autoWrapRow: true,
-    autoWrapCol: true,
-    height: 'auto',
-    headerClassName: 'htCenter',
-    columns: [
-        { headerClassName: 'htRight' },
-        { headerClassName: 'htLeft' },
-        {},
-    ],
-    licenseKey: 'non-commercial-and-evaluation'
+  data: [
+    ['A1', 'B1', 'C1'],
+    ['A2', 'B2', 'C2'],
+    ['A3', 'B3', 'C3'],
+  ],
+  colHeaders: true,
+  rowHeaders: true,
+  autoWrapRow: true,
+  autoWrapCol: true,
+  height: 'auto',
+  headerClassName: 'htCenter',
+  columns: [{ headerClassName: 'htRight' }, { headerClassName: 'htLeft' }, {}],
+  licenseKey: 'non-commercial-and-evaluation',
 });
