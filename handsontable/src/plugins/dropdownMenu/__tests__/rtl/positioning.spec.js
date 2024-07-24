7f8f4f9d3667df1f87474bf2990c0c655b0a0908
Krzysztof ‘Budzio’ Budnik <571316+budnix@users.noreply.github.com>
2024-07-24 13:57:21 +0200

    Fix submenu misalignment after horizontal scroll (#11106)

--- handsontable/src/plugins/dropdownMenu/__tests__/rtl/positioning.spec.js
+++ handsontable/src/plugins/dropdownMenu/__tests__/rtl/positioning.spec.js
@@ -1,22 +1,24 @@
 describe('DropdownMenu (RTL mode)', () => {
+  beforeEach(function() {
+    $('html').attr('dir', 'rtl');
+    this.$container = $('<div id="testContainer"></div>').appendTo('body');
+  });
+
+  afterEach(function() {
+    $('html').attr('dir', 'ltr');
+
+    if (this.$container) {
+      destroy();
+      this.$container.remove();
+    }
+  });
+
   using('configuration object', [
     { htmlDir: 'rtl', layoutDirection: 'inherit' },
     { htmlDir: 'ltr', layoutDirection: 'rtl' },
   ], ({ htmlDir, layoutDirection }) => {
-    const id = 'testContainer';
-
-    beforeEach(function() {
+    beforeEach(() => {
       $('html').attr('dir', htmlDir);
-      this.$container = $(`<div id="${id}"></div>`).appendTo('body');
-    });
-
-    afterEach(function() {
-      $('html').attr('dir', 'ltr');
-
-      if (this.$container) {
-        destroy();
-        this.$container.remove();
-      }
     });
 
     describe('subMenu opening', () => {
@@ -34,7 +36,6 @@ describe('DropdownMenu (RTL mode)', () => {
 
         const $dropdownMenu = $('.htDropdownMenu');
         const dropdownOffset = $dropdownMenu.offset();
-
         const subMenuItem = $('.htDropdownMenu .ht_master .htCore  td:contains(Alignment)');
         const subMenuItemOffset = subMenuItem.offset();
         const subMenuRoot = $('.htDropdownMenuSub_Alignment');
@@ -42,7 +43,7 @@ describe('DropdownMenu (RTL mode)', () => {
         const subMenuWidth = subMenuRoot.outerWidth();
 
         expect(subMenuOffset.top).toBeCloseTo(subMenuItemOffset.top - 1, 0);
-        expect(subMenuOffset.left).toBeCloseTo(dropdownOffset.left - subMenuWidth, 0);
+        expect(subMenuOffset.left).toBe(Math.floor(dropdownOffset.left - subMenuWidth));
       });
 
       it('should open subMenu on the right of the main menu if on the left there\'s no space left', async() => {
@@ -60,7 +61,6 @@ describe('DropdownMenu (RTL mode)', () => {
         const $dropdownMenu = $('.htDropdownMenu');
         const dropdownOffset = $dropdownMenu.offset();
         const dropdownWidth = $dropdownMenu.outerWidth();
-
         const subMenuItem = $('.htDropdownMenu .ht_master .htCore  td:contains(Alignment)');
         const subMenuItemOffset = subMenuItem.offset();
         const subMenuRoot = $('.htDropdownMenuSub_Alignment');
@@ -90,6 +90,58 @@ describe('DropdownMenu (RTL mode)', () => {
 
       expect(tickItemOffset.top).toBe(135);
       expect(tickItemOffset.left).toBe(dropdownMenuOffset.left + $dropdownMenuRoot.outerWidth() - 4);
+    });
+  });
+
+  describe('subMenu opening', () => {
+    it('should open subMenu by default on the left-bottom position of the main menu (scrolled viewport) #dev-1895', async() => {
+      handsontable({
+        data: createSpreadsheetData(4, 100),
+        dropdownMenu: true,
+        colHeaders: true,
+      });
+
+      openDropdownSubmenuOption('Alignment', 0);
+
+      await sleep(350);
+
+      const $dropdownMenu = $('.htDropdownMenu');
+      const dropdownOffset = $dropdownMenu.offset();
+      const subMenuItem = $('.htDropdownMenu .ht_master .htCore  td:contains(Alignment)');
+      const subMenuItemOffset = subMenuItem.offset();
+      const subMenuRoot = $('.htDropdownMenuSub_Alignment');
+      const subMenuOffset = subMenuRoot.offset();
+      const subMenuWidth = subMenuRoot.outerWidth();
+
+      expect(subMenuOffset.top).toBeCloseTo(subMenuItemOffset.top - 1, 0);
+      expect(subMenuOffset.left).toBe(Math.floor(dropdownOffset.left - subMenuWidth));
+    });
+
+    it('should open subMenu on the right-bottom of the main menu if on the left there\'s no space left (scrolled viewport) #dev-1895', async() => {
+      handsontable({
+        data: createSpreadsheetData(4, 100),
+        dropdownMenu: true,
+        colHeaders: true,
+      });
+
+      scrollViewportTo(0, 99);
+
+      await sleep(50);
+
+      openDropdownSubmenuOption('Alignment', 93);
+
+      await sleep(350);
+
+      const $dropdownMenu = $('.htDropdownMenu');
+      const dropdownOffset = $dropdownMenu.offset();
+      const dropdownWidth = $dropdownMenu.outerWidth();
+      const subMenuItem = $('.htDropdownMenu .ht_master .htCore  td:contains(Alignment)');
+      const subMenuItemOffset = subMenuItem.offset();
+      const subMenuRoot = $('.htDropdownMenuSub_Alignment');
+      const subMenuOffset = subMenuRoot.offset();
+
+      expect(subMenuOffset.top).toBeCloseTo(subMenuItemOffset.top - 1, 0);
+      expect(subMenuOffset.left).toBe(Math.floor(dropdownOffset.left + dropdownWidth));
     });
   });
 });
