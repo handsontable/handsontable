1f1f714bf29133c743a31380cb1a0d783cdbaf3f
Jan Siegel <js.ziggle@gmail.com>
2024-07-30 12:22:25 +0200

    Revert 70569043732e1cc2271321f10179f2a22ba1b32a.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,14 +56,6 @@ const environmentHead = isProduction
 createSymlinks();
 
 module.exports = {
-  // by default this always returns true, this is a issue as it preloads every documentation pages content
-  shouldPrefetch: (_, type) => {
-    if (type === 'script') {
-      return false;
-    }
-
-    return true;
-  },
   define: {
     GA_ID: 'UA-33932793-7',
   },
