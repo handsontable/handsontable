80faacf9fe6b8d731eb5b8c23ca83622fef2f806
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2024-08-13 14:05:01 +0200

    Fix React demo

--- examples/next/visual-tests/react/demo/src/EditorsGrid.tsx
+++ examples/next/visual-tests/react/demo/src/EditorsGrid.tsx
@@ -2,30 +2,28 @@ import { HotTable } from '@handsontable/react';
 
 const EditorsGrid = () => {
   return (
-    <div>
-      <HotTable
-        data={[
-          { brand: 'Mercedes', model: 'A 160', year: 2017, price_usd: 7000, sell_date: '01/01/2021' },
-          { brand: 'Citroen', model: 'C4', year: 2018, price_usd: 8330, sell_date: '04/12/2020' },
-          { brand: 'Audi', model: 'A4', year: 2019, price_usd: 33900, sell_date: '11/04/2022' },
-          { brand: 'Opel', model: 'Astra', year: 2020, price_usd: 5000, sell_date: '21/08/2024' },
-          { brand: 'BMW', model: '320i', year: 2021, price_usd: 30500, sell_date: '03/06/2021' },
-        ]}
-        colHeaders={true}
-        height={350}
-        rowHeaders={true}
-        columns={[
-          { data: 'brand', type: 'dropdown', source: ['Mercedes', 'Citroen', 'Audi', 'Opel', 'BMW'] },
-          { data: 'model', type: 'autocomplete', source: ['A 160', 'C4', 'A4', 'Astra', '320i'] },
-          { data: 'year', type: 'select', selectOptions: ['2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'] },
-          { data: 'price_usd', type: 'numeric' },
-          { data: 'sell_date', type: 'date', dateFormat: 'DD/MM/YYYY' },
-        ]}
-        autoWrapCol={true}
-        autoWrapRow={true}
-        licenseKey='non-commercial-and-evaluation'
-      />
-    </div>
+    <HotTable
+      data={[
+        { brand: 'Mercedes', model: 'A 160', year: 2017, price_usd: 7000, sell_date: '01/01/2021' },
+        { brand: 'Citroen', model: 'C4', year: 2018, price_usd: 8330, sell_date: '04/12/2020' },
+        { brand: 'Audi', model: 'A4', year: 2019, price_usd: 33900, sell_date: '11/04/2022' },
+        { brand: 'Opel', model: 'Astra', year: 2020, price_usd: 5000, sell_date: '21/08/2024' },
+        { brand: 'BMW', model: '320i', year: 2021, price_usd: 30500, sell_date: '03/06/2021' },
+      ]}
+      colHeaders={true}
+      height={350}
+      rowHeaders={true}
+      columns={[
+        { data: 'brand', type: 'dropdown', source: ['Mercedes', 'Citroen', 'Audi', 'Opel', 'BMW'] },
+        { data: 'model', type: 'autocomplete', source: ['A 160', 'C4', 'A4', 'Astra', '320i'] },
+        { data: 'year', type: 'select', selectOptions: ['2017', '2018', '2019', '2020', '2021', '2022', '2023', '2024'] },
+        { data: 'price_usd', type: 'numeric' },
+        { data: 'sell_date', type: 'date', dateFormat: 'DD/MM/YYYY' },
+      ]}
+      autoWrapCol={true}
+      autoWrapRow={true}
+      licenseKey='non-commercial-and-evaluation'
+    />
   );
 };
 
