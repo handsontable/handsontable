d7edeef5dce97c02557d27d0703bd7213b9a5d65
Skoczowianin <cgepfert@gmail.com>
2024-08-30 09:01:27 +0200

    debug framework segment

--- docs/netlify/edge_functions/next.ts
+++ docs/netlify/edge_functions/next.ts
@@ -4,9 +4,14 @@ export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
 
-  const params = url.searchParams;
-  console.log(url, params.get('framework'), params);
-  const framework = params.get('framework') === 'react-data-grid' ? 'react-data-grid' : 'javascript-data-grid';
+  const params = new URLSearchParams(url.search);
+  // const framework = params.get('framework')
+
+  //const framework = url.searchParams.get('framework') === 'react-data-grid' ? 'react-data-grid' : 'javascript-data-grid';
+  const framework = params.get('framework')
+
+  console.log('framework', framework, context, request);
+
 
   // console.log(framework, context, request);
   return Response.redirect(`/docs/${framework}`, 301);
