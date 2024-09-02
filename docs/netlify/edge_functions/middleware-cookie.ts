59aceff8250b7d3bd0fcf77e59b159f4bb118fc8
Skoczowianin <cgepfert@gmail.com>
2024-09-02 12:01:41 +0200

    trying to set a cookie globally

--- docs/netlify/edge_functions/middleware-cookie.ts
+++ docs/netlify/edge_functions/middleware-cookie.ts
@@ -1,10 +1,15 @@
 export default async (req: Request, { cookies }: Context) => {
 
-const framework = cookies.get('docs_fw') === 'react' ? 'react-data-grid' : 'javascript-data-grid';
+   const framework = cookies.get('docs_fw') === 'react' ? 'react-data-grid' : 'javascript-data-grid';
 
    if (framework === 'react-data-grid') {
         console.log('cookie was set to react');
-        cookies.set('docs_fw', 'react', { path: '/' });
+        cookies.set('docs_fw', 'react', { path: '/', maxAge: 60 * 60 * 24 * 7 });
    }
    return;
-  };
+};
+
+export const config: Config = {
+    path: ['/*']
+  }
+  
