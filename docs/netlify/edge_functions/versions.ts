cf919e0f8d8e36d625828354ee420be24b235412
Skoczowianin <cgepfert@gmail.com>
2024-09-11 09:01:46 +0200

    debug logs

--- docs/netlify/edge_functions/versions.ts
+++ docs/netlify/edge_functions/versions.ts
@@ -3,6 +3,7 @@ import type { Context, Config } from "@netlify/edge-functions";
 export default async (request: Request, context: Context) => {
 
   const url = new URL(request.url);
+  console.log('url', url);
   const pattern = new URLPattern({ pathname: '/docs/:version(\\d+\\.\\d+|next)' });
   const match = pattern.exec(url.pathname);
 
@@ -10,10 +11,13 @@ export default async (request: Request, context: Context) => {
   const framework = cookieValue === 'react' ? 'react-data-grid' : 'javascript-data-grid';
 
   if (match) {
+  
+    console.log('match', match);
     const version = match.pathname.groups.version;
     const newUrl = `${url.origin}/docs/${version}/${framework}/`;
     return Response.redirect(newUrl, 301);
   }
+  console.log('no match');
 
   return fetch(request);
 }
