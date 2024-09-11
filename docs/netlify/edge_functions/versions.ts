f0886ff600471a0a54ad71834baa1197a2268f89
Skoczowianin <cgepfert@gmail.com>
2024-09-11 09:44:51 +0200

    disable versions

--- docs/netlify/edge_functions/versions.ts
+++ docs/netlify/edge_functions/versions.ts
@@ -1,29 +1,27 @@
-// import type { Context, Config } from "@netlify/edge-functions";
+import type { Context, Config } from "@netlify/edge-functions";
 
-// export default async (request: Request, context: Context) => {
+export default async (request: Request, context: Context) => {
 
-//   const url = new URL(request.url);
-//   console.log('url', url);
-//   const pattern = new URLPattern({ pathname: '/docs/:version(\\d+\\.\\d+|next)' });
-//   const match = pattern.exec(url.pathname);
+  const url = new URL(request.url);
+  console.log('url', url);
+  const pattern = new URLPattern({ pathname: '/docs/:version(\\d+\\.\\d+|next)' });
+  const match = pattern.exec(url.pathname);
 
-//   const cookieValue = context.cookies.get("docs_fw");
-//   const framework = cookieValue === 'react' ? 'react-data-grid' : 'javascript-data-grid';
+  const cookieValue = context.cookies.get("docs_fw");
+  const framework = cookieValue === 'react' ? 'react-data-grid' : 'javascript-data-grid';
 
-//   if (match) {
+  if (match) {
   
-//     console.log('match', match);
-//     const version = match.pathname.groups.version;
-//     const newUrl = `${url.origin}/docs/${version}/${framework}/`;
-//     return Response.redirect(newUrl, 301);
-//   }
-//   console.log('no match');
+    console.log('match', match);
+    const version = match.pathname.groups.version;
+    const newUrl = `${url.origin}/docs/${version}/${framework}/`;
+    return Response.redirect(newUrl, 301);
+  }
+  console.log('no match');
 
-//   return fetch(request);
-// }
+  return fetch(request);
+}
 
-// // export const config: Config = {
-// //   path: ["/docs/:version(\\d+\\.\\d+|next)'"],
-// // }
-
-
+export const config: Config = {
+  path: ["disablethis/docs/:version(\\d+\\.\\d+|next)'"],
+}
