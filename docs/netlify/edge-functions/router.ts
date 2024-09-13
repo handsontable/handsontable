5fb2620f2158a4b736d4d57cec0dbc2c65a42fea
Skoczowianin <cgepfert@gmail.com>
2024-09-13 08:16:51 +0200

    redirects + log asset path

--- docs/netlify/edge-functions/router.ts
+++ docs/netlify/edge-functions/router.ts
@@ -13,6 +13,51 @@ const redirects = [
     to: "https://hyperformula.handsontable.com/$1",
     status: STATUS_PERMANENT_REDIRECT,
   },
+  {
+    from: /^\/docs\/react-hot-column$/,
+    to: "/docs/react-data-grid/hot-column",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/frameworks-wrapper-for-react-hot-column$/,
+    to: "/docs/react-data-grid/hot-column",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/disablethis\/docs\/(.*)$/,
+    to: "/docs/$1",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/(\d+\.\d+|next)$/,
+    to: "/docs/$1/javascript-data-grid/",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/(\d+\.\d+|next)\/(react-data-grid|javascript-data-grid)\/(.*)$/,
+    to: "/docs/$1/$2/$3",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/react-hot-column$/,
+    to: "/docs/react-data-grid/hot-column",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/frameworks-wrapper-for-react-hot-column$/,
+    to: "/docs/react-data-grid/hot-column",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/react-data-grid$/,
+    to: "/docs/react-data-grid/getting-started",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
+  {
+    from: /^\/docs\/javascript-data-grid$/,
+    to: "/docs/javascript-data-grid/getting-started",
+    status: STATUS_PERMANENT_REDIRECT,
+  },
 ]
 
 export default async function handler(request: Request, context: Context) {
@@ -35,6 +80,7 @@ export default async function handler(request: Request, context: Context) {
 
   try {
     // Fetch the static file from the Netlify-deployed assets
+    console.log('Trying to fetch', `${url.origin}${path}`);
     const assetResponse = await fetch(`${url.origin}${path}`);
   
     // If the asset exists, serve it back to the user
@@ -43,6 +89,7 @@ export default async function handler(request: Request, context: Context) {
     }
 
     // If the asset is not found, return a 404 response
+
     return new Response('Asset not found', { status: 404 });
   } catch (error) {
     // Handle any errors that occurred during fetch
