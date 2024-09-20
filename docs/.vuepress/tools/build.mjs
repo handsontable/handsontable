a1806b93f47b7e7091d63db4a8af307076d2988d
Skoczowianin <cgepfert@gmail.com>
2024-09-20 07:03:17 +0200

    exceptions in staging

--- docs/.vuepress/tools/build.mjs
+++ docs/.vuepress/tools/build.mjs
@@ -33,14 +33,14 @@ async function buildVersion(version) {
 
   if (version !== 'next' || buildMode === 'staging') {
     await spawnProcess(
-      'node node_modules/vuepress/cli.js build -d .vuepress/dist/pre-' +
+      'node node_modules/.bin/vuepress build -d .vuepress/dist/pre-' +
         `${versionEscaped}/${NO_CACHE ? ' --no-cache' : ''}`,
       { cwd, env: { DOCS_BASE: version }, }
     );
   }
 
   await spawnProcess(
-    'node node_modules/vuepress/cli.js build -d .vuepress/dist/pre-latest-' +
+    'node node_modules/.bin/vuepress build -d .vuepress/dist/pre-latest-' +
       `${versionEscaped}/${NO_CACHE ? ' --no-cache' : ''}`,
     { cwd, env: { DOCS_BASE: 'latest' }, }
   );
