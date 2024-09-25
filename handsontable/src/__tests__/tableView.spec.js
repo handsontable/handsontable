b5a63365fabb0c5ebae28b2cd07421a3c976d432
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2024-09-25 15:21:15 +0200

    Add tests for new TableView methods

--- handsontable/src/__tests__/tableView.spec.js
+++ handsontable/src/__tests__/tableView.spec.js
@@ -416,4 +416,26 @@ describe('TableView', () => {
       expect(hot.view._wt.wtViewport.getWorkspaceHeight).toHaveBeenCalledTimes(1);
     });
   });
+
+  describe('hasVerticalScroll()', () => {
+    it('should internally call `hasVerticalScroll` method of the Viewport module of the Walkontable', () => {
+      const hot = handsontable({});
+
+      spyOn(hot.view._wt.wtViewport, 'hasVerticalScroll').and.returnValue(100);
+
+      expect(hot.view.hasVerticalScroll()).toBe(100);
+      expect(hot.view._wt.wtViewport.hasVerticalScroll).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('hasHorizontalScroll()', () => {
+    it('should internally call `hasHorizontalScroll` method of the Viewport module of the Walkontable', () => {
+      const hot = handsontable({});
+
+      spyOn(hot.view._wt.wtViewport, 'hasHorizontalScroll').and.returnValue(100);
+
+      expect(hot.view.hasHorizontalScroll()).toBe(100);
+      expect(hot.view._wt.wtViewport.hasHorizontalScroll).toHaveBeenCalledTimes(1);
+    });
+  });
 });
