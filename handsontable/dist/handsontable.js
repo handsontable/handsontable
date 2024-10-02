9969d0b84e15f45d35667bd0ba6f226872630246
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-10-02 14:34:18 +0200

    add style fixes and improvements

--- handsontable/dist/handsontable.js
+++ handsontable/dist/handsontable.js
@@ -26,7 +26,7 @@
  * USE OR INABILITY TO USE THIS SOFTWARE.
  *
  * Version: 14.5.0
- * Release date: 30/07/2024 (built at 01/10/2024 12:26:05)
+ * Release date: 30/07/2024 (built at 02/10/2024 12:16:51)
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -104,7 +104,7 @@ Handsontable.hooks = _pluginHooks.default.getSingleton();
 Handsontable.CellCoords = _src.CellCoords;
 Handsontable.CellRange = _src.CellRange;
 Handsontable.packageName = 'handsontable';
-Handsontable.buildDate = "01/10/2024 12:26:05";
+Handsontable.buildDate = "02/10/2024 12:16:51";
 Handsontable.version = "14.5.0";
 Handsontable.languages = {
   dictionaryKeys: _registry.dictionaryKeys,
