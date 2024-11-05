46884fb5d20db4fcae166c8fd86cd8dc5bf723cf
adrianspdev <adrian.dusinkiewicz@soppo.team>
2024-11-05 11:35:55 +0100

    add css classes to example containers

--- docs/.vuepress/containers/examples/examples.js
+++ docs/.vuepress/containers/examples/examples.js
@@ -110,7 +110,7 @@ module.exports = function(docsVersion, base) {
         const htmlContent = htmlToken
           ? htmlToken.content
           : `<div id="${id}" class="hot ${klass} ht-theme-main-dark-auto"></div>`;
-        const htmlContentRoot = `<div data-preset-type="${preset}" data-example-id="${id}" >${htmlContent}</div>`;
+        const htmlContentRoot = `<div data-preset-type="${preset}" data-example-id="${id}" class="ht-theme-main-dark-auto">${htmlContent}</div>`;
 
         const cssPos = args.match(/--css (\d*)/)?.[1];
         const cssIndex = cssPos ? index + Number.parseInt(cssPos, 10) : 0;
