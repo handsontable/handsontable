b5497dfb73a0fabf08a5f0b427aa4bf657d3d2be
Jan Siegel <js.ziggle@gmail.com>
2025-01-02 15:52:55 +0100

    Correct the regexp.

--- docs/.vuepress/code-structure-builder/getBody.js
+++ docs/.vuepress/code-structure-builder/getBody.js
@@ -8,7 +8,7 @@ const { buildVueBody } = require('./buildVueBody');
 const getBody = ({ id, html, js, css, docsVersion, preset, sandbox, lang }) => {
   const version = formatVersion(docsVersion);
   const hyperformulaVersion = '^2.4.0';
-  const themeName = html.match(/(ht-theme-([a-zA-Z])*)/)?.[0] || '';
+  const themeName = html.match(/class="[^"]*(ht-theme-[^"\s]*)[^"]*"/)?.[1] || '';
 
   if (/hot(-.*)?/.test(preset)) {
     return buildJavascriptBody({
