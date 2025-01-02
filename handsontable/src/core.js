6302b66834fb566bc4cc153194618cac9e4198b2
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2025-01-02 14:24:57 +0100

    Trigger tests

--- handsontable/src/core.js
+++ handsontable/src/core.js
@@ -2625,10 +2625,6 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
    *
    * For column headers and row headers, returns `null`.
    *
-   *
-   *
-   *
-   *
    * @memberof Core#
    * @function getValue
    * @returns {*} The value of the focused cell.
@@ -3980,8 +3976,6 @@ export default function Core(rootElement, userSettings, rootInstanceSymbol = fal
    * @returns {number} Number of visible rows or -1.
    */
   this.countVisibleRows = function() {
-
-
     return instance.view._wt.drawn ? instance.view._wt.wtTable.getVisibleRowsCount() : -1;
   };
 
