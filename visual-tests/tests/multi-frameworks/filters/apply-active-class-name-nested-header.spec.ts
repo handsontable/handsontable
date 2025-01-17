e1c13514ecc688b8d34a7c575ab6732ea5cc660c
adrianspdev <adrian.dusinkiewicz@soppo.team>
2025-01-17 11:39:25 +0100

    Update nested header visual test viewport

--- visual-tests/tests/multi-frameworks/filters/apply-active-class-name-nested-header.spec.ts
+++ visual-tests/tests/multi-frameworks/filters/apply-active-class-name-nested-header.spec.ts
@@ -2,6 +2,12 @@ import { test } from '../../../src/test-runner';
 import { helpers } from '../../../src/helpers';
 import { selectCell } from '../../../src/page-helpers';
 
+test.beforeEach(async({ page }) => {
+  await page.setViewportSize({ width: 1920, height: 1080 });
+});
+
+test.skip(helpers.hotWrapper !== 'js', 'This test case is only for JavaScript framework');
+
 /**
  * Checks if active class is applied on the filtered column header with nested headers.
  */
