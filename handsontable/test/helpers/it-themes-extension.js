144b12cedcea6a376ce2d054e6885e5c7a3cd7c2
Jan Siegel <js.ziggle@gmail.com>
2025-02-05 13:31:22 +0100

    Modify the 'it' function extension to allow running the 'classic' tests for 'horizon'.

--- handsontable/test/helpers/it-themes-extension.js
+++ handsontable/test/helpers/it-themes-extension.js
@@ -1,3 +1,5 @@
+const KNOWN_THEMES = ['classic', 'main'];
+
 /**
  * This extension allows to run specs only for a specific theme.
  * If the currently loaded theme doesn't match the specified one, the spec will be marked as pending.
@@ -24,7 +26,10 @@ function it(description, specDefinitions) {
 }
 
 it.forTheme = themeName => (description, specDefinition) => {
-  if ((__ENV_ARGS__.HOT_THEME || 'classic') === themeName) {
+  if (
+    ((__ENV_ARGS__.HOT_THEME || 'classic') === themeName) ||
+    (!KNOWN_THEMES.includes(__ENV_ARGS__.HOT_THEME) && themeName === 'classic')
+  ) {
     originalIt(description, specDefinition);
   }
 };
