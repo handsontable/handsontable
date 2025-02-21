9028176170d3168727654a8aa5a3de3f59ec1cc6
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2025-02-21 10:26:14 +0100

    Remove additional 1px border for themes

--- handsontable/src/plugins/mergeCells/mergeCells.js
+++ handsontable/src/plugins/mergeCells/mergeCells.js
@@ -1504,7 +1504,6 @@ export class MergeCells extends BasePlugin {
    * @returns {number}
    */
   #sumCellsHeights(row, rowspan) {
-    const stylesHandler = this.hot.view.getStylesHandler();
     const defaultHeight = this.hot.view.getDefaultRowHeight();
     const autoRowSizePlugin = this.hot.getPlugin('autoRowSize');
     let height = 0;
@@ -1512,10 +1511,6 @@ export class MergeCells extends BasePlugin {
     for (let i = row; i < row + rowspan; i++) {
       if (!this.hot.rowIndexMapper.isHidden(i)) {
         height += autoRowSizePlugin?.getRowHeight(i) ?? defaultHeight;
-
-        if (i === 0 && !stylesHandler.isClassicTheme()) {
-          height += 1; // border-top-width
-        }
       }
     }
 
