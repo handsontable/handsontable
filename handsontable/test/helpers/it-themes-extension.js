211e89e3c20d46e213335074e1ebd38153d31a19
Jan Siegel <js.ziggle@gmail.com>
2025-03-05 13:15:38 +0100

    - Add support for 'fit' and 'xit' with forTheme.

--- handsontable/test/helpers/it-themes-extension.js
+++ handsontable/test/helpers/it-themes-extension.js
@@ -10,6 +10,34 @@
  * ```
  */
 const originalIt = global.it;
+const originalFit = global.fit;
+const originalXit = global.xit;
+
+/**
+ * Runs the original function with the provided description and spec definitions.
+ *
+ * @param {Function} originalFn - The original function to be executed.
+ * @param {string} description - The description of the spec.
+ * @param {Function} specDefinitions - The function containing the spec definitions.
+ * @returns {*} The result of the original function execution.
+ */
+function runOriginalFn(originalFn, description, specDefinitions) {
+  return originalFn(description, specDefinitions);
+}
+
+/**
+ * Runs the original function under specific conditions based on the theme name.
+ *
+ * @param {Function} originalFn - The original function to be executed.
+ * @param {string} themeName - The name of the theme to check against.
+ * @param {string} description - The description of the spec.
+ * @param {Function} specDefinition - The function containing the spec definitions.
+ */
+function runOriginalFnUnderConditions(originalFn, themeName, description, specDefinition) {
+  if ((__ENV_ARGS__.HOT_THEME || 'classic') === themeName) {
+    originalFn(description, specDefinition);
+  }
+}
 
 /**
  * Helper function allowing to run specs only for a specific theme.
@@ -19,14 +47,19 @@ const originalIt = global.it;
  * @param {Function} specDefinitions The function containing the spec.
  * @returns {Function}
  */
-function it(description, specDefinitions) {
-  return originalIt(description, specDefinitions);
-}
+const it = (description, specDefinitions) => runOriginalFn(originalIt, description, specDefinitions);
+const fit = (description, specDefinitions) => runOriginalFn(originalFit, description, specDefinitions);
+const xit = (description, specDefinitions) => runOriginalFn(originalXit, description, specDefinitions);
 
-it.forTheme = themeName => (description, specDefinition) => {
-  if ((__ENV_ARGS__.HOT_THEME || 'classic') === themeName) {
-    originalIt(description, specDefinition);
-  }
-};
+it.forTheme = themeName => (description, specDefinition) =>
+    runOriginalFnUnderConditions(originalIt, themeName, description, specDefinition);
+
+fit.forTheme = themeName => (description, specDefinition) =>
+    runOriginalFnUnderConditions(originalFit, themeName, description, specDefinition);
+
+xit.forTheme = themeName => (description, specDefinition) =>
+    runOriginalFnUnderConditions(originalXit, themeName, description, specDefinition);
 
 global.it = it;
+global.fit = fit;
+global.xit = xit;
