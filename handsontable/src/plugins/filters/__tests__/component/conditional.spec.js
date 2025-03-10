bf297ef7fe30adc060a71cd1e9860ed1c179df6d
Jan Siegel <js.ziggle@gmail.com>
2025-03-10 10:43:53 +0100

    Correct post-merge test fail.

--- handsontable/src/plugins/filters/__tests__/component/conditional.spec.js
+++ handsontable/src/plugins/filters/__tests__/component/conditional.spec.js
@@ -95,10 +95,10 @@ describe('Filters UI Conditional component', () => {
     const rect = document.querySelector('.htFiltersConditionsMenu.handsontable table').getBoundingClientRect();
 
     // 3px comes from borders
-    expect(window.scrollY + rect.top - 3).forThemes(({ classic, main, horizon }) => {
+    expect(window.scrollY + rect.top).forThemes(({ classic, main, horizon }) => {
       classic.toBeAroundValue(755, 1);
       main.toBeAroundValue(718, 1);
-      horizon.toBeAroundValue(673, 1);
+      horizon.toBeAroundValue(676, 1);
     });
     hot.rootElement.style.marginTop = '';
   });
