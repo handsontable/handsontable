fd8aeb40853a4e11bedf90b9e24dd93e1f7600e3
adrianspdev <adrian.dusinkiewicz@soppo.team>
2025-04-03 11:34:15 +0200

    Remove unused type definition

--- handsontable/types/helpers/mixed.d.ts
+++ handsontable/types/helpers/mixed.d.ts
@@ -1,5 +1,4 @@
 export function stringify(value: any): string;
-export function isBoolean(value: any): boolean;
 export function isDefined(variable: any): boolean;
 export function isUndefined(variable: any): boolean;
 export function isEmpty(variable: any): boolean;
