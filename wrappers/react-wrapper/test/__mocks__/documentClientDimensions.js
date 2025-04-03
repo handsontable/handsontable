619a6b378dadcc3498947fe3b1f1c7ee6ca287e6
Jan Siegel <js.ziggle@gmail.com>
2025-04-03 09:18:13 +0200

    Fix the lint errors.

--- wrappers/react-wrapper/test/__mocks__/documentClientDimensions.js
+++ wrappers/react-wrapper/test/__mocks__/documentClientDimensions.js
@@ -1,3 +1,6 @@
+/**
+ * Mock the `documentElement`'s `clientHeight` and `clientWidth` properties to return the window's inner height and width.
+ */
 export function mockDocumentClientDimensions() {
   Object.defineProperty(document.documentElement, 'clientHeight', {
     value: window.innerHeight
