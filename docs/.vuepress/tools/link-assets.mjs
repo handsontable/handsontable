cdb84f3dc349ffa174b9bbec4e80de06bbddd48f
Jan Siegel <js.ziggle@gmail.com>
2025-04-15 16:14:21 +0200

    Replace @handsontable/angular with @handsontable/angular-wrapper in the docs setup.

--- docs/.vuepress/tools/link-assets.mjs
+++ docs/.vuepress/tools/link-assets.mjs
@@ -11,7 +11,7 @@ const SYMLINK_PATHS = [
   { source: '../handsontable/tmp/', target: './.vuepress/public/handsontable/' },
   { source: '../wrappers/react/', target: './.vuepress/public/@handsontable/react/' },
   { source: '../wrappers/react-wrapper/', target: './.vuepress/public/@handsontable/react-wrapper/' },
-  { source: '../wrappers/angular/dist/hot-table/', target: './.vuepress/public/@handsontable/angular/' },
+  { source: '../wrappers/angular-wrapper/dist/hot-table/', target: './.vuepress/public/@handsontable/angular-wrapper/' },
   { source: '../wrappers/vue/', target: './.vuepress/public/@handsontable/vue/' },
   { source: '../wrappers/vue3/', target: './.vuepress/public/@handsontable/vue3/' },
 ];
