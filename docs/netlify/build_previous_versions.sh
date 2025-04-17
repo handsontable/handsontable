0031bd7dd234360baadc1ada88094c8c0a5c52e6
Mateusz Wojczal <mateusz@qunabu.com>
2025-04-17 11:41:03 +0200

    fetch previous versions from github api not from bash variable

--- docs/netlify/build_previous_versions.sh
+++ docs/netlify/build_previous_versions.sh
@@ -1,8 +1,11 @@
 #/bin/bash
 mkdir -p docs/docs
-versions="9.0 11.0 11.1 12.0 12.1 12.2 12.3 12.4 13.0 14.0 14.1 14.2 14.3 14.4 14.5 14.6 15.0"
-for version in $versions
+node getListOrPreviousVerions.mjs > VERSIONS_VARS
+source VERSIONS_VARS
+rm VERSIONS_VARS
+for version in $PREVIOUS_VERSIONS
 do
+    echo "Building version $version"
     img_id=$(docker create ghcr.io/handsontable/handsontable/handsontable-documentation:v$version)
     docker cp $img_id:/usr/share/nginx/html/docs/$version  ./docs/docs/$version
 done
