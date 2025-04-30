016ea7d872096a4de8868e42e9ff06210e19a2b5
Jan Siegel <js.ziggle@gmail.com>
2025-04-30 13:47:41 +0200

    Make the angular UMD builder install its own dependency (and not rely on those defined at the root level)

---
+++ docs/.vuepress/tools/angular-umd-builder/index.js
@@ -0,0 +1,5 @@
+const path = require('path');
+const { spawnProcess } = require('../../tools/utils');
+const cwd = path.resolve(__dirname, '.');
+
+spawnProcess('npm run umd-builder:full-build', { cwd }, true);
