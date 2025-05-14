983265fa434fcdb65edb8fb495878da09ce46c58
Kuba Sekowski <kuba.sekowski.dev@gmail.com>
2025-05-14 17:53:54 +0200

    Add more tests for insert_col_start

--- handsontable/test/e2e/core/alter/insertColStart.spec.js
+++ handsontable/test/e2e/core/alter/insertColStart.spec.js
@@ -654,6 +654,29 @@ describe('Core.alter', () => {
         expect(getSourceDataAtRow(0)).toEqual(['A1', 'B1', 'C1', null, 'D1', null, 'E1', null]);
       });
 
+      it('should insert column at proper position when column index sequence is shifted', async() => {
+        handsontable({
+          data: createSpreadsheetData(5, 5)
+        });
+
+        columnIndexMapper().setIndexesSequence([4, 0, 1, 2, 3]);
+
+        await alter('insert_col_start', 1, 1);
+
+        expect(getDataAtRow(0)).toEqual(['E1', null, 'A1', 'B1', 'C1', 'D1']);
+        expect(getSourceDataAtRow(0)).toEqual([null, 'A1', 'B1', 'C1', 'D1', 'E1']);
+
+        await alter('insert_col_start', 0, 1);
+
+        expect(getDataAtRow(0)).toEqual([null, 'E1', null, 'A1', 'B1', 'C1', 'D1']);
+        expect(getSourceDataAtRow(0)).toEqual([null, 'A1', 'B1', 'C1', 'D1', null, 'E1']);
+
+        await alter('insert_col_start', 7, 1);
+
+        expect(getDataAtRow(0)).toEqual([null, 'E1', null, 'A1', 'B1', 'C1', 'D1', null]);
+        expect(getSourceDataAtRow(0)).toEqual([null, 'A1', 'B1', 'C1', 'D1', null, 'E1', null]);
+      });
+
       it('should not copy column filters from the source column to the new one', async() => {
         handsontable({
           data: createSpreadsheetData(2, 2),
@@ -670,6 +693,72 @@ describe('Core.alter', () => {
         expect(columnIndexMapper().getIndexesSequence()).toEqual([0, 1, 2]);
         expect(getPlugin('filters').conditionCollection.getConditions(1)).toEqual([]);
         expect(getPlugin('filters').conditionCollection.getConditions(2)[0].args).toEqual([['B1']]);
+      });
+
+      it('should keep the column filters when the index sequence is reversed', async() => {
+        handsontable({
+          data: createSpreadsheetData(5, 5),
+          filters: true
+        });
+
+        columnIndexMapper().setIndexesSequence([4, 3, 2, 1, 0]);
+
+        // addCondition works by visual index
+        await getPlugin('filters').addCondition(0, 'by_value', [['E1']]);
+        await getPlugin('filters').addCondition(1, 'by_value', [['D1']]);
+        await getPlugin('filters').addCondition(2, 'by_value', [['C1']]);
+        await getPlugin('filters').addCondition(3, 'by_value', [['B1']]);
+        await getPlugin('filters').addCondition(4, 'by_value', [['A1']]);
+
+        // getConditions works by physical index
+        expect(getPlugin('filters').conditionCollection.getConditions(4)[0].args).toEqual([['E1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(3)[0].args).toEqual([['D1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(2)[0].args).toEqual([['C1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(1)[0].args).toEqual([['B1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(0)[0].args).toEqual([['A1']]);
+
+        await alter('insert_col_start', 1);
+
+        expect(columnIndexMapper().getIndexesSequence()).toEqual([5, 3, 4, 2, 1, 0]);
+        expect(getPlugin('filters').conditionCollection.getConditions(5)[0].args).toEqual([['E1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(4)).toEqual([]);
+        expect(getPlugin('filters').conditionCollection.getConditions(3)[0].args).toEqual([['D1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(2)[0].args).toEqual([['C1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(1)[0].args).toEqual([['B1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(0)[0].args).toEqual([['A1']]);
+      });
+
+      it('should keep the column filters when the index sequence is shifted', async() => {
+        handsontable({
+          data: createSpreadsheetData(5, 5),
+          filters: true
+        });
+
+        columnIndexMapper().setIndexesSequence([4, 0, 1, 2, 3]);
+
+        // addCondition works by visual index
+        await getPlugin('filters').addCondition(0, 'by_value', [['E1']]);
+        await getPlugin('filters').addCondition(1, 'by_value', [['A1']]);
+        await getPlugin('filters').addCondition(2, 'by_value', [['B1']]);
+        await getPlugin('filters').addCondition(3, 'by_value', [['C1']]);
+        await getPlugin('filters').addCondition(4, 'by_value', [['D1']]);
+
+        // getConditions works by physical index
+        expect(getPlugin('filters').conditionCollection.getConditions(4)[0].args).toEqual([['E1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(0)[0].args).toEqual([['A1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(1)[0].args).toEqual([['B1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(2)[0].args).toEqual([['C1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(3)[0].args).toEqual([['D1']]);
+
+        await alter('insert_col_start', 2);
+
+        expect(columnIndexMapper().getIndexesSequence()).toEqual([5, 0, 1, 2, 3, 4]);
+        expect(getPlugin('filters').conditionCollection.getConditions(5)[0].args).toEqual([['E1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(0)[0].args).toEqual([['A1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(1)).toEqual([]);
+        expect(getPlugin('filters').conditionCollection.getConditions(2)[0].args).toEqual([['B1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(3)[0].args).toEqual([['C1']]);
+        expect(getPlugin('filters').conditionCollection.getConditions(4)[0].args).toEqual([['D1']]);
       });
     });
   });
