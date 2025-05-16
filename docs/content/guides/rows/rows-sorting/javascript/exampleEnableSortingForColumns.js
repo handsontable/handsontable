903ecac5def3767d513585e10df4ec33f09699a6
Adrian Dusinkiewicz <155736789+adrianspdev@users.noreply.github.com>
2025-05-16 06:55:48 +0200

    Fix currency format in the documentation (#11610)

--- docs/content/guides/rows/rows-sorting/javascript/exampleEnableSortingForColumns.js
+++ docs/content/guides/rows/rows-sorting/javascript/exampleEnableSortingForColumns.js
@@ -69,7 +69,7 @@ new Handsontable(container, {
       type: 'numeric',
       data: 'price',
       numericFormat: {
-        pattern: '$ 0,0.00',
+        pattern: '$0,0.00',
         culture: 'en-US',
       },
       // disable sorting for the 'Price' column
