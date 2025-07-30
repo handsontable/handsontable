7c99d5f3b733d61e343beb41115bea9173a53ba1
Mateusz Wojczal <mateusz@qunabu.com>
2025-07-30 10:01:11 +0200

    Markdown fix that broke hooks page (#11758)
    * Markdown fix that broke hooks page
    
    * Markdown fix that broke hooks page

--- handsontable/src/core/hooks/constants.js
+++ handsontable/src/core/hooks/constants.js
@@ -1250,6 +1250,7 @@ export const REGISTERED_HOOKS = [
    * />
    * ```
    * :::
+   *
    * ::: only-for angular
    *```ts
    * // To alter a single change, overwrite the desired value with `changes[i][3]`
