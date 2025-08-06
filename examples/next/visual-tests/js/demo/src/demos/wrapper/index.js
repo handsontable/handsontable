451a2344e2134ef102e46843e6d0092becd03436
adrianspdev <adrian.dusinkiewicz@handsontable.com>
2025-08-06 07:51:03 +0200

    Add visual test

---
+++ examples/next/visual-tests/js/demo/src/demos/wrapper/index.js
@@ -0,0 +1,34 @@
+
+import Handsontable from "handsontable/base";
+import { registerAllModules } from "handsontable/registry";
+import { getDirectionFromURL, getThemeNameFromURL } from "../../utils";
+import { registerLanguageDictionary, arAR } from "handsontable/i18n";
+
+registerAllModules();
+registerLanguageDictionary(arAR);
+
+const root = document.getElementById('root');
+const example = document.createElement('div');
+
+example.style.width = '500px';
+example.style.height = '400px';
+
+root.appendChild(example);
+
+export function init() {
+  new Handsontable(example, {
+    data: Handsontable.helper.createSpreadsheetData(20, 20),
+    layoutDirection: getDirectionFromURL(),
+    language: getDirectionFromURL() === "rtl" ? arAR.languageCode : "en-US",
+    themeName: getThemeNameFromURL(),
+    width: '100%',
+    height: '100%',
+    colWidths: 100,
+    rowHeaders: true,
+    colHeaders: true,
+    navigableHeaders: true,
+    licenseKey: "non-commercial-and-evaluation"
+  });
+
+  console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate})`);
+}
