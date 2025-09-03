bc2348b1b71323e15e3c9d31ab8d661d16d50b2a
adrianspdev <adrian.dusinkiewicz@handsontable.com>
2025-09-03 09:05:21 +0200

    Update docs example

--- docs/content/guides/dialog/loading/javascript/example2.ts
+++ docs/content/guides/dialog/loading/javascript/example2.ts
@@ -88,7 +88,8 @@ const hot = new Handsontable(container, {
   height: 300,
   stretchH: 'all',
   loading: {
-    title: 'Processing Data',
+    icon: '<svg class="ht-loading__icon-svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 16"><path stroke="currentColor" stroke-width="2" d="M15 8a7 7 0 1 1-3.5-6.062"></path></svg>',
+    title: 'Processing Data...',
     description: 'Please wait while we load your inventory data...',
   },
   licenseKey: 'non-commercial-and-evaluation',
