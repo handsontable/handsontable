15cde290a1c8672876ef1bab77bda8f216290fdc
Adrian Dusinkiewicz <155736789+adrianspdev@users.noreply.github.com>
2025-09-04 14:56:37 +0200

    Fix classic (legacy) theme deprecation warning (#11821)
    * Fix classic theme deprecation warning logic in Core module
    
    * Add changelog entry file
    
    * Update test cases

--- handsontable/src/__tests__/core/deprecatedWarn.spec.js
+++ handsontable/src/__tests__/core/deprecatedWarn.spec.js
@@ -15,11 +15,32 @@ describe('Test deprecatedWarn in core.js classic theme', () => {
   it('should warn user by log at console when classic theme is enabled', async() => {
     const warnSpy = spyOn(console, 'warn');
 
-    const hot = handsontable({
+    handsontable({
       data: createSpreadsheetData(5, 5),
     });
 
-    if (hot.stylesHandler.isClassicTheme()) {
+    if (spec().loadedTheme === 'classic') {
+      // eslint-disable-next-line max-len
+      expect(warnSpy).toHaveBeenCalledWith('Deprecated: Handsontable classic theme is a legacy theme and will be removed in version 17.0. Please update your theme settings to ensure compatibility with future versions.');
+    } else {
+      expect(warnSpy).not.toHaveBeenCalled();
+    }
+  });
+
+  it('should not warn user by log at console when classic theme is enabled and not root instance', async() => {
+    const warnSpy = spyOn(console, 'warn');
+
+    handsontable({
+      type: 'dropdown',
+      source: ['Misubishi', 'Chevrolet', 'Lamborgini'],
+    });
+
+    await selectCell(0, 0);
+
+    await keyDownUp('enter');
+
+    if (spec().loadedTheme === 'classic') {
+      expect(warnSpy).toHaveBeenCalledTimes(1);
       // eslint-disable-next-line max-len
       expect(warnSpy).toHaveBeenCalledWith('Deprecated: Handsontable classic theme is a legacy theme and will be removed in version 17.0. Please update your theme settings to ensure compatibility with future versions.');
     } else {
