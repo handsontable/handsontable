e3e9bdebc79caa6e340aa5ebb971b105127e082a
Adrian Dusinkiewicz <155736789+adrianspdev@users.noreply.github.com>
2025-09-05 12:45:17 +0200

    Fix deprecatedWarn calls (#11823)
    * Fix deprecatedWarn calls
    
    * Update handsontable/src/__tests__/core/deprecatedWarn.spec.js
    
    Co-authored-by: Krzysztof ‘Budzio’ Budnik <571316+budnix@users.noreply.github.com>
    
    * Fix lint
    
    ---------
    
    Co-authored-by: Krzysztof ‘Budzio’ Budnik <571316+budnix@users.noreply.github.com>

--- handsontable/src/__tests__/core/deprecatedWarn.spec.js
+++ handsontable/src/__tests__/core/deprecatedWarn.spec.js
@@ -47,4 +47,55 @@ describe('Test deprecatedWarn in core.js classic theme', () => {
       expect(warnSpy).not.toHaveBeenCalled();
     }
   });
+
+  it('should call warn only once by log at console when classic (legacy) theme is enabled and updateSettings is called', async() => {
+    const warnSpy = spyOn(console, 'warn');
+
+    handsontable({
+      data: createSpreadsheetData(5, 5),
+    });
+
+    await updateSettings({
+      data: createSpreadsheetData(2, 2),
+    });
+
+    await updateSettings({
+      data: createSpreadsheetData(3, 3),
+    });
+
+    if (spec().loadedTheme === 'classic') {
+      expect(warnSpy).toHaveBeenCalledTimes(1);
+      // eslint-disable-next-line max-len
+      expect(warnSpy).toHaveBeenCalledWith('Deprecated: Handsontable classic theme is a legacy theme and will be removed in version 17.0. Please update your theme settings to ensure compatibility with future versions.');
+    } else {
+      expect(warnSpy).not.toHaveBeenCalled();
+    }
+  });
+
+  it('should recall warn by log at console when classic (legacy) theme is enabled via updateSettings', async() => {
+    const warnSpy = spyOn(console, 'warn');
+
+    handsontable({
+      data: createSpreadsheetData(5, 5),
+    });
+
+    await updateSettings({
+      themeName: 'ht-theme-test',
+    });
+
+    await updateSettings({
+      themeName: undefined,
+    });
+
+    if (spec().loadedTheme === 'classic') {
+      expect(warnSpy).toHaveBeenCalledTimes(3);
+      // eslint-disable-next-line max-len
+      expect(warnSpy).toHaveBeenCalledWith('Deprecated: Handsontable classic theme is a legacy theme and will be removed in version 17.0. Please update your theme settings to ensure compatibility with future versions.');
+    } else {
+      expect(warnSpy).toHaveBeenCalledTimes(2);
+
+      // eslint-disable-next-line max-len
+      expect(warnSpy).toHaveBeenCalledWith('Deprecated: Handsontable classic theme is a legacy theme and will be removed in version 17.0. Please update your theme settings to ensure compatibility with future versions.');
+    }
+  });
 });
