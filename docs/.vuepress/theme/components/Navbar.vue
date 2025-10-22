64aa55209d830391764e6342352327b15117ded7
Mateusz Wojczal <mateusz@qunabu.com>
2025-10-22 14:11:08 +0200

    [Docs] Previous versions dropdown  (#11888)
    * uncommint dropdown to create preview URL
    
    * restore versiosn dropdown
    
    * restore versiosn dropdown
    
    * dummy workaround for vue compile error
    
    * dummy workaround for vue compile error
    
    * earliest version of docs is 9.0
    
    * Missing radix parameter

--- docs/.vuepress/theme/components/Navbar.vue
+++ docs/.vuepress/theme/components/Navbar.vue
@@ -13,7 +13,7 @@
         <div class="top-bar_left">
           <div class="framework-and-version">
             <FrameworksDropdown/>
-            <!--<VersionsDropdown></VersionsDropdown>-->
+            <VersionsDropdown></VersionsDropdown>
           </div>
         </div>
 
@@ -52,7 +52,7 @@ import AlgoliaSearch from '@theme/components/AlgoliaSearch.vue';
 import Logo from '@theme/components/Logo.vue';
 import SidebarButton from '@theme/components/SidebarButton.vue';
 import NavLinks from '@theme/components/NavLinks.vue';
-// import VersionsDropdown from '@theme/components/VersionsDropdown.vue';
+import VersionsDropdown from '@theme/components/VersionsDropdown.vue';
 // import ThemeSwitcher from '@theme/components/ThemeSwitcher.vue';
 import FrameworksDropdown from '@theme/components/FrameworksDropdown.vue';
 import ExternalNavLinks from '@theme/components/ExternalNavLinks.vue';
@@ -66,7 +66,7 @@ export default {
     SidebarButton,
     NavLinks,
     AlgoliaSearch,
-    // VersionsDropdown,
+    VersionsDropdown,
     // ThemeSwitcher,
     ExternalNavLinks,
     SidebarLinks
