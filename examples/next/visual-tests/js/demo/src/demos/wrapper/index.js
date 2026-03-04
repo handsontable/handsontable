a5f46c73b787f351769b7ccec62e5c9384b21eb3
adrianspdev <adrian.dusinkiewicz@handsontable.com>
2026-03-04 13:25:59 +0100

    Fix the scrolling issue that occurs when `preventOverflow` is enabled

--- examples/next/visual-tests/js/demo/src/demos/wrapper/index.js
+++ examples/next/visual-tests/js/demo/src/demos/wrapper/index.js
@@ -1,7 +1,7 @@
 
 import Handsontable from "handsontable/base";
 import { registerAllModules } from "handsontable/registry";
-import { getDirectionFromURL, getThemeNameFromURL } from "../../utils";
+import { getDirectionFromURL, getFromURL, getThemeNameFromURL } from "../../utils";
 import { registerLanguageDictionary, arAR } from "handsontable/i18n";
 
 export function init() {
@@ -11,24 +11,41 @@ export function init() {
     const root = document.getElementById('root');
     const example = document.createElement('div');
 
-    example.style.width = '500px';
-    example.style.height = '400px';
-
     root.appendChild(example);
 
-    new Handsontable(example, {
-        data: Handsontable.helper.createSpreadsheetData(20, 20),
-        layoutDirection: getDirectionFromURL(),
-        language: getDirectionFromURL() === "rtl" ? arAR.languageCode : "en-US",
-        themeName: getThemeNameFromURL(),
-        width: '100%',
-        height: '100%',
-        colWidths: 100,
-        rowHeaders: true,
-        colHeaders: true,
-        navigableHeaders: true,
-        licenseKey: "non-commercial-and-evaluation"
-    });
+    const preventOverflow = getFromURL('preventOverflow', false);
+
+    if (preventOverflow) {
+        new Handsontable(example, {
+            columns: [
+                {
+                title: 'TEST LONGER HEADER',
+                },
+                {
+                title: 'TEST NO:2 LONG HEADER',
+                },
+            ],
+            preventOverflow: 'horizontal',
+            licenseKey: 'non-commercial-and-evaluation',
+        });
+    } else {
+        example.style.width = '500px';
+        example.style.height = '400px';
+
+        new Handsontable(example, {
+            data: Handsontable.helper.createSpreadsheetData(20, 20),
+            layoutDirection: getDirectionFromURL(),
+            language: getDirectionFromURL() === "rtl" ? arAR.languageCode : "en-US",
+            themeName: getThemeNameFromURL(),
+            width: '100%',
+            height: '100%',
+            colWidths: 100,
+            rowHeaders: true,
+            colHeaders: true,
+            navigableHeaders: true,
+            licenseKey: "non-commercial-and-evaluation"
+        });
+    }
 
     console.log(`Handsontable: v${Handsontable.version} (${Handsontable.buildDate})`);
 }
