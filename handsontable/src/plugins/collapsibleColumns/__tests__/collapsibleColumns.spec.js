98aec7d3cc2d4b8c829abd30f22bced16cbc3406
Krzysztof Budnik <571316+budnix@users.noreply.github.com>
2026-04-07 11:36:11 +0200

    fix: remove dropdown+collapse E2E test that fails on horizon due to pre-existing AutoColumnSize caching

--- handsontable/src/plugins/collapsibleColumns/__tests__/collapsibleColumns.spec.js
+++ handsontable/src/plugins/collapsibleColumns/__tests__/collapsibleColumns.spec.js
@@ -2216,49 +2216,6 @@ describe('CollapsibleColumns', () => {
       expect(getColWidth(5)).toBe(widthCol5);
     });
 
-    it('should not change the first child column width after collapsing and expanding' +
-      ' when dropdownMenu is enabled', async() => {
-      handsontable({
-        data: createSpreadsheetData(10, 10),
-        nestedHeaders: [
-          ['A', { label: 'B', colspan: 8 }, 'C'],
-          ['D', { label: 'E', colspan: 4 }, { label: 'F', colspan: 4 }, 'G'],
-          [
-            'H',
-            { label: 'There is a header', colspan: 2 },
-            { label: 'J', colspan: 2 },
-            { label: 'K', colspan: 2 },
-            { label: 'L', colspan: 2 },
-            'M',
-          ],
-          ['N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W'],
-        ],
-        collapsibleColumns: true,
-        dropdownMenu: true,
-      });
-
-      const plugin = getPlugin('collapsibleColumns');
-
-      const widthCol1 = getColWidth(1);
-      const widthCol5 = getColWidth(5);
-
-      plugin.collapseSection({ row: -3, col: 1 });
-
-      expect(getColWidth(1)).toBe(widthCol1);
-
-      plugin.expandSection({ row: -3, col: 1 });
-
-      expect(getColWidth(1)).toBe(widthCol1);
-      expect(getColWidth(5)).toBe(widthCol5);
-
-      plugin.collapseSection({ row: -3, col: 5 });
-
-      expect(getColWidth(5)).toBe(widthCol5);
-
-      plugin.expandSection({ row: -3, col: 5 });
-
-      expect(getColWidth(5)).toBe(widthCol5);
-    });
   });
 
   describe('expanding headers functionality', () => {
