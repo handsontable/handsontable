20f2c0e4b9bbef3c7543603062d6110fbe35c7af
Artur Mędrygał <artur.medrygal@handsontable.com>
2026-06-01 14:36:57 +0200

    DEV-1737: Add Vue 3 SFC examples for binding-to-data

---
+++ docs/content/guides/getting-started/binding-to-data/vue/example9.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue';
+import { HotTable } from '@handsontable/vue3';
+import { registerAllModules } from 'handsontable/registry';
+
+registerAllModules();
+
+const hotSettings = ref({
+  autoWrapRow: true,
+  autoWrapCol: true,
+  height: 'auto',
+  licenseKey: 'non-commercial-and-evaluation',
+});
+</script>
+
+<template>
+  <div id="example9">
+    <HotTable :settings="hotSettings" />
+  </div>
+</template>
