c0b8f03f6ccf52b7d2374152675551176791d58c
Krzysztof Zieliński <krzysztof.zielinski.speednet@handsontable.com>
2026-06-02 14:57:30 +0200

    DEV-1821 - Angular StackBlitz examples broken

--- docs/content/guides/getting-started/demo/angular/example1.ts
+++ docs/content/guides/getting-started/demo/angular/example1.ts
@@ -1216,7 +1216,7 @@ export function addClassesToRows(
   row: number,
   column: number,
   _prop: string | number,
-  _value: CellValue,
+  _value: Handsontable.CellValue,
   cellProperties: CellProperties
 ) {
   // Adding classes to `TR` just while rendering first visible `TD` element
