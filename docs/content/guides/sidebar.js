dd7f314097578a586e7e62896c66b10b2c30776c
Artur Mędrygał <artur.medrygal@handsontable.com>
2026-06-08 12:45:32 +0200

    DEV-1758: Improve Getting Started documentation titles and sidebar consistency
    Refine the titles and meta titles for the 'HotColumn component' and 'Pinia state management' guides to be more concise and descriptive.
    
    Reorder items within the 'Getting Started' sidebar for better logical flow and organization.

--- docs/content/guides/sidebar.js
+++ docs/content/guides/sidebar.js
@@ -8,12 +8,12 @@ const gettingStartedItems = [
   { path: 'guides/getting-started/angular-hot-instance/angular-hot-instance', onlyFor: ['angular'] },
   { path: 'guides/getting-started/vue3-hot-reference/vue3-hot-reference', onlyFor: ['vue'] },
   { path: 'guides/getting-started/vue3-hot-column/vue3-hot-column', onlyFor: ['vue'] },
-  { path: 'guides/getting-started/vue3-nuxt/vue3-nuxt', onlyFor: ['vue'] },
-  { path: 'guides/getting-started/license-key/license-key' },
-  { path: 'guides/getting-started/react-redux/react-redux', onlyFor: ['react'] },
   { path: 'guides/getting-started/vue3-custom-id-class-style/vue3-custom-id-class-style', onlyFor: ['vue'] },
+  { path: 'guides/getting-started/react-redux/react-redux', onlyFor: ['react'] },
   { path: 'guides/getting-started/vue3-vuex/vue3-vuex', onlyFor: ['vue'] },
   { path: 'guides/getting-started/vue3-pinia/vue3-pinia', onlyFor: ['vue'] },
+  { path: 'guides/getting-started/vue3-nuxt/vue3-nuxt', onlyFor: ['vue'] },
+  { path: 'guides/getting-started/license-key/license-key' },
 ];
 
 const stylingItems = [
