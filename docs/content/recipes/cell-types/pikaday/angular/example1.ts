bd4fc612ae769fb232a61f642c91c08a61d5bf82
Artur Mędrygał <artur.medrygal@handsontable.com>
2026-08-03 15:14:18 +0200

    DEV-2177: Migrate the pikaday recipe to Intl-based numericFormat options

--- docs/content/recipes/cell-types/pikaday/angular/example1.ts
+++ docs/content/recipes/cell-types/pikaday/angular/example1.ts
@@ -154,9 +154,11 @@ export class AppComponent {
         type: 'numeric',
         width: 120,
         className: 'htRight',
+        locale: 'en-US',
         numericFormat: {
-          pattern: '$0,0.00',
-          culture: 'en-US',
+          style: 'currency',
+          currency: 'USD',
+          minimumFractionDigits: 2,
         },
       },
     ],
