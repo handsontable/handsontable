432e726549fef42cf42927b68323087822168fa5
Aleksandra Budnik <AMBudnik@users.noreply.github.com>
2026-09-23 13:38:13 +0200

    Update docs/content/guides/columns/column-header/javascript/example5.ts
    Co-authored-by: Daniel Zyto <daniel.zyto@handsontable.com>

--- docs/content/guides/columns/column-header/javascript/example5.ts
+++ docs/content/guides/columns/column-header/javascript/example5.ts
@@ -24,7 +24,7 @@ const hot = new Handsontable(container, {
     { headerClassName: 'italic-text' },
     { headerClassName: 'bold-text italic-text' },
     { headerClassName: 'htRight bold-text italic-text', type: 'numeric', numericFormat: { maximumFractionDigits: 0, useGrouping: false } },
-    { type: 'numeric', numericFormat: { minimumFractionDigits: 1, maximumFractionDigits: 1, useGrouping: false } },
+    { type: 'numeric', numericFormat: { minimumFractionDigits: 3, maximumFractionDigits: 3, useGrouping: false } },
   ],
   licenseKey: 'non-commercial-and-evaluation',
 });
